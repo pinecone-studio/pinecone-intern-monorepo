8015de3dbfe3cbf322ef6215f4030f64dec11cd0
Elbeg Amar <elbeg@nestsolutions.llc>
2024-03-22 15:40:14 +0800

    Added vercel deployment and federation setup

---
+++ apps/federation/src/index.ts
@@ -0,0 +1,77 @@
+import {
+  ApolloGateway,
+  IntrospectAndCompose,
+  RemoteGraphQLDataSource,
+} from '@apollo/gateway';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { json } from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(json());
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest({ request, context }) {
+    request.http.headers.set('authorization', context?.headers?.authorization);
+  }
+}
+
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      {
+        name: 'assessment',
+        url: process.env.ASSESSMENT_SERVICE,
+      },
+      {
+        name: 'challenge',
+        url: process.env.CHALLENGE_SERVICE,
+      },
+      {
+        name: 'courses',
+        url: process.env.COURSES_SERVICE,
+      },
+      {
+        name: 'leaderboard',
+        url: process.env.LEADERBOARD_SERVICE,
+      },
+      {
+        name: 'profile',
+        url: process.env.PROFILE_SERVICE,
+      },
+    ],
+  }),
+  buildService({ url }) {
+    return new AuthenticatedDataSource({ url });
+  },
+});
+
+const apolloServer = new ApolloServer({
+  gateway,
+  introspection: true,
+});
+
+async function context({ req }) {
+  return {
+    headers: req.headers,
+  };
+}
+
+async function startServer() {
+  await apolloServer.start();
+  app.use('/graphql', expressMiddleware(apolloServer, { context: context }));
+
+  const port = process.env.PORT || 3333;
+  const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/graphql`);
+  });
+  server.on('error', console.error);
+}
+
+startServer();
