2b129efe15a57329d5d91e7938f90bfe7de09941
Elbeg Amar <elbeg@nestsolutions.llc>
2024-03-28 01:07:02 +0800

    feat(ci/cd): added CI workflow

--- libs/scripts/src/utils/github/add-comment-to-pull-request.ts
+++ libs/scripts/src/utils/github/add-comment-to-pull-request.ts
@@ -2,9 +2,7 @@
 /* eslint-disable camelcase */
 import { handleApiError } from '../handle-errors/handle-api-error';
 import { logAndThrowError } from '../handle-errors/log-and-throw-error';
-import { sendMessageToTeams } from '../teams/send-message-to-teams';
 import { createComment } from './create-comment';
-import { getPullRequestData } from './get-pull-request-data';
 import { getGithubContext, getGithubOctokit, getPullRequestNumber } from './github-utils';
 import { GithubBaseType } from './types';
 import { updateComment } from './update-comment';
@@ -13,9 +11,6 @@ type AddCommentToPullRequestType = {
   name: string;
   url: string | boolean;
 };
-
-const PR_CHANNEL_WEBHOOK_URL =
-  'https://nestcore.webhook.office.com/webhookb2/19649155-c2ed-4a18-98e5-b64d7ccb6862@aae8fbe2-488b-49d9-b471-e4be61674a71/IncomingWebhook/85032b7fe75d4abcb3be397826baf53a/9a3f54d2-50d7-4e6c-9349-7a24da74e5b4';
 
 export const getExistingComments = async ({ context, pullRequestNumber, octokit }: GithubBaseType) => {
   try {
@@ -61,25 +56,10 @@ export const addCommentToPullRequest = async (projects: AddCommentToPullRequestT
   const commentBody = filteredProjects.map(({ name, url }) => `**${name.toLocaleUpperCase()} Preview Link:** [${url}](${url})`).join('\n');
   const comment = `${pullRequestNumber}:\n${commentBody}`;
 
-  const teamsNotificationActions = [
-    {
-      description: 'Click to see the pull request',
-      url: `https://github.com/pinecone-studio/pinecone-intern-monorepo/pull/${pullRequestNumber}`,
-    },
-  ];
-
   try {
     const existingComments = await getExistingComments({ octokit: githubOctokit, context: githubContext, pullRequestNumber });
     const isCommented = findExistingComment(existingComments, pullRequestNumber);
     await updateOrCreateComment({ octokit: githubOctokit, context: githubContext, existingComment: isCommented, pullRequestNumber, commentBody: comment });
-    const { creator, title } = await getPullRequestData();
-    await sendMessageToTeams({
-      teamsWebhookUrl: PR_CHANNEL_WEBHOOK_URL,
-      projects: filteredProjects,
-      notificationSummary: title,
-      notificationDescription: `This notification includes preview links generated during the execution of a pull request action initiated by ${creator.toLocaleUpperCase()}.`,
-      clickableLinks: teamsNotificationActions,
-    });
   } catch (error) {
     logAndThrowError('Error adding/updating comment to the pull request:', error);
   }
