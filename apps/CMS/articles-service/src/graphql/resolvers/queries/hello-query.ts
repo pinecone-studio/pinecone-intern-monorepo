23ac24b5074c801bef33904de8e0e00cd5b301c7
sumiyab <sumiyabazaramarjargal@gmail.com>
2024-04-10 16:56:08 +0800

    feat: new

--- apps/CMS/articles-service/src/graphql/resolvers/queries/hello-query.ts
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/hello-query.ts
@@ -1,3 +1,3 @@
 export const helloQueryFromArticlesService = () => {
-  return 'This message is from the hello query Articles service';
+  return 'This message is from the hello query Articles services';
 };
