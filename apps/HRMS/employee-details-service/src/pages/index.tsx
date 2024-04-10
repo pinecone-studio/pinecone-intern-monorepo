25bd469eab7114f08339f0961336b19cd86faccb
Munkh-Orgil Lkhavgajav <23LP0045@G301-CC0203.local>
2024-04-10 13:23:10 +0800

    feat(hrms/eds): add schema

--- apps/HRMS/employee-details-service/src/pages/index.tsx
+++ apps/HRMS/employee-details-service/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Payroll Service hello </div>
+      <div className="">Welcome to Employee Details Service hello </div>
     </>
   );
 };
