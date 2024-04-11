d0b575990629f7ca25d79f4f8249339743c5497c
Batchuluun0605 <145735591+Batchuluun0605@users.noreply.github.com>
2024-04-11 15:59:48 +0800

    feat(hrms): eds-dependent-mutation

--- apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/dependent-create-mutation.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/dependent-create-mutation.ts
@@ -1,7 +1,7 @@
-import { MutationCreateDependentArgs, MutationResolvers } from '@/graphql/generated';
+import { Dependent, MutationResolvers } from '@/graphql/generated';
 import { DependentModel } from '../../../models/dependent';
 
-export const createDependent: MutationResolvers['Dependent'] = async (_: string, { firstName, lastName, phone, dependency }: MutationCreateDependentArgs) => {
+export const createDependent: MutationResolvers['Dependent'] = async (_: string, { firstName, lastName, phone, dependency }: Dependent) => {
   const create = await DependentModel.create({
     firstName: firstName,
     lastName: lastName,
