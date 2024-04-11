37cf1c2c88c14e219cb28553b20996b28166dd22
Отгонбаяр Лхасүрэн <23LP4826@G303-CC0139.local>
2024-04-11 15:43:40 +0800

    feat(cms-dashboard): dashboard search input

--- apps/CMS/cms-dashboard/specs/dashboard/SearchInput.spec.tsx
+++ apps/CMS/cms-dashboard/specs/dashboard/SearchInput.spec.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { SearchInput } from "../../../cms-dashboard/src/app/dashboard/_components/SearchInput";
 
 describe('SearchInput', () => {
     it('Should call searchInput component', async () => {
-        await SearchInput()
-        expect(SearchInput).toBeDefined();
+        const { Search } = render(<SearchInput />);
+        expect(Search).toBeDefined();
     });
   });
