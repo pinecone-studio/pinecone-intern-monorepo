f28260d5b1e5c413cc846d50261cf0bcfa93ca58
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-04-11 17:27:56 +0800

    fix(glms-dashboard courses-service): database test check

--- apps/GLMS/courses-service/specs/queries/data-base.spec.ts
+++ apps/GLMS/courses-service/specs/queries/data-base.spec.ts
@@ -1,10 +1,10 @@
 import { connectDataBase } from '@/config/data-base';
 import mongoose from 'mongoose';
- 
+
 jest.mock('mongoose', () => ({
   connect: jest.fn(),
 }));
- 
+
 describe('Database connection check', () => {
   beforeAll(() => {
     jest.clearAllMocks();
@@ -13,13 +13,13 @@ describe('Database connection check', () => {
     jest.clearAllMocks();
     jest.resetAllMocks();
   });
- 
+
   it('1. should call connectDataBase', async () => {
     (mongoose.connect as jest.Mock).mockResolvedValue({ connections: [{ readyState: 1 }] });
     await connectDataBase();
     expect(mongoose.connect).toBeTruthy();
   });
- 
+
   it('2. should handle error', async () => {
     (mongoose.connect as jest.Mock).mockRejectedValue(new Error('error'));
     try {
@@ -29,21 +29,19 @@ describe('Database connection check', () => {
     }
   });
 });
- 
+
 describe('Database url null check', () => {
   beforeAll(() => {
     process.env = { ...process.env, MONGODB_URI: undefined };
   });
- 
+
   afterAll(() => {
     process.env = { ...process.env };
     jest.clearAllMocks();
     jest.resetAllMocks();
   });
- 
+
   it('1. should return when data base url is undefined', async () => {
     await connectDataBase();
   });
 });
-
- 
