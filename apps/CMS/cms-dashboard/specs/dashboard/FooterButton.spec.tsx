53413d0ca89111f8e08bae311aa880aa4daaa26e
zorurunn <udvalfora@gmail.com>
2024-04-11 14:24:23 +0900

    feat(cms-dashboard): create footer button feature

--- apps/CMS/cms-dashboard/specs/dashboard/FooterButton.spec.tsx
+++ apps/CMS/cms-dashboard/specs/dashboard/FooterButton.spec.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { FooterButton } from '../../src/app/dashboard/_components/FooterButton';
 
 describe('FooterButton', () => {
-  it('Should render footer button component', () => {
-    const { container } = render(<FooterButton text="hello test footer" />);
-    expect(container).toBeDefined();
+  it('1. Should render footer button component', () => {
+    const setSelectedButtonMock = jest.fn();
+    const { getByTestId } = render(<FooterButton text="Test Button" selectedButton="Test Button" setSelectedButton={setSelectedButtonMock} />);
+
+    const buttonElement = getByTestId('footer-button-test-id');
+    expect(buttonElement).toBeDefined();
+  });
+
+  it('2. should call button when clicked', () => {
+    const setSelectedButtonMock = jest.fn();
+
+    const { getByTestId } = render(<FooterButton text="Test Button" selectedButton="Selected Button" setSelectedButton={setSelectedButtonMock} />);
+
+    const buttonElement = getByTestId('footer-button-test-id');
+
+    fireEvent.click(buttonElement);
+
+    expect(setSelectedButtonMock).toHaveBeenCalledWith('Test Button');
+  });
+
+  it('3. should have a background color when selectedButton matches with text', () => {
+    const setSelectedButtonMock = jest.fn();
+
+    const { getByTestId } = render(<FooterButton text="Test Button" selectedButton="Test Button" setSelectedButton={setSelectedButtonMock} />);
+
+    const buttonElement = getByTestId('footer-button-test-id');
+
+    expect(buttonElement).toBeDefined();
+  });
+
+  it('4. should not have background when selectedButton does not matches with text', () => {
+    const setSelectedButtonMock = jest.fn();
+
+    const { getByTestId } = render(<FooterButton text="Test Button" selectedButton="Button" setSelectedButton={setSelectedButtonMock} />);
+
+    const buttonElement = getByTestId('footer-button-test-id');
+
+    expect(buttonElement).toBeDefined();
   });
 });
