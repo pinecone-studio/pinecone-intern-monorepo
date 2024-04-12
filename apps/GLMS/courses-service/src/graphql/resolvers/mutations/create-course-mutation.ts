42877600a1861fc49274658b52949397840d4ffc
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-04-12 19:51:12 +0800

    fix(glms-dashboard): build deer alda

--- apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-course-mutation.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-course-mutation.ts
@@ -1,7 +1,7 @@
 import { MutationResolvers } from '@/graphql/generated';
 import courseModel from '@/model/create-course-model';
 
-export const createCourse: MutationResolvers['createCourse'] = async (_, { content,title,thumbnail,position}) => {
-  const newObj = await courseModel.create({ content,title,thumbnail,position});
+export const createCourse: MutationResolvers['createCourse'] = async (_, { title, thumbnail, position }) => {
+  const newObj = await courseModel.create({ title, thumbnail, position });
   return newObj.toObject();
 };
