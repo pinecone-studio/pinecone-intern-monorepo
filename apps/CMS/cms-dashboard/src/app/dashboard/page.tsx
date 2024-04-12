d4216713282ef94da4cb31aa8bb4491be669b025
Bayra1 <isvhbaatar5@gmail.com>
2024-04-12 08:13:40 +0800

    fix(cms-dashboard): made calendar that represents date picker rangers

--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import FilterByUsingCalendar from './_components/FilterByDate';
 
-
 const Home = () => {
   return (
     <>
       <div>Welcome to Cms Dashboard hello </div>
-      <FilterByUsingCalendar/>
     </>
   );
 };
