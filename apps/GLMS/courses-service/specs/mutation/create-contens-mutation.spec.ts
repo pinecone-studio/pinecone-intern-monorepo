5b6d3fa3b1de747a76c327c97b001bd8d1aa1ccb
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-04-15 22:07:13 +0800

    feat(glms-dashboard courses-service): create content service deer hiidg bolov

--- apps/GLMS/courses-service/specs/mutation/create-contens-mutation.spec.ts
+++ apps/GLMS/courses-service/specs/mutation/create-contens-mutation.spec.ts
@@ -2,44 +2,46 @@ import { createContents } from '@/graphql/resolvers/mutations';
 import contentModel from '@/model/create-content-model';
 
 jest.mock('@/model/create-content-model', () => ({
-    create: jest.fn(),
-  }));
-  describe('createContents resolver', () => {
+  create: jest.fn(),
+}));
+
+describe('createContents resolver', () => {
+  beforeEach(() => {
+      jest.clearAllMocks();
+  });
+
+  it('should create content with provided data', async () => {
       const mockInput = {
-        title: 'Test Title',
-        description: 'Test Description',
-        contentImage: 'test-image-url',
+          title: 'Test Title',
+          description: 'Test Description',
+          contentImage: 'Test Image URL'
       };
-  
-      const mockOutput = {
-        _id: 'mocked-id',
-        title: mockInput.title,
-        description: mockInput.description,
-        contentImage: mockInput.contentImage,
+
+      const mockNewContent = {
+          _id: '321321',
+          title:mockInput.title,
+          description:mockInput.description,
+          contentImage:mockInput.contentImage,
+            toObject: jest.fn(),
       };
-  
-      contentModel.create.mockResolvedValue(mockOutput);
-  
-   t
+
+      (contentModel.create as jest.Mock).mockResolvedValue(mockNewContent);
       const result = await createContents({}, mockInput);
 
       expect(contentModel.create).toHaveBeenCalledWith(mockInput);
-  
-      expect(result).toEqual(mockOutput);
-    });
-  
-    it('should throw an error when content creation fails', async () => {
+      expect(result).toEqual(mockNewContent.toObject());
+  });
 
+
+  it('should throw an unknown error if the error type is not recognized', async () => {
       const mockInput = {
-        title: 'Test Title',
-        description: 'Test Description',
-        contentImage: 'test-image-url',
+          title: 'Test Title',
+          description: 'Test Description',
+          contentImage: 'Test Image URL'
       };
-  
-    
-      const mockError = new Error('Test error message');
-      contentModel.create.mockRejectedValue(mockError);
-  
 
-      await expect(createContents({}, mockInput)).rejects.toThrowError(mockError);
-    });
+      (contentModel.create as jest.Mock).mockRejectedValue('Unknown error');
+
+      await expect(createContents({}, mockInput)).rejects.toThrow('An unknown error occurred');
+  });
+});
