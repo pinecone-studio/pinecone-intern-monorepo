5554b5d0802550b19fdee74541cdf662b0916ad6
Arvinbuyan Otgonjargal <23LP6614@G303-CC0146.local>
2024-04-15 13:21:58 +0800

    feat(cms-dashboard): morevert

---
+++ apps/CMS/cms-dashboard/src/app/dashboard/_components/MoreOverlay.tsx
@@ -0,0 +1,15 @@
+import { Stack, Typography } from '@mui/material';
+
+const MoreOverlay = () => {
+  return (
+    <Stack>
+      <Stack>
+        <Typography fontSize={14} fontWeight={400} color={'#121316'}>
+          Статистик харах
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MoreOverlay;
--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MoreOverlay from './_components/MoreOverlay';
 
 const Home = () => {
   return (
     <>
-      <div>Welcome to Cms Dashboard hello </div>
+      {/* <div>Welcome to Cms Dashboard hello </div> */}
+      <MoreOverlay />
     </>
-  ); 
+  );
 };
 export default Home;
