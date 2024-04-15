927fb2158218a4c491d27cf81801eee75f3f61e4
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-04-15 22:07:13 +0800

    feat(glms-dashboard courses-service): create content service deer hiidg bolov

--- apps/GLMS/courses-service/specs/queries/contets-query.spec.ts
+++ apps/GLMS/courses-service/specs/queries/contets-query.spec.ts
@@ -1,16 +1,16 @@
 import { getContents } from '@/graphql/resolvers/queries';
-import courseModel from '@/model/create-course-model';
+import contentModel from '@/model/create-content-model';
 
-jest.mock('@/model/create-course-model', () => ({
-    find: jest.fn(),
-  }));
-  describe('getContents', () => {
-    it('should return courses from courseModel', async () => {
-      const content: any[] = [{ title: 'Course 1' }, { title: 'Course 2' }];
-      (courseModel.find as jest.Mock).mockResolvedValue(content);
-      const result = await getContents();
-      expect(result).toEqual(content);
-  
-      expect(courseModel.find).toHaveBeenCalledTimes(1);
-    });
+jest.mock('@/model/create-content-model', () => ({
+  find: jest.fn(),
+}));
+describe('getContents', () => {
+  it('should return content from courseModel', async () => {
+    const content: any[] = [{ title: 'Course 1' }, { title: 'Course 2' }];
+    (contentModel.find as jest.Mock).mockResolvedValue(content);
+    const result = await getContents();
+    expect(result).toEqual(content);
+
+    expect(contentModel.find).toHaveBeenCalledTimes(1);
   });
+});
