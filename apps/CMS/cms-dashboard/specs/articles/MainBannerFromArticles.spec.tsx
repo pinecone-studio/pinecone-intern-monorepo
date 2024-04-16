edeef6cb826f4e72a1fdfbcb780d7b98ff55a66e
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-04-16 12:38:00 +0800

    feat(cms-dashboard): main article

--- apps/CMS/cms-dashboard/specs/articles/MainBannerFromArticles.spec.tsx
+++ apps/CMS/cms-dashboard/specs/articles/MainBannerFromArticles.spec.tsx
@@ -4,9 +4,7 @@ import MainBannerFromArticles from '../../src/app/articles/_components/MainBanne
 
 describe('MainBannerFromArticles', () =>
   it('should have correct props', () => {
-    const { getByTestId } = render(
-      <MainBannerFromArticles date="2024.04.12" articlesTitle="Welcome" categories="Coding" cover="https://getwallpapers.com/wallpaper/full/2/6/e/1179758-cool-cat-hd-wallpapers-1080p-1920x1080.jpg" />
-    );
+    const { getByTestId } = render(<MainBannerFromArticles date="2024.04.12" articlesTitle="Welcome" categories="Coding" cover="/Academy.svg" />);
 
     const articlesTitle = getByTestId('articlesTitle');
     expect(articlesTitle.textContent).toMatch('Welcome');
@@ -18,5 +16,5 @@ describe('MainBannerFromArticles', () =>
     expect(categories.textContent).toMatch('Coding');
 
     const cover = getByTestId('cover');
-    expect(cover.getAttribute('src')).toContain('https%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F2%2F6%2Fe%2F1179758-cool-cat-hd-wallpapers-1080p-1920x1080.jpg&w=3840&q=75');
+    expect(cover.getAttribute('src')).toContain('/Academy.svg');
   }));
