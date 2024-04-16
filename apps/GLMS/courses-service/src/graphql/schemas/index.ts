d0e3df59363dbe19ebb763eaf70d6895923b0ba2
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-04-16 12:34:36 +0800

    build(glms-dashboard): generate deer aldaa garav

--- apps/GLMS/courses-service/src/graphql/schemas/index.ts
+++ apps/GLMS/courses-service/src/graphql/schemas/index.ts
@@ -1,6 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { helloCoursesSchema } from './hello.schema';
-import { lessonSchema } from './course.schema';
+import { lessonSchema } from './lesson.schema';
 import { contentSchema } from './content.schema';
 
 export const typeDefs = mergeTypeDefs([helloCoursesSchema, lessonSchema,contentSchema]);
