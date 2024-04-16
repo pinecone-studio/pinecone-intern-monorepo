ae5e546eabd548e4465dbce8d561d95152f8ca85
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-04-16 12:00:43 +0800

    refactor(glms-dashboard): code change hiij lint deerh aldaa zasaj ajiluulaw

--- apps/GLMS/courses-service/src/graphql/schemas/hello.schema.ts
+++ apps/GLMS/courses-service/src/graphql/schemas/hello.schema.ts
@@ -5,7 +5,6 @@ export const helloCoursesSchema = gql`
     helloQueryFromCoursesService: String
   }
 
-  # ******************** Mutations ********************
   type Mutation {
     helloMutationFromCoursesService: String
   }
