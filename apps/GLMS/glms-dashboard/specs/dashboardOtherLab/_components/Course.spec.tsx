e7cd75b89fa6588ef04b29ca2895fd211875d31d
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-04-16 11:03:02 +0800

    fix(glms-dashboard): umnuh merge deerh lint aldaa garav

--- apps/GLMS/glms-dashboard/specs/dashboardOtherLab/_components/Course.spec.tsx
+++ apps/GLMS/glms-dashboard/specs/dashboardOtherLab/_components/Course.spec.tsx
@@ -12,18 +12,7 @@ describe('Course component', () => {
   };
 
   it('renders with correct props and structure', () => {
-    const { getByTestId, getByText } = render(<Course {...props} />);
-
+    const { getByTestId} = render(<Course {...props} />);
     expect(getByTestId('courseContain')).toBeDefined();
-
-    // expect(getByText('https.//pinecone')).toBeDefined();
-    // expect(getByText('HTML')).toBeDefined();
-    // expect(getByText('hi welcome to my code')).toBeDefined();
-    // expect(getByText('12')).toBeDefined();
-
-    // expect(getByTestId('lessonImage')).toBeDefined();
-    // expect(getByTestId('titleTest')).toBeDefined();
-    // expect(getByTestId('infoTest')).toBeDefined();
-    // expect(getByTestId('lessonCountTest')).toBeDefined();
   });
 });
