59ea2decb144fbcee400d91808790c305757c3cf
Arvinbuyan Otgonjargal <23LP6614@G303-CC0146.local>
2024-04-16 09:09:31 +0800

    feat(cms-dashboard): dashboard onclick morevert

--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Cms Dashboard hello </div>
+      <div>Welcome to Cms Dashboard hello</div>
     </>
   );
 };
