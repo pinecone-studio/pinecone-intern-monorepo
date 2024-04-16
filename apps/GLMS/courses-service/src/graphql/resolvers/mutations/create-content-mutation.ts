b2147a73351dc3075e9f333ff47273017edefed1
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-04-16 16:04:56 +0800

    fix(glms-dashborad): test2

--- apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-content-mutation.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-content-mutation.ts
@@ -1,15 +1,12 @@
 import { MutationResolvers } from '@/graphql/generated';
 import contentModel from '@/model/create-content-model';
+import { GraphQLError } from 'graphql';
 
 export const createContents:MutationResolvers['createContents'] =  async (_,{title, description, contentImage}) =>{
 try {
     const newContent = await contentModel.create({title,description,contentImage})
     return newContent.toObject()
 } catch (error:unknown) {
-    if (error instanceof Error) {
-        throw new Error(error.message)
-      } else {
-        throw new Error('An unknown error occurred');
-      }
+    throw new GraphQLError('An unknown error occurred'); 
     }
 }
--- apps/GLMS/courses-service/src/graphql/resolvers/queries/contents-query.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/queries/contents-query.ts
@@ -1,10 +1,10 @@
+import { GraphQLError } from 'graphql';
 import contentModel from '@/model/create-content-model';
 export const getContents = async () => {
     try {
         return await contentModel.find();
     } catch (error) {
-        throw new Error("cannot find content");
-        
+        throw new GraphQLError('cannot find content'); 
     }
 
 };
