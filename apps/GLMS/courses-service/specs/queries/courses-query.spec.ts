cc4f93770ab4514e4fdd77a325110183790294a4
Elbeg Amar <elbeg@nestsolutions.llc>
2024-04-17 11:12:09 +0800

    fix(nx version): downgraded NX to 17

--- apps/GLMS/courses-service/specs/queries/courses-query.spec.ts
+++ apps/GLMS/courses-service/specs/queries/courses-query.spec.ts
@@ -2,15 +2,15 @@ import { getCourses } from '../../src/graphql/resolvers/queries/courses-query';
 import courseModel from '@/model/create-course-model';
 
 jest.mock('@/model/create-course-model', () => ({
-    find: jest.fn(),
-  }));
-  describe('getCourses', () => {
-    it('should return courses from courseModel', async () => {
-      const courses: any[] = [{ title: 'Course 1' }, { title: 'Course 2' }];
-      (courseModel.find as jest.Mock).mockResolvedValue(courses);
-      const result = await getCourses();
-      expect(result).toEqual(courses);
-  
-      expect(courseModel.find).toHaveBeenCalledTimes(1);
-    });
+  find: jest.fn(),
+}));
+describe('getCourses', () => {
+  it('should return courses from courseModel', async () => {
+    const courses = [{ title: 'Course 1' }, { title: 'Course 2' }];
+    (courseModel.find as jest.Mock).mockResolvedValue(courses);
+    const result = await getCourses();
+    expect(result).toEqual(courses);
+
+    expect(courseModel.find).toHaveBeenCalledTimes(1);
   });
+});
