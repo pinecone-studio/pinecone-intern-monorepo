3f525a5a41f05a626fd8ed8bfa9d9c99d5dca912
Batchuluun0605 <145735591+Batchuluun0605@users.noreply.github.com>
2024-04-17 10:52:16 +0800

    feat(hrms): hrms-dashboard-personal-information-components

--- apps/HRMS/hrms-dashboard/specs/employee-details/UpdateButton.spec.tsx
+++ apps/HRMS/hrms-dashboard/specs/employee-details/UpdateButton.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { UpdateButton } from '../../src/app/employee-details/_components';
 describe('UpdateButton', () => {
-  it('Should render update button component', () => {
+  it('Should render update button component ', () => {
     const { container } = render(<UpdateButton />);
     expect(container).toBeDefined();
   });
