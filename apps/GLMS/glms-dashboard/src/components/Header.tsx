2ad540d8d04500825de0d59e90d8d601baecd1b2
altangerel <altangerelg203@gmail.com>
2024-04-18 10:11:27 +0800

    feat: header layout page

--- apps/GLMS/glms-dashboard/src/components/Header.tsx
+++ apps/GLMS/glms-dashboard/src/components/Header.tsx
@@ -3,30 +3,32 @@ import AppsIcon from '@mui/icons-material/Apps';
 import SearchIcon from '@mui/icons-material/Search';
 const Header = () => {
   return (
-    <Stack data-testid="header-artivle-detail" direction="row" height="48px" bgcolor="#F7F7F8" width="100%" alignItems="center" px="24px" justifyContent="space-between">
-      <img width="32px" height="24px" src="svgCompanyLogo.png" />
-      <Stack direction="row" gap="8px" alignItems="center">
-        <TextField
-          data-testid="text-data-search"
-          InputProps={{
-            sx: {
-              borderRadius: '8px',
-              width: '180px',
-              height: '32px',
-              p: '6px',
-            },
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-          type="search"
-          placeholder="Search"
-        />
-        <AppsIcon sx={{ color: '#5E6166', width: '36xp', height: '36px' }} />
-        <Stack width="32px" height="32px" borderRadius="100%" border={1}>
-          <img src="Avatar.png" />
+    <Stack paddingX="96px" bgcolor="#F7F7F8" width="100%" height="48px" justifyContent="center">
+      <Stack data-testid="header-artivle-detail" direction="row" width="100%" alignItems="center" px="24px" justifyContent="space-between">
+        <img width="32px" height="24px" src="svgCompanyLogo.png" />
+        <Stack direction="row" gap="8px" alignItems="center">
+          <TextField
+            data-testid="text-data-search"
+            InputProps={{
+              sx: {
+                borderRadius: '8px',
+                width: '180px',
+                height: '32px',
+                p: '6px',
+              },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon sx={{ color: 'black' }} />
+                </InputAdornment>
+              ),
+            }}
+            type="search"
+            placeholder="Search"
+          />
+          <AppsIcon sx={{ color: '#5E6166', width: '36xp', height: '36px' }} />
+          <Stack width="32px" height="32px" borderRadius="100%">
+            <img src="Avatar.png" />
+          </Stack>
         </Stack>
       </Stack>
     </Stack>
