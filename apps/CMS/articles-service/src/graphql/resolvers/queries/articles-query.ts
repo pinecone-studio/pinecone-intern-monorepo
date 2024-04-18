66bb0fd001fd14a9f37bb50f6bef0dfebc5f298b
Arvinbuy7n <arwinbuyan88705548@gmail.com>
2024-04-18 09:28:22 +0800

    feat(cms-dashboard): cms-dashboard

--- apps/CMS/articles-service/src/graphql/resolvers/queries/articles-query.ts
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/articles-query.ts
@@ -2,6 +2,7 @@ import { articleModel } from '@/models/article.model';
 
 export const articles = async () => {
   const getArticles = await articleModel.find({});
+  console.log(getArticles);
 
   return getArticles;
 };
