95c8402ec5d991b6737abf227e3a412f97f9bb88
khangerel14 <162080218+khangerel14@users.noreply.github.com>
2024-04-18 10:33:51 +0800

    166 category (#236)
    * feat(a): a
    
    a
    
    * feat(cms-dashboard): menuBar map
    
    * feat(cms-dashboard): menuBar feature update (did map)
    
    * feat(cms-dashboard): menu bar updated
    
    * feat(cms-dashboard): menu bar update
    
    * feat(cms-dashboard): menuBar update
    
    ---------
    
    Co-authored-by: Khangerel Be-ochir <23LP5567@G303-CC0149.local>

---
+++ apps/CMS/cms-dashboard/specs/dashboard/MenuBtn.spec.tsx
@@ -0,0 +1,29 @@
+import { render, fireEvent, act } from '@testing-library/react';
+import React from 'react';
+import { MenuBtn } from '../../src/app/dashboard/_components/MenuBtn'
+
+describe('MenuBar', () => {
+  it('1. Should render menu button component', async () => {
+    const setSelected = jest.fn();
+    const menu = 'Test'
+    const { getByTestId } = render(<MenuBtn menu='Test' selected="Test Button" setSelected={setSelected} number={18}/>)
+
+    const title = getByTestId('title-test-id');
+    const number = getByTestId('number-test-id');
+    expect(title).toBeDefined();
+    expect(number).toBeDefined();
+
+    await act(() =>{ fireEvent.click(getByTestId('menu-btn-test-id')) })
+
+    expect(setSelected).toHaveBeenCalledWith(menu)
+
+  });
+  it('1. Should render menu button component', () => {
+    const setSelected = jest.fn();
+    const menu = 'Test'
+    const { getByTestId } = render(<MenuBtn menu='Test' selected="Test" setSelected={setSelected} number={18}/>)
+
+    fireEvent.click(getByTestId('menu-btn-test-id'));
+    expect(setSelected).toHaveBeenCalledWith(menu)
+  });
+});
