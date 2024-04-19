f9984b4a31dce2fd8063d9c31abf493fc46e2787
Zolboo <n.zolbooyuro@gmail.com>
2024-04-19 14:51:19 +0800

    feat(terraform): hRMS terraform deployment

---
+++ apps/HRMS/hrms-dashboard/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+resource "vercel_project" "intern-hrms-dashboard-prod" {
+  name             = "intern-hrms-dashboard-prod"
+  build_command    = "nx build --skip-nx-cache hrms-dashboard"
+  output_directory = "./dist/apps/HRMS/hrms-dashboard/.next"
+  framework        = "nextjs"
+  team_id          = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
+
+variable "VERCEL_TOKEN" {
+  type        = string
+  description = "Optionally say something about this variable"
+}
+
+provider "vercel" {
+  # Or omit this for the api_token to be read
+  # from the VERCEL_API_TOKEN environment variable
+  api_token = var.VERCEL_TOKEN
+
+  # Optional default team for all resources
+  team = "team_0ASDilhqwPl5fll9OnzqDM30"
+}
