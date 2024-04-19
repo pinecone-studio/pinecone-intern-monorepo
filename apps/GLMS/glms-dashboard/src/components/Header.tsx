742ede9d4da7c030f162196c96ac6deee24b46e4
altangerel <altangerelg203@gmail.com>
2024-04-19 10:57:40 +0800

    feat: header

--- apps/GLMS/glms-dashboard/src/components/Header.tsx
+++ apps/GLMS/glms-dashboard/src/components/Header.tsx
@@ -1,37 +1,35 @@
-import { InputAdornment, Stack, TextField } from '@mui/material';
+import { Container, InputAdornment, Stack, TextField } from '@mui/material';
 import AppsIcon from '@mui/icons-material/Apps';
 import SearchIcon from '@mui/icons-material/Search';
 const Header = () => {
   return (
-    <Stack paddingX="96px" bgcolor="#F7F7F8" width="100%" height="48px" justifyContent="center">
-      <Stack data-testid="header-artivle-detail" direction="row" width="100%" alignItems="center" px="24px" justifyContent="space-between">
-        <img width="32px" height="24px" src="svgCompanyLogo.png" />
-        <Stack direction="row" gap="8px" alignItems="center">
-          <TextField
-            data-testid="text-data-search"
-            InputProps={{
-              sx: {
-                borderRadius: '8px',
-                width: '180px',
-                height: '32px',
-                p: '6px',
-              },
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon sx={{ color: 'black' }} />
-                </InputAdornment>
-              ),
-            }}
-            type="search"
-            placeholder="Search"
-          />
-          <AppsIcon sx={{ color: '#5E6166', width: '36xp', height: '36px' }} />
-          <Stack width="32px" height="32px" borderRadius="100%">
-            <img src="Avatar.png" />
-          </Stack>
+    <Container data-testid="header-artivle-detail" maxWidth="xl" sx={{ display: 'flex', bgcolor: '#F7F7F8', height: '48px', alignItems: 'center', justifyContent: 'space-between' }}>
+      <img width="32px" height="24px" src="svgCompanyLogo.png" />
+      <Stack direction="row" gap="8px" alignItems="center">
+        <TextField
+          data-testid="text-data-search"
+          InputProps={{
+            sx: {
+              borderRadius: '8px',
+              width: '180px',
+              height: '32px',
+              p: '6px',
+            },
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon sx={{ color: 'black' }} />
+              </InputAdornment>
+            ),
+          }}
+          type="search"
+          placeholder="Search"
+        />
+        <AppsIcon sx={{ color: '#5E6166', width: '36xp', height: '36px' }} />
+        <Stack width="32px" height="32px" borderRadius="100%">
+          <img src="Avatar.png" />
         </Stack>
       </Stack>
-    </Stack>
+    </Container>
   );
 };
 export default Header;
