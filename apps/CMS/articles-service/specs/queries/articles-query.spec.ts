bdaa96c61a6e0e8c3462a84f5b2dffa7640f3de3
Arvinbuy7n <arwinbuyan88705548@gmail.com>
2024-04-19 14:52:30 +0800

    feat(cms-dashbaoard): cms-dashboard get-articles

--- apps/CMS/articles-service/specs/queries/articles-query.spec.ts
+++ apps/CMS/articles-service/specs/queries/articles-query.spec.ts
@@ -1,11 +1,15 @@
+import { GraphQLResolveInfo } from 'graphql';
 import { getArticles } from '../../src/graphql/resolvers/queries/articles-query';
 import { articleModel } from '../../src/models/article.model';
 
 describe('articles', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('1. should return an array of articles', async () => {
     articleModel.find = jest.fn().mockResolvedValue(['title', 'status', 'create']);
 
-    const result = await getArticles();
+    const result = await getArticles!({}, {}, {}, {} as GraphQLResolveInfo);
 
     expect(result).toEqual(['title', 'status', 'create']);
   });
@@ -13,7 +17,7 @@ describe('articles', () => {
   it('2. should handle empty array when no articles are found', async () => {
     articleModel.find = jest.fn().mockResolvedValue([]);
 
-    const result = await getArticles();
+    const result = await getArticles!({}, {}, {}, {} as GraphQLResolveInfo);
 
     expect(result).toEqual([]);
   });
--- apps/CMS/articles-service/src/graphql/resolvers/queries/articles-query.ts
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/articles-query.ts
@@ -1,6 +1,7 @@
+import { QueryResolvers } from '@/graphql/generated';
 import { articleModel } from '@/models/article.model';
 
-export const getArticles = async () => {
+export const getArticles: QueryResolvers['getArticles'] = async () => {
   const articles = await articleModel.find({});
 
   return articles;
