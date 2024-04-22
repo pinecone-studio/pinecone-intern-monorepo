113c79c78b1ad02e28de98865af276b8f3c8107f
Binderya Enkhtuvshin <binderyaenkhtuvshin@Binderyas-MacBook-Pro.local>
2024-04-22 10:55:37 +0800

    [hrms]-153-delete-employee

--- apps/HRMS/employee-details-service/specs/mutations/employee-delete-mutation.spec.ts
+++ apps/HRMS/employee-details-service/specs/mutations/employee-delete-mutation.spec.ts
@@ -1,4 +1,6 @@
 import { deleteEmployee } from '@/graphql/resolvers/mutations';
+import { GraphQLResolveInfo } from 'graphql';
+import graphqlErrorHandler, { errorTypes } from '@/graphql/resolvers/error';
 
 jest.mock('@/models/employee', () => ({
   EmployeeModel: {
@@ -11,12 +13,13 @@ jest.mock('@/models/employee', () => ({
         phone: '90909090',
         dependency: 'brother',
       })
+      .mockResolvedValueOnce(undefined)
       .mockReturnValueOnce(null),
   },
 }));
 describe('delete employee', () => {
   it('should delete an employee', async () => {
-    const result = await deleteEmployee!({} as string, { id: '1' });
+    const result = await deleteEmployee!({} as string, { id: '1' }, {}, {} as GraphQLResolveInfo);
     expect(result).toEqual({
       id: '1',
       firstName: 'bat',
@@ -26,11 +29,18 @@ describe('delete employee', () => {
     });
   });
 
-  it("should throw an error if the dependent doesn't exist", async () => {
+  it("should throw an error if the employee doesn't exist", async () => {
     try {
-      await deleteEmployee!({} as string, { id: '1' });
+      await deleteEmployee!({}, { id: '1' }, {}, {} as GraphQLResolveInfo);
     } catch (error) {
-      expect(error).toEqual(new Error('failed to delete Employee'));
+      expect(error).toEqual(graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.NOT_FOUND));
+    }
+  });
+  it('should throw an error if an error occurs during an employee retrieval', async () => {
+    try {
+      await deleteEmployee!({}, { id: '1' }, {}, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect(error).toEqual(graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST));
     }
   });
 });
--- apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/employee-delete-mutation.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/employee-delete-mutation.ts
@@ -1,10 +1,15 @@
 import { Employee, MutationResolvers } from '@/graphql/generated';
 import { EmployeeModel } from '@/models/employee';
+import graphqlErrorHandler, { errorTypes } from '../error';
 
 export const deleteEmployee: MutationResolvers['EmployeeDelete'] = async (_: string, { id }: Employee) => {
-  const deleteEmployeeId = await EmployeeModel.findByIdAndDelete(id);
-  if (!deleteEmployeeId) {
-    throw new Error('failed to delete Employee');
+  try {
+    const deleteEmployeeId = await EmployeeModel.findByIdAndDelete(id);
+    if (!deleteEmployee) {
+      throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.NOT_FOUND);
+    }
+    return deleteEmployeeId;
+  } catch (error) {
+    throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST);
   }
-  return deleteEmployeeId;
 };
