ce70e52f8c619d77d69f1fdfd1ebe945f0efdfa1
Chinguun Sainbat <23LP1573@G303-CC0136.local>
2024-04-22 11:47:41 +0800

    docs: hayj bga branch shuu

--- apps/CMS/articles-service/src/pages/adminDashboard/index.tsx
+++ apps/CMS/articles-service/src/pages/adminDashboard/index.tsx
@@ -4,9 +4,10 @@ import { connectToDatabase } from '@/config/connect-to-database';
 
 const AdminDashboard = () => {
   connectToDatabase()
+  console.log("test")
   return (
     <>
-      <div>sans undertale</div>
+      <div>test</div>
       <Button variant="contained">Hello world</Button>
     </>
   );
