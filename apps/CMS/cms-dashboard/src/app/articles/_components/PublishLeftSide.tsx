0a3207780b3dd08c0e79f0d0af74974a7917c97e
NyamjargalMTMN <metamancode@gmail.com>
2024-04-22 11:39:13 +0800

    feat(cms-article): publishLeftSide implemented

--- apps/CMS/cms-dashboard/src/app/articles/_components/PublishLeftSide.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_components/PublishLeftSide.tsx
@@ -1,21 +1,23 @@
 'use client';
 import { Stack, TextField, Typography } from '@mui/material';
 import { NavigateBefore } from '@mui/icons-material';
+
 const PublishLeftSide = () => {
-
   return (
-    <Stack width={'100%'} height={'100vh'} flexDirection={'row'}>
-          <NavigateBefore data-testid="publish-arrowIcon" color="secondary" />
-          <Stack sx={{gap:"15px"}}>         
-            <Typography fontSize={18}>Гарчиг өгөх</Typography>
-            <TextField sx={{padding:"18px 0px 18px 24px", alignItems:"center", borderRadius:"16px", height:"64px"}}  placeholder='Энд гарчгаа бичнэ үү...'/>
-          </Stack>
-          <Stack sx={{gap:"15px"}}>         
-            <Typography fontSize={18}>Нийтлэлээ бичих</Typography>
-            <TextField sx={{padding:"18px 0px 18px 24px", alignItems:"center", borderRadius:"16px", height:"64px"}}  placeholder='Энд гарчгаа бичнэ үү...'/>
-          </Stack>
-          
+    <Stack width={'100%'} height={'100vh'} flexDirection={'column'} pl={12} pt={8} pr={10} gap={6} bgcolor={'#F7F7F8'}>
+      <Stack gap={2}>
+        <NavigateBefore data-testid="publish-arrowIcon" color="primary" />
+        <Stack flexDirection={'column'} gap={2}>
+          <Typography fontSize={18}>Гарчиг өгөх</Typography>
+          <TextField sx={{  height: '64px', backgroundColor: "white" ,}} fullWidth placeholder="Энд гарчгаа бичнэ үү..." />
+        </Stack>
+      </Stack>
 
+      <Stack flexDirection={'column'}  gap={2}>
+        <Typography fontSize={18}>Нийтлэлээ бичих</Typography>
+        <TextField multiline sx={{ alignItems: 'center', borderRadius: '16px', height: '237px', backgroundColor:"white", borderColor:"#fff", border:"none"  }} fullWidth placeholder="Бичиж эхлэх..." inputProps={{style:{ color:"green", height:'230px', border:'none'}}}  />
+        
+      </Stack>
     </Stack>
   );
 };
--- apps/CMS/cms-dashboard/src/app/articles/createArticle/page.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/createArticle/page.tsx
@@ -2,8 +2,8 @@ import { Stack } from "@mui/material";
 import PublishLeftSide from "../_components/PublishLeftSide";
 const CreateArticle = () => {
   return (
-    <Stack width={'100%'} height={'100vh'} flexDirection={'row'}>
-      <PublishLeftSide/>
+    <Stack width={'100%'} height={'100vh'} flexDirection={'row'} >
+      <PublishLeftSide />
     </Stack>
   );
 };
