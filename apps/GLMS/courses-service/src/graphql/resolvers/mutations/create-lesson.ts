498782d5b4081952bbd51a0ffb8d6eabd0d4d7eb
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-04-22 13:07:32 +0800

    fix(glms-dashboard): log arilgav

--- apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-lesson.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/mutations/create-lesson.ts
@@ -3,10 +3,8 @@ import lessonModel from '@/model/create-lesson-model';
 import { GraphQLError } from 'graphql';
 
 export const createLesson: MutationResolvers['createLesson'] = async (_, { lessonInput }) => {
-  console.log(lessonInput)
   try {
     const newLesson = await lessonModel.create(lessonInput);
-    console.log(newLesson)
     return newLesson.toObject();
   } catch (error) {
     throw new GraphQLError('An unknown error occurred'); 
