022775ae8d8526d2e83e64040b68158197155263
Arvinbuyan Otgonjargal <23LP6614@G303-CC0146.local>
2024-04-23 10:46:54 +0800

    feat(cms-dashboard): cms-dashboard cypress test

--- apps/CMS/articles-service/src/graphql/resolvers/queries/get-articles-query.ts
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/get-articles-query.ts
@@ -7,7 +7,6 @@ export const getArticlesQuery: QueryResolvers['getArticlesQuery'] = async () => 
     const articles = await ArticleModel.find({}).populate('category').populate('author');
     return articles;
   } catch (error) {
-    console.log(error);
     throw new GraphQLError('Error in get articles query');
   }
 };
