37056d91bc72c9a1fe243d10f90c0c4da0ce551e
Munkh-Orgil Lkhavgajav <23LP0045@G303-CC0161.local>
2024-04-23 11:24:20 +0800

    feat(hrms): sidebar dashboard

--- apps/HRMS/hrms-dashboard/cypress/e2e/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/index.cy.ts
@@ -10,15 +10,24 @@ describe('home page', () => {
     cy.get('[data-cy="dashboardSidebar"]').should('exist');
   });
   it('4.Sidebar menu clickable and redirect to employee details page', () => {
-    cy.get('[data-cy="sidebarItem-1"]').should('exist').should('be.visible').click().url().should('include', '/employee-details');
+    cy.get('[data-cy="sidebarItem-1"]').should('exist').should('be.visible').click();
+    cy.url().should('include', '/employee-details');
   });
   it('5.Sidebar menu clickable and redirect to leaving page', () => {
-    cy.get('[data-cy="sidebarItem-2"]').should('exist').should('be.visible').click().url().should('include', '/leaving');
+    cy.get('[data-cy="sidebarItem-2"]').should('exist').should('be.visible').click();
+    cy.url().should('include', '/leaving');
   });
   it('6.Sidebar menu clickable and redirect to recruiting page', () => {
-    cy.get('[data-cy="sidebarItem-3"]').should('exist').should('be.visible').click().url().should('include', '/recruiting');
+    cy.get('[data-cy="sidebarItem-3"]').should('exist').should('be.visible').click();
+    cy.url().should('include', '/recruiting');
   });
-  it('6.Sidebar menu clickable and redirect to home page', () => {
-    cy.get('[data-cy="sidebarItem-0"]').should('exist').should('be.visible').click().url().should('include', '/');
+  it('7.Sidebar menu clickable and redirect to home page', () => {
+    cy.get('[data-cy="sidebarItem-0"]').should('exist').should('be.visible').click();
+    cy.url().should('include', '/');
+  });
+  it('8.Header items visible', () => {
+    cy.get('[data-cy="headerIcon"]').should('exist').should('be.visible');
+    cy.get('[data-cy="headerMenu"]').should('exist').should('be.visible');
+    cy.get('[data-cy="headerProfile"]').should('exist').should('be.visible');
   });
 });
