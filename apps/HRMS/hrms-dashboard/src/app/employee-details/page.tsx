c1d5a78e6ec23de1ac780745ccbfa0bd6e303290
Munkh-Orgil Lkhavgajav <23LP0045@G303-CC0161.local>
2024-04-23 14:42:17 +0800

    feat(hrms): dashboard-sidebar

--- apps/HRMS/hrms-dashboard/src/app/employee-details/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/page.tsx
@@ -5,9 +5,7 @@ const EmployeeDetailsPage = () => {
     <div>
       <h1>hello from HRMS dashboard Employee details Page1</h1>
       <EmployeePagination />
-      <button onClick={handleNavigateToHomePageButton} data-cy="Home-Page-Button">
-        Go back to home page
-      </button>
+      <button data-cy="Home-Page-Button">Go back to home page</button>
     </div>
   );
 };
