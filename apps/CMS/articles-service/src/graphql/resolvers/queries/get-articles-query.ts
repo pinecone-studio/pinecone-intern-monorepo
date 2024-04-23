870447642265860a9c1fcc263f6df87858955824
Ganuuh <145634432+Ganuuh@users.noreply.github.com>
2024-04-23 17:57:06 +0800

    feat(queries to get articles by category and given quantity): two seperate queries to get articles

--- apps/CMS/articles-service/src/graphql/resolvers/queries/get-articles-query.ts
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/get-articles-query.ts
@@ -10,3 +10,26 @@ export const getArticlesQuery: QueryResolvers['getArticlesQuery'] = async () => 
     throw new GraphQLError('Error in get articles query');
   }
 };
+
+export const getArticlesByCategory: QueryResolvers['getArticlesByCategory'] = async (_, { categoryId, getAll }) => {
+  try {
+    const articles = await ArticleModel.find({ category: categoryId })
+      .populate('category')
+      .populate('author')
+      .limit(getAll ? 0 : 4);
+
+    return articles;
+  } catch (error) {
+    throw new GraphQLError('Error in getArticlesByCategory');
+  }
+};
+
+export const getArticlesByQuantity: QueryResolvers['getArticlesByQuantity'] = async (_, { quantity }) => {
+  try {
+    const articles = await ArticleModel.find({}).limit(quantity).populate('category').populate('author');
+
+    return articles;
+  } catch (error) {
+    throw new GraphQLError('Error in getArticlesByQuantity');
+  }
+};
