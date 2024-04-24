6b500d77a40e034b14851a0481e9ccfacba1f0bb
Bat Mandal <batmandal@Bats-MacBook-Pro.local>
2024-04-24 17:31:32 +0800

    feat(glms-dashboard): glms
    glms

--- apps/GLMS/glms-dashboard/cypress/e2e/create-course/page.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/create-course/page.cy.ts
@@ -14,7 +14,6 @@ describe('createCoursePage', () => {
   it('4. check create button be enable and inputs', () => {
     cy.get('#title-test').type('some text');
     cy.get('#description-test').type('some more text');
-    cy.get('#file-test').click();
     cy.get('#file-test').selectFile('public/js.png', { force: true });
     cy.get('[data-testid="create-button"]').should('not.be.disabled');
     cy.get('[data-testid="create-button"]').click();
