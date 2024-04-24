306b7f7b4611555bdf2b6b4a7369d9859bdd62a2
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-25 00:02:42 +0800

    fix: preview

--- apps/HRMS/recruiting-service/specs/config/database-connect.spec.ts
+++ apps/HRMS/recruiting-service/specs/config/database-connect.spec.ts
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
 import { connectToDatabase, connection } from '@/config/database-connect';
+import mongoose from 'mongoose';
 
 jest.mock('mongoose', () => ({
   connect: jest.fn(),
 }));
 
-describe('connectToDatabase', () => {
+describe('connectDatabase', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     connection.isConnected = 0;
   });
 
-  it('1. Should connect to database', async () => {
+  it('1. should call connectToDatabase', async () => {
     (mongoose.connect as jest.Mock).mockResolvedValue({ connections: [{ readyState: 1 }] });
     await connectToDatabase();
     expect(mongoose.connect);
@@ -23,13 +23,12 @@ describe('connectToDatabase', () => {
     expect(connection.isConnected).toBe(1);
     expect(mongoose.connect).not.toHaveBeenCalled();
   });
-
   it('3. Should handle error', async () => {
     (mongoose.connect as jest.Mock).mockRejectedValue(new Error(''));
     try {
       await connectToDatabase();
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
     }
   });
 });
--- apps/HRMS/recruiting-service/src/config/database-connect.ts
+++ apps/HRMS/recruiting-service/src/config/database-connect.ts
@@ -4,17 +4,17 @@ export const connection = {
   isConnected: 0,
 };
 
-export async function connectToDatabase() {
-  const DATABASE_URI = process.env.MONGODB_URI!;
+export const connectToDatabase = async () => {
+  const databaseUri = process.env.MONGODB_URI;
 
   if (connection.isConnected > 0) {
     return;
   }
 
   try {
-    const dbConnection = await mongoose.connect(DATABASE_URI);
-    connection.isConnected = dbConnection.connections[0].readyState;
+    const connectedDB = await mongoose.connect(databaseUri!);
+    connection.isConnected = connectedDB.connections[0].readyState;
   } catch (error) {
-    throw new Error('Failed to connect to database');
+    throw new Error('connection failed');
   }
-}
+};
