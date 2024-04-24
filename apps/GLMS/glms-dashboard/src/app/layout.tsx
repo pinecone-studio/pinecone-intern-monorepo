4e102afdbb09760ccecd114f5060149669fa84fe
Bat Mandal <batmandal@Bats-MacBook-Pro.local>
2024-04-24 13:44:45 +0800

    feat: glms dashboard

--- apps/GLMS/glms-dashboard/src/app/layout.tsx
+++ apps/GLMS/glms-dashboard/src/app/layout.tsx
@@ -1,16 +1,13 @@
 'use client';
-import { ThemeProvider } from '@mui/material';
+
 import { FederationProvider } from '../common';
 import './global.css';
-import { theme } from '../theme';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <FederationProvider>
-        <body>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </body>
+        <body>{children}</body>
       </FederationProvider>
     </html>
   );
