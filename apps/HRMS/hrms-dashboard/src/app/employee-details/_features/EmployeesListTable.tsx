363344c8a006439aee8acf9e6756ff8747a47d83
Munkh-Orgil Lkhavgajav <23LP0045@G303-CC0161.local>
2024-04-24 15:34:55 +0800

    feat(hrms): all employee table

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_features/EmployeesListTable.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_features/EmployeesListTable.tsx
@@ -3,9 +3,7 @@ import * as React from 'react';
 import { CircularProgress, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useGetAllEmployeeQuery } from '../../../generated';
-import { LocalDining } from '@mui/icons-material';
 
-// const employeesList = [{ id: '1', firstName: 'Нэр', lastName: 'Овог', jobTitle: 'Developer', email: 'morgild@gmail.com', phone: '88088722', status: 'Үндсэн' }];
 export const EmployeesListTable = () => {
   const tableHeader = ['Ажилтан', 'Мэргэжил', 'И-мэйл', 'Хэлтэс', 'Төлөв'];
   const { data, loading } = useGetAllEmployeeQuery();
