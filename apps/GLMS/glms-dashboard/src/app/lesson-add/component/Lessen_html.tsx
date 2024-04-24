e7f573c9dff2e3d17e3960063e72c3e45772a755
altangerel <altangerelg203@gmail.com>
2024-04-24 18:35:18 +0800

    feat: lesson page

--- apps/GLMS/glms-dashboard/src/app/lesson-add/component/Lessen_html.tsx
+++ apps/GLMS/glms-dashboard/src/app/lesson-add/component/Lessen_html.tsx
@@ -1,8 +1,23 @@
+'use client';
 import { Card, Container, Stack, Typography } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import { useGetSectionsQuery } from '@/generated/index';
+
 const Lessen_html = () => {
+  const { data, loading, error } = useGetSectionsQuery();
+
+  if (loading)
+    return (
+      <Stack sx={{ width: '100%', height: '800px' }} justifyContent="center" alignItems="center">
+        <Typography fontSize="40px" fontWeight="700">
+          Loading...
+        </Typography>
+      </Stack>
+    );
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <Stack bgcolor="#F7F7F8">
       <Container maxWidth="xl">
@@ -12,34 +27,33 @@ const Lessen_html = () => {
             Сэдвүүд
           </Typography>
         </Stack>
-        <Stack paddingY="48px" paddingX="24px" bgcolor="white" borderRadius="12px" gap="24px">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontSize="32px" fontWeight="700">
-              HTML Intro
-            </Typography>
-            <Stack direction="row" gap="16px">
-              <Card sx={{ display: 'flex', gap: '8px', padding: '20px 16px' }}>
-                <Typography fontSize="18px" fontWeight="600">
-                  Ерөнхийн мэдээлэл
-                </Typography>
-                <CreateOutlinedIcon />
-              </Card>
-              <Card sx={{ padding: '16px' }}>
-                <DeleteOutlinedIcon sx={{ color: 'black' }} />
-              </Card>
+        {data?.getSections.map((item) => (
+          <Stack key={item.id} paddingY="48px" paddingX="24px" bgcolor="white" borderRadius="12px" gap="24px">
+            <Stack direction="row" justifyContent="space-between">
+              <Typography fontSize="32px" fontWeight="700">
+                {item.title}
+              </Typography>
+              <Stack direction="row" gap="16px">
+                <Card sx={{ display: 'flex', gap: '8px', padding: '20px 16px' }}>
+                  <Typography fontSize="18px" fontWeight="600">
+                    Ерөнхийн мэдээлэл
+                  </Typography>
+                  <CreateOutlinedIcon />
+                </Card>
+                <Card sx={{ padding: '16px' }}>
+                  <DeleteOutlinedIcon sx={{ color: 'black' }} />
+                </Card>
+              </Stack>
+            </Stack>
+            <Stack direction="row" gap="97px" justifyContent="space-between">
+              <Typography>{item.description}</Typography>
+              <img width="306px" height="209px" style={{ borderRadius: '12px', border: '1px' }} src={'html.png'} />
             </Stack>
           </Stack>
-          <Stack direction="row" gap="97px">
-            <Typography>
-              HTML-ийн үндсэн ойлголтуудыг сурч, эхнээс нь үндсэн вэбсайтуудыг зохион бүтээж, бүр байгуулж эхлээрэй. HTML-ийн тусламжтайгаар та вэбсайтуудын бүтэц, агуулгын талаар ойлголттой болно.
-              Энэ нь хэрэглэгчийн туршлагыг бүрдүүлдэг гарчиг, догол мөр, зураг, холбоос зэрэг элементүүдийг тодорхойлдог хэл юм. HTML-тэй танилцсанаар та эдгээр элементүүд хэрхэн нэгдэж, нэгдмэл,
-              сонирхол татахуйц вэб хуудсыг бий болгохыг ойлгох боломжтой болно.
-            </Typography>
-            <img width="306px" height="209px" flex-shrink="0" style={{ borderRadius: '12px' }} src="" />
-          </Stack>
-        </Stack>
+        ))}
       </Container>
     </Stack>
   );
 };
+
 export default Lessen_html;
