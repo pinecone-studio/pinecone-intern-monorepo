945267167fb7adbbbec8dd248f18a8d0d67d6cb5
altangerel <altangerelg203@gmail.com>
2024-04-24 19:37:53 +0800

    feat: header

--- apps/GLMS/glms-dashboard/public/Logo.tsx
+++ apps/GLMS/glms-dashboard/public/Logo.tsx
@@ -1,4 +1,4 @@
-export default function Logo() {
+const Logo = () => {
   return (
     <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="24" height="22" fill="#373F54" />
@@ -32,4 +32,5 @@ export default function Logo() {
       />
     </svg>
   );
-}
+};
+export default Logo;
