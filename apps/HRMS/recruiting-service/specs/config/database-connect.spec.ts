47ba50c9031d6d4113ce19371cd8c80ed90590cd
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-25 00:41:42 +0800

    fix: prev

--- apps/HRMS/recruiting-service/specs/config/database-connect.spec.ts
+++ apps/HRMS/recruiting-service/specs/config/database-connect.spec.ts
@@ -1,17 +1,17 @@
-import { connectToDatabase, connection } from '@/config/database-connect';
 import mongoose from 'mongoose';
+import { connectToDatabase, connection } from '@/config/database-connect';
 
 jest.mock('mongoose', () => ({
   connect: jest.fn(),
 }));
 
-describe('connectDatabase', () => {
+describe('connectToDatabase', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     connection.isConnected = 0;
   });
 
-  it('1. should call connectToDatabase', async () => {
+  it('1. Should connect to database', async () => {
     (mongoose.connect as jest.Mock).mockResolvedValue({ connections: [{ readyState: 1 }] });
     await connectToDatabase();
     expect(mongoose.connect);
@@ -23,12 +23,13 @@ describe('connectDatabase', () => {
     expect(connection.isConnected).toBe(1);
     expect(mongoose.connect).not.toHaveBeenCalled();
   });
+
   it('3. Should handle error', async () => {
     (mongoose.connect as jest.Mock).mockRejectedValue(new Error(''));
     try {
       await connectToDatabase();
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
     }
   });
 });
--- apps/HRMS/recruiting-service/src/config/database-connect.ts
+++ apps/HRMS/recruiting-service/src/config/database-connect.ts
@@ -4,17 +4,17 @@ export const connection = {
   isConnected: 0,
 };
 
-export const connectToDatabase = async () => {
-  const databaseUri = process.env.MONGODB_URI;
+export async function connectToDatabase() {
+  const DATABASE_URI = process.env.MONGODB_URI!;
 
   if (connection.isConnected > 0) {
     return;
   }
 
   try {
-    const connectedDB = await mongoose.connect(databaseUri!);
-    connection.isConnected = connectedDB.connections[0].readyState;
+    const dbConnection = await mongoose.connect(DATABASE_URI);
+    connection.isConnected = dbConnection.connections[0].readyState;
   } catch (error) {
-    throw new Error('ERROR: Database connection failed');
+    throw new Error('Failed to connect to database');
   }
-};
+}
