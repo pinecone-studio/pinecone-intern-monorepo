62d3653d46cfeda800e73427b8e3b8052743b560
Tsolmon Togtokhsaikhan <23LP9801@G303-CC0142.local>
2024-04-24 18:29:44 +0800

    fix(cms-dashboard): fixed lint error

--- apps/CMS/cms-dashboard/specs/articles/ArticleCard.spec.tsx
+++ apps/CMS/cms-dashboard/specs/articles/ArticleCard.spec.tsx
@@ -4,7 +4,7 @@ import ArticleCard from '../../src/app/articles/_components/ArticleCard';
 
 describe('ArticlesCard', () => {
   it('it should render with correct props', () => {
-    const { getAllByTestId,container } = render(<ArticleCard date="2024.04.16" title="Morphosis" category="Coding" description="It is just description" cover="/Academy.svg" />);
+    const { container } = render(<ArticleCard date="2024.04.16" title="Morphosis" category="Coding" description="It is just description" cover="/Academy.svg" />);
     expect(container).toBeDefined();
 
   });
