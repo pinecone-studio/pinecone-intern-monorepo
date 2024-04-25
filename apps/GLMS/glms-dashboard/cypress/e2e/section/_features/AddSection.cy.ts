573275971cae304b4f18411e131c6989fab4a424
Bymbaabatdelger <baymba.0108@icloud.com>
2024-04-25 11:38:41 +0800

    feat(glms-dashboard): "add section push"

--- apps/GLMS/glms-dashboard/cypress/e2e/section/_features/AddSection.cy.ts
+++
@@ -1,7 +0,0 @@
-describe (' render section page ' , () => {
-  beforeEach(() => cy.visit('section'))
-
-  it ('Should display AddSection feature' , () => {
-    cy.get('[data-testid="add-section-component"]').should('exist').should('be.visible')
-  })
-})
