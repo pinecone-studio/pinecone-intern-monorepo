95506ca020daa98249bad30bfab10d5c82c242d6
LifeNeedZoya <145326293+LifeNeedZoya@users.noreply.github.com>
2024-04-25 18:44:11 +0800

    feat(glms-dashboard): "add challnge modal& radio button & select" (#443)
    * feat(glms-dashboard): "add challnge modal& radio button & select"
    
    * feat(glms-dashboard): create radio button & add challenge modal
    
    ---------
    
    Co-authored-by: Zoya <zoya@Zoyas-MacBook-Air.local>

---
+++ apps/GLMS/glms-dashboard/src/app/challenge-dashboard/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useState } from 'react';
+import { RadioButton } from './_components';
+import { SelectButton } from './_components';
+import { AddChallengeModal } from './_feature/AddChallengeModal';
+
+const Page = () => {
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <RadioButton value={false} checked={true} onClick={() => {}} />
+      <SelectButton handleSelectChange={handleSelectChange} options={['yuu', 'bn', 'da']} selectedOption={selectedOption} />
+      <AddChallengeModal />
+    </div>
+  );
+};
+
+export default Page;
