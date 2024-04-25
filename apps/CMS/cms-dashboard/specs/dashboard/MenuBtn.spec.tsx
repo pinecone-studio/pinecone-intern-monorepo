7dd9c68b3915230c7db9ad7142ab161efd01f563
khangerel14 <162080218+khangerel14@users.noreply.github.com>
2024-04-25 09:37:58 +0800

    feat(cms-dashboard): used useQuery instead of useState (#408)

--- apps/CMS/cms-dashboard/specs/dashboard/MenuBtn.spec.tsx
+++ apps/CMS/cms-dashboard/specs/dashboard/MenuBtn.spec.tsx
@@ -1,29 +1,32 @@
-import { render, fireEvent, act } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import React from 'react';
 import { MenuBtn } from '../../src/app/dashboard/_components/MenuBtn'
-
+ 
 describe('MenuBar', () => {
   it('1. Should render menu button component', async () => {
-    const setStatus = jest.fn();
-    const statusName = 'Test'
-    const { getByTestId } = render(<MenuBtn statusName='Test' status="Test Button" setStatus={setStatus} number={18}/>)
-
+    const { getByTestId } = render(<MenuBtn statusName='Test' status="Test Button" number={18}/>)
+ 
     const title = getByTestId('title-test-id');
-    const number = getByTestId('number-test-id');
+    const numberElement = getByTestId('number-test-id');
+    const divider = getByTestId('divider-test-id');
+ 
     expect(title).toBeDefined();
-    expect(number).toBeDefined();
-
-    await act(() =>{ fireEvent.click(getByTestId('menu-btn-test-id')) })
-
-    expect(setStatus).toHaveBeenCalledWith(statusName)
-
+    expect(numberElement).toBeDefined();
+    expect(divider).toBeDefined();
   });
-  it('1. Should render menu button component', () => {
-    const setStatus = jest.fn();
-    const statusName = 'Test'
-    const { getByTestId } = render(<MenuBtn statusName='Test' status="Test" setStatus={setStatus} number={18}/>)
-
-    fireEvent.click(getByTestId('menu-btn-test-id'));
-    expect(setStatus).toHaveBeenCalledWith(statusName)
+ 
+  it('should render menu button component with provided props', () => {
+    const statusName = 'Test';
+    const status = 'Test';
+    const number = 18;
+    const { getByTestId } = render(<MenuBtn statusName={statusName} status={status} number={number}/>);
+ 
+    const title = getByTestId('title-test-id');
+    const numberElement = getByTestId('number-test-id');
+    const divider = getByTestId('divider-test-id');
+ 
+    expect(title).toBeDefined();
+    expect(numberElement).toBeDefined();
+    expect(divider).toBeDefined();
   });
 });
