1095381db46c38d93c7347ccd8cde1faef40acfc
Anujin Purew <23LP0977@G303-CC0163.local>
2024-04-25 16:10:07 +0800

    docs(leaving service mail sender): mail sender

--- apps/HRMS/leaving-service/specs/mail/mail-sender.spec.ts
+++ apps/HRMS/leaving-service/specs/mail/mail-sender.spec.ts
@@ -7,14 +7,14 @@ jest.mock('nodemailer', () => ({
 }));
 describe('sendMail', () => {
   it('success', async () => {
-    const result = await sendMail('amoramgl@gmail.com', 'shit happened', 'andaa', 'Day', new Date());
+    const result = await sendMail('amoramgl@gmail.com', 'shit happened', 'andaa', 'Day', "2024-04-25");
     expect(result).toEqual({
       message: 'Email sent successfully',
     });
   });
 
   it('error', async () => {
-    const result = await sendMail('amoramgl@gmail.com', 'shit happened', 'andaa', 'Day', new Date());
+    const result = await sendMail('amoramgl@gmail.com', 'shit happened', 'andaa', 'Day', "2024-04-25");
     expect(result).toEqual({
       error: 'Failed to send email',
     });
