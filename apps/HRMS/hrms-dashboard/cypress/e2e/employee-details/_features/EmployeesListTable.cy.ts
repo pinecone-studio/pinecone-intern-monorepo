67dd47cd8d0f8417a93a30556dc812a7f069687b
Munkh-Orgil Lkhavgajav <23LP0045@G303-CC0161.local>
2024-04-25 14:53:05 +0800

    feat(hrms): all employee table

---
+++ apps/HRMS/hrms-dashboard/cypress/e2e/employee-details/_features/EmployeesListTable.cy.ts
@@ -0,0 +1,11 @@
+describe('All employee table', () => {
+  beforeEach(() => cy.visit('/employee-details'));
+
+  it('Table header should be visible', () => {
+    cy.get('[data-cy="employeesList"]').should('exist').should('be.visible');
+    cy.get('[data-cy="tableHeader-0"]').should('be.visible').should('have.text', 'Ажилтан');
+    cy.get('[data-cy="tableHeader-1"]').should('be.visible').should('have.text', 'Мэргэжил');
+    cy.get('[data-cy="tableHeader-2"]').should('be.visible').should('have.text', 'И-мэйл');
+    cy.get('.MuiTableBody-root').should('exist');
+  });
+});
