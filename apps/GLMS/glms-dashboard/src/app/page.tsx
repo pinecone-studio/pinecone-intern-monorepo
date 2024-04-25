bf533045bb1438301b3d78ea3308f7d4ad467410
altangerel <altangerelg203@gmail.com>
2024-04-25 11:26:24 +0800

    feat: header

--- apps/GLMS/glms-dashboard/src/app/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/page.tsx
@@ -1,10 +1,23 @@
 'use client';
+import { useRouter } from 'next/navigation';
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleProfilePageButton = () => {
+    router.push('/profile');
+  };
+
+  const handleAssessmentPageButton = () => {
+    router.push('/assessment');
+  };
+
   return (
     <div>
       <h1>hello from GLMS dashboard</h1>
       <h1>This is the environment {process.env.ENVIRONMENT}</h1>
+      <button onClick={handleProfilePageButton}>Go to profile page</button>
+      <button onClick={handleAssessmentPageButton}>Go to assessment page</button>
     </div>
   );
 };
--- apps/GLMS/glms-dashboard/src/app/profile/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/profile/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
+import { useHelloQueryFromProfileServiceQuery } from '@/generated/index';
 import Link from 'next/link';
 import { ProfileMain } from './_features';
 
 const ProfilePage = () => {
+  const { data } = useHelloQueryFromProfileServiceQuery();
   return (
     <div>
       <h1>hello from GLMS dashboard Profile Page</h1>
-      <h1>hello from Profile Service Query </h1>
+      <h1>hello from Profile Service Query {data?.helloQueryFromProfileService}</h1>
       <ProfileMain />
       <Link href="/">
         <button>Go back to home page</button>
