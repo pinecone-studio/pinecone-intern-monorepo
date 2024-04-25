f5e1943052ee8ff152e15e5382fc0f4511744ebc
Bilguun.G <143570232+Bilguungantogos@users.noreply.github.com>
2024-04-25 18:35:40 +0800

    feat(HRMS-Recruiting): hRMS recruiting core-button hiilee (#438)
    * feat(core-button): hRMS recruiting core-button hiilee
    
    * fix: ungu soliv
    
    * fix: ungi dahin zasav
    
    * feat: svg g component bolgoloo
    
    * fix: button outline border color uur bsnig zasav
    
    * fix: border color zasaj testiig n bs zasav

---
+++ apps/HRMS/hrms-dashboard/src/app/recruiting/_components/icons/PlusIcon.tsx
@@ -0,0 +1,13 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+const PlusIcon = () => {
+  return (
+    <SvgIcon fontSize="small">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.2} stroke="currentColor">
+        <path strokeLinecap="square" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+    </SvgIcon>
+  );
+};
+
+export default PlusIcon;
