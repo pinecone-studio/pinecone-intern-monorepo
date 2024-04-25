65327e7cbedf388cddf2e131d525b94a5c0d084e
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-25 12:55:43 +0800

    perf: commentuud ustgalaa

--- apps/HRMS/recruiting-service/src/graphql/resolvers/mutations/index.ts
+++ apps/HRMS/recruiting-service/src/graphql/resolvers/mutations/index.ts
@@ -1,3 +1,1 @@
 export * from './hello-mutation';
-// export * from './job-create-mutation';
-// export * from './job-update-mutation';
--- apps/HRMS/recruiting-service/src/graphql/schemas/job.schema.ts
+++ apps/HRMS/recruiting-service/src/graphql/schemas/job.schema.ts
@@ -27,69 +27,8 @@ export const Job = gql`
     CLOSED
   }
 
-  # input CreateJobInput {
-  #   title: String!
-  #   description: String!
-  #   requirements: RequirementInput!
-  #   minSalary: String!
-  #   maxSalary: String!
-  #   status: JobStatus!
-  # }
-
-  # input RequirementInput {
-  #   skill: String
-  #   education: String
-  #   language: String
-  #   qualification: String
-  #   workExperience: String
-  #   others: String
-  # }
-
-  # enum ApplicantStatus {
-  #   PENDING
-  #   SCHEDULED
-  #   INTERVIEW_STAGE
-  #   PASSED
-  #   REJECTED
-  # }
-
-  #   input CreateApplicantInput {
-  #     firstname: String!
-  #     lastname: String!
-  #     email: String!
-  #     phone: String!
-  #     cv: String!
-  #   }
-
-  # input UpdateJobInput {
-  #   title: String!
-  #   description: String!
-  #   requirements: RequirementInput
-  #   minSalary: String
-  #   maxSalary: String
-  # }
-
-  #   input ChangeApplicantStatusInput {
-  #     status: ApplicantStatus
-  #   }
-
-  #   input editTodoInput {
-  #     title: String
-  #   }
-
   type Query {
     getJobs: [Job!]!
     getJobById(jobId: ID!): Job
-    # getApplicants(jobId: ID): [Applicant]
   }
-
-  # type Mutation {
-  # createJobRecruit(input: CreateJobInput): Job!
-  #     createApplicant(input: CreateApplicantInput): Applicant!
-  # updateJobRecruit(jobId: ID!, input: UpdateJobInput): Job!
-  #     deleteJob(id: ID!): ID!
-  #     changeApplicantsStatus(id: ID!, input: ChangeApplicantStatusInput): Applicant!
-  #     publishJobAd(jobId: ID): Job!
-  # }
-  #
 `;
