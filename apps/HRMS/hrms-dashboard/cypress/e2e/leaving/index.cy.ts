87922ab053fae695f4e1999915a18959e55f895d
Sarangoo Tsogtbaatar <23LP8796@G303-CC0155.local>
2024-04-26 12:50:32 +0800

    cypress test page/index

--- apps/HRMS/hrms-dashboard/cypress/e2e/leaving/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/leaving/index.cy.ts
@@ -2,6 +2,6 @@ describe('assessment page', () => {
   beforeEach(() => cy.visit('/leaving'));
 
   it('Should display welcome message', () => {
-    cy.get('h1').contains('hello from HRMS dashboard Leaving Page');
+    cy.get('[data-cy="open-request"]').contains('Чөлөөний хуудас бөглөх').should('be.visible');
   });
 });
