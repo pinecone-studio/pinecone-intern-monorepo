bd9c0406f675593a29c496c4dd6a28c517d20141
Bymbaabatdelger <baymba.0108@icloud.com>
2024-04-28 00:47:29 +0800

    feat(glms-dashboard): "create section"

--- apps/GLMS/glms-dashboard/cypress/e2e/section/page.cy.ts
+++
@@ -1,9 +0,0 @@
-describe ("It should render section page" , () => {
-    beforeEach(() => {
-        cy.visit("/section")
-    })
-}) 
-
-it ("Should display AddSection component" , () => {
-    cy.get('[data-cy="add-section-component"]').should("exist").should('be.visible')
-})
