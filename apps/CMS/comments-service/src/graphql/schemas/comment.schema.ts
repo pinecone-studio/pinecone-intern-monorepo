d7874b21466337c674b0f47a4da0133f4c3f7713
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-04-27 17:06:00 +0800

    feat(cms comments-service): implement delete comment mutation
    BREAKING CHANGE: delete-comment, delete-comment.spec

--- apps/CMS/comments-service/src/graphql/schemas/comment.schema.ts
+++ apps/CMS/comments-service/src/graphql/schemas/comment.schema.ts
@@ -24,9 +24,14 @@ export const commentsSchema = gql`
     name: String!
     token: String
   }
+  input DeleteCommentInput {
+    _id: ID!
+    token: String
+  }
   type Mutation {
     publishComment(createInput: CreateCommentInput!): ID
     updateComment(updateInput: UpdateCommentInput!): ID
+    deleteComment(deleteInput: DeleteCommentInput!): ID
   }
   type Query {
     getComments: [Comment]
