cbaf45a5ea04124eebc0b5807d178c177d6fa0da
Arvinbuy7n <arwinbuyan88705548@gmail.com>
2024-04-29 12:53:39 +0800

    feat(cms-dashboard): cms-dashboard category name

--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
     <Stack data-cy="dashboard-page-cy-id" bgcolor={'#ECEDF0'}>
       <Navbar />
       <Stack>
-        <Container maxWidth="lg">
+        <Container maxWidth="lg" sx={{ height: '100vh' }}>
           <Stack gap={1} py={3}>
             <Stack direction={'row'} justifyContent={'space-between'}>
               <SearchInput />
