6952ed7dc25379f3838e3e54a3109e6b42d4f667
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-04-29 22:07:36 +0800

    feat(cms comments-service): implement get replies by commentid query
    BREAKING CHANGE: get-replies-by-commentid-query, get-replies-by-commentid-query.spec
    
    #512

---
+++ apps/CMS/comments-service/specs/queries/get-replies-by-commentId-query.spec.ts
@@ -0,0 +1,27 @@
+import { errorTypes, graphqlErrorHandler } from '@/graphql/resolvers/error';
+import { getRepliesByCommentId } from '@/graphql/resolvers/queries';
+import ReplyModel from '@/models/reply.model';
+import { GraphQLResolveInfo } from 'graphql';
+jest.mock('@/models/reply.model', () => ({
+  find: jest.fn(),
+}));
+
+describe('This query should replies by commentId', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  const mock = [{ _id: 'asdf', commentId: '6628b35adfc5622cb34cedee', createdAt: new Date(), ipAddress: 'asdf', name: 'ace', parent: '', reply: 'asdf' }];
+  it('should return by commentId and return its replies', async () => {
+    jest.spyOn(ReplyModel, 'find').mockResolvedValueOnce(mock);
+    const replies = await getRepliesByCommentId!({}, { commentId: mock[0].commentId }, {}, {} as GraphQLResolveInfo);
+    expect(replies).toEqual(mock);
+  });
+  it('should return error if failed to return replies', async () => {
+    jest.spyOn(ReplyModel, 'find').mockRejectedValueOnce(graphqlErrorHandler({ message: 'cannot get replies by commentId' }, errorTypes.INTERVAL_SERVER_ERROR));
+    try {
+      await getRepliesByCommentId!({}, { commentId: mock[0].commentId }, {}, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect(error).toEqual(graphqlErrorHandler({ message: 'cannot get replies by commentId' }, errorTypes.INTERVAL_SERVER_ERROR));
+    }
+  });
+});
