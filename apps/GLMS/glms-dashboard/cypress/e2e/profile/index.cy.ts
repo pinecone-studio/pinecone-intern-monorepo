838702f404c66b10f6358a33fad307cb9a70b362
Zoya <zoya@Zoyas-MacBook-Air.local>
2024-04-29 13:43:43 +0800

    feat: glms cypress

--- apps/GLMS/glms-dashboard/cypress/e2e/profile/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/profile/index.cy.ts
@@ -1,7 +1,25 @@
-describe('profile page', () => {
-  beforeEach(() => cy.visit('/profile'));
+describe('ProfilePage Component', () => {
+  beforeEach(() => {
+    cy.intercept('POST', '**/graphql', (req) => {
+      if (req.body.operationName === 'HelloQueryFromProfileService') {
+        req.alias = 'helloQueryFromProfileServiceQuery';
+        req.reply({
+          statusCode: 200,
+          body: {
+            data: {
+              helloQueryFromProfileService: 'Hello from Profile Service',
+            },
+          },
+        });
+      }
+    });
 
-  it('Should display welcome message', () => {
-    cy.get('h1').contains('hello from GLMS dashboard Profile Page');
+    cy.visit('/profile');
+  });
+
+  it('should render ProfilePage with correct content', () => {
+    cy.contains('h1', 'hello from GLMS dashboard Profile Page').should('be.visible');
+
+    cy.get('[data-testid="profile-main"]').should('exist');
   });
 });
