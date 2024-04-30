bdf8d4d66d5ce393eef5c0628c3c3868485e68d5
Zolzaya Udval <23LP6080@G303-CC0143.local>
2024-04-30 08:42:08 +0800

    feat(articles-service): get articles by status query

---
+++ apps/CMS/articles-service/src/graphql/resolvers/queries/get-articles-by-status.ts
@@ -0,0 +1,16 @@
+import { QueryResolvers } from '@/graphql/generated';
+import { ArticleModel } from '@/models/article.model';
+import { errorTypes, graphqlErrorHandler } from '../error';
+
+export const getArticlesByStatus: QueryResolvers['getArticlesByStatus'] = async (_, { status }) => {
+  try {
+    const articles = await ArticleModel.find({ status: status.toString() }).populate('category author');
+    if (!articles) {
+      throw graphqlErrorHandler({ message: 'Failed to get articles' }, errorTypes.INTERVAL_SERVER_ERROR);
+    }
+
+    return articles;
+  } catch (error) {
+    throw graphqlErrorHandler({ message: 'Failed to get articles' }, errorTypes.INTERVAL_SERVER_ERROR);
+  }
+};
