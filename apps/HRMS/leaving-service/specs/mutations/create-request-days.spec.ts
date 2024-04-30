83a452ce64789d582f992b5bf1f97dbe27885d71
Anujin Purew <23LP0977@G303-CC0163.local>
2024-04-30 19:58:54 +0800

    docs: leaving service send mail one last time

--- apps/HRMS/leaving-service/specs/mutations/create-request-days.spec.ts
+++ apps/HRMS/leaving-service/specs/mutations/create-request-days.spec.ts
@@ -1,25 +1,29 @@
 import { GraphQLResolveInfo } from 'graphql';
 import { errorTypes, graphqlErrorHandler } from '@/graphql/resolvers/error';
-import { createLeaveRequestDays} from '@/graphql/resolvers/mutations';
+import { createLeaveRequestDays } from '@/graphql/resolvers/mutations';
 import { DurationType, LeaveType } from '@/graphql/generated';
+
+jest.mock('../../src/mail/mail-sender', () => ({
+  sendMail: jest.fn(),
+}));
 
 jest.mock('../../src/graphql/model/leave-request', () => ({
   LeaveRequestModel: {
     create: jest
       .fn()
       .mockReturnValueOnce({
-        _id: "66260763e488966d7a390e43",
+        _id: '66260763e488966d7a390e43',
         declinedReasoning: null,
-        description: "lagshin muu baina",
-        durationType: "Day",
-        employeeId: "AJILTANIB2299",
-        leaveType: "SHIT_HAPPENED",
+        description: 'lagshin muu baina',
+        durationType: 'Day',
+        employeeId: 'AJILTANIB2299',
+        leaveType: 'SHIT_HAPPENED',
         endHour: null,
-        startDate: "2024-04-26T02:52:27.560Z",
+        startDate: '2024-04-26T02:52:27.560Z',
         startHour: null,
-        status: "PENDING",
-        superVisor: "BOSS",
-        totalHour: 8
+        status: 'PENDING',
+        superVisor: 'BOSS',
+        totalHour: 8,
       })
       .mockRejectedValueOnce(null),
   },
@@ -33,25 +37,29 @@ const requestInput = {
   leaveType: LeaveType.ShitHappened,
   superVisor: 'BOSS',
   durationType: DurationType.Day,
+  email: 'amoramgl@gmail.com',
+  substitute: 'andad ogov',
 };
 
 describe('createLeaveRequestDays', () => {
   it('should create a leave request days', async () => {
-    const result = await createLeaveRequestDays?.({}, { requestInput }, {}, {} as GraphQLResolveInfo)
-    expect(result).toEqual([{
-      _id: "66260763e488966d7a390e43",
-      declinedReasoning: null,
-      description: "lagshin muu baina",
-      durationType: "Day",
-      employeeId: "AJILTANIB2299",
-      leaveType: "SHIT_HAPPENED",
-      endHour: null,
-      startDate: "2024-04-26T02:52:27.560Z",
-      startHour: null,
-      status: "PENDING",
-      superVisor: "BOSS",
-      totalHour: 8
-    }]);
+    const result = await createLeaveRequestDays?.({}, { requestInput }, {}, {} as GraphQLResolveInfo);
+    expect(result).toEqual([
+      {
+        _id: '66260763e488966d7a390e43',
+        declinedReasoning: null,
+        description: 'lagshin muu baina',
+        durationType: 'Day',
+        employeeId: 'AJILTANIB2299',
+        leaveType: 'SHIT_HAPPENED',
+        endHour: null,
+        startDate: '2024-04-26T02:52:27.560Z',
+        startHour: null,
+        status: 'PENDING',
+        superVisor: 'BOSS',
+        totalHour: 8,
+      },
+    ]);
   });
 
   it('should throw error', async () => {
