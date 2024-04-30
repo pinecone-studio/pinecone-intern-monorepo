2ecb1f70fcc704a53830237efa7342caebba4de7
NyamjargalMTMN <metamancode@gmail.com>
2024-04-30 22:21:13 +0800

    feat(cms-article): create article button, imageuploader, commentPermission implemented

--- apps/CMS/cms-dashboard/specs/articles/create-article/CommentPermission.spec.tsx
+++ apps/CMS/cms-dashboard/specs/articles/create-article/CommentPermission.spec.tsx
@@ -2,12 +2,12 @@ import { fireEvent, render } from '@testing-library/react';
 import CommentPermission from '../../../../cms-dashboard/src/app/articles/_components/create-article/CommentPermission';
 
 describe('Comment Permission Component', () => {
-  it('1. Should render commentPermission Mui Switch Component', () => {
+  it('1. Should render commentPermission Component', () => {
     const { getByTestId } = render(<CommentPermission />);
     const switchBtn = getByTestId('commentPermission');
     expect(switchBtn).toBeDefined();
   });
-  it('2. Change MuiSwitch button clicked', async () => {
+  it('2. Permission button clicked', async () => {
     const { getByTestId } = render(<CommentPermission />);
     const switchBtn = getByTestId('commentPermission');
     fireEvent.click(switchBtn);
--- apps/CMS/cms-dashboard/src/app/articles/_components/create-article/CommentPermission.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_components/create-article/CommentPermission.tsx
@@ -1,19 +1,17 @@
 'use client';
-import { Box, Typography, Switch, FormControlLabel } from "@mui/material";
 
 type ArticleCommentProps = {
     checked?: boolean  ;
+    setChecked?: ()=> void;
   };
 
 const CommentPermission = (props: ArticleCommentProps)=> {
-    const { checked } = props;
+    const { checked, setChecked } = props;
     return (
-        <Box sx={{ display: "flex", padding: "24px", gap: '16px', alignItems: "center", justifyContent: "space-between" }}>
-            <Typography data-testid="comment-header-text" sx={{ fontSize: '18px', fontWeight: '600' }}>
-                Сэтгэгдэл идэвхтэй
-            </Typography>
-            <FormControlLabel  data-testid="commentPermission" label="" control={<Switch data-testid="switch-input" color="success" checked={checked}/>}/>
-        </Box>
+        <div className=" flex p-6 gap-4 items-center justify-between ">
+            <p data-testid="comment-header-text" className=" text-lg">   Сэтгэгдэл идэвхтэй</p>
+            <input data-testid="commentPermission" type="checkbox" className="toggle" checked={checked} onChange={setChecked} />
+        </div>
     );
 }
 export default CommentPermission
