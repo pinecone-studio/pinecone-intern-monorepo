d7a9cc8d8a5360c81866ac95df183b2946870578
Zoya <zoya@Zoyas-MacBook-Air.local>
2024-04-30 13:25:21 +0800

    feat: new : true

--- apps/GLMS/challenge-service/src/graphql/resolvers/mutations/update-quiz.ts
+++ apps/GLMS/challenge-service/src/graphql/resolvers/mutations/update-quiz.ts
@@ -4,7 +4,7 @@ import { MutationResolvers } from '../../generated';
 
 export const updateQuiz: MutationResolvers['updateQuiz'] = async (_, { quizId, updateQuiz }) => {
   try {
-    const quiz = await QuizModel.findByIdAndUpdate(quizId, updateQuiz);
+    const quiz = await QuizModel.findByIdAndUpdate(quizId, updateQuiz, { new: true });
     return quiz;
   } catch (error) {
     throw graphqlErrorHandler({ message: 'can not update a quiz' }, errorTypes.INTERVAL_SERVER_ERROR);
