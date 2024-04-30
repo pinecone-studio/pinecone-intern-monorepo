30bf937038b52bbf6fb93abce19724bba2b77968
Bilguun.G <143570232+Bilguungantogos@users.noreply.github.com>
2024-04-30 13:41:16 +0800

    fix(recruiting dashboard button): bunlock yarnlock ustgav (#539)
    Co-authored-by: Билгүүн Гантогос <LP230023@G305-CC0070.local>

--- apps/HRMS/hrms-dashboard/src/app/recruiting/_components/feedJob/index.tsx
+++ apps/HRMS/hrms-dashboard/src/app/recruiting/_components/feedJob/index.tsx
@@ -1,55 +1,31 @@
-import { CreateErrorModal } from '../../_features';
 import { Grid } from '@mui/material';
+import { Button } from '../core';
+import { useRouter } from 'next/navigation';
 
-const containerStyle = {
-  container: 'inherit',
-  marginBlock: '20px',
-  marginInline: '50px',
-  backgroundColor: 'white',
-  borderRadius: '50px',
-};
+const containerStyle = { container: 'inherit', marginBlock: '24px', marginInline: '32px', backgroundColor: 'white', borderRadius: '12px' };
 
 export const JobRecruitDashboard = () => {
+  const router = useRouter();
+  const addRecruit = () => {
+    router.push('/recruiting/add-job');
+  };
   return (
-    <Grid sx={containerStyle}>
-      <Grid sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <h1> Ажлын зар</h1>
-        <div data-testid="open-button">
-          <CreateErrorModal text={'Aмжилттай үүслээ'} label={'Зар нэмэх'} />
-        </div>
+    <div style={containerStyle}>
+      <Grid sx={{ paddingInline: '24px', paddingTop: '16px' }}>
+        <Grid sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <h1 style={{ fontSize: '18px', fontWeight: 700 }}> Ажлын зар</h1>
+          <div data-testid="jobAdd-button">
+            <Button label="Зар нэмэх" plusIcon onClick={addRecruit} />
+          </div>
+        </Grid>
       </Grid>
       <div>
-        <div style={{ marginTop: '20px', borderBottom: '1px solid gray', width: '100%' }}>
-          <div style={{ display: 'flex', paddingBottom: '10px', gap: '20px' }}>
-            <p>Зар</p>
-            <p>Ирсэн өргөдөл</p>
-          </div>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '20px', paddingInline: '24px', height: '56px' }}>
+          <p style={{ fontSize: '14px', paddingBlock: '4px', fontWeight: 500 }}>Зар</p>
+          <p style={{ fontSize: '14px', paddingBlock: '4px' }}>Ирсэн өргөдөл</p>
         </div>
-        <Grid style={{ marginTop: '50px', display: 'flex', gap: '10px' }}>
-          <Grid style={{ border: '1px solid gray', width: '100%' }}>
-            <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', gap: '20px' }}>
-              <div>
-                <h1>Staff Software Engineer, Machine Learning, Core</h1>
-                <p>Байршил: Гурван гол оффис центр</p>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'end', width: '100%' }}>
-                <p style={{ backgroundColor: 'black', color: 'white' }}>Дэлгэрэнгүй</p>
-              </div>
-            </div>
-          </Grid>
-          <Grid style={{ border: '1px solid gray', width: '100%' }}>
-            <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', gap: '20px' }}>
-              <div>
-                <h1>Staff Software Engineer, Machine Learning, Core</h1>
-                <p>Байршил: Гурван гол оффис центр</p>
-              </div>
-              <div style={{ width: '100%', display: 'flex', justifyContent: 'end' }}>
-                <p style={{ backgroundColor: 'black', color: 'white' }}>Дэлгэрэнгүй</p>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
       </div>
-    </Grid>
+      <div style={{ borderTop: '1px solid #ECEDF0', width: '100%' }}></div>
+    </div>
   );
 };
