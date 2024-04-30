bbce55eecc40ad16406f854330884d62a063742c
Zorurunn <89830021+Zorurunn@users.noreply.github.com>
2024-04-30 10:15:44 +0800

    feat(articles-service): get articles by status query (#524)
    * feat(articles-service): get articles by status query
    
    * feat(articles-service): get articles by status
    
    * feat(articles-service): get articles by status
    
    ---------
    
    Co-authored-by: Zolzaya Udval <23LP6080@G303-CC0143.local>

---
+++ apps/CMS/articles-service/specs/queries/get-article-by-status.spec.ts
@@ -0,0 +1,67 @@
+import { errorTypes, graphqlErrorHandler } from '@/graphql/resolvers/error';
+import { getArticlesByStatus } from '@/graphql/resolvers/queries';
+import { GraphQLResolveInfo } from 'graphql';
+
+jest.mock('../../src/models/article.model', () => ({
+  ArticleModel: {
+    find: jest.fn().mockReturnValue({
+      populate: jest
+        .fn()
+        .mockResolvedValueOnce([
+          {
+            id: 'testId',
+            title: 'Title',
+            coverPhoto: 'image_uri',
+            content: 'article',
+            author: '#id1',
+            category: '#id1',
+            status: 'PUBLISHED',
+            slug: 'slug',
+            createdAt: '2024-09-09',
+            publishedAt: '2024-09-09',
+            updatedAt: '2024-09-09',
+            scheduledAt: '2024-09-09',
+          },
+        ])
+        .mockResolvedValueOnce(undefined)
+        .mockRejectedValueOnce(null),
+    }),
+  },
+}));
+
+const mockData = [
+  {
+    id: 'testId',
+    title: 'Title',
+    coverPhoto: 'image_uri',
+    content: 'article',
+    author: '#id1',
+    category: '#id1',
+    status: 'PUBLISHED',
+    slug: 'slug',
+    createdAt: '2024-09-09',
+    publishedAt: '2024-09-09',
+    updatedAt: '2024-09-09',
+    scheduledAt: '2024-09-09',
+  },
+];
+describe('This function should return articles with matching status', () => {
+  it('Should return all articles with matching status', async () => {
+    const articles = await getArticlesByStatus!({}, { status: 'PUBLISHED' }, {}, {} as GraphQLResolveInfo);
+    expect(articles).toEqual(mockData);
+  });
+  it('should throw an error if the article cannot be found', async () => {
+    try {
+      await getArticlesByStatus!({}, { status: 'PUBLISHED' }, {}, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect(error).toEqual(graphqlErrorHandler({ message: 'Failed to get articles' }, errorTypes.INTERVAL_SERVER_ERROR));
+    }
+  });
+  it('should throw an error when an error occurs during fetching', async () => {
+    try {
+      await getArticlesByStatus!({ undefined }, { status: 'PUBLISHED' }, { undefined }, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect(error).toEqual(graphqlErrorHandler({ message: 'Failed to get articles' }, errorTypes.INTERVAL_SERVER_ERROR));
+    }
+  });
+});
