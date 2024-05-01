c9a41cd46d15b225859d2b906dd763aad3dfe3d7
Zolzaya Udval <23LP6080@G303-CC0143.local>
2024-05-01 11:09:06 +0800

    perf(cms-dashboard): changed into tailwind css "FooterButtons" feature

---
+++ apps/CMS/cms-dashboard/src/app/dashboard/_components/NavigateLinkWithIcon.tsx
@@ -0,0 +1,19 @@
+'use client';
+import Link from 'next/link';
+type NavigateLinkWithIconProps = {
+  text: string;
+  myPathName: string;
+  icon?: JSX.Element;
+};
+
+export const NavigateLinkWithIcon = (props: NavigateLinkWithIconProps) => {
+  const { text, myPathName, icon } = props;
+  return (
+    <Link href={myPathName} data-testid="navigate-link-with-icon-test-id" className="h-full font-semibold text-[#121316] rounded-[50px] cursor-pointer flex justify-center items-center bg-[black]">
+      <div className="flex flex-row justify-center items-center gap-2 px-6 text-white">
+        {icon}
+        <p>{text}</p>
+      </div>
+    </Link>
+  );
+};
