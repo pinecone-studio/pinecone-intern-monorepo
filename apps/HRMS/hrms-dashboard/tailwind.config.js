47777dbade9c0a72a55b1e9ccca59a506803503e
Munkh-Orgil Lkhavgajav <23LP0045@G303-CC0161.local>
2024-05-01 10:01:49 +0800

    feat(hrms): header tailwind

--- apps/HRMS/hrms-dashboard/tailwind.config.js
+++ apps/HRMS/hrms-dashboard/tailwind.config.js
@@ -5,6 +5,13 @@ const { join } = require('path');
 module.exports = {
   content: [join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'), ...createGlobPatternsForDependencies(__dirname)],
   theme: {
+    colors:{
+      black:'#000',
+      white:'#FFF',
+      main:'#121316',
+      dark:'#3F4145',
+      light:'#F7F7F8',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
