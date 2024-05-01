b069297d12506f961ddbebcd678dbe4e383c6ab9
Battugs Bilguun <145326133+rimzss@users.noreply.github.com>
2024-05-01 12:29:19 +0800

    feat(frontend setup): challenge page setup (#506)
    * feat(frontend setup): challenge page setup
    
    added GetChallengeByStatus in Frontend also setup Challenge Page
    
    * feat: used query
    
    * fix: added query in front, fixed query

---
+++ apps/GLMS/glms-dashboard/src/app/challenge/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useGetChallengesByStatusQuery } from '@/generated/index';
+
+const ChallengePage = () => {
+  const { data, loading } = useGetChallengesByStatusQuery();
+  return (
+    <div>
+      <h1>Welcome to Challenge page</h1>
+      <h1>{!loading && data?.getChallengesByStatus?.map((challenge) => challenge?.title)}</h1>
+    </div>
+  );
+};
+
+export default ChallengePage;
