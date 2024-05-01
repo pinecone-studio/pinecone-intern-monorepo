6e8c9ab21abfc1b1bbae0109e1ce0a586325e2ca
Arvinbuyan Otgonjargal <145421583+Arvinbuy7n@users.noreply.github.com>
2024-05-01 17:26:02 +0800

    feat(cms-dashboard): cms-dashboard table menu button (#612)
    * feat(cms-dashboard): cms-dashboard table menu button
    
    * feat(cms-dashboard): cms-dashboard menu
    
    * feat: cms-dashboard
    
    * feat(cms-dashboard): cms-dashboard menu button
    
    * feat(cms-dashboard): cms-dashboard menu button
    
    ---------
    
    Co-authored-by: Arvinbuyan Otgonjargal <23LP6614@G303-CC0146.local>

--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import ArticleMenuButton from './_components/ArticleMenuButton';
 import { AdminNavigateLinksFeature } from './_features';
+
 const Home = () => {
   return (
     <div className="w-full max-w-screen-xl mx-auto">
       <AdminNavigateLinksFeature />
+      <ArticleMenuButton id="661c87fd6837efa536464d26" />
     </div>
   );
 };
