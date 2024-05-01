f5147fffc018bf6f17d3fe93e3b2af28e35f0a1c
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-05-01 14:20:41 +0800

    perf: modal mui-tailwind bolgov

--- apps/HRMS/hrms-dashboard/specs/recruit/Modal.spec.tsx
+++ apps/HRMS/hrms-dashboard/specs/recruit/Modal.spec.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { CreateErrorModal } from '../../src/app/recruiting/_features';
+import Image from 'next/image';
 
 describe('<CreateErrorModal />', () => {
-  test('modal closes when close button is clicked', () => {
-    const { getByText, getByTestId, queryByTestId } = render(<CreateErrorModal text="Aмжилттай үүслээ" label="Зар нэмэх" />);
-    const buttonElement = getByText('Зар нэмэх');
+  test('modal close and open when label and close button is clicked', () => {
+    const { getByText, getByTestId, queryByTestId } = render(<CreateErrorModal text="Aмжилттай устлаа" label="Устгах" />);
+
+    const buttonElement = getByText('Устгах');
+    expect(getByText('Устгах')).toBeDefined();
     fireEvent.click(buttonElement);
-    const modalElementBefore = queryByTestId('error-modal');
+
+    const modalElementBefore = queryByTestId('modal');
     expect(modalElementBefore).toBeDefined();
 
     const closeButton = getByTestId('close-button');
     fireEvent.click(closeButton);
-    const modalElementAfter = queryByTestId('error-modal');
+
+    const modalElementAfter = queryByTestId('modal');
     expect(modalElementAfter).toBeDefined();
+  });
+  test('renders correct message based on label prop', () => {
+    const text = 'Test Message';
+    const labelDelete = 'Устгах';
+    const alt = 'svg';
+    const src = '/successCreated.png';
+
+    const { getByTestId: getByTestIdEdit } = render(<CreateErrorModal text={text} label={labelDelete} />);
+    render(<Image src={src} alt={alt} width={50} height={50} />);
+
+    const img = getByTestIdEdit('modal').querySelector('img');
+    expect(img!.getAttribute('alt')).toBe(alt);
+  });
+  test('renders correct message based on label prop', () => {
+    const text = 'Test Message';
+    const labelEdit = 'Засварлах';
+    const alt = 'svg';
+    const src = '/successDeleted.png';
+
+    const { getByTestId: getByTestIdEdit } = render(<CreateErrorModal text={text} label={labelEdit} />);
+    render(<Image src={src} alt={alt} width={50} height={50} />);
+
+    const img = getByTestIdEdit('modal').querySelector('img');
+    expect(img!.getAttribute('alt')).toBe(alt);
   });
 });
