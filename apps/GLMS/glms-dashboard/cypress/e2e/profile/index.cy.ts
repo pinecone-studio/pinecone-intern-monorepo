e120479aac3ec019c0c3f8a31d88ad260e84258b
Мөнхжин Алтангэрэл <23LP7957@G303-CC0138.local>
2024-05-01 17:06:42 +0800

    fix(glms-dashboard): profile deer nmej test bichiv

--- apps/GLMS/glms-dashboard/cypress/e2e/profile/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/profile/index.cy.ts
@@ -1,25 +1,16 @@
 describe('ProfilePage Component', () => {
   beforeEach(() => {
-    cy.intercept('POST', '**/graphql', (req) => {
-      if (req.body.operationName === 'HelloQueryFromProfileService') {
-        req.alias = 'helloQueryFromProfileServiceQuery';
-        req.reply({
-          statusCode: 200,
-          body: {
-            data: {
-              helloQueryFromProfileService: 'Hello from Profile Service',
-            },
-          },
-        });
-      }
-    });
-
     cy.visit('/profile');
   });
 
   it('should render ProfilePage with correct content', () => {
     cy.contains('h1', 'hello from GLMS dashboard Profile Page').should('be.visible');
-    cy.get('[data-testid="profile-main"]').should('exist')
-    cy.get('[data-testid="profile-btn"]').should('exist');;
-  });;
+    cy.get('[data-testid="profile-main"]').should('exist');
+    cy.get('[data-testid="profile-btn"]').should('exist');
+  });
+
+  it('should navigate to home page when "Go back to home page" button is clicked', () => {
+    cy.get('[data-testid="profile-btn"]').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+  });
 });
