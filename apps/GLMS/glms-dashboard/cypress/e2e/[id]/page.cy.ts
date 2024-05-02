c34e31b123a22964662c3f556c3f805fd3e41188
hero_dev <you@example.com>
2024-05-02 16:29:15 +0800

    feat(glms-challenge-dashboard): input-core to tailwind

--- apps/GLMS/glms-dashboard/cypress/e2e/[id]/page.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/[id]/page.cy.ts
@@ -1,6 +1,6 @@
 describe(' course Id page should visit [id]', ()=>{
     beforeEach(()=>{
-        cy.visit(`/6630947579f907760d17b9c7`)
+        cy.visit(`/6633305c94d4584898bb049a`)
     })
     it('should display courseid page' , ()=>{
        cy.get('[data-cy="idCourse"]').should('exist');
