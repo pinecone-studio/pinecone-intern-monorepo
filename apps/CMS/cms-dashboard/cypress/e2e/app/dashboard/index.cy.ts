88125f6c85f7ed6e7395f9a34a0efe18936a3b47
Zolzaya Udval <23LP6080@G303-CC0143.local>
2024-05-02 09:23:19 +0800

    perf(cms-dashboard): changed into tailwind (menubar feature)

--- apps/CMS/cms-dashboard/cypress/e2e/app/dashboard/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/app/dashboard/index.cy.ts
@@ -1,5 +1,11 @@
 describe('dashboard page', () => {
   beforeEach(() => cy.visit('/dashboard'));
+
+  // ArticleStatusTabs feature
+  it('ArticleStatusTabs must be defined', () => {
+    cy.get('[data-cy="article-status-tabs-feature-cy-id"]').should('exist');
+  });
+
   // AdminNavigateLinksFeature
   it('AdminNavigateLinksFeature must be defined', () => {
     cy.get('[data-cy="admin-navigate-links-feature-cy-id"]').should('exist');
--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import ArticleMenuButton from './_components/ArticleMenuButton';
-import { AdminNavigateLinksFeature } from './_features';
+import { AdminNavigateLinksFeature, ArticleStatusTabsFeature } from './_features';
 
 const Home = () => {
   return (
     <div className="w-full max-w-screen-xl mx-auto">
-      <AdminNavigateLinksFeature />
-      <ArticleMenuButton id="661c87fd6837efa536464d26" />
+      <div className="flex flex-col gap-[10px]">
+        <ArticleStatusTabsFeature />
+        <ArticleMenuButton id="661c87fd6837efa536464d26" />
+        <AdminNavigateLinksFeature />
+      </div>
     </div>
   );
 };
