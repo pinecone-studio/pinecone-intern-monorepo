eaba3f332cf4793bc845b40784bea158e6f544e7
Unenbat <batbaatarunenbat20@gmail.com>
2024-05-02 14:05:58 +0800

    feat(mui convert tailwind): mui convert tailwind commentCard and page.tsx

--- apps/CMS/cms-dashboard/src/app/comments/_components/CommentsCard/index.tsx
+++ apps/CMS/cms-dashboard/src/app/comments/_components/CommentsCard/index.tsx
@@ -1,59 +1,45 @@
-import { Box, Button, Typography } from '@mui/material';
 import { PiEyeClosed } from 'react-icons/pi';
 import React from 'react';
 import { MdDeleteOutline, MdReply } from 'react-icons/md';
-// import { TbPointFilled } from 'react-icons/tb';
 
 const Index = () => {
   return (
-    <Box width={928} borderRadius={2} bgcolor={'white'} height={240} data-testid="comments-dialog-form-container">
-      <Box padding={2}>
-        <Typography fontSize={18} fontWeight={'bold'} lineHeight={2}>
-          Сувдан
-        </Typography>
-        <Box sx={{ display: 'flex', gap: '4px' }}>
-          <Typography fontSize={16} color={'#5E6166'} lineHeight={2}>
-            suvdaa2@gmail.com
-          </Typography>
-          <Typography fontSize={16} color={'#5E6166'} lineHeight={2} sx={{ display: 'flex', alignItems: 'center' }}>
-            {/* <TbPointFilled /> 1 минутийн өмнө */}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography fontSize={16} fontWeight={'semi-bold'} lineHeight={2}>
-            Post:
-          </Typography>
-          <Typography fontSize={16} color={'grey'}>
-            Morphosis Хөтөлбөр: Гадны зах зээлд ажиллах сонирхолтой инженерүүдэд
-          </Typography>
-        </Box>
-        <Typography fontSize={18} fontWeight={'bold'} lineHeight={2}>
-          Хулхины газар байна оо
-        </Typography>
-      </Box>
-      <Box borderTop={1} borderColor={'#5E6166'} sx={{ display: 'flex', justifyContent: 'space-between' }} padding={3}>
-        <Box>
-          <Button sx={{ gap: 1 }}>
+    <div className="w-96 rounded-lg bg-white h-60" data-testid="comments-dialog-form-container">
+      <div className="p-8">
+        <p className="text-lg font-bold leading-8">Сувдан</p>
+        <div className="flex gap-4">
+          <p className="text-base text-gray-700 leading-8">suvdaa2@gmail.com</p>
+          <p className="text-base text-gray-700 leading-8 flex items-center">1 минутийн өмнө</p>
+        </div>
+        <div className="flex items-center">
+          <p className="text-base font-semibold leading-8">Post:</p>
+          <p className="text-base text-gray-500">Morphosis Хөтөлбөр: Гадны зах зээлд ажиллах сонирхолтой инженерүүдэд</p>
+        </div>
+        <p className="text-lg font-bold leading-8">Хулхины газар байна оо</p>
+      </div>
+      <div className="border-t border-gray-700 flex justify-between p-8">
+        <div>
+          <button className="flex gap-1">
             <PiEyeClosed />{' '}
-            <Typography fontSize={12} data-cy="Hidden-Button">
+            <p className="text-sm" data-cy="Hidden-Button">
               Нуухаа болих
-            </Typography>
-          </Button>
-          <Button sx={{ gap: 1 }}>
+            </p>
+          </button>
+          <button className="flex gap-1">
             <MdDeleteOutline />
-            <Typography fontSize={12} data-cy="Deleted-Button">
+            <p className="text-sm" data-cy="Deleted-Button">
               Устгах
-            </Typography>
-          </Button>
-        </Box>
-        <Button sx={{ gap: 1 }}>
+            </p>
+          </button>
+        </div>
+        <button className="flex gap-1">
           <MdReply />
-          <Typography fontSize={12} data-cy="Reply-Button">
+          <p className="text-sm" data-cy="Reply-Button">
             Хариулах
-          </Typography>
-        </Button>
-      </Box>
-    </Box>
+          </p>
+        </button>
+      </div>
+    </div>
   );
 };
 export default Index;
--- apps/CMS/cms-dashboard/src/app/comments/page.tsx
+++ apps/CMS/cms-dashboard/src/app/comments/page.tsx
@@ -3,16 +3,15 @@
 import React from 'react';
 import Link from 'next/link';
 import { CommentsMain } from './_features';
-import { Grid } from '@mui/material';
 
 const CommentsPage = () => {
   return (
-    <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#cccccc', mt: 4 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#cccccc', marginTop: 4 }}>
       <CommentsMain />
       <Link href="/">
         <button>Go back to home page</button>
       </Link>
-    </Grid>
+    </div>
   );
 };
 
