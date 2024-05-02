b95f1bf1bdfd875e804da168a5a5c65949f17472
Zoya <zoya@Zoyas-MacBook-Air.local>
2024-05-02 11:35:26 +0800

    feat: cypress

--- apps/GLMS/glms-dashboard/src/app/challenge-dashboard/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/challenge-dashboard/page.tsx
@@ -1,29 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-import { RadioButton, SelectButton, Button } from './_components';
-import { Box } from '@mui/material';
-
 const Page = () => {
-  const [selectedOption, setSelectedOption] = useState<string>('');
-  const [checked, setChecked] = useState<boolean>(false);
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-  };
-  const handleRadioButtonChecked = () => {
-    setChecked(!checked);
-  };
-
-  return (
-    <Box data-testid="challenge-dashboard-page">
-      <Box data-cy="radio-button">
-        <RadioButton checked={checked} handleRadioButtonChecked={handleRadioButtonChecked} />
-      </Box>
-      <SelectButton handleSelectChange={handleSelectChange} options={['yuu', 'bn', 'da']} selectedOption={selectedOption} />
-      <Button label={'text'} />
-    </Box>
-  );
+  return <section data-testid="challenge-dashboard-page">It is challenge dahsboard page</section>;
 };
 
 export default Page;
