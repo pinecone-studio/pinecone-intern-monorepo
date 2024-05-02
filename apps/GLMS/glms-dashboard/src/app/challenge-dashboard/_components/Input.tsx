452e3ff494d1919241fd3ba47fb65000d0678e9e
hero_dev <you@example.com>
2024-05-02 13:46:08 +0800

    feat(glms-dashboard): input-core-to tailwind

---
+++ apps/GLMS/glms-dashboard/src/app/challenge-dashboard/_components/Input.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useState } from 'react'
+
+export const Input = () => {
+    const [quizeInput, setQuizeInput] = useState('')
+    function onChangeHandler(e:React.ChangeEvent<HTMLInputElement>){
+        setQuizeInput(e.target.value)
+    }
+  return (
+    <div>
+        <input type="text" value={quizeInput} placeholder="Оруулна уу..." data-testid="search-text-field" onChange={(e)=>onChangeHandler(e)} />
+    </div>
+  )
+}
