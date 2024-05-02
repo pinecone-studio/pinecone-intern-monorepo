27e17334f6528a94dd9c15fb763ad4d619da6526
User <user@Users-MacBook-Air.local>
2024-05-02 17:05:55 +0800

    feat: text or image uplad button changed from  mui to tailwind

---
+++ apps/GLMS/glms-dashboard/src/app/challenge-dashboard/_components/TextOrFileCreateQuestion.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+
+export const TextOrFileQuestionCreate = () => {
+  const [selectedBtn, setSelectedBtn] = useState<'text' | 'file'>('text');
+
+  const handleSelectText = () => {
+    setSelectedBtn('text');
+  };
+
+  const handleSelectFile = () => {
+    setSelectedBtn('file');
+  };
+
+  const centerStyle = 'flex flex-col justify-center items-center ';
+  const containerTextOrFile = 'flex w-[220px] rounded-2xl border border-slate-400';
+  const uploadTextClass = `flex justify-evenly items-center w-[110px] rounded-l-2xl font-semibold text-md bg-black py-2  ${selectedBtn === 'text' ? 'bg-black text-white' : 'bg-slate-200 text-black'}`;
+  const uploadFileClass = `flex justify-evenly items-center w-[110px] rounded-r-2xl font-semibold text-md  py-2  ${selectedBtn === 'file' ? 'bg-black text-white' : 'bg-slate-200 text-black'}`;
+
+  return (
+    <div className={centerStyle}>
+      <div className={containerTextOrFile}>
+        <div className={uploadTextClass} onClick={handleSelectText} data-testid="select-text-button">
+          <RadioButtonCheckedIcon />
+          <span> Сонгох</span>
+        </div>
+        <div className={uploadFileClass} onClick={handleSelectFile} data-testid="select-file-button">
+          <AddPhotoAlternateIcon />
+          <span> Зураг</span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- apps/GLMS/glms-dashboard/src/app/challenge-dashboard/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/challenge-dashboard/page.tsx
@@ -1,7 +1,13 @@
 'use client';
+import { TextOrFileQuestionCreate } from './_components/TextOrFileCreateQuestion';
 
 const Page = () => {
-  return <section data-testid="challenge-dashboard-page">It is challenge dahsboard page</section>;
+  return (
+    <section data-testid="challenge-dashboard-page">
+      It is challenge dahsboard page
+      <TextOrFileQuestionCreate />
+    </section>
+  );
 };
 
 export default Page;
