11df70faaa3f44ae2d0437dfb416123874aa2152
Kh-BatErdene <hadabagi85@gmail.com>
2024-05-02 11:01:26 +0800

    fix(header): lint error fix

--- apps/GLMS/glms-dashboard/public/AddIcon.tsx
+++
@@ -1,8 +0,0 @@
-const AddIcon = (isHover: any) => {
-  return (
-    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M11.8327 6.83268H6.83268V11.8327H5.16602V6.83268H0.166016V5.16602H5.16602V0.166016H6.83268V5.16602H11.8327V6.83268Z" fill={isHover ? 'black' : 'white'} />
-    </svg>
-  );
-};
-export default AddIcon;
