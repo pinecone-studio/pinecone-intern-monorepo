a6fbecd1e0f2d7fdfc0ea208f1372741d0fc4434
Zolzaya Udval <23LP6080@G303-CC0143.local>
2024-05-03 11:44:41 +0800

    feat(cms-dashboard): pagination component created using rect-paginate

--- apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Article, useGetArticlesQueryQuery } from '../../generated';
+import { Pagination } from './_components';
 import { SearchInput } from './_components/SearchInput';
 import { AdminNavigateLinksFeature, ArticleStatusTabsFeature } from './_features';
 import DashboardTableT from './_features/DashboardTableT';
@@ -10,10 +11,11 @@ const Home = () => {
   const articles = article?.getArticlesQuery as Article[] | undefined;
 
   return (
-    <div data-cy="dashboard-table-cy" className="w-full max-w-screen-xl mx-auto">
+    <div data-cy="dashboard-table-cy" className="w-full max-w-screen-xl mx-auto flex flex-col gap-[10px]">
       <SearchInput />
       <DashboardTableT articles={articles} />
       <ArticleStatusTabsFeature />
+      <Pagination />
       <AdminNavigateLinksFeature />
     </div>
   );
