59243dd0d3332f4070d1dc30c9b05f7eabba969d
Bymbaabatdelger <145735499+Bymbaabatdelger@users.noreply.github.com>
2024-05-06 13:26:40 +0800

    feat(glms-dashboard): create single section ans get sections query (#719)
    * feat(glms-dashboard): create single section ans get sections query
    
    * feat(glms-dashbaord): glms-dashboard
    
    create single section and get sections query
    
    * feat(glms-dashboard): lint zasav
    
    * feat(glms-dashboard): test
    
    * feat(glms-dashboard): neg section uusgedeg bolson hedii ch file-management saijruulalt heregtei
    
    zurag oruulj bolohgui bga

---
+++ apps/GLMS/glms-dashboard/src/app/section/_features/GetSections.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useGetSectionsQuery } from '../../../generated';
+import EditIcon from '../assets/EditIcon';
+import DeleteIcon from '../assets/DeleteIcon';
+
+const GetSections = () => {
+  const { data } = useGetSectionsQuery();
+
+  return (
+    <div data-testid="get-sections-query" className="bg-[#fff] rounded-[6px]">
+      {data?.getSections.map((section) => (
+        <div key={section.id} className="flex flex-col justify-center items-center">
+          <div  data-testid="section-form" className="flex flex-col gap-[4px] bg-[#fff] border-1 rounded-[4px] justify-center items-center p-6">
+            <div className="'flex flex-col gap-4 border-2 border-dashed rounded-4 p-8 border-[#D6D8DB] rounded-[8px]">
+              <div className="flex flex-col py-2">
+                <p className="font-bold">Хэсгийн гарчиг</p>
+                <p data-testid="title" className="w-[588px] h-fit border rounded-[4px] p-2">
+                  {section.title}
+                </p>
+              </div>
+              <div className="flex flex-col py-2">
+                <p className="font-bold">Дэлгэрэнгүй</p>
+                <p data-testid="description" className="w-[588px] h-fit border rounded-[4px] p-2">
+                  {section.description}
+                </p>
+              </div>
+              <div className="flex flex-col py-2">
+                <p className="font-bold"> Хэсгийн зураг</p>
+                <div data-testid="contentImage" className="w-[588px] h-60 border-2 border-dashed border-gray-300 rounded-xl flex flex-col justify-center items-center">
+                    <img src={`${section.contentImage}`} alt="sectionImage"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center py-4">
+            <button className=" w-[101px] bg-transparent border-2 border font-bold rounded-[12px] p-2 text-black flex items-center justify-center gap-2 hover:bg-[#D6D8DB]">
+              Засах <EditIcon />
+            </button>
+            <button className="w-[101px] bg-transparent border-2 border font-bold rounded-[12px] p-2 text-black flex items-center judtify-center gap-2 hover:bg-[#D6D8DB]">
+              Устгах <DeleteIcon />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default GetSections;
