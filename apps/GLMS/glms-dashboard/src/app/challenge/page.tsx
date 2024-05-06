196d7c89bb2f98d297b32c2858988df77b97b2a5
ReiGnZ <macbook@macbooks-MacBook-Pro.local>
2024-05-06 14:57:27 +0800

    feat(glms-dashboard): xp circle

--- apps/GLMS/glms-dashboard/src/app/challenge/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/challenge/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useGetChallengesByStatusQuery } from '@/generated/index';
+import { TestScore } from './_components/TestScore';
 
 const ChallengePage = () => {
   const { data, loading } = useGetChallengesByStatusQuery();
   return (
     <div>
-      <h1>Welcome to Challenge page</h1>
+      <TestScore />
       <h1>{!loading && data?.getChallengesByStatus?.map((challenge) => challenge?.title)}</h1>
     </div>
   );
