0c535c0dbae95ccb0d1edb1bd5d9167538205f25
Bymbaabatdelger <baymba.0108@icloud.com>
2024-05-07 17:27:16 +0800

    feat(courses-service): get section by id

--- apps/GLMS/courses-service/specs/queries/get-section-by-id.spec.ts
+++ apps/GLMS/courses-service/specs/queries/get-section-by-id.spec.ts
@@ -1,45 +1,38 @@
-import { getSectionById } from '@/graphql/resolvers/queries/get-section-by-id';
-import { GraphQLError, GraphQLResolveInfo } from 'graphql';
- 
-jest.mock('@/model/section-model', () => ({
-  sectionModel: {
-    findById: jest.fn()
-      .mockReturnValueOnce({
-        _id: '1',
-        title: "Html",
-        description: "Html intro",
-        contentImage: "image.png",
-      })
-      .mockResolvedValueOnce(undefined)
-      .mockReturnValueOnce(null),
-  },
-}));
+import { getSectionById } from '@/graphql/resolvers/queries';
+import sectionModel from '@/model/section-model';
+import { GraphQLResolveInfo } from 'graphql';
 
-describe('get section', () => {
-  it('should get a section', async () => {
-
-    const result = await getSectionById!({} , { id: '1' }, {}, {} as GraphQLResolveInfo);
-    expect(result).toEqual({
-      _id: '1',
-      title: "Html",
-      description: "Html intro",
-      contentImage: "image.png",
+describe('getSectionById resolver', () => {
+    it('should return section if found', async () => {
+      const mockSectionId = 'mockSectionId';
+      const mockSection = { _id: mockSectionId, name: 'Mock Section' };
+      const mockArgs = { id: mockSectionId };
+      
+      jest.spyOn(sectionModel, 'findById').mockResolvedValue(mockSection);
+  
+      const result = await getSectionById!({}, mockArgs, {},{} as GraphQLResolveInfo);
+  
+      expect(result).toEqual(mockSection);
+      expect(sectionModel.findById).toHaveBeenCalledWith(mockSectionId);
+    });
+  
+    it('should throw an error if section is not found', async () => {
+      const mockArgs = { id: 'nonExistingId' };
+  
+      jest.spyOn(sectionModel, 'findById').mockResolvedValue(null);
+  
+      await expect(getSectionById!({}, mockArgs, {}, {} as GraphQLResolveInfo)).rejects.toThrow('unknown error');
+      expect(sectionModel.findById).toHaveBeenCalledWith('nonExistingId');
+    });
+  
+    it('should throw an error if an unknown error occurs', async () => {
+      const mockArgs = { id: 'mockId' };
+      const mockError = new Error('Mock Error');
+  
+      jest.spyOn(sectionModel, 'findById').mockRejectedValue(mockError);
+  
+      await expect(getSectionById!({}, mockArgs, {}, {} as GraphQLResolveInfo)).rejects.toThrow('unknown error');
+      expect(sectionModel.findById).toHaveBeenCalledWith('mockId');
     });
   });
-
-  it('should throw an error if the dsection cannot be found', async () => {
-    try {
-      await getSectionById!({}, { id: '2' }, {}, {} as GraphQLResolveInfo);
-    } catch (error) {
-      expect(error).toEqual(new GraphQLError('unknown error'));
-    }
-  });
-
-  it('should throw an error if an error occurs during section retrieval', async () => {
-    try {
-      await getSectionById!({}, { id: '1' }, {}, {} as GraphQLResolveInfo);
-    } catch (error) {
-        expect(error).toEqual(new GraphQLError('unknown error'));
-    }
-  });
-});
+  
