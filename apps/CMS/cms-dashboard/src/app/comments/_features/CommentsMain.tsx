54a27d87fd82b7dcb77f60bd76f51d7e50bba159
unknown <otgoohghg@yahoo.com>
2024-05-07 15:51:23 +0800

    feat(cms-dashboard): added limit offset
    #744

--- apps/CMS/cms-dashboard/src/app/comments/_features/CommentsMain.tsx
+++ apps/CMS/cms-dashboard/src/app/comments/_features/CommentsMain.tsx
@@ -1,13 +1,27 @@
-// import CommentsCard from '../_components/CommentsCard';
-// import { useGetCommentsQuery } from '../../../generated';
+import { useState } from 'react';
+import CommentsCard from '../_components/CommentsCard';
+import { useGetCommentsQuery } from '../../../generated';
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+
+const perPage = 5;
 
 export const CommentsMain = () => {
-  // const { data } = useGetCommentsQuery();
-  // const comments = data?.getComments || [];
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data } = useGetCommentsQuery({
+    variables: {
+      input: {
+        limit: perPage,
+        offset: (currentPage - 1) * perPage,
+      }
+    }
+  });
+  const comments = data?.getComments || [];
 
   return (
     <div>
-      {/* {comments.map((item) => (
+      <CommentsCard/>
+       {comments.map((item) => (
         <div  key={item?._id}>
           {item?.comment && (
             <CommentsCard 
@@ -18,7 +32,12 @@ export const CommentsMain = () => {
               articleId={item.articleId ?? ""} />
           )}
         </div>
-      ))} */}
+      ))}
+      <div className='flex items-center justify-evenly w-full'>
+        <button className='btn btn-default items-center' onClick={() => setCurrentPage(currentPage - 1)}><FaArrowLeft/>Prev</button>
+        <p className='px-2 rounded-sm bg-slate-300 text-#1C2024'>{currentPage}</p>
+        <button className='btn btn-default items-center' onClick={() => setCurrentPage(currentPage + 1)}>Next<FaArrowRight/></button>
+      </div>
     </div>
   );
 };
--- apps/CMS/cms-dashboard/src/app/comments/page.tsx
+++ apps/CMS/cms-dashboard/src/app/comments/page.tsx
@@ -1,17 +1,13 @@
 'use client';
-
 import Link from 'next/link';
-import { useHelloQueryFromCommentsServiceQuery } from '../../generated';
-// import { CommentsMain } from './_features';
+import { CommentsMain } from './_features';
 
 const CommentsPage = () => {
-  const { data } = useHelloQueryFromCommentsServiceQuery();
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'grey' }}>
       <h1>hello from CMS dashboard Comments Page</h1>
-      <h1>hello from Comments Service Query {data?.helloQueryFromCommentsService}</h1>
-      {/* <CommentsMain /> */}
+      <CommentsMain />
       <Link href="/">
         <button>Go back to home page</button>
       </Link>
