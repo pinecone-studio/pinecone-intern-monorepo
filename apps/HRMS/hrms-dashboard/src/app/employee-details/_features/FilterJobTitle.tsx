b63f6e4559252159c446b84f2ebb3dbd76ba251f
Munkh-Orgil Lkhavgajav <23LP0045@G302-CC0108.local>
2024-05-07 14:37:07 +0800

    feat(hrms): table filter

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_features/FilterJobTitle.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_features/FilterJobTitle.tsx
@@ -1,24 +1,21 @@
-import { useGetAllEmployeeQuery } from '@/generated';
+import { useGetJobTitlesQuery } from '@/generated';
 
 export const FilterJobTitle = () => {
-  const { data, loading } = useGetAllEmployeeQuery();
+  const { data, loading } = useGetJobTitlesQuery();
   const jobs = data?.getAllEmployee?.map((employee) => employee?.jobTitle).filter((value, index, currentValue) => currentValue.indexOf(value) === index);
 
   return (
     <div className="rounded-xl border border-[#ECEDF0] overflow-hidden">
-      <select className="flex bg-white w-fit justify-center items-center rounded-xl border-r-8 border-transparent py-2 px-3 text-sm font-semibold text-main">
+      <select data-cy="filterJob" className="flex bg-white w-fit justify-center items-center rounded-xl border-r-8 border-transparent py-2 px-3 text-sm font-semibold text-main">
         <option value="" className="gap-2 bg-white flex justify-between text-light" selected hidden>
           Мэргэжил
         </option>
-        {!loading ? (
+        {!loading &&
           jobs?.map((job, index) => (
             <option className="bg-error" key={index} value={job ?? ''}>
               {job}
             </option>
-          ))
-        ) : (
-          <option className="loading loading-spinner loading-xs"></option>
-        )}
+          ))}
       </select>
     </div>
   );
