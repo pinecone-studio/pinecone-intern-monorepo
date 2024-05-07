a60f8bea3485df470e3a04576a70cb6e2334ea6e
Altangerel Gantulga <23LP1264@G304-CC0117.local>
2024-05-07 14:11:11 +0800

    feat: course style zassan

--- apps/GLMS/glms-dashboard/src/app/dashboard/_components/Course.tsx
+++ apps/GLMS/glms-dashboard/src/app/dashboard/_components/Course.tsx
@@ -1,25 +1,27 @@
 'use client';
 import { Course } from '@/generated';
+import { CourseDelete } from 'apps/GLMS/glms-dashboard/public/assets/CourseDelete';
 
 const Courses = (props: Course) => {
   const { id, thumbnail, title, description } = props;
 
   return (
-    <div className=" bg-white cursor-pointer h-[240px] w-[281px] overflow-hidden border-solid border-[1px] border-[#0000001A] rounded-xl" key={id} data-testid="courseContain">
+    <div className=" bg-white cursor-pointer w-[281px] overflow-hidden border-solid border-[1px] border-[#0000001A] rounded-xl" key={id} data-testid="courseContain">
       <div className="w-full h-[120px] rounded-md overflow-hidden">
         <img data-testid="lessonImage" src={`${thumbnail}`} alt="lessonImage" />
       </div>
-      <div className="h-[120px] w-full px-[21px] justify-between py-1">
-        <p className="text-[16px] font-bold  " data-testid="titleTest" color={'#121316'}>
+      <div className=" w-full px-[21px] justify-between py-2">
+        <p className="text-[16px] font-bold mb-2" data-testid="titleTest" color={'#121316'}>
           {title}
         </p>
-        <p className="test-[14px] font-normal overflow-hidden h-[48px]" data-testid="infoTest" color={'#3F4145'}>
+        <p className="text-[14px] font-normal overflow-hidden h-[40px] text-[#3F4145]" data-testid="infoTest" color={'#3F4145'}>
           {description}
         </p>
-        <div className=" w-fit px-1 py-[2px] bg-[#C1E6CF] rounded-xl">
-          <p className="flex text-[14px] font-normal" data-testid="lessonCountTest">
+        <div className="py-3 flex justify-between items-center ">
+          <p className="flex w-fit bg-[#C1E6CF]  px-1 py-[2px] rounded-xl  text-[14px] font-normal" data-testid="lessonCountTest">
             Lessons
           </p>
+          <CourseDelete />
         </div>
       </div>
     </div>
