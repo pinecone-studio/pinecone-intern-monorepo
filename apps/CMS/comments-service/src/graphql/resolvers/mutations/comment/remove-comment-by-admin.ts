d2b1a8408fa1f3501799efe625f39b5f65061dba
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-05-07 13:27:07 +0800

    feat(cms comments-service): rebased

--- apps/CMS/comments-service/src/graphql/resolvers/mutations/comment/remove-comment-by-admin.ts
+++ apps/CMS/comments-service/src/graphql/resolvers/mutations/comment/remove-comment-by-admin.ts
@@ -5,6 +5,7 @@ import { CommentsModel } from '@/models/comment.model';
 export const deleteCommentByAdmin: MutationResolvers['deleteCommentByAdmin'] = async (_, { removeInput }) => {
   try {
     const deletedComment = await CommentsModel.findByIdAndUpdate(removeInput._id, { status: 'DELETED' });
+
     return deletedComment._id;
   } catch (error) {
     throw graphqlErrorHandler({ message: `cannot remove comment by admin` }, errorTypes.INTERVAL_SERVER_ERROR);
