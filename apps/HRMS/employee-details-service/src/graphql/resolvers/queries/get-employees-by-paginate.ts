31f1f7d15764f16b3f8695ddc875548b34d08163
батчулуун цогбаяр <23LP2014@G302-CC0088.local>
2024-05-07 12:34:52 +0800

    feat(hrms): eds-search-filter

--- apps/HRMS/employee-details-service/src/graphql/resolvers/queries/get-employees-by-paginate.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/queries/get-employees-by-paginate.ts
@@ -1,12 +1,17 @@
 import { QueryResolvers } from '@/graphql/generated';
 import { EmployeeModel } from '@/models/employee';
 
-export const getEmployeesByPaginate: QueryResolvers['getEmployeesByPaginate'] = async (_, { paginationInput }) => {
+export const getEmployeesByPaginate: QueryResolvers['getEmployeesByPaginate'] = async (_, { filterInput, paginationInput }) => {
   const { limit, page } = paginationInput;
-  const employees = await EmployeeModel.find({})
+  const { searchedValue } = filterInput;
+
+  const searchQuery = { firstName: { $regex: searchedValue, $options: 'i' } };
+
+  const employees = await EmployeeModel.find(searchQuery)
     .limit(limit)
     .skip(limit * (page - 1));
-  const totalEmployees = await EmployeeModel.find({}).countDocuments();
+
+  const totalEmployees = await EmployeeModel.find(searchQuery).countDocuments();
 
   return { employees, totalEmployees };
 };
