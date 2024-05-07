b632ffc4a5411c8e24a059eddb8ce88379c0db7a
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2024-05-07 09:36:52 +0500

    feat(employee-details-service): sign in service

---
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/sign-in.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { MutationResolvers } from '@/graphql/generated';
+import { GraphQLError } from 'graphql';
+import { EmployeeModel } from '@/models/employee';
+import graphqlErrorHandler, { errorTypes } from '../error';
+
+export const signIn: MutationResolvers['signIn'] = async (_, { input }) => {
+  try {
+    const { emailOrPhoneNumber } = input;
+    const employee = await EmployeeModel.findOne({
+      $or: [{ email: emailOrPhoneNumber }, { phone: emailOrPhoneNumber }],
+    });
+
+    if (!employee) {
+      throw graphqlErrorHandler({ message: 'Бүртгэлтэй хэрэглэгч алга' }, errorTypes.NOT_FOUND);
+    }
+
+    const id = employee.id;
+    const firstName = employee.firstName;
+    const lastName = employee.lastName;
+    const email = employee.email;
+
+    const token = jwt.sign({ id, firstName, email, lastName }, 'secret-key');
+
+    return { token, message: 'Амжилттай нэвтэрлээ' };
+  } catch (error) {
+    if (error instanceof GraphQLError) throw error;
+    throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST);
+  }
+};
--- apps/HRMS/employee-details-service/src/graphql/schemas/index.ts
+++ apps/HRMS/employee-details-service/src/graphql/schemas/index.ts
@@ -2,5 +2,6 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { helloEmployeeDetailsSchema } from './hello.schema';
 import { employeeDetailsSchema } from './employee.schema';
 import { dependentSchema } from './dependent.schema';
+import { signSchema } from './signin.schema';
 
-export const typeDefs = mergeTypeDefs([helloEmployeeDetailsSchema, employeeDetailsSchema, dependentSchema]);
+export const typeDefs = mergeTypeDefs([helloEmployeeDetailsSchema, employeeDetailsSchema, dependentSchema, signSchema]);
