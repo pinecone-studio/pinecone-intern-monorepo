8cd73d65e52aff9f965cfb5cd52a98fb75f7e8b5
Munkh-Erdene Beleg-Urnukh <23LP1379@G305-employeeiMac.local>
2024-05-07 11:58:05 +0800

    fix(hrms-dashboard): cypress test fixed

--- apps/HRMS/hrms-dashboard/cypress/e2e/leaving/request-details.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/leaving/request-details.cy.ts
@@ -6,12 +6,12 @@ describe('Detail Feature', () => {
 
   it('1.should approve request when approve button is clicked', () => {
     cy.get('[data-testid="approve-button"]').click();
-    cy.get('[data-testid="request-status"]').should('contain.text', 'approved');
+    cy.get('[data-testid="request-status"]').should('contain.text', 'Зөвшөөрөх');
   });
 
   it('2.should decline request when decline button is clicked', () => {
     cy.get('[data-testid="decline-button"]').click();
-    cy.get('[data-testid="request-status"]').should('contain.text', 'declined');
+    cy.get('[data-testid="request-status"]').should('contain.text', 'Татгалзах');
   });
 
   it('3.should navigate back when back button is clicked', () => {
