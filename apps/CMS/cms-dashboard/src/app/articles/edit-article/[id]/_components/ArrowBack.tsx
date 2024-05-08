507e97961ad3b5e454d8aa2ec043a9d626f4bc49
Bayra1 <isvhbaatar5@gmail.com>
2024-05-08 09:59:16 +0800

    fix(cms-dashboard and cms-article): repaired some problems at filterByDate and submitButton

--- apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/ArrowBack.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/ArrowBack.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ArrowBackIcon } from '@/icons';
-
+import "../_styles/editArticle.css";
 export const ArrowBack = () => {
   return (
     <button data-testid="arrow-back-button-id" className="hoverRotate">
