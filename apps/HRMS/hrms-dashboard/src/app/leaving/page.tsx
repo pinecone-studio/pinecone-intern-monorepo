7138a9b28697b0464e44623570370df210f0f6eb
Мөнхжин Алтангэрэл <23LP7957@Setups-iMac.local>
2024-05-08 15:31:32 +0800

    fix(hrms-dashboard): conflict

--- apps/HRMS/hrms-dashboard/src/app/leaving/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/leaving/page.tsx
@@ -3,7 +3,7 @@ import { CreateLeaveRequestMain } from './_features';
 
 const LeavingPage = () => {
   return (
-    <div className="flex flex-col hap-[24px] pt-[32px] px-[32px] w-[100%] bg-[#F7F7F8]">
+    <div className="flex flex-col  hap-[24px] pt-[32px] px-[32px] w-[100%] bg-[#F7F7F8]">
       <CreateLeaveRequestMain />
       <Requests />
     </div>
