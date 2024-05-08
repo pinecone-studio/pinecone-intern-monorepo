8f109a5efaf172809a2921a5bb292cf4e701cacc
Bymbaabatdelger <baymba.0108@icloud.com>
2024-05-09 01:07:31 +0800

    feat(glms-dashboard): test zasav

--- apps/GLMS/glms-dashboard/cypress/e2e/section/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/section/index.cy.ts
@@ -13,7 +13,6 @@ describe('Handle section page', () => {
     cy.get('[data-cy="description"]').should('exist');
     cy.get('[data-cy="contentImage"]').should('exist');
     cy.get('[data-cy="delete-btn"]').should('exist')
-    cy.get('[data-cy="delete-btn"]').click()
     cy.get('[data-cy="update-btn"]').should('exist')
     cy.get('[data-cy="update-btn"]').click()
   });
