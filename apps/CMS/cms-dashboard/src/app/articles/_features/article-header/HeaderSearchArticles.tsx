de1ce9f30efc8fc166242178d541f66675ef205c
Ganuuh <145634432+Ganuuh@users.noreply.github.com>
2024-05-08 10:32:45 +0800

    feat(cms-dashboard): article header

--- apps/CMS/cms-dashboard/src/app/articles/_features/article-header/HeaderSearchArticles.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_features/article-header/HeaderSearchArticles.tsx
@@ -15,7 +15,9 @@ const SearchArticles = (props: SearchArticleProps) => {
       <p className="text-xl text-black">Search result</p>
       <div data-cy="searched-articles-container" className="w-full" style={{ display: 'grid', gridTemplateColumns: 'repeat(2,1fr)', padding: '20px' }}>
         {loading ? (
-          <p>Loading...</p>
+          <div className="w-full h-40 flex items-center justify-center">
+            <p>Loading...</p>
+          </div>
         ) : (
           data?.getArticlesByStatus.map((data) => {
             const { title, ...rest } = data;
