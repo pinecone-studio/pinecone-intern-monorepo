d10f389a176000aeaaee8a50db2e5fec09f4a0ec
Bymbaabatdelger <145735499+Bymbaabatdelger@users.noreply.github.com>
2024-05-09 14:26:38 +0800

    feat(glms-dashboard): front deer section update hiideg bolov (#814)
    * feat(glms-dashboard): front deer section update hiideg bolov
    
    * feat(glms-dashboard): section updated in front
    
    * feat(glms-dashboard): front deer section update hiideg bolov
    
    * feat(glms-dashboard): front deer section update hiideg bolov
    
    * feat(glms-dashboard): front deer section update hiideg bolson
    
    * feat(glms-dashboard): front deer section updateledeg bolov
    
    * feat(glms-dashboard): front deer section update hiideg bolov
    
    * feat(glms-dashboard): test zasav
    
    * feat(glms-dashboard): test
    
    * feat(glms-dashboard): test
    
    * feat(glms-dashboard): test
    
    * feat(glms-dashboard): hello test
    
    * feat(glms-dashboard): update section test zasav
    
    * feat(glms-dashboard): front der section update hiideg bolov

---
+++ apps/GLMS/glms-dashboard/src/app/update-section/page.tsx
@@ -0,0 +1,122 @@
+'use client';
+import { useGetSectionByIdQuery, useUpdateSectionMutation } from '@/generated';
+import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { ArrowLeftIcon } from '../../../public/assets/ArrowLeftIcon';
+import { useRouter } from 'next/navigation';
+import FileUploader from '../../components/FileUploader';
+
+const UpdateSection = () => {
+  const router = useRouter();
+  const[sectionID , setSectionID] = useState("")
+  const [updateSection] = useUpdateSectionMutation();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const sectionId = localStorage.getItem('sectionId');
+      if (sectionId !== null) {
+        setSectionID(sectionId);
+      }
+    }
+  }, []);
+  
+ 
+  const id = sectionID? sectionID : '';
+
+  const validatinSchema = yup.object({
+    title: yup.string().required(),
+    description: yup.string().required(),
+    contentImage: yup.string(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      thumbnail: '',
+    },
+    validationSchema: validatinSchema,
+    onSubmit: (values) => {
+      updateSection({
+        variables: {
+          id: id,
+          sectionInput: {
+            title: values.title,
+            description: values.description,
+            contentImage: values.thumbnail,
+          },
+        },
+      });
+      router.push('section');
+      localStorage.clear();
+    },
+  });
+
+  const { data, loading } = useGetSectionByIdQuery({
+    variables: { id: id },
+  });
+
+  useEffect(() => {
+    if (data && data.getSectionById) {
+      const section = data.getSectionById;
+
+      formik.setFieldValue('title', section.title);
+      formik.setFieldValue('description', section.description);
+      formik.setFieldValue('thumbnail', section.contentImage);
+    }
+  }, [data]);
+
+  if (loading) return <p>Loading...</p>;
+  return (
+    <div data-testid="update-section-page-container" className="flex flex-col justify-center bg-[#F7F7F8] px-20">
+      <div data-cy="handle-back-page" onClick={() => router.push('/section')} className=" flex flex-row justify-center items-center gap-1 w-[140px] h-fit py-4">
+        <ArrowLeftIcon />
+        <p>Нүүр</p>
+      </div>
+      <div data-testid="update-section-form" className="flex flex-col gap-[4px] bg-[#fff] border-1 rounded-[4px] justify-center items-center p-6">
+        <div className="'flex flex-col  gap-4 border-2 border-dashed rounded-4 p-8 border-[#D6D8DB] rounded-[8px]">
+          <div className="flex flex-col py-2">
+            <p className="font-bold">Хэсгийн гарчиг</p>
+            <input
+              data-cy="title"
+              className="w-[588px] h-fit border rounded-[4px] p-2"
+              type="text"
+              name="title"
+              placeholder="Оруулна уу..."
+              id="title-test"
+              onChange={formik.handleChange}
+              value={formik.values.title}
+            ></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <p className="font-bold">Дэлгэрэнгүй</p>
+            <textarea
+              data-cy="description"
+              className="w-[588px] h-[160px] border rounded-[4px] p-2"
+              id="description-test"
+              name="description"
+              onChange={formik.handleChange}
+              value={formik.values.description}
+              placeholder="Энд бичнэ үү..."
+            ></textarea>
+          </div>
+          <div className="flex flex-col py-2">
+            <p className="font-bold"> Хэсгийн зураг</p>
+            <FileUploader thumbnail={formik.values.thumbnail} setFieldValue={formik.setFieldValue} />
+          </div>
+        </div>
+        <div className="flex gap-4 jutify-center items-center py-4">
+          <button
+            data-cy="update-section-handle-btn"
+            className="px-2 py-1 bg-black text-white rounded-[8px] flex items-center justify-center text-[20px] pb-2 hover:bg-[#D6D8DB] hover:text-black hover:cursor:pointer"
+            onClick={() => formik.handleSubmit()}
+          >
+            засах
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default UpdateSection;
