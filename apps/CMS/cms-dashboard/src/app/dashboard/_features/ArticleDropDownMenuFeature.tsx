3d8f87ee7368546fbd6ae4e0a432210ee5e378e0
Zorurunn <89830021+Zorurunn@users.noreply.github.com>
2024-05-09 16:48:18 +0800

    feat(cms-dashboard): update article (#895)
    Co-authored-by: Zolzaya Udval <23LP6080@G302-CC0097.local>

---
+++ apps/CMS/cms-dashboard/src/app/dashboard/_features/ArticleDropDownMenuFeature.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import React, { useCallback, useState } from 'react';
+import { LinkButtonIcon } from '@/icons';
+import { MorevertButtonIcon } from '@/icons';
+import { ArchiveButtonIcon } from '@/icons';
+import { ArticleStatus, useUpdateArticleStatusByIdMutation } from '@/generated';
+import { toast } from 'react-toastify';
+import { ApolloError } from '@apollo/client';
+import { ArchivedSuccessfullyModal } from '../_components/ArchivedSuccessfullyModal';
+import { useRefetch } from '@/common/providers/RefetchProvider';
+
+export const ArticleDropDownMenuFeature = ({ id }: { id: string }) => {
+  const [anchorEl, setAnchorEl] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [updateArticleStatusById] = useUpdateArticleStatusByIdMutation();
+  const refetch = useRefetch();
+
+  const handleClick = () => {
+    setAnchorEl(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(false);
+  };
+
+  const handleClickCopy = useCallback(async () => {
+    setCopied(true);
+
+    const domain = window.location.origin;
+
+    await navigator.clipboard.writeText(`${domain}/articles/copy-article/${id}`);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1300);
+  }, [id]);
+
+  const archiveArticle = async () => {
+    try {
+      const { data } = await updateArticleStatusById({
+        variables: {
+          id,
+          newStatus: ArticleStatus.Archived,
+        },
+      });
+
+      const title = data?.updateArticleStatusById.title;
+
+      toast(<ArchivedSuccessfullyModal title={title ?? ''} />, {
+        progressStyle: { background: '#01E17B' },
+        position: 'top-center',
+        autoClose: 3000,
+      });
+
+      refetch();
+    } catch (error) {
+      if (error instanceof ApolloError) {
+        toast.error(error.graphQLErrors[0].message, {
+          position: 'top-center',
+          autoClose: 3000,
+          hideProgressBar: true,
+        });
+      }
+    }
+  };
+  return (
+    <div className="relative dropdown">
+      <div data-testid="menu-button-test-id" data-cy="morevert-button-test-cy" tabIndex={0} role="button" onClick={handleClick}>
+        <MorevertButtonIcon />
+      </div>
+
+      <div className="absolute left-[-50px] top-8">
+        {anchorEl && (
+          <div onClick={handleClose} data-cy="drop-down-menu-test-cy">
+            <ul tabIndex={0} className="dropdown-content z-10 menu p-2 shadow rounded-xl border border-slate-200">
+              <li onClick={archiveArticle} data-testid="close-menu-button-test-id" className="z-10 bg-white">
+                <a>
+                  <ArchiveButtonIcon />
+                  Архив
+                </a>
+              </li>
+
+              <li data-testid="copy-clipboard-button-test-id" className="z-10 bg-white" onClick={handleClickCopy}>
+                <a className="whitespace-nowrap">
+                  <LinkButtonIcon />
+                  Линк хуулах
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {copied && <p className="text-slate-50 bg-[#8e8d8d] px-3 w-fit rounded-lg mt-2 ml-9 z-10">Copied</p>}
+      </div>
+    </div>
+  );
+};
