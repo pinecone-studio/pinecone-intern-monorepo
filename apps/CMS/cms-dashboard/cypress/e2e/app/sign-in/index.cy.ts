59c26597abc1ea923bb38b1393c5b732efb609aa
ulaaka211 <uulaaka73gmail.com>
2024-05-09 19:35:09 +0800

    feat(reset-password frontend): reset-password frontend implementation

--- apps/CMS/cms-dashboard/cypress/e2e/app/sign-in/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/app/sign-in/index.cy.ts
@@ -4,6 +4,7 @@ describe('articles page', () => {
   it('1. Should display signin form', () => {
     cy.get('[data-testid="sign-in-form-container"]').should('exist').should('be.visible');
   });
+
   it('2. When user enters no value on the inputs and click on the sign in button, it should display an error messages', () => {
     cy.get('[data-cy="Sign-In-Button"]').should('exist').click();
     cy.get('[data-cy="Sign-In-Button"]').should('be.disabled');
