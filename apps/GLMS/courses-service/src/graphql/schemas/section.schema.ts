2c1b0c2a6f1032e487c719d96327e7bbcc79fb07
Бат-Эрдэнэ Хадбаатар <23LP6564@G302-CC0115.local>
2024-05-09 20:33:30 +0800

    feat(section): section-connection&update

--- apps/GLMS/courses-service/src/graphql/schemas/section.schema.ts
+++ apps/GLMS/courses-service/src/graphql/schemas/section.schema.ts
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-tag';
 export const sectionSchema = gql`
-   type Query {
+  type Query {
     getSections: [Section!]!
     getSectionById(id: ID!): Section!
   }
@@ -9,16 +9,18 @@ export const sectionSchema = gql`
     title: String
     description: String
     contentImage: String
-    createdAt:Date
+    posted: Boolean
+    createdAt: Date
   }
   input SectionInput {
     title: String
     description: String
     contentImage: String
+    posted: Boolean
   }
   type Mutation {
-    createSection(sectionInput:SectionInput!): Section!
-    updateSection(id: ID!, sectionInput:SectionInput!): Section!
-    deleteSection(id:ID!):Section!
+    createSection(sectionInput: SectionInput!): Section!
+    updateSection(id: ID!, sectionInput: SectionInput!): Section!
+    deleteSection(id: ID!): Section!
   }
 `;
