fcb15a2dbcd9a1dfe2050e693143d26c3057a2c8
Бат-Эрдэнэ Хадбаатар <23LP6564@G302-CC0115.local>
2024-05-09 21:39:57 +0800

    test(section-connect-to-lesson): not ready

--- apps/GLMS/glms-dashboard/src/app/section/_features/AddSection.tsx
+++ apps/GLMS/glms-dashboard/src/app/section/_features/AddSection.tsx
@@ -1,18 +1,18 @@
-"use client"
+'use client';
 import * as yup from 'yup';
-import {useCreateSectionMutation} from "../../../generated"
+import { useCreateSectionMutation, useUpdateLessonMutation } from '../../../generated';
 import { useFormik } from 'formik';
 import FileUploader from '../../../components/FileUploader';
 
 const validatinSchema = yup.object({
-    title: yup.string().required(),
-    description: yup.string().required(),
-    contentImage:yup.string()
-  });
+  title: yup.string().required(),
+  description: yup.string().required(),
+  contentImage: yup.string(),
+});
 
 const AddSection = () => {
-
-  const [createSection] = useCreateSectionMutation()
+  const [createSection] = useCreateSectionMutation();
+  const [updateLesson] = useUpdateLessonMutation();
   const formik = useFormik({
     initialValues: {
       title: '',
@@ -23,63 +23,79 @@ const AddSection = () => {
     onSubmit: (values) => {
       createSection({
         variables: {
-          sectionInput : {
-            title:values.title,
+          sectionInput: {
+            title: values.title,
             description: values.description,
-            contentImage:values.thumbnail
-          }
-        }
-      })
+            contentImage: values.thumbnail,
+          },
+        },
+      });
       formik.resetForm();
     },
   });
 
+  const handleClick = () => {
+    const id = localStorage.getItem('lessonID');
+    updateLesson({
+      variables: {
+        id: id || '',
+        lessonInput: {
+          sections: 
+        },
+      },
+    });
+  };
 
-    return(
-        <div  data-testid="add-section-form" className="flex flex-col gap-[4px] bg-[#fff] border-1 rounded-[4px] justify-center items-center p-6">
-        <div className="'flex flex-col  gap-4 border-2 border-dashed rounded-4 p-8 border-[#D6D8DB] rounded-[8px]">
-          <div className="flex flex-col py-2">
-            <p className="font-bold">Хэсгийн гарчиг</p>
-            <input
-             data-cy="title"
-             className="w-[588px] h-fit border rounded-[4px] p-2" 
-             type="text"
-             name="title"
-             placeholder="Оруулна уу..."
-             id="title-test"
-             onChange={formik.handleChange}
-             value={formik.values.title}
-             ></input>
-          </div>
-          <div className="flex flex-col py-2">
-            <p className="font-bold">Дэлгэрэнгүй</p>
-            <textarea
-              data-cy="description"
-              className="w-[588px] h-[160px] border rounded-[4px] p-2"
-              id="description-test"
-              name="description"
-              onChange={formik.handleChange}
-              value={formik.values.description}
-              placeholder="Энд бичнэ үү..."
-            ></textarea>
-          </div>
-          <div className="flex flex-col py-2">
-            <p className="font-bold"> Хэсгийн зураг</p>
-            <FileUploader thumbnail={formik.values.thumbnail} setFieldValue={formik.setFieldValue} />
-          </div>
+  return (
+    <div data-testid="add-section-form" className="flex flex-col gap-[4px] bg-[#fff] border-1 rounded-[4px] justify-center items-center p-6">
+      <div className="'flex flex-col  gap-4 border-2 border-dashed rounded-4 p-8 border-[#D6D8DB] rounded-[8px]">
+        <div className="flex flex-col py-2">
+          <p className="font-bold">Хэсгийн гарчиг</p>
+          <input
+            data-cy="title"
+            className="w-[588px] h-fit border rounded-[4px] p-2"
+            type="text"
+            name="title"
+            placeholder="Оруулна уу..."
+            id="title-test"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          ></input>
         </div>
-        <div className="flex gap-4 jutify-center items-center py-4">
-            <button data-cy="add-section-handle-btn" className="w-[36px] bg-black h-[36px] text-white rounded-[8px] flex items-center justify-center text-[26px] pb-2 hover:bg-[#D6D8DB] hover:text-black "
-             onClick={() => {
-               formik.handleSubmit();
-             }}
-             disabled={!formik.values.title || !formik.values.description || !formik.values.thumbnail }
-            >
-              +
-            </button>
-          </div>
+        <div className="flex flex-col py-2">
+          <p className="font-bold">Дэлгэрэнгүй</p>
+          <textarea
+            data-cy="description"
+            className="w-[588px] h-[160px] border rounded-[4px] p-2"
+            id="description-test"
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+            placeholder="Энд бичнэ үү..."
+          ></textarea>
+        </div>
+        <div className="flex flex-col py-2">
+          <p className="font-bold"> Хэсгийн зураг</p>
+          <FileUploader thumbnail={formik.values.thumbnail} setFieldValue={formik.setFieldValue} />
+        </div>
       </div>
-    )
-}
+      <div className="flex gap-4 jutify-center items-center py-4">
+        <button
+          data-cy="add-section-handle-btn"
+          className="w-[36px] bg-black h-[36px] text-white rounded-[8px] flex items-center justify-center text-[26px] pb-2 hover:bg-[#D6D8DB] hover:text-black "
+          onClick={() => {
+            formik.handleSubmit();
+          }}
+          disabled={!formik.values.title || !formik.values.description || !formik.values.thumbnail}
+        >
+          +
+        </button>
+      </div>
+      <button name="submitBtn" className={`bg-[#121316] rounded-lg w-[280px] h-[56px] text-white flex justify-center items-center btn`} data-testid="create-button" onClick={handleClick}>
+        <p className="text-[18px] font-semibold">Хадгалах</p>
+      </button>
+    </div>
+  );
+};
 
-export default AddSection
+export default AddSection;
