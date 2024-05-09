2a0feedbca06b5d55b9eb17594d190507c674987
unknown <otgoohghg@yahoo.com>
2024-05-09 15:30:05 +0800

    fix(cms-dashboard): resolve changed
    #873

--- apps/CMS/cms-dashboard/src/app/comments/_features/CommentsMain.tsx
+++ apps/CMS/cms-dashboard/src/app/comments/_features/CommentsMain.tsx
@@ -9,6 +9,12 @@ const perPage = 5;
 
 export const CommentsMain = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const handleNext = () => {
+     setCurrentPage(currentPage + 1)
+  };
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1)
+ };
   const { data } = useGetCommentsQuery({
     variables: {
       input: {
@@ -37,11 +43,11 @@ export const CommentsMain = () => {
       <div className='w-full justify-center items-center flex'>
         <div className='flex items-center justify-between w-[860px]'>
         <div className='w-1/2'>
-             <div><button className='btn btn-default bg-white items-center' onClick={() => setCurrentPage(currentPage - 1)} style={{ display: currentPage === 1 ? 'none' : 'block' }}><span className='flex items-center gap-2'><FaArrowLeft/>Prev</span></button></div>    
+             <div><button className='btn btn-default bg-white items-center' onClick={handlePrev} style={{ display: currentPage === 1 ? 'none' : 'block' }}><span className='flex items-center gap-2'><FaArrowLeft/>Prev</span></button></div>    
         </div>
         <div className='flex items-center justify-between w-1/2'>
          <p className=' rounded-lg -ml-[30px] bg-slate-200 text-[black] text-xl w-[40px] h-[40px] items-center justify-center flex'>{currentPage}</p>
-         <button className='btn btn-default bg-white items-center' onClick={() => setCurrentPage(currentPage + 1)}>Next<FaArrowRight/></button>
+         <button className='btn btn-default bg-white items-center' onClick={handleNext}>Next<FaArrowRight/></button>
         </div>
         </div>
       </div>
