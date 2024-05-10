90a83d1f4fe27debca6bec73c95a21365393d204
Nakii0603 <naakii0603@gmail.com>
2024-05-10 10:21:45 +0800

    feat(hrms): mui to tailwind

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/UpdateButton.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/UpdateButton.tsx
@@ -2,7 +2,7 @@ import { PenIcon } from '../../../../public/assets/PenIcon';
 
 export const UpdateButton = () => {
   return (
-    <button data-testid="update-button" className="bg-[#F6F6F6] px-2 py-[10px] rounded-[8px] flex gap-2 ">
+    <button data-testid="update-button" className="bg-[#F6F6F6] px-2 py-[10px] items-center rounded-[8px] flex gap-2 ">
       <PenIcon />
       <p className="text-main text-[14px] items-center">Засварлах</p>
     </button>
