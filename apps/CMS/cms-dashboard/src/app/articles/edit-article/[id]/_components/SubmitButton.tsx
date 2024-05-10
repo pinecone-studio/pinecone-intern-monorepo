f8d40d7cb3a5b4062f2fc759f0d3c7503dfda821
Arvinbuyan Otgonjargal <145421583+Arvinbuy7n@users.noreply.github.com>
2024-05-10 09:15:51 +0800

    feat(cms-dashboard): cms-dashboard (#927)

--- apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/SubmitButton.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/SubmitButton.tsx
@@ -8,15 +8,15 @@ type propstype = {
 
 export const SubmitButton = ({ text, bgColor, onClick }: propstype) => {
   return (
-    <div data-testid="contained-submit-buttons-id" className="flex flex-col gap-[20px]">
+    <div data-testid="contained-submit-buttons-id" className="flex flex-col">
       <button
         type="submit"
         onClick={onClick}
         data-testid="submit-button-id"
-        className="w-[339px] h-[56px] justify-center flex flex-col items-center self-stretch rounded-lg  border-0 transition-transform transform active:scale-95 hover:scale-100 duration-300 ease-in-out"
+        className="w-full h-[56px] justify-center flex flex-col items-center self-stretch rounded-lg  border-0 transition-transform transform active:scale-95 hover:scale-100 duration-300 ease-in-out"
         style={{ background: bgColor }}
       >
-        <span data-testid="content-draft-button-id" className="text-lg font-semibold non-italic leading-6" style={{ color: 'rgba(28, 32, 36, 0.24)' }}>
+        <span data-testid="content-draft-button-id" className="text-lg font-semibold non-italic leading-6 text-white">
           {text}
         </span>
       </button>
