4770f811029872479eb94260e035cad01553083a
Kh-BatErdene <hadabagi85@gmail.com>
2024-05-13 12:22:51 +0800

    fix(lesson-and-section-query): resolve test error

--- apps/GLMS/courses-service/src/graphql/resolvers/queries/lessons-get.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/queries/lessons-get.ts
@@ -2,7 +2,7 @@ import lessonModel from '@/model/lesson-model';
 import { GraphQLError } from 'graphql';
 export const getLessons = async () => {
   try {
-    return await lessonModel.find().populate('sections');
+    return await lessonModel.find();
   } catch (error) {
     throw new GraphQLError('cannot find lesson');
   }
