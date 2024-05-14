8ddb091c37ade0c87dfedbd554d65e48a1ec7795
Arvinbuyan Otgonjargal <145421583+Arvinbuy7n@users.noreply.github.com>
2024-05-14 13:22:20 +0800

    feat(cms-dashboard): cms-dashboard (#991)
    Co-authored-by: Arvinbuyan Otgonjargal <23LP6614@G302-CC0093.local>

--- apps/CMS/cms-dashboard/specs/articles/edit-article/ArrowBack.spec.tsx
+++ apps/CMS/cms-dashboard/specs/articles/edit-article/ArrowBack.spec.tsx
@@ -1,15 +1,17 @@
-import { render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import { ArrowBack } from '../../../src/app/articles/edit-article/[id]/_components/ArrowBack';
 
-describe('Arrow back component', () => {
-  it('1 -> should verify if arrow back component is displayed or not', () => {
-    const { container } = render(<ArrowBack />);
-    expect(container).toBeDefined();
-  });
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn().mockReturnValue({ push: jest.fn() }),
+}));
 
+describe('Arrow back component', () => {
   it('2 -> should check whether button exists or not', () => {
     const { getByTestId } = render(<ArrowBack />);
     const arrowButton = getByTestId('arrow-back-button-id');
-    expect(arrowButton).toBeDefined();
+
+    act(() => {
+      fireEvent.click(arrowButton);
+    });
   });
 });
--- apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/ArrowBack.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/edit-article/[id]/_components/ArrowBack.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import { ArrowBackIcon } from '@/icons';
-import "../_styles/editArticle.css";
+import '../_styles/editArticle.css';
+import { useRouter } from 'next/navigation';
 
 export const ArrowBack = () => {
+  const router = useRouter();
+
   return (
-    <button data-testid="arrow-back-button-id" className="hoverRotate">
+    <button
+      data-testid="arrow-back-button-id"
+      className="hoverRotate"
+      onClick={() => {
+        router.push('/dashboard');
+      }}
+    >
       <ArrowBackIcon />
-      
     </button>
   );
 };
