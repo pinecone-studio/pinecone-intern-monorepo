fef520caa081087761bcf14ca830f4bc3497a5ba
Bymbaabatdelger <baymba.0108@icloud.com>
2024-05-14 15:45:06 +0800

    test(glms-dashboard): improved update course and section's test

--- apps/GLMS/glms-dashboard/cypress/e2e/update-course/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/update-course/index.cy.ts
@@ -1,6 +1,8 @@
 describe ('Handle update-course page' , () => {
     beforeEach(() => cy.visit('/update-course'))
 
+    localStorage.setItem('courseID', '123');
+
     it('1. Update section page' , () => {
       cy.get('[data-testid="update-course-container"]').should('exist')
     })
@@ -12,9 +14,17 @@ describe ('Handle update-course page' , () => {
 
     it('3.Should display update section container', () => {
       cy.get('[data-testid="update-course-form"]').should('exist');
-      cy.get('[data-cy="title"]').should('exist').type('Html')
-      cy.get('[data-cy="description"]').should('exist').type('Html intro')
+      cy.get('[data-cy="title"]').should('exist')
+      cy.get('[data-cy="description"]').should('exist')
       cy.get('#file-test').selectFile('public/js.png', { force: true });
       cy.get('[data-cy="update-button"]').should('exist')
+    });
+    it('4.Should display the process of update section', () => {
+      cy.get('[data-testid="update-course-form"]').should('exist');
+      cy.get('[data-cy="title"]').type('Html')
+      cy.get('[data-cy="description"]').type('Html intro')
+      cy.get('#file-test').selectFile('public/js.png', { force: true });
+      cy.get('[data-cy="update-button"]').click()
+      cy.url().should('include', '/123');
     });
 })
--- apps/GLMS/glms-dashboard/cypress/e2e/update-section/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/update-section/index.cy.ts
@@ -6,8 +6,8 @@ describe ('Handle update-section page' , () => {
     })
 
     it('2. check back to dashboard page button click ', () => {
-      cy.get('[data-cy="handle-back-page"]').should('exist');
-      cy.get('[data-cy="handle-back-page"]').click();
+      cy.get('[data-cy="handle-back-page"]').should('exist').click();
+      cy.url().should('include', '/section');
     });
 
     it('3.Should display update section container', () => {
@@ -15,6 +15,14 @@ describe ('Handle update-section page' , () => {
       cy.get('[data-cy="title"]').should('exist')
       cy.get('[data-cy="description"]').should('exist')
       cy.get('#file-test').selectFile('public/js.png', { force: true });
-      cy.get('[data-cy="update-section-handle-btn"]').click();
+      cy.get('[data-cy="update-section-handle-btn"]').should('exist')
+    });
+    it('4.Should display the process of update section', () => {
+      cy.get('[data-testid="update-section-form"]').should('exist');
+      cy.get('[data-cy="title"]').type('HTML basic')
+      cy.get('[data-cy="description"]').type( 'HTML basic description')
+      cy.get('#file-test').selectFile('public/js.png', { force: true });
+      cy.get('[data-cy="update-section-handle-btn"]').click()
+      cy.url().should('include', '/section');
     });
 })
