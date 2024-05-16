bbe5ba08ddc429b5a8f1f1fe3c597d3cb250a5a4
unknown <otgoohghg@yahoo.com>
2024-05-16 16:29:46 +0800

    fix(comments-service): bugfix
    #1035

--- apps/CMS/comments-service/specs/queries/comment/get-comments.spec.ts
+++ apps/CMS/comments-service/specs/queries/comment/get-comments.spec.ts
@@ -7,13 +7,11 @@ jest.mock('@/models/comment.model', () => ({
     find: jest.fn(),
   },
 }));
-
 describe('This query should return comments', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
- 
-  it('should return comments if found', async () => {
+  it('1.should return comments if found', async () => {
     (CommentsModel.find as jest.Mock).mockReturnValueOnce({
       limit: jest.fn().mockReturnThis(),
       skip: jest.fn().mockReturnThis(),
@@ -27,7 +25,6 @@ describe('This query should return comments', () => {
         articleId: 'asdf' 
       }]),
     });
-
     const input = { limit: 10, offset: 0, status: [] };
     const comments = await getComments!({}, { input }, {}, {} as GraphQLResolveInfo);
     expect(comments).toEqual([{ 
@@ -40,15 +37,17 @@ describe('This query should return comments', () => {
       articleId: 'asdf' 
     }]);
   });
-
-  it('should return GraphQLError if comments not found', async () => {
-    (CommentsModel.find as jest.Mock).mockReturnValueOnce({
-      limit: jest.fn().mockReturnThis(),
-      skip: jest.fn().mockReturnThis(),
-      exec: jest.fn().mockResolvedValueOnce([]), 
-    });
-
-    const input = { limit: 10, offset: 0, status: [] };
-    await expect(getComments!({}, { input }, {}, {} as GraphQLResolveInfo)).rejects.toThrow(GraphQLError);
+  it('2.should return GraphQLError if comments not found', async () => {
+    try {
+      (CommentsModel.find as jest.Mock).mockReturnValueOnce({
+        limit: jest.fn().mockReturnThis(),
+        skip: jest.fn().mockReturnThis(),
+        exec: jest.fn().mockResolvedValueOnce([]), 
+      });
+      const input = { limit: 10, offset: 0, status: [] };
+      await getComments!({}, { input}, {}, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect(error).toEqual(new GraphQLError('Error in get comments query'));
+    }
   });
 });
