9d54fe7de2d275fbc04f23ab49649567f85a9087
Bymbaabatdelger <baymba.0108@icloud.com>
2024-05-16 15:30:04 +0800

    feat(glms-dashboard): added signup form

--- apps/GLMS/glms-dashboard/src/app/layout.tsx
+++ apps/GLMS/glms-dashboard/src/app/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 import './global.css';
 import { FederationProvider } from '../common';
+import {AuthProvider} from '../common/providers/AuthProvider'
 import Header from '../components/Header';
 import { PineconeLogo } from '../../public/assets/PineconeLogo';
 import { Challengeprovider } from './challenge-dashboard/context/Challenge';
+import { ToastContainer } from 'react-toastify';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,6 +13,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         <Challengeprovider>
           <FederationProvider>
+            <AuthProvider>
+              <ToastContainer />
+            </AuthProvider>
             <Header />
             {children}
             <div className="py-2 pb-6 bg-[#F7F7F8] flex justify-center">
