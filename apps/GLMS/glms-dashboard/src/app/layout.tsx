65a1c59fd99f8710aede63609711a7839a2f02ed
Zoya <zoya@Zoyas-MacBook-Air.local>
2024-05-20 12:16:43 +0800

    feat: lint error

--- apps/GLMS/glms-dashboard/src/app/layout.tsx
+++ apps/GLMS/glms-dashboard/src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 import './global.css';
 import { FederationProvider } from '../common';
-import {AuthProvider} from '../common/providers/AuthProvider'
 import Header from '../components/Header';
 import { PineconeLogo } from '../../public/assets/PineconeLogo';
 import { ToastContainer } from 'react-toastify';
