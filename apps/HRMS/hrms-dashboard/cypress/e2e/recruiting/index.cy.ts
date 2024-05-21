88c63d3c346dd9f67fa13d1f1ebeca2493d451d6
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-05-22 05:35:24 +0800

    feat(jobslist table, tabs, saijrullaa): jobslist table, tabs, saijrullaa

--- apps/HRMS/hrms-dashboard/cypress/e2e/recruiting/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/recruiting/index.cy.ts
@@ -8,26 +8,6 @@ describe('Recruiting Page', () => {
     cy.get('[data-testid="jobAdd-button"]').click();
   });
 
-  it('displays the ad tab', () => {
-    cy.contains('Зар').should('exist');
-    cy.get('[data-testid="adtab"]').should('be.visible');
-  });
-
-  it('displays the offer tab', () => {
-    cy.contains('Ирсэн өргөдөл').should('exist');
-    cy.get('[data-testid="offertab"]').should('be.visible');
-  });
-
-  it('displays the date filter', () => {
-    cy.contains('Огноо').should('exist');
-    cy.get('[data-testid="date-filter"]').should('be.visible');
-  });
-
-  it('displays the status filter', () => {
-    cy.contains('Төлөв').should('exist');
-    cy.get('[data-testid="status-filter"]').should('be.visible');
-  });
-
   it('navigates to add job page on button click', () => {
     cy.get('[data-testid="jobAdd-button"]').click();
     cy.url().should('include', '/recruiting/add-job');
