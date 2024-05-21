c6fc04ef5c449bb9abfeba2225a3cd4f4c71597e
ReiGnZ <macbook@macbooks-MacBook-Pro-2.local>
2024-05-21 14:16:34 +0800

    perf: timer next button deleted

--- apps/GLMS/glms-dashboard/src/app/challenge/[id]/Timer.tsx
+++ apps/GLMS/glms-dashboard/src/app/challenge/[id]/Timer.tsx
@@ -4,10 +4,6 @@ import { useEffect, useState } from 'react';
 export const Timer = () => {
 
     const [time, setTime] = useState(30);
-
-    const nextClick = () => {
-        setTime(30);
-    };
     useEffect(() => {
         const timer = setInterval(() => {
             setTime((time) => {
@@ -24,7 +20,7 @@ export const Timer = () => {
                 <div className='w-16 h-16 border-4 border-yellow-200 rounded-full flex items-center justify-center'>
                     {time}
                 </div>
-                <button onClick={nextClick} className='ml-16 w-[100px] h-[50px] text-white bg-[#0000FF]'>Next</button>
+
             </div>
         </main>
     );
