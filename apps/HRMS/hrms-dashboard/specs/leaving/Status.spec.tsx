a6effeb0751eac90bc4ca8178617d188294ea0fc
Munkh-Erdene Beleg-Urnukh <23LP1379@G305-employeeiMac.local>
2024-05-21 14:36:57 +0800

    fix(hrms-dashboard): fixed detail page

--- apps/HRMS/hrms-dashboard/specs/leaving/Status.spec.tsx
+++ apps/HRMS/hrms-dashboard/specs/leaving/Status.spec.tsx
@@ -24,7 +24,7 @@ describe('Status component', () => {
     expect(typographyElement).toBeTruthy();
   });
   it('renders correctly with unknown status', () => {
-    const data = { status: 'unknown' };
+    const data = { status: '' };
     const { getByText } = render(<Status dat={data} />);
     expect(getByText).toBeTruthy();
   });
