7363227d538d2bd060107fb2a59ac06679de914f
rimzss <bobbebe40@gmail.com>
2024-05-22 15:27:46 +0800

    feat(challenge-result): display quiz display

--- apps/GLMS/glms-dashboard/cypress/e2e/challenge/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/challenge/index.cy.ts
@@ -4,7 +4,19 @@ describe('Challenge page', () => {
   it('1. Should display welcome message', () => {
     cy.get('h1').contains('Welcome to Challenge page');
   });
-  it('2. Should challenge title', () => {
-    cy.get('[data-cy="Challenge-Result-Title"]').should('be.visible');
+});
+
+describe('Challenge quiz page', () => {
+  beforeEach(() => cy.visit('/challenge/6639b508ad9ecc26d93ecafd'));
+  it('1. Should display ProgressBar', () => {
+    cy.get('[data-testid="progress-bar"]').should('exist');
+  });
+});
+
+describe('Challenge result page', () => {
+  beforeEach(() => cy.visit('/challenge/result/664d99c9fb7b77a2937a2a30'));
+
+  it('1. Should display "Quiz harah"', () => {
+    cy.get('h1').contains('Quiz алдаа харах');
   });
 });
--- apps/GLMS/glms-dashboard/src/app/challenge/_feature/ChallengeResults.tsx
+++ apps/GLMS/glms-dashboard/src/app/challenge/_feature/ChallengeResults.tsx
@@ -1,42 +1,44 @@
-'use client';
-import { useGetChallengesByStatusQuery } from '@/generated';
+import { Quiz } from '@/generated';
 import { CheckTrueIcon, CheckFalseIcon } from '../../../assets';
 
-export const ChallengeResults = () => {
-  const { data, loading } = useGetChallengesByStatusQuery();
-
+export const ChallengeResults = ({ challenge }: { challenge: Quiz[] }) => {
+  const studentChoices = JSON.parse(localStorage.getItem('studentChoices')!);
+  const checkIsCorrect = (studentChoice: string, choice: string | undefined | null, isCorrect: boolean | undefined | null) => {
+    if (studentChoice === choice && !isCorrect) {
+      return 'border-red-500';
+    }
+    if (isCorrect) {
+      return 'border-green-500';
+    }
+  };
+  const checkIsCorrectIcon = (studentChoice: string, choice: string | undefined | null, isCorrect: boolean | undefined | null) => {
+    if (studentChoice === choice && !isCorrect) {
+      return <CheckFalseIcon />;
+    }
+    if (isCorrect) {
+      return <CheckTrueIcon />;
+    }
+  };
   return (
-    <div className="w-full">
-      <h1 className="h-40 text-3xl flex items-center ml-[120px]">Quiz алдаа харах</h1>
+    <div className="w-full text-black bg-white">
       <div className="flex flex-col items-center">
-        {!loading &&
-          data?.getChallengesByStatus?.map((challenge) => {
-            return (
-              <main key={challenge?._id} data-cy="Challenge-Result-Title">
-                <div className="flex gap-2">
-                  <p>Challenge title:</p>
-                  <p>{challenge?.title}</p>
+        {challenge?.map((quiz, index) => (
+          <main key={quiz?._id} className="w-[700px] my-16 ">
+            <div className="flex justify-center text-center">
+              <p className="text-3xl font-bold mb-8">
+                {index + 1}. {quiz?.question}
+              </p>
+            </div>
+            {quiz?.choices?.map((choice) => (
+              <main key={choice?._id} className={`flex justify-between  mb-8 rounded-lg border-2 border-b-4 ${checkIsCorrect(studentChoices[index].choiceId, choice?._id, choice?.isCorrect)}  `}>
+                <div className="p-3 ">
+                  <label>{choice?.choice}</label>
                 </div>
-                {challenge?.quiz?.map((quiz, index) => (
-                  <main key={quiz?._id} className="w-[700px] my-16 ">
-                    <div className="flex justify-center text-center">
-                      <p className="text-3xl font-bold mb-8">
-                        {index + 1}. {quiz?.question}
-                      </p>
-                    </div>
-                    {quiz?.choices?.map((choice, choiceIndex) => (
-                      <main key={choice?._id} className={`flex justify-between  mb-8 rounded-lg border-2 border-b-4 ${choice?.isCorrect ? 'border-green-500 ' : 'border-red-500 '}`}>
-                        <div className="p-3 ">
-                          <label htmlFor={`choice-${index}-${choiceIndex}`}>{choice?.choice}</label>
-                        </div>
-                        <div className="p-3 flex justify-center items-center">{choice?.isCorrect ? <CheckTrueIcon /> : <CheckFalseIcon />}</div>
-                      </main>
-                    ))}
-                  </main>
-                ))}
+                <div className="p-3 flex justify-center items-center">{checkIsCorrectIcon(studentChoices[index].choiceId, choice?._id, choice?.isCorrect)}</div>
               </main>
-            );
-          })}
+            ))}
+          </main>
+        ))}
       </div>
     </div>
   );
