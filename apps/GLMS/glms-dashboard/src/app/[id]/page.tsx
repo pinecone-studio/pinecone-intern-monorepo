ecaf07f905d215c295bc32575df2fcf37b256597
Kh-BatErdene <hadabagi85@gmail.com>
2024-05-24 20:05:45 +0800

    fix: lint error fixed

--- apps/GLMS/glms-dashboard/src/app/[id]/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/[id]/page.tsx
@@ -4,10 +4,8 @@ import CourseRender from './_feature/CourseRender';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Loading from '../../components/Loading';
-import { useRouter } from 'next/navigation';
 const Home = () => {
   const pathname = usePathname().substring(1);
-  const router = useRouter();
   const [newCourse, setNewCourse] = useState<Course>();
   const [newLesson, setNewLesson] = useState<Lesson[]>();
   const { data: lessonData, loading: lessonLoading, error: lessonError, refetch: lessonRefetch } = useGetLessonByIdQuery({ variables: { getLessonByIdId: pathname } });
