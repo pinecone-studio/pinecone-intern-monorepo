cda2631f95e42fd9659760ed54d8e4fe18fbd4a7
Zolzaya Udval <23LP6080@G302-CC0097.local>
2024-05-24 18:12:43 +0800

    perf(articles-service): get articles by role

--- apps/CMS/cms-dashboard/cypress/e2e/app/dashboard/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/app/dashboard/index.cy.ts
@@ -1,44 +1,70 @@
 describe('dashboard page', () => {
-  beforeEach(() => cy.visit('/dashboard'));
+  it('sign in by user', () => {
+    cy.visit('/sign-in');
+    cy.get('input[name="emailOrPhoneNumber"]').type('admin@gmail.com');
+    cy.get('input[name="password"]').type('Admin1234!');
+    cy.get('[data-cy="Sign-In-Button"]').should('exist').click();
+    cy.visit('/dashboard');
 
-  // ArticleStatusTabs feature
-  it('1. ArticleStatusTabs must be defined', () => {
+    // ArticleStatusTabs feature
     cy.get('[data-cy="article-status-tabs-feature-cy-id"]').should('exist');
-  });
 
-  // SearchInput component
-  it('2. SearchInput component must be defined', () => {
+    // SearchInput component
     cy.get('[data-cy="search-input-cy-id"]').should('exist');
-  });
 
-  // AdminNavigateLinksFeature
-  it('3. AdminNavigateLinksFeature must be defined', () => {
+    // AdminNavigateLinksFeature
     cy.get('[data-cy="admin-navigate-links-feature-cy-id"]').should('exist');
-  });
 
-  it('4. Dashboard table must be defined', () => {
-    cy.get('[data-cy="dashboard-table-cy"]').should('exist').should('be.visible');
-  });
-
-  it('5. Filter by date must be defined', () => {
-    cy.get('[data-cy="filter-by-date-cy-id"]').should('exist').should('be.visible');
-  });
-
-  it('6. Navbar must be defined', () => {
-    cy.get('[data-cy="navbar-cy-id"]').should('exist').should('be.visible');
-  });
-
-  it('7. Dashboard table', () => {
+    // Dashboard table must be defined
     cy.get('[data-cy="dashboard-table-cy-id"]').should('exist').should('be.visible');
-  });
 
-  it('8. Morevert button click', () => {
+    // Filter by date must be defined
+    cy.get('[data-cy="filter-by-date-cy-id"]').should('exist').should('be.visible');
+    // Navbar must be defined
+    cy.get('[data-cy="navbar-cy-id"]').should('exist').should('be.visible');
+
+    // Dashboard table
+    cy.get('[data-cy="dashboard-table-cy-id"]').should('exist').should('be.visible');
+
+    // Morevert button click
     cy.get('[data-cy="morevert-button-test-cy"]').eq(0).should('exist').click();
     cy.get('[data-cy="drop-down-menu-test-cy"]').eq(0).should('exist').click({ force: true });
-  });
 
-  // Pagination component
-  it('9. Pagination component must be defined', () => {
+    // Pagination component
+    cy.get('[data-cy="pagination-cy-id"]').eq(0).should('exist').click();
+  });
+  it('sign in by admin', () => {
+    cy.visit('/sign-in');
+    cy.get('input[name="emailOrPhoneNumber"]').type('testadmin@gmail.com');
+    cy.get('input[name="password"]').type('@Pi72365');
+    cy.get('[data-cy="Sign-In-Button"]').should('exist').click();
+    cy.visit('/dashboard');
+
+    // ArticleStatusTabs feature
+    cy.get('[data-cy="article-status-tabs-feature-cy-id"]').should('exist');
+
+    // SearchInput component
+    cy.get('[data-cy="search-input-cy-id"]').should('exist');
+
+    // AdminNavigateLinksFeature
+    cy.get('[data-cy="admin-navigate-links-feature-cy-id"]').should('exist');
+
+    // Dashboard table must be defined
+    cy.get('[data-cy="dashboard-table-cy-id"]').should('exist').should('be.visible');
+
+    // Filter by date must be defined
+    cy.get('[data-cy="filter-by-date-cy-id"]').should('exist').should('be.visible');
+    // Navbar must be defined
+    cy.get('[data-cy="navbar-cy-id"]').should('exist').should('be.visible');
+
+    // Dashboard table
+    cy.get('[data-cy="dashboard-table-cy-id"]').should('exist').should('be.visible');
+
+    // Morevert button click
+    cy.get('[data-cy="morevert-button-test-cy"]').eq(0).should('exist').click();
+    cy.get('[data-cy="drop-down-menu-test-cy"]').eq(0).should('exist').click({ force: true });
+
+    // Pagination component
     cy.get('[data-cy="pagination-cy-id"]').eq(0).should('exist').click();
   });
 });
