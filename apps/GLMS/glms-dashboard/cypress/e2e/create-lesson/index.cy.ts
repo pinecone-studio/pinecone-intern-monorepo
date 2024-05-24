5b8c31d142134a0000ed78d9f233cc06c0866596
Bymbaabatdelger <baymba.0108@icloud.com>
2024-05-24 13:45:54 +0800

    test(glms-dashboard): test saijruulalt

--- apps/GLMS/glms-dashboard/cypress/e2e/create-lesson/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/create-lesson/index.cy.ts
@@ -2,26 +2,33 @@ describe('LessonAdd Component', () => {
   beforeEach(() => {
     cy.visit('/create-lesson'); 
   })
+
   it('should render the LessonAdd form', () => {
     cy.get('[data-testid="create-lesson-container"]').should('be.visible');
   });
+
   it('should navigate back to the dashboard when the back div is clicked', () => {
     cy.get('[data-testid="test-back-div"]').click();
     cy.url().should('include', '/dashboard');
   });
+
   it('should fill out the form and submit', () => {
     cy.window().then((win) => {
       win.localStorage.setItem('courseID', '6633305c94d4584898bb049a');
     });
-    cy.get('input[name="title"]').type('Test Lesson Title');
+
+    // Increase the timeout for this specific command
+    cy.get('input[name="title"]').type('Test Lesson Title', { timeout: 10000 });
     cy.get('#file-test').selectFile('public/js.png', { force: true }); 
     cy.get('[data-testid="create-button-of-lesson"]').click();
     cy.url().should('include', '/6633305c94d4584898bb049a');
   });
+
   it('should disable submit button if form is not valid', () => {
     cy.window().then((win) => {
       win.localStorage.setItem('courseID', '6633305c94d4584898bb049a');
     });
+
     cy.get('[data-testid="create-button-of-lesson"]').should('be.disabled');
     cy.get('input[name="title"]').type('Test Lesson Title');
     cy.get('[data-testid="create-button-of-lesson"]').should('be.disabled');
@@ -29,6 +36,6 @@ describe('LessonAdd Component', () => {
     cy.get('[data-testid="create-button-of-lesson"]').should('not.be.disabled');
     cy.get('[data-testid="create-button-of-lesson"]').click();
     cy.url().should('include', '/6633305c94d4584898bb049a');
-  })
-
+  });
 });
+Cypress.config('defaultCommandTimeout', 10000);
