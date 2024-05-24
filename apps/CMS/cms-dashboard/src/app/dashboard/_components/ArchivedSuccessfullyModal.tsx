3387c14c23803f4a04d2f4087357613ef916a27f
Bayra1 <isvhbaatar5@gmail.com>
2024-05-24 13:41:06 +0800

    fix(cms-dashboard): fixed some style problems for overlay

--- apps/CMS/cms-dashboard/src/app/dashboard/_components/ArchivedSuccessfullyModal.tsx
+++ apps/CMS/cms-dashboard/src/app/dashboard/_components/ArchivedSuccessfullyModal.tsx
@@ -1,12 +1,15 @@
 import { SuccessIcon } from '../../../assets/icons/SuccessIcon';
+import "../_styles/overlayDesign.css"
 
 export const ArchivedSuccessfullyModal = ({ title }: { title: string }) => {
   return (
-    <div id="mydiv" className="absolute w-full h-full top-0 left-0 flex items-center pl-4 bg-[#EAF6ED] gap-2">
-      <SuccessIcon />
+    <div id="mydiv" className="w-full h-full flex items-center gap-2">
+      <div className="shine-round">
+        <SuccessIcon />
+      </div>
       <div>
         <h3 className="font-semibold text-textPrimary">Archived Successfully</h3>
-        <p>{title}</p>
+        <p className='text-container'>{title}</p>
       </div>
     </div>
   );
