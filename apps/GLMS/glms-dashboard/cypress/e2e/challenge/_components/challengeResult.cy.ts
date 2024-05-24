9be20ef990e91f9d551b4322db626a251ef9ae17
Bilguun <bilguunzx011030@gmail.com>
2024-05-24 13:38:04 +0800

    fix: test fix

--- apps/GLMS/glms-dashboard/cypress/e2e/challenge/_components/challengeResult.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/challenge/_components/challengeResult.cy.ts
@@ -1,5 +1,57 @@
 describe('Challenge result page', () => {
-  beforeEach(() => cy.visit('/challenge/result/664d99c9fb7b77a2937a2a30'));
+  const localStorageKey = 'studentChoices';
+  const localStorageValue = JSON.stringify([
+    {
+      quizId: '664d99c9fb7b77a2937a2a0b',
+      choiceId: '664d99c9fb7b77a2937a2a0d',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a0b',
+      choiceId: '664d99c9fb7b77a2937a2a11',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a13',
+      choiceId: '664d99c9fb7b77a2937a2a16',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a19',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a1e',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a21',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a24',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a27',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a2a',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a2d',
+    },
+    {
+      quizId: '664d99c9fb7b77a2937a2a18',
+      choiceId: '664d99c9fb7b77a2937a2a2d',
+    },
+  ]);
+  beforeEach(() => {
+    cy.visit('/challenge/result/664d99c9fb7b77a2937a2a30');
+    cy.window().then((window) => {
+      window.localStorage.setItem(localStorageKey, localStorageValue);
+    });
+  });
 
   it('1. Should display', () => {
     cy.get('[data-testid="challenge-result"]').should('exist');
