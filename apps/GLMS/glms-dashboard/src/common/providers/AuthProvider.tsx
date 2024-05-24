dc8e08b203833f5228615a3a68948b398233f39a
Kh-BatErdene <hadabagi85@gmail.com>
2024-05-24 16:14:01 +0800

    fix: gg

--- apps/GLMS/glms-dashboard/src/common/providers/AuthProvider.tsx
+++ apps/GLMS/glms-dashboard/src/common/providers/AuthProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 'use client';
 import { PropsWithChildren, useContext, createContext } from 'react';
 import { useLessonSignInMutation, useLessonSignUpMutation } from '@/generated';
