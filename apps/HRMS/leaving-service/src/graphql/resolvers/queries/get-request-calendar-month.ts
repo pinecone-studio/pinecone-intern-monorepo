157932d86670875db65ee700d9c7042560b6ee88
Anujin Purew <23LP0977@setups-iMac-2.local>
2024-05-24 13:47:03 +0800

    docs: leaveReqeust query change

--- apps/HRMS/leaving-service/src/graphql/resolvers/queries/get-request-calendar-month.ts
+++ apps/HRMS/leaving-service/src/graphql/resolvers/queries/get-request-calendar-month.ts
@@ -1,30 +1,24 @@
-import dayjs from "dayjs"
+import { startOfMonth, addDays } from 'date-fns';
 import { QueryResolvers } from "@/graphql/generated";
 import { LeaveRequestModel } from "@/graphql/model";
 import { errorTypes, graphqlErrorHandler } from "../error";
 
-export const getRequestByCalendarMonth: QueryResolvers['getRequestByCalendarMonth'] = async (_, { startDate, department}) => {
-    try{
-        const year = dayjs(startDate).year();
-        const month = dayjs(startDate).month();
-
-        const firstDayOfInputMonth = dayjs(new Date(year, month, 1));
-        const weekDayOfInputMonth = firstDayOfInputMonth.day();
-
-        const startDateOfMonth= 1 - weekDayOfInputMonth
-        const leaveRequestsStartFrom = dayjs(new Date(year, month,)).add(startDateOfMonth, "day")
-
-        const leaveRequestEndTo = dayjs(leaveRequestsStartFrom).add(35, "day")
+export const getRequestByCalendarMonth: QueryResolvers['getRequestByCalendarMonth'] = async (_, { startDate, department }) => {
+    try {
+        const firstDayOfMonth = startOfMonth(startDate);
+    
+        const leaveRequestsStartFrom = addDays(firstDayOfMonth, -firstDayOfMonth.getDay());
+        const leaveRequestEndTo = addDays(leaveRequestsStartFrom, 35);
     
         const getMonth = await LeaveRequestModel.find({
             startDate: {
                 $gte: leaveRequestsStartFrom,
                 $lte: leaveRequestEndTo
             },
-            department:{$in: [department]}
+            department: { $in: [department] }
         });
         return getMonth;
-    }catch(error){
-        throw graphqlErrorHandler({message: "Something went wrong"}, errorTypes.BAD_REQUEST)
+    } catch (error) {
+        throw graphqlErrorHandler({ message: "Something went wrong" }, errorTypes.BAD_REQUEST)
     }
 }
