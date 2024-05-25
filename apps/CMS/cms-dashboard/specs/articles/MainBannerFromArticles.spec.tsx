fbd0e4a6ddd10b19f3ac33a69c7e54062f1e5822
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-05-25 18:55:41 +0800

    feat(cms-dashboard): enhancements

--- apps/CMS/cms-dashboard/specs/articles/MainBannerFromArticles.spec.tsx
+++ apps/CMS/cms-dashboard/specs/articles/MainBannerFromArticles.spec.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { fireEvent, render, act } from '@testing-library/react';
 import MainBannerFromArticles from '../../src/app/articles/_components/MainBannerFromArticles';
+import { useRouter } from 'next/navigation';
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn().mockReturnValue({ push: jest.fn() }),
 }));
-describe('MainBannerFromArticles', () =>
+describe('MainBannerFromArticles', () => {
+  let getItemMock: jest.SpyInstance;
+  let routerPushMock: jest.Mock;
+  let setIsShownMock: jest.Mock;
+
+  beforeEach(() => {
+    getItemMock = jest.spyOn(Storage.prototype, 'getItem');
+    routerPushMock = jest.fn();
+    setIsShownMock = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      push: routerPushMock,
+    });
+
+    // Mock useState to return a mock function for setIsShown
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('should have correct props', () => {
     const { getByTestId } = render(<MainBannerFromArticles date="2024.04.12" articlesTitle="Welcome" categories="Coding" cover="/Academy.svg" id="663097b58073930529faddfc" />);
     const button = getByTestId('mainBtn');
@@ -16,8 +35,33 @@ describe('MainBannerFromArticles', () =>
     act(() => {
       fireEvent.click(imgButton);
     });
-    const jumper = getByTestId('jumper');
+    const jumperBtn = getByTestId('jumper');
     act(() => {
-      fireEvent.click(jumper);
+      fireEvent.click(jumperBtn);
     });
-  }));
+  });
+  it('should redirect to /sign-in if no token is found', () => {
+    getItemMock.mockReturnValue(null);
+    const { getByTestId } = render(<MainBannerFromArticles date="2024.04.12" articlesTitle="Welcome" categories="Coding" cover="/Academy.svg" id="663097b58073930529faddfc" />);
+    const btn = getByTestId('jumper');
+
+    render(<MainBannerFromArticles />);
+
+    fireEvent.click(btn);
+
+    expect(getItemMock).toHaveBeenCalledWith('token');
+    expect(routerPushMock).toHaveBeenCalledWith('/sign-in');
+    expect(setIsShownMock).not.toHaveBeenCalled();
+  });
+  it('should call setIsShown(true) if token is found', () => {
+    getItemMock.mockReturnValue('mockToken');
+
+    const { getByTestId } = render(<MainBannerFromArticles />);
+
+    fireEvent.click(getByTestId('jumper'));
+
+    expect(getItemMock).toHaveBeenCalledWith('token');
+    expect(routerPushMock).not.toHaveBeenCalled();
+    expect(setIsShownMock).toHaveBeenCalledTimes(0);
+  });
+});
