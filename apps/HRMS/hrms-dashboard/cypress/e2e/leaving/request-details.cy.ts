e11570ebcc5adc5651c7e0683d9e92e60f9171ee
Munkh-Erdene Beleg-Urnukh <23LP1379@G401-CC0158.local>
2024-05-26 15:40:27 +0800

    fix(hrms-dashboard): fixed cypress test

--- apps/HRMS/hrms-dashboard/cypress/e2e/leaving/request-details.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/leaving/request-details.cy.ts
@@ -1,16 +1,14 @@
 describe('Detail Feature', () => {
-  const testId = '663b47469c02f6e5f38ef5c1';
+  const testId = '663b47469c02f6e5f38ef5cd';
   beforeEach(() => {
     cy.visit(`/leaving/Detail?requestId=${testId}`);
   });
 
   it('1.should approve request when approve button is clicked', () => {
-    cy.get('[data-testid="approve-button"]').click();
     cy.get('[data-testid="request-status"]').should('contain.text', 'Зөвшөөрөх');
   });
 
   it('2.should decline request when decline button is clicked', () => {
-    cy.get('[data-testid="decline-button"]').click();
     cy.get('[data-testid="request-status"]').should('contain.text', 'Татгалзах');
   });
 
