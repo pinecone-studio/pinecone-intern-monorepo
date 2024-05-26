00c8a16c54b974590cc63d7f8cfbc76284f22b9c
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-05-26 14:55:08 +0800

    feat(cms-dashboard): test

--- apps/CMS/cms-dashboard/src/app/articles/_components/Modal.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_components/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { MouseEvent } from 'react';
-import ModalLogo from './create-article/ModalLogo';
+import { IoClose } from 'react-icons/io5';
 
 type CreateArticleModalType = {
   isVisible: boolean;
@@ -19,14 +19,14 @@ const Modal = ({ isVisible, onClose }: CreateArticleModalType) => {
   };
   return (
     <div data-testid="modalCloseBtn" id="wrapper" className=" fixed inset-2 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-11" onClick={handleCLose}>
-      <div data-testid="modal" className=" w-[528px] flex flex-col bg-white relative rounded-2xl gap-8 z-12">
-        <button className=" py-3 text-black text-xl absolute top-[13px] right-6 font-semibold " onClick={onClose}>
-          X
+      <div data-testid="modal" className=" w-[528px] flex flex-col bg-white relative rounded-2xl gap-6 z-12">
+        <button className="py-3 text-black text-xl absolute top-[13px] right-6 font-semibold " onClick={onClose}>
+          <IoClose style={{ width: 28, height: 28 }} />
         </button>
-        <div className=" mt-12 flex justify-center items-center">
-          <ModalLogo />
-        </div>
-        <p className=" text-center text-2xl font-bold tracking-tight mt-12 text-[#1c2024]">Системээс гарахдаа итгэлтэй байна уу?</p>
+        <p className=" text-center text-2xl font-bold tracking-tight mt-12 text-[#1c2024]">
+          Системээс гарахдаа итгэлтэй
+          <br /> байна уу?
+        </p>
         <div className=" flex gap-9 justify-end p-6 border-t">
           <button type="submit" className={`text-[18px] px-[20px] py-[12px] rounded-[8px] items-center bg-[#1C20240A] text-[#1c2024]`} onClick={onClose}>
             Үгүй
