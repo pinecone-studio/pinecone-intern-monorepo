f19b6c190b0f06ea0169b6dedd3345d369bbc208
Elbeg Amar <elbeg@nestsolutions.llc>
2024-05-27 14:30:45 +0800

    fix(courses-service): fixed create mutation

--- apps/GLMS/courses-service/src/graphql/schemas/course.schema.ts
+++ apps/GLMS/courses-service/src/graphql/schemas/course.schema.ts
@@ -2,24 +2,24 @@ import gql from 'graphql-tag';
 
 export const courseSchema = gql`
   scalar Date
-  type Query {
-    getCourses: [Course!]!
-    getCourseById(id: ID!): Course!
-  }
+
   type Course {
-    id: ID
     title: String
     description: String
     thumbnail: String
-    position: Int
     status: String
     createdAt: Date
   }
+
   input CourseInput {
     title: String
     description: String
     thumbnail: String
     status: String
+  }
+  type Query {
+    getCourses: [Course!]!
+    getCourseById(id: ID!): Course!
   }
   type Mutation {
     updateCourse(id: ID!, courseInput: CourseInput!): Course!
--- apps/GLMS/courses-service/src/pages/index.tsx
+++ apps/GLMS/courses-service/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Courses Service </div>
+      <div>Welcome to Courses Service {process.env.ENVIRONMENT} </div>
     </>
   );
 };
