fcde2116af28b17ef6c4ae3ad0ecaeeb2999353f
Цэрэн янжин Бүдээхүү <LP230127@G305-CC0072.local>
2024-05-30 11:29:07 +0800

    feat(challenge-service): bug

--- apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-approved-challenges.ts
+++ apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-approved-challenges.ts
@@ -4,8 +4,8 @@ import { GraphQLError } from 'graphql';
 
 export const getApprovedChallenges: QueryResolvers['getApprovedChallenges'] = async () => {
   try {
-    const approvedChallenge = await ChallengeModel.find({ status: 'APPROVED' }).populate('courseId').populate('quiz');
-    return approvedChallenge;
+    const approvedChallenges = await ChallengeModel.find({ status: 'APPROVED' }).populate('courseId').populate('quiz');
+    return approvedChallenges;
   } catch (error) {
     throw new GraphQLError('Error in get challenges by approved status query');
   }
