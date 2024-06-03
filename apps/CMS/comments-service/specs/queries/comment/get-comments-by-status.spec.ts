a0fdc61bbaf8dc09c380cc040ead5f94eaed6b9b
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-06-03 12:07:54 +0800

    feat(comments-serivce): get reversed comments

--- apps/CMS/comments-service/specs/queries/comment/get-comments-by-status.spec.ts
+++ apps/CMS/comments-service/specs/queries/comment/get-comments-by-status.spec.ts
@@ -27,7 +27,7 @@ describe('This query should return comments', () => {
 
   it('1.should return comments if found', async () => {
     const comments = await getComments!({}, { input: { limit: 10, offset: 0, status: 'NORMAL' } }, {}, {} as GraphQLResolveInfo);
-    expect(comments).toEqual([{ _id: 'asdf', name: 'adsf', email: 'asdfejf', comment: 'test', ipAddress: 'adf', createdAt: expect.any(Date), articleId: 'asdf' }]);
+    expect(comments).toEqual([{ _id: 'asdf', name: 'adsf', email: 'asdfejf', comment: 'test', ipAddress: 'adf', createdAt: expect.any(Date), articleId: 'asdf' }].reverse());
   });
 
   it('2.should return GraphQLError if comments not found', async () => {
--- apps/CMS/comments-service/src/graphql/resolvers/queries/comment/get-comments-by-articleid.ts
+++ apps/CMS/comments-service/src/graphql/resolvers/queries/comment/get-comments-by-articleid.ts
@@ -5,7 +5,7 @@ import { CommentsModel } from '../../../../models/comment.model';
 export const getCommentsByArticleId: QueryResolvers['getCommentsByArticleId'] = async (_, { articleId }) => {
   try {
     const comments = await CommentsModel.find({ articleId });
-    return comments;
+    return comments.reverse();
   } catch (error) {
     throw graphqlErrorHandler({ message: `cannot delete comment` }, errorTypes.INTERVAL_SERVER_ERROR);
   }
