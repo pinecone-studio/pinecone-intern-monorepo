43b84651cd6905252e0bf78d4899b204cdc2e30a
dzone24 <164152320+derrick23LP8019@users.noreply.github.com>
2024-06-04 12:59:43 +0800

    docs: general info component created

---
+++ apps/HRMS/hrms-dashboard/components/GeneralInfo.tsx
@@ -0,0 +1,25 @@
+const GeneralInfo = () => {
+  return (
+    <div className="flex flex-col justify-evenly w-[700px] h-[176px] p-4 border-[1px] rounded-[8px] border-[#E0E0E0]">
+      <h2 className="font-bold">Ерөнхий мэдээлэл</h2>
+      <div className="flex justify-between">
+        <label>Үндсэн цалин</label>
+        <label>8’000’000₮</label>
+      </div>
+      <div className="flex justify-between">
+        <label>7 сард ажиллах ёстой хоног (цаг)</label>
+        <label>16 хоног (128 цаг)</label>
+      </div>
+      <div className="flex justify-between">
+        <label>Урьдчилгаа цалин тооцох хоног</label>
+        <label>5 хоног</label>
+      </div>
+      <div className="flex justify-between">
+        <label>Үлдэгдэл цалин тооцох хоног</label>
+        <label>11 хоног</label>
+      </div>
+    </div>
+  );
+};
+
+export default GeneralInfo;
--- apps/HRMS/hrms-dashboard/src/app/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import GeneralInfo from '../../components/GeneralInfo';
+
 export default async function Index() {
   return (
     <div>
       <h1>hello from HRMS dashboard</h1>
       <h1>This is the environment {process.env.ENVIRONMENT}</h1>
+      <GeneralInfo />
     </div>
   );
 }
