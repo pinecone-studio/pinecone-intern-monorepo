6751f3aa01d2c24aad138e8f282803f6b9aa68fe
Bolormaa Ihkbayar <bolorboloroo1127@gmail.com>
2024-06-04 11:29:48 +0800

    perf(challenge-service): get challenge by course id

--- apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-challenge-by-id.ts
+++ apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-challenge-by-id.ts
@@ -2,9 +2,9 @@ import { QueryResolvers } from '@/graphql/generated';
 import { ChallengeModel } from '@/model/challenge.model';
 import { GraphQLError } from 'graphql';
 
-export const getChallengeById: QueryResolvers['getChallengeById'] = async (_, { challengeId }) => {
+export const getChallengeById: QueryResolvers['getChallengeById'] = async (_, { courseId }) => {
   try {
-    const challenge = await ChallengeModel.findById(challengeId).populate('quiz');
+    const challenge = await ChallengeModel.findOne({ courseId }).populate('quiz');
     return challenge;
   } catch (error) {
     throw new GraphQLError('Error in get challenge by id query');
