f50630c3818af45c221cb8959434a5a8d53c525d
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-06-05 19:23:47 +0800

    feat: button test in payroll

--- apps/HRMS/hrms-dashboard/cypress/e2e/payroll/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/payroll/index.cy.ts
@@ -1,13 +1,7 @@
-describe('payroll page on hrms', () => {
-  // beforeEach(() => cy.visit('/payroll'));
-  it('Should display button click', () => {
-    // cy.get('button').contains('/employee-details').click();
-    cy.visit('/payroll'),
-      {
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'User-Agent': 'axios/0.18.0',
-        },
-      };
+describe('profile page', () => {
+  beforeEach(() => cy.visit('/payroll'));
+
+  it('Should display welcome message', () => {
+    cy.get('button').contains('/http://localhost:4200/employee-details').click();
   });
 });
