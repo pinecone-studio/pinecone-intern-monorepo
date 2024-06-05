49df3673bc943b2ebf8048e85e16108129c0eb4d
Bolormaa Ihkbayar <bolorboloroo1127@gmail.com>
2024-06-05 18:45:26 +0800

    fix(challenge-service): frontendees get hiij chadahgui bsniig ynzlav

--- apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-challenge-by-id.ts
+++ apps/GLMS/challenge-service/src/graphql/resolvers/queries/get-challenge-by-id.ts
@@ -4,7 +4,7 @@ import { GraphQLError } from 'graphql';
 
 export const getChallengeById: QueryResolvers['getChallengeById'] = async (_, { courseId }) => {
   try {
-    const challenge = await ChallengeModel.findOne({ courseId }).populate('quiz');
+    const challenge = await ChallengeModel.findOne({}, {}, { courseId: courseId }).populate('quiz');
     return challenge;
   } catch (error) {
     throw new GraphQLError('Error in get challenge by id query');
