ad4a0415c2d06120c8fda08f9b5808265959eb96
MNDE25 <mendbayarbyambadorj5@gmail.com>
2024-06-05 17:45:08 +0800

    feat(sidebar nemsen): sidebar done

---
+++ apps/GLMS/glms-dashboard/src/app/profile/_components/SideBarProfile.tsx
@@ -0,0 +1,9 @@
+export const SideBarProfile = () => {
+  return (
+    <div className="rounded-[12px] p-[32px] w-full flex flex-col gap-[24px] bg-white text-black dark:text-[#ededed] dark:bg-[#3d3d3def]">
+      <h1>Хэрэглэгч</h1>
+      <h1>Сорилтын түүх</h1>
+      <h1>Даалгаврын түүх</h1>
+    </div>
+  );
+};
