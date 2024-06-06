e012421d181bc0f55ab3574400854f94ea1cd3fa
Danzkreig <festeringwound@proton.me>
2024-06-06 18:55:34 +0800

    Added list

---
+++ apps/REPORT/report-dashboard/src/app/_report/_components/list.tsx
@@ -0,0 +1,86 @@
+interface Category {
+  name: string;
+  grade: number;
+  desc: string;
+}
+interface Item {
+  name: string;
+  category: Category[];
+}
+export const List = (items: Item[]) => {
+  return (
+    <div className="w-screen">
+      <table className="table table-lg table-zebra bg-black border-collapse border-2 border-solid border-black" width="100%">
+        {/* head */}
+        <thead>
+          <tr className="*:border-2 *:border-solid *:border-black *:border-collapse border-collapse border-">
+            <th>№</th>
+            <th>Овог нэр</th>
+            <th>Сэдэв</th>
+            <th>Тэмдэглэл</th>
+            <th>Үнэлгээ</th>
+            <th>Ирц</th>
+            <th>Төлөв</th>
+          </tr>
+        </thead>
+        <tbody className="h-1/2">
+          {/* row 1 */}
+          {items.map((item: Item, index: number) => (
+            <>
+              <tr className="flex text-center">
+                <th rowSpan={item.category.length} className="border">
+                  {index + 1}
+                </th>
+                <th rowSpan={item.category.length}>{item.name}</th>
+                <td className="text-center">{item.category[0].name}</td>
+                <td className="">
+                  <textarea className="textarea textarea-bordered" defaultValue={item.category[0].desc}></textarea>
+                </td>
+                <td>
+                  <input type="number" className="grow input" placeholder="0" defaultValue={item.category[0].grade} max={100} min={0} />
+                </td>
+                <td className="block m-auto">
+                  <h1>5/12</h1>
+                  <select className="select select-bordered w-full max-w-xs" defaultValue={'PRESENT'}>
+                    <option value="PRESENT">PRESENT</option>
+                    <option value="ABSENT">ABSENT</option>
+                    <option value="LATE">LATE</option>
+                    <option value="EXCUSED">EXCUSED</option>
+                    <option value="SICK">SICK</option>
+                  </select>
+                </td>
+                <td>
+                  <input type="checkbox" className="checkbox" />
+                </td>
+              </tr>
+              {item.category.slice(1).map((item: Category, index: number) => (
+                <tr key={index}>
+                  <td className="text-center">{item.name}</td>
+                  <td className="">
+                    <textarea className="textarea textarea-bordered" defaultValue={item.desc}></textarea>
+                  </td>
+                  <td>
+                    <input type="number" className="grow input" placeholder="0" defaultValue={item.grade} max={100} min={0} />
+                  </td>
+                  <td className="block m-auto">
+                    <h1>5/12</h1>
+                    <select className="select select-bordered w-full max-w-xs" defaultValue={'PRESENT'}>
+                      <option value="PRESENT">PRESENT</option>
+                      <option value="ABSENT">ABSENT</option>
+                      <option value="LATE">LATE</option>
+                      <option value="EXCUSED">EXCUSED</option>
+                      <option value="SICK">SICK</option>
+                    </select>
+                  </td>
+                  <td className="block m-auto">
+                    <input type="checkbox" className="checkbox" />
+                  </td>
+                </tr>
+              ))}
+            </>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
---
+++ apps/REPORT/report-dashboard/src/app/_report/_features/ReportMain.tsx
@@ -0,0 +1,28 @@
+import { List } from '../_components/List';
+const testData = [
+  {
+    name: 'Анастасия',
+    category: [
+      { name: 'HTML entry.', grade: 80, desc: 'good' },
+      { name: 'CSS entry.', grade: 30, desc: 'good' },
+      { name: 'JS entry.', grade: 80, desc: 'good' },
+      { name: 'SQL entry.', grade: 80, desc: 'good' },
+    ],
+  },
+  {
+    name: 'Анастасия',
+    category: [
+      { name: 'HTML entry.', grade: 80, desc: 'good' },
+      { name: 'CSS entry.', grade: 30, desc: 'good' },
+      { name: 'JS entry.', grade: 80, desc: 'good' },
+      { name: 'SQL entry.', grade: 80, desc: 'good' },
+    ],
+  },
+];
+export const ReportMain = () => {
+  return (
+    <div className="w-screen">
+      <h1>Hello from Report Main with the {List(testData)}</h1>
+    </div>
+  );
+};
