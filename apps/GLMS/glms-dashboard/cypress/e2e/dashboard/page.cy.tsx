ba1ff2f61c951642f3f36d7f9abfd4c3e96a6260
LifeNeedZoya <145326293+LifeNeedZoya@users.noreply.github.com>
2024-06-06 13:09:06 +0800

    Delete apps/GLMS/glms-dashboard/cypress/e2e/dashboard/page.cy.tsx

--- apps/GLMS/glms-dashboard/cypress/e2e/dashboard/page.cy.tsx
+++
@@ -1,7 +0,0 @@
-describe('createLessonPage', () => {
-  beforeEach(() => cy.visit('/dashboard'));
-
-  it('1. Should display fashboard page', () => {
-    cy.get('[data-testid="create-lesson-container"]').should('exist').should('be.visible');
-  });
-});
