2a35e7baff29728f3d830854138796ea14614db3
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-06-06 17:18:14 +0800

    feat(cms comments-service): add bad word

--- apps/CMS/comments-service/src/middlewares/filter-words.ts
+++ apps/CMS/comments-service/src/middlewares/filter-words.ts
@@ -5,9 +5,7 @@ export const filterWords = async (target: string) => {
     const splittedTarget = target.split(' ');
     const badWords = await BadWordModel.find();
     const badWordsSet = new Set(badWords.map((bw) => bw.word.toLowerCase()));
-
     const filteredTarget = splittedTarget.map((word) => (badWordsSet.has(word.toLowerCase()) ? '***' : word)).join(' ');
-
     return filteredTarget;
   } catch (error) {
     throw new Error('Failed to filter words');
