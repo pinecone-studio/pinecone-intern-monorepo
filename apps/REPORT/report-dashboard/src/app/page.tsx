d9dcfe0f46fc4e4495a5522da523d59c0d94909a
Danzkreig <festeringwound@proton.me>
2024-06-06 18:55:34 +0800

    Added list

--- apps/REPORT/report-dashboard/src/app/page.tsx
+++ apps/REPORT/report-dashboard/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { ReportMain } from './_report/_features/ReportMain';
 import { StudentMain } from './_student/_features';
 import { TopicMain } from './_topic/_features';
 
 export default async function Index() {
   return (
     <div>
-      <h1>hello from REPORT dashboard</h1>
+      <ReportMain />
       <TopicMain />
       <StudentMain />
       <h1>This is the environment {process.env.ENVIRONMENT}</h1>
