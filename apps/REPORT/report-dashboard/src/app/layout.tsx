c1f9b075f02f467b374fdeeefab7cef6deeb9117
temuujin044 <99674595+temuujin044@users.noreply.github.com>
2024-06-07 00:31:24 +0800

    feat(student-attendence): initialize shadcn (#1386)
    * feat(student-attendence): initialize shadcn
    
    * feat(student-attendance): initialize shadcn
    
    ---------
    
    Co-authored-by: Temuujin undefined <23LP5268@G304-CC0218.local>
    
    resolved pull conflict again

--- apps/REPORT/report-dashboard/src/app/layout.tsx
+++ apps/REPORT/report-dashboard/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { FederationProvider } from '../common';
 import './global.css';
 
+import { cn } from '../lib/utils';
+
 export const metadata = {
   title: 'Welcome to report-dashboard',
   description: 'WELCOME',
@@ -10,7 +12,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <FederationProvider>
-        <body>{children}</body>
+        <body className={cn('min-h-screen bg-background font-sans antialiased')}>{children}</body>
       </FederationProvider>
     </html>
   );
