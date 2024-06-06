96e69a29ada9d6a24896848ebf547b5f9109fed6
Danzkreig <festeringwound@proton.me>
2024-06-06 20:02:17 +0800

    Added daisyUI and made the list a bit better. Now to add date.

--- apps/REPORT/report-dashboard/src/app/_report/_components/list.tsx
+++ apps/REPORT/report-dashboard/src/app/_report/_components/list.tsx
@@ -10,11 +10,11 @@ interface ListItemProps {
 export const List: React.FC<ListItemProps[]> = (props) => {
   const items = props;
   return (
-    <div className="w-screen">
-      <table className="table table-lg table-zebra bg-black border-collapse border-2 border-solid border-black" width="100%">
+    <div className="w-[90vw]">
+      <table className="table table-lg w-full">
         {/* head */}
         <thead>
-          <tr className="*:border-2 *:border-solid *:border-black *:border-collapse border-collapse border-">
+          <tr className="">
             <th>№</th>
             <th>Овог нэр</th>
             <th>Сэдэв</th>
@@ -28,52 +28,32 @@ export const List: React.FC<ListItemProps[]> = (props) => {
           {/* row 1 */}
           {items.map((item: ListItemProps, index: number) => (
             <>
-              <tr className="flex text-center">
-                <th rowSpan={item.category.length} className="border">
-                  {index + 1}
-                </th>
+              <tr key={index} className="*:border-2 *:border-solid *:border-white">
+                <th rowSpan={item.category.length}>{index + 1}</th>
                 <th rowSpan={item.category.length}>{item.name}</th>
-                <td className="text-center">{item.category[0].name}</td>
+                <td className="">{item.category[0].name}</td>
                 <td className="">
-                  <textarea className="textarea textarea-bordered" defaultValue={item.category[0].desc}></textarea>
+                  <textarea className="textarea textarea-bordered w-full" defaultValue={item.category[0].desc}></textarea>
                 </td>
                 <td>
                   <input type="number" className="grow input" placeholder="0" defaultValue={item.category[0].grade} max={100} min={0} />
                 </td>
-                <td className="block m-auto">
-                  <h1>5/12</h1>
-                  <select className="select select-bordered w-full max-w-xs" defaultValue={'PRESENT'}>
-                    <option value="PRESENT">PRESENT</option>
-                    <option value="ABSENT">ABSENT</option>
-                    <option value="LATE">LATE</option>
-                    <option value="EXCUSED">EXCUSED</option>
-                    <option value="SICK">SICK</option>
-                  </select>
-                </td>
+
                 <td>
                   <input type="checkbox" className="checkbox" />
                 </td>
               </tr>
               {item.category.slice(1).map((item: ListItemPropsCategory, index: number) => (
-                <tr key={index}>
-                  <td className="text-center">{item.name}</td>
+                <tr key={'cat' + index} className="*:border-2 *:border-solid *:border-white *:border-collapse">
+                  <td className="">{item.name}</td>
                   <td className="">
-                    <textarea className="textarea textarea-bordered" defaultValue={item.desc}></textarea>
+                    <textarea className="textarea textarea-bordered w-full" defaultValue={item.desc}></textarea>
                   </td>
                   <td>
                     <input type="number" className="grow input" placeholder="0" defaultValue={item.grade} max={100} min={0} />
                   </td>
-                  <td className="block m-auto">
-                    <h1>5/12</h1>
-                    <select className="select select-bordered w-full max-w-xs" defaultValue={'PRESENT'}>
-                      <option value="PRESENT">PRESENT</option>
-                      <option value="ABSENT">ABSENT</option>
-                      <option value="LATE">LATE</option>
-                      <option value="EXCUSED">EXCUSED</option>
-                      <option value="SICK">SICK</option>
-                    </select>
-                  </td>
-                  <td className="block m-auto">
+
+                  <td className="">
                     <input type="checkbox" className="checkbox" />
                   </td>
                 </tr>
--- apps/REPORT/report-dashboard/src/app/_report/_features/ReportMain.tsx
+++ apps/REPORT/report-dashboard/src/app/_report/_features/ReportMain.tsx
@@ -8,19 +8,10 @@ export const testData = [
       { name: 'JS entry.', grade: 80, desc: 'good' },
     ],
   },
-  {
-    name: 'Анастасия',
-    category: [
-      { name: 'HTML entry.', grade: 80, desc: 'good' },
-      { name: 'CSS entry.', grade: 30, desc: 'good' },
-      { name: 'JS entry.', grade: 80, desc: 'good' },
-      { name: 'SQL entry.', grade: 80, desc: 'good' },
-    ],
-  },
 ];
 export const ReportMain = () => {
   return (
-    <div className="w-screen">
+    <div className="w-screen flex justify-center items-center">
       <h1>Hello from Report Main with the {List(testData)}</h1>
     </div>
   );
