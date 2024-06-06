05510b79894291f42ca343804c92e3a4eae0e992
Temuujin undefined <23LP5268@G304-CC0218.local>
2024-06-06 18:52:47 +0800

    feat(student-attendance): initialize shadcn

--- apps/REPORT/report-dashboard/src/app/page.tsx
+++ apps/REPORT/report-dashboard/src/app/page.tsx
@@ -1,18 +1,15 @@
-import { Button } from '../shadcn/Button';
-import { Checkbox } from '../shadcn/Checkbox';
-// import { StudentMain } from './_student/_features';
-// import { TopicMain } from './_topic/_features';
+'use client';
+
+import { StudentMain } from './_student/_features';
+import { TopicMain } from './_topic/_features';
 
 export default async function Index() {
   return (
     <div>
-      <Button>Hello world!</Button>
-      <div className="flex items-center space-x-2">
-        <Checkbox id="terms" />
-        <label htmlFor="terms" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-          Accept terms and conditions
-        </label>
-      </div>
+      <h1>hello from REPORT dashboard</h1>
+      <TopicMain />
+      <StudentMain />
+      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
     </div>
   );
 }
