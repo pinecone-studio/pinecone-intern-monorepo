78eea09bf53ac0839ec47f24f779ffdcc2c0a65e
Zulsar Khasarkhuu <23LP5930@G304CC0215.local>
2024-06-07 19:08:14 +0800

    feat(student-attendance): stepper component front

---
+++ apps/REPORT/report-dashboard/specs/student/Stepper.spec.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Stepper } from '../../src/app/_student/_components/Stepper';
+
+describe('StepperComponent', () => {
+  it('Renders the Stepper component with default props', () => {
+    render(<Stepper currentStep={0} numberOfSteps={3} />);
+    render(<Stepper currentStep={1} numberOfSteps={3} />);
+    render(<Stepper currentStep={2} numberOfSteps={3} />);
+
+  });
+});
