a99b3e4c98d8438596809b0b205bd7266ea7a989
ashrv12 <23LP01611@nest.edu.mn>
2024-06-07 15:23:57 +0800

    feat(adding shadcn configuration): adding shadcn configuration

--- apps/GLMS/glms-dashboard/src/app/layout.tsx
+++ apps/GLMS/glms-dashboard/src/app/layout.tsx
@@ -4,11 +4,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Header from '../components/Header';
 import { AuthProvider, ThemeProvider, FederationProvider } from '../common';
+import { cn } from '../lib/utils';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased')}>
         <FederationProvider>
           <AuthProvider>
             <ThemeProvider>
