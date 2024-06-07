410dcb14bf75da13019c168e877d5f4bc115fb34
temuu-ux <151482172+temuu-ux@users.noreply.github.com>
2024-06-07 11:59:31 +0800

    Delete apps/HRMS/hrms-dashboard/src/app/page.tsx

--- apps/HRMS/hrms-dashboard/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default async function Index() {
-  return (
-    <div>
-      <h1>hello from HRMS dashboard</h1>
-      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
-    </div>
-  );
-}
