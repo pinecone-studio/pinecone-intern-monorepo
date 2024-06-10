573f39f0fccac0d60ce97cf2e9a079080ef31ed5
Galkhuu Amgalanbaatar <164152106+Amgalanbaatar888@users.noreply.github.com>
2024-06-10 10:55:21 +0800

    student-service (update-student) (#1408)
    * feat(fixed frontend issue): fixed front end by rebasing
    
    * fix(connect database): connect database
    
    * feat(fixed delete.student test): fixed delete.student-test
    
    * feat(student-service): student update back
    
    * feat(student-service): connect to mongodb
    
    ---------
    
    Co-authored-by: Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>

---
+++ apps/REPORT/student-service/src/graphql/resolvers/mutations/update-student.ts
@@ -0,0 +1,17 @@
+import { MutationResolvers } from '@/graphql/generated/index';
+import { GraphQLError } from 'graphql';
+import { StudentsModel } from '@/graphql/models/student.models';
+
+export const updateStudent: MutationResolvers['updateStudent'] = async (_, { updateInput }) => {
+  const { _id, firstName, lastName, studentCode, profileImgUrl, classId } = updateInput;
+  try {
+    const updateStudent = await StudentsModel.findByIdAndUpdate(_id, { firstName, lastName, studentCode, profileImgUrl, classId });
+
+    if (!updateStudent) {
+      throw new GraphQLError('Could not find Student');
+    }
+    return updateStudent._id;
+  } catch (e) {
+    throw new GraphQLError('Cannot update student');
+  }
+};
--- apps/REPORT/student-service/src/graphql/schemas/student.schema.ts
+++ apps/REPORT/student-service/src/graphql/schemas/student.schema.ts
@@ -21,6 +21,14 @@ export const studentsSchema = gql`
   input deleteStudentInput {
     studentId: String!
   }
+  input UpdateStudentInput {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    studentCode: String!
+    profileImgUrl: String!
+    classId: String!
+  }
 
   type Query {
     getStudentsByClassId(classId: String!): [Student!]
@@ -29,5 +37,6 @@ export const studentsSchema = gql`
   type Mutation {
     createStudent(input: CreateStudentInput!): Student!
     deleteStudent(input: deleteStudentInput!): ID!
+    updateStudent(updateInput: UpdateStudentInput!): ID!
   }
 `;
