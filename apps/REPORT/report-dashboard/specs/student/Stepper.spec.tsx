4bc02341b7b7eaa28b5d5e7aaeedbd2460ed7840
Zulsar199 <150428096+Zulsar199@users.noreply.github.com>
2024-06-10 08:34:54 +0800

    feat(student-attendance): stepper component front (#1414)
    * feat(student-attendance): stepper component front
    
    * fix(student-attendance): cleared screen from stepper.spec.tsx file
    
    ---------
    
    Co-authored-by: Zulsar Khasarkhuu <23LP5930@G304CC0215.local>

---
+++ apps/REPORT/report-dashboard/specs/student/Stepper.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Stepper } from '../../src/app/_student/_components/Stepper';
+
+describe('StepperComponent', () => {
+  it('Renders the Stepper component with default props', () => {
+    render(<Stepper currentStep={0} numberOfSteps={3} />);
+    render(<Stepper currentStep={1} numberOfSteps={3} />);
+    render(<Stepper currentStep={2} numberOfSteps={3} />);
+
+  });
+});
