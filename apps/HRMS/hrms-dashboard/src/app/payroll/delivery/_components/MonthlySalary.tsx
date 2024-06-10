984d54f7705f160568d060b008f445f6de3ea697
Zaya <zaya@Zayas-MacBook-Air.local>
2024-06-10 18:15:42 +0800

    feat(hrms-payroll-delivery): test bug

--- apps/HRMS/hrms-dashboard/src/app/payroll/delivery/_components/MonthlySalary.tsx
+++ apps/HRMS/hrms-dashboard/src/app/payroll/delivery/_components/MonthlySalary.tsx
@@ -1,10 +1,3 @@
-import { Dispatch, SetStateAction } from 'react';
-
-type PropsType = {
-  setPage: Dispatch<SetStateAction<number | undefined>>;
-  checked: number;
-  defaultValue: string;
-};
 export const MonthlySalary = () => {
   return <div>sar buriin tsalingiin medeelel hiiged maplah</div>;
 };
--- apps/HRMS/hrms-dashboard/src/app/payroll/delivery/_components/index.ts
+++ apps/HRMS/hrms-dashboard/src/app/payroll/delivery/_components/index.ts
@@ -1,1 +1,2 @@
 export * from './PayrollPagination';
+export * from './MonthlySalary';
