a37d7251419f5a0a7a69cb2b2661883a92820124
Elbeg Amar <elbeg@nestsolutions.llc>
2024-06-11 21:15:54 +0800

    fix(hrms): fixed HRMS dashoard unit test

--- apps/HRMS/hrms-dashboard/specs/recruiting/LeavingButton.spec.tsx
+++ apps/HRMS/hrms-dashboard/specs/recruiting/LeavingButton.spec.tsx
@@ -1,9 +1,9 @@
-import { RecruitingButton } from '../../src/app/_recruiting/_components';
+import { RecruitingButton } from '../../src/app/recruiting/_components';
 import React from 'react';
 import { render } from '@testing-library/react';
 
-describe('Leaving', () => {
-  it('Should render profile button component', () => {
+describe('Recruiting Button', () => {
+  it('Should render recruiting button component', () => {
     const { container } = render(<RecruitingButton text="hello test" />);
     expect(container).toBeDefined();
   });
