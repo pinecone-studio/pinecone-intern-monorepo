9060f4ee1a641c3bffd77f59b5a10bbde097d1e7
Gantulga0722 <mgtulga22@gmail.com>
2024-06-13 23:50:42 +0800

    fix: to fix the bug of page

--- apps/HRMS/hrms-dashboard/src/app/employee-details/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/page.tsx
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
 import { Stepper } from './_components/modal/Stepper';
-
-const EmployeeDetailsPage = () => {
-  return (
-    <div>
-      <Stepper />
-=======
 import { AddEmployee } from './_components/AddEmployee';
 
 const EmployeeDetailsPage = () => {
   return (
-    <div className="flex justify-center ">
-      <AddEmployee />
->>>>>>> main
+    <div>
+      <div className="flex justify-center ">
+        <AddEmployee />
+      </div>
+      <div>
+        <Stepper />
+      </div>
     </div>
   );
 };
