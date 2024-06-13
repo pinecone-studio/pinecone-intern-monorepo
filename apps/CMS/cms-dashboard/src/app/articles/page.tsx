769d4d49a8312235978b59ceefd67bf99cf4953b
khaliun bayndalai <23LP7845@G304-CC0231.local>
2024-06-13 21:50:29 +0800

    feat(component): a component that accepts img to uplaod, and previews the img
    and it sends the uploaded file to the parent element via a function

--- apps/CMS/cms-dashboard/src/app/articles/page.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/page.tsx
@@ -1,10 +1,12 @@
 'use client';
+import { ImageInput } from "./_components/ImageInput";
 
 export default async function Index() {
   return (
     <div>
       <h1 data-cy="Articles-Page">hello from Articles Page</h1>
       <h1>This is the environment {process.env.ENVIRONMENT}</h1>
+      <ImageInput/>
     </div>
   );
 }
