627e2c2cf5219a2b7144f850a5aeec4003d12cd7
Huslen1027 <151482110+Huslen1027@users.noreply.github.com>
2024-06-13 15:51:52 +0800

    1436 front sign in input (#1471)
    * feat: cms-dashbboard-sign-in-input add
    
    * feat: added input fields
    
    * feat: added sign-in test
    
    * fix: data-cy-name-changed
    
    * fix: name changed
    
    ---------
    
    Co-authored-by: Huslen Batzorig <23LP2437@G304-CC0220.local>

--- apps/CMS/cms-dashboard/src/app/sign-in/_features/SignInModal.tsx
+++ apps/CMS/cms-dashboard/src/app/sign-in/_features/SignInModal.tsx
@@ -1,4 +1,22 @@
+import { Input } from '@/components/ui/input';
+
 const SignInModal = () => {
-  return <div className="w-[440px] h-[424px] border border-[1px solid #ECEDF0] rounded-[16px] p-[40px]"></div>;
+  return (
+    <div className="w-[440px] h-[424px] border border-[1px solid #ECEDF0] rounded-[16px] p-[40px]">
+      <div className="flex flex-col items-center gap-[24px]">
+        <h1 className="text-[36px] font-bold">Нэвтрэх</h1>
+        <div className="flex flex-col items-center gap-[16px]">
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold">Таны имэйл эсвэл утасны дугаар</p>
+            <Input name="email" type="email" className="w-[360px] h-[56px] text-[18px] font-normal bg-[#F7F7F8]" placeholder="Имэйл эсвэл утасны дугаар" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold">Нууц үг</p>
+            <Input name="password" type="password" className="w-[360px] h-[56px] text-[18px] font-normal bg-[#F7F7F8]" placeholder="Нууц үгээ оруулна уу" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default SignInModal;
