59012146c22fc41a9e126abab8cd4cbbc144646a
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-06-13 18:01:06 +0800

    fix: bun fix

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/AddEmployee.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/AddEmployee.tsx
@@ -1,13 +1,12 @@
-import { Button } from '@/components/ui/button';
 import { MdOutlineAdd } from 'react-icons/md';
 export const AddEmployee = () => {
   return (
-    <div data-testid="container" className="flex justify-between w-[1154px] py-5 px-6 ">
-      <h1 className="text-2xl font-bold not-italic ">Ажилчид</h1>
-      <Button data-testid="addButton" variant={'secondary'}>
+    <div data-testid="container" className="flex justify-between w-[1154px] py-5 px-6 items-center">
+      <h1 className="text-2xl font-bold  ">Ажилчид</h1>
+      <button data-testid="addButton" className="flex items-center justify-center gap-2 bg-[#F7F7F8] hover:bg-gray-200 ease-in-out duration-500 px-4 py-2 text-[#121316] rounded-lg">
         <MdOutlineAdd className="w-5 h-5" />
         Ажилтан нэмэх
-      </Button>
+      </button>
     </div>
   );
 };
