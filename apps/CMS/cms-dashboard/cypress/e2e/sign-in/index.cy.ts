485d710cc0765832026b3dd77e0204b9b1054199
Huslen Batzorig <23LP2437@G304-CC0220.local>
2024-06-13 15:29:32 +0800

    fix: name changed

--- apps/CMS/cms-dashboard/cypress/e2e/sign-in/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/sign-in/index.cy.ts
@@ -1,4 +1,4 @@
-describe('sign-in page', () => {
+describe('sign-in-page', () => {
   beforeEach(() => cy.visit('/sign-in'));
   it('Should render home page', () => {
     cy.get('[data-cy="sign-in-page"]').should('exist');
