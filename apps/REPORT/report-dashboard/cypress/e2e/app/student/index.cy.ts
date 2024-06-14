a8dddcc411c7e0f8c7fe6020ff25c6f73503dd68
Амгаланбаатар Галхүү <23LP5157@G304-CC0235.local>
2024-06-14 18:53:41 +0800

    feat: fix bug

--- apps/REPORT/report-dashboard/cypress/e2e/app/student/index.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Student subpage', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('Should display welcome message', () => {
-    cy.get('[data-cy="Student-Main"]').should('exist').should('be.visible');
-  });
-});
