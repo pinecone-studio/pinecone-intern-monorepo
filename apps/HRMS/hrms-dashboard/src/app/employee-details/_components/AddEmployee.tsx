26581d416fdba6514c985f840af7a52ee95dad93
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-06-14 16:23:05 +0800

    feat: modal hiih cypress test hiisen

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/AddEmployee.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/AddEmployee.tsx
@@ -1,12 +1,10 @@
 import { MdOutlineAdd } from 'react-icons/md';
+import { AddModal } from './AddModal';
 export const AddEmployee = () => {
   return (
     <div data-testid="container" className="flex justify-between w-[1154px] py-5 px-6 items-center">
       <h1 className="text-2xl font-bold  ">Ажилчид</h1>
-      <button data-testid="addButton" className="flex items-center justify-center gap-2 bg-[#F7F7F8] hover:bg-gray-200 ease-in-out duration-500 px-4 py-2 text-[#121316] rounded-lg">
-        <MdOutlineAdd className="w-5 h-5" />
-        Ажилтан нэмэх
-      </button>
+      <AddModal />
     </div>
   );
 };
---
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/AddModal.tsx
@@ -0,0 +1,27 @@
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { MdOutlineAdd } from 'react-icons/md';
+import { IoArrowForwardSharp } from 'react-icons/io5';
+export const AddModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button data-testid="addEmployeeBtn" variant="secondary">
+          <MdOutlineAdd data-testid="add-icon" className="w-5 h-5" />
+          Ажилтан нэмэх
+        </Button>
+      </DialogTrigger>
+      <DialogContent data-testid="modalContent" className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle data-testid="title">Ажилтан нэмэх</DialogTitle>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button  variant={'outline'} className="flex gap-2 text-[#1C20243D] bg-[#D6D8DB] duration-500" data-testid="SubmitBtn" type="submit">
+            Дараах <IoArrowForwardSharp data-testid="arrow-icon" className="w-4 h-4" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
