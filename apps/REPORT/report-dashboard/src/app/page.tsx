489b6cb3663ba6ebecda57ec94b9749e8df537cd
Амгаланбаатар Галхүү <23LP5157@G304-CC0235.local>
2024-06-14 18:44:33 +0800

    feat: fix bug

--- apps/REPORT/report-dashboard/src/app/page.tsx
+++ apps/REPORT/report-dashboard/src/app/page.tsx
@@ -1,14 +1,9 @@
 'use client';
 
-import { StudentMain } from './_student/_features';
-import { TopicMain } from './_topic/_features';
-
 export default async function Index() {
   return (
     <div>
       <h1>hello from REPORT dashboard</h1>
-      <TopicMain />
-      <StudentMain />
       <h1>This is the environment {process.env.ENVIRONMENT}</h1>
     </div>
   );
