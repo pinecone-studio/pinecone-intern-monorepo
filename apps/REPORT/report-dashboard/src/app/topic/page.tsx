bcb82aa184db585c1a516b54d5e3105bf0abbe37
Амгаланбаатар Галхүү <23LP5157@G304-CC0235.local>
2024-06-14 19:47:54 +0800

    fix(report-dashboard): fixxxx errrrrooooor

---
+++ apps/REPORT/report-dashboard/src/app/topic/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default async function Index() {
+  return (
+    <div>
+      <h1>hello from REPORT dashboard</h1>
+      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
+    </div>
+  );
+}
