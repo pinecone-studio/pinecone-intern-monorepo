129a202c097645c72463d29da3019f956cd4b795
batuka228 <batukagshhh@gmail.com>
2024-06-16 15:52:22 +0800

    fix: bug fix

--- apps/HRMS/hrms-dashboard/src/app/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/page.tsx
@@ -2,7 +2,6 @@ export default async function Index() {
   return (
     <div>
       <h1 data-cy="Home-Page">hello from HRMS dashboard</h1>
-      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
     </div>
   );
 }
