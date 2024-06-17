66de77f06d49d245e8694296a646b1ff2130fd96
Gantulga0722 <mgtulga22@gmail.com>
2024-06-17 14:27:18 +0800

    fix(hrms): to fix bug

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/modal/Stepper.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/modal/Stepper.tsx
@@ -14,7 +14,8 @@ export const Stepper = ({ currentStep }: { currentStep: number }) => {
             <div data-testid="circle-contain" className="flex-1 relative items-center justify-center px-1">
               <div
                 data-testid="step-circle"
-                className={`flex items-center justify-center w-9 h-9 rounded-full mx-auto ${index <= currentStep ? 'bg-[#121316] text-white' : 'bg-[#ECEDF0] text-black'}`}
+                className="flex items-center justify-center w-9 h-9 rounded-full mx-auto"
+                style={{ backgroundColor: index <= currentStep ? '#121316' : '#ECEDF0', color: index <= currentStep ? 'white' : 'black' }}
               >
                 <p data-testid="step-number" className="text-4 font-[600] leading-5 tracking-[-0.3px]">
                   {index + 1}
@@ -23,8 +24,8 @@ export const Stepper = ({ currentStep }: { currentStep: number }) => {
               {index < steps.length - 1 && (
                 <div
                   data-testid="step-item-0"
-                  className={`absolute top-1/2 transform -translate-y-1/2 h-1 ${currentStep >= index + 1 ? 'bg-black' : 'bg-[#ECEDF0]'}`}
-                  style={{ left: 'calc(50% + 18px)', width: 'calc(100% - 20px)' }}
+                  className="absolute top-1/2 transform -translate-y-1/2 h-1"
+                  style={{ left: 'calc(50% + 18px)', width: 'calc(100% - 20px)', backgroundColor: currentStep >= index + 1 ? 'black' : '#ECEDF0' }}
                 ></div>
               )}
               <div className="absolute flex justify-center items-center w-[100px] ml-[30px] mt-2">
