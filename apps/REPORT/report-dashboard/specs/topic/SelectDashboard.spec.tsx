aa5fcf57f2f8282205c5b50710a7b2bb4040e39c
bymbabayr <23LP1570@nest.edu.mn>
2024-06-17 15:52:08 +0800

    feat: selectedHeader

---
+++ apps/REPORT/report-dashboard/specs/topic/SelectDashboard.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { SelectDashboard } from '../../src/app/topic/_features/SelectDashboard';
+import { act } from 'react-dom/test-utils';
+
+describe('SelectDashboard component', () => {
+  it('renders menu items correctly', () => {
+    const { getByTestId } = render(<SelectDashboard />);
+    expect(getByTestId('option-1')).toBeDefined();
+  });
+
+  it('selects menu item correctly', () => {
+    const { getByTestId } = render(<SelectDashboard />);
+    act(() => {
+      fireEvent.click(getByTestId('option-2'));
+    });
+    expect(getByTestId('Black-2')).toBeDefined();
+  });
+});
---
+++ apps/REPORT/report-dashboard/src/app/SelectedClass/page.tsx
@@ -0,0 +1,10 @@
+import 'tailwindcss/tailwind.css';
+import { SelectedClassMain } from '../topic/_features/SelectedClassMain';
+
+export default async function Index() {
+  return (
+    <div className="">
+      <SelectedClassMain />
+    </div>
+  );
+}
