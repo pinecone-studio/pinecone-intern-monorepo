e2936a982a5e39a4a0499ec4f04655f108b8ace8
Senge21 <Ssenge1515@gmail.com>
2024-06-17 11:33:35 +0700

    add the header to max width

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/header/Header.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/header/Header.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 export const Header = () => {
   return (
     <>
-      <div className=" bg-[#121316] py-1 px-6 flex justify-between">
+      <div className=" bg-[#121316] py-1 px-6 flex justify-between max-w-[1440px] ">
         <div className="px-1 flex  items-center">
           <HeaderIcon />
         </div>
