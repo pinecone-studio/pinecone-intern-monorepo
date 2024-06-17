64bf5b2e43960a7eb6c20d6e3f21792924b8ea4f
Gantulga0722 <mgtulga22@gmail.com>
2024-06-17 19:22:02 +0800

    fix(hrms): to fix bug

--- apps/HRMS/employee-details-service/src/graphql/resolvers/queries/employee-query.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/queries/employee-query.ts
@@ -1,15 +1,16 @@
-import { QueryResolvers } from '@/graphql/generated';
+import { QueryResolvers, QueryGetEmployeeArgs } from '@/graphql/generated';
 import { EmployeeModel } from '@/models/employee';
 import graphqlErrorHandler, { errorTypes } from '../error';
 
-export const getEmployee: QueryResolvers['getEmployee'] = async (QueryGetEmployeeArgs, { id }) => {
+export const getEmployee: QueryResolvers['getEmployee'] = async (_parent, args: QueryGetEmployeeArgs) => {
   try {
+    const { id } = args;
     const employee = await EmployeeModel.findById(id).populate('relative');
     if (!employee) {
-      throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.INTERVAL_SERVER_ERROR);
+      throw graphqlErrorHandler({ message: 'Ажилтан олдсонгүй' }, errorTypes.NOT_FOUND);
     }
     return employee;
   } catch (error) {
-    throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.INTERVAL_SERVER_ERROR);
+    throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST);
   }
 };
