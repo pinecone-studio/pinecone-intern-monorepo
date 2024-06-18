5bc19c3f78ae57835142f4f2871be12f5a012d83
Тайванжаргал Лхагвасүрэн <23LP9793@G304-CC0222.local>
2024-06-18 20:50:24 +0800

    feat: create article button

--- apps/CMS/cms-dashboard/src/app/articles/_components/ArticlesButton.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_components/ArticlesButton.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 type ArticlesButtonProps = {
-  text: string;
+  text?: string;
+  bgColor?: string | number;
+  type?: 'submit' | 'reset' | 'button';
+  disabled?: boolean;
+  onClick?: () => void;
 };
+import { Button } from '@/components/ui/button';
 
 export const ArticlesButton: React.FC<ArticlesButtonProps> = (props) => {
-  const { text } = props;
-  return <button>{text}</button>;
+  const { text, bgColor, disabled, type, onClick } = props;
+  return (
+    <div>
+      <Button onClick={onClick} disabled={disabled} className={`${bgColor} `} type={type} data-testid="create-article-button">
+        {text}
+      </Button>
+    </div>
+  );
 };
