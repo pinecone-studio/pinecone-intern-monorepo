aef5291ce7011036d4b8ae8c0709f8b58d8035eb
Gantulga0722 <mgtulga22@gmail.com>
2024-06-18 19:45:42 +0800

    fix(hrms): to fix bug

--- apps/HRMS/employee-details-service/src/graphql/resolvers/queries/employee-query.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/queries/employee-query.ts
@@ -6,13 +6,12 @@ export const getEmployeeDetails: QueryResolvers['getEmployee'] = async (_: unkno
   console.log('ID', id);
   try {
     const employee = await EmployeeModel.findById(id).lean();
-    console.log('EE', employee);
+
     if (!employee) {
       throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.INTERVAL_SERVER_ERROR);
     }
     return employee;
   } catch (error) {
-    console.log('error', error);
     throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.INTERVAL_SERVER_ERROR);
   }
 };
