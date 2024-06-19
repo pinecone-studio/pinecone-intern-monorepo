89d1044e9bc69c834c42b32e06b994575577c351
batuka228 <batukagshhh@gmail.com>
2024-06-19 18:04:58 +0800

    fix: bug

--- apps/HRMS/hrms-dashboard/cypress/e2e/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/index.cy.ts
@@ -1,11 +1,6 @@
 describe('home page', () => {
   beforeEach(() => cy.visit('/'));
   it('1.Page should have content', () => {
-    cy.get('[data-cy="dashboardSidebar"]').should('exist');
-    cy.get('[data-cy="dashboardSidebar').children().should('have.length', 4);
-  });
-  it('2.Sidebar menu clickable and redirect to employee details page', () => {
-    cy.get('[data-cy="sidebarItem"]').eq(1).should('exist').should('be.visible');
-    cy.get('[data-cy="sidebarItem"]').eq(1).click();
+    cy.get('[data-cy="Home-Page"]').should('exist');
   });
 });
