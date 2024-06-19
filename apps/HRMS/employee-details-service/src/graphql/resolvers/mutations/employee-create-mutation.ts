6519e95fea95e92ecc276c9ee60a1e83932ac29d
Gantulga0722 <mgtulga22@gmail.com>
2024-06-19 19:25:43 +0800

    fix(hrms): fix bug

--- apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/employee-create-mutation.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/employee-create-mutation.ts
@@ -3,10 +3,8 @@ import { EmployeeModel } from '@/models/employee';
 import graphqlErrorHandler, { errorTypes } from '../error';
 
 export const createEmployee: MutationResolvers['createEmployee'] = async (_: unknown, { input }: { input: CreateEmployeeInput }) => {
-  console.log('inp', input);
   try {
     const createEmloyee = await EmployeeModel.create(input);
-    console.log('re', createEmloyee);
     return createEmloyee;
   } catch (error) {
     throw graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST);
