d74a31949752fa1baa90b923910c16e684b5031f
n0m1n <nomin.register@gmail.com>
2024-06-19 18:41:48 +0800

    feat(cms-dashboard content table list): cms-dashboard content table list component

--- apps/CMS/cms-dashboard/src/app/articles/page.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { TableContent } from './_components';
+
 export default async function Index() {
   return (
     <div>
-      <h1 data-cy="Articles-Page">hello from Articles Page</h1>
-      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
+      {/* <h1 data-cy="Articles-Page">hello from Articles Page</h1>
+      <h1>This is the environment {process.env.ENVIRONMENT}</h1> */}
+      <TableContent></TableContent>
     </div>
   );
 }
