4d36e766129b825859b8c229f9158b3c19cd3c70
Gantulga0722 <mgtulga22@gmail.com>
2024-06-19 18:47:17 +0800

    fix(hrms): bug fix

--- apps/HRMS/employee-details-service/specs/mutations/employee-create-mutation.spec.ts
+++ apps/HRMS/employee-details-service/specs/mutations/employee-create-mutation.spec.ts
@@ -1,5 +1,4 @@
 import { Department, EmploymentStatus } from '@/graphql/generated';
-import graphqlErrorHandler, { errorTypes } from '@/graphql/resolvers/error';
 import { createEmployee } from '@/graphql/resolvers/mutations';
 import { GraphQLResolveInfo } from 'graphql';
 
@@ -18,7 +17,7 @@ jest.mock('@/models/employee', () => ({
         dateOfEmployment: '2024-7-22',
         employmentStatus: 'FULL_TIME',
       })
-      .mockRejectedValueOnce(null),
+      .mockRejectedValueOnce(new Error('Алдаа гарлаа')),
   },
 }));
 
@@ -32,38 +31,30 @@ const input = {
   dateOfEmployment: '2024-7-22',
   employmentStatus: EmploymentStatus.FullTime,
 };
-describe('create employee', () => {
-  // it('should create a employee', async () => {
-  //   const result = await createEmployee!({}, { input }, {}, {} as GraphQLResolveInfo);
-  //   expect(result).toEqual({
-  //     _id: '2',
-  //     firstname: 'bataa',
-  //     lastname: 'od',
-  //     email: 'bataa@gmail.com',
-  //     department: Department.Software,
-  //     jobTitle: ['developer', 'fullstack'],
-  //     salary: 3500000,
-  //     dateOfEmployment: '2024-7-22',
-  //     employmentStatus: EmploymentStatus.FullTime,
-  //   });
-  // });
 
-  it("should throw an error if the employee doesn't exist ", async () => {
+describe('create employee', () => {
+  it('should create an employee', async () => {
+    const result = await createEmployee!({}, { input }, {}, {} as GraphQLResolveInfo);
+    expect(result).toEqual({
+      _id: '2',
+      firstname: 'bataa',
+      lastname: 'od',
+      email: 'bataa@gmail.com',
+      department: Department.Software,
+      jobTitle: ['developer', 'fullstack'],
+      salary: 3500000,
+      dateOfEmployment: '2024-7-22',
+      employmentStatus: EmploymentStatus.FullTime,
+    });
+  });
+
+  it('should throw an error if the employee creation fails', async () => {
     try {
-      const result = await createEmployee!({}, { input }, {}, {} as GraphQLResolveInfo);
-      expect(result).toEqual({
-        _id: '2',
-        firstname: 'bataa',
-        lastname: 'od',
-        email: 'bataa@gmail.com',
-        department: Department.Software,
-        jobTitle: ['developer', 'fullstack'],
-        salary: 3500000,
-        dateOfEmployment: '2024-7-22',
-        employmentStatus: EmploymentStatus.FullTime,
-      });
+      await createEmployee!({}, { input }, {}, {} as GraphQLResolveInfo);
     } catch (error) {
-      expect(error).toEqual(graphqlErrorHandler({ message: 'Алдаа гарлаа' }, errorTypes.BAD_REQUEST));
+      const typedError = error as { message: string; extensions: { code: string } };
+      expect(typedError.message).toBe('Алдаа гарлаа');
+      expect(typedError.extensions.code).toBe('BAD_REQUEST');
     }
   });
 });
