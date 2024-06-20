fd06138c630d84dc7235748c056778a44904d3fa
Бямбабаяр Munkhjargal <23LP1570@G304-CC0231.local>
2024-06-20 16:07:17 +0800

    feat: selectedclas header ui

--- apps/REPORT/report-dashboard/src/app/topic/_features/SelectedClassMain.tsx
+++ apps/REPORT/report-dashboard/src/app/topic/_features/SelectedClassMain.tsx
@@ -3,7 +3,7 @@ import { SelectDashboard } from './SelectDashboard';
 
 export const SelectedClassMain = () => {
   return (
-    <div className="bg-[#F7F7F8]  ">
+    <div>
       <div className="container mx-auto">
         <div className="flex justify-between gap-4 h-[136px] items-end">
           <SelectedClassHeader text={'Class'} students={25} />
