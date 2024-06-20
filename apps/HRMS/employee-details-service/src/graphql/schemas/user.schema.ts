1a5a493f4bfbf4b770d1522cf0e816728eb2eba5
ashrv12 <23LP01611@nest.edu.mn>
2024-06-20 14:40:34 +0800

    fix(fixed schema error): fixed schema error

--- apps/HRMS/employee-details-service/src/graphql/schemas/user.schema.ts
+++ apps/HRMS/employee-details-service/src/graphql/schemas/user.schema.ts
@@ -1,46 +1,43 @@
 import { gql } from 'apollo-server-cloud-functions';
 
 export const userSchema = gql`
-
-type User {
+  type User {
     _id: ID
     firstName: String!
     lastName: String!
     email: String!
-    role: userRole!
+    role: Roles!
     password: String!
   }
 
-enum userRole{
-  EMPLOYEE
-  ADMIN
-}
+  enum Roles {
+    EMPLOYEE
+    ADMIN
+  }
 
-input CreateUserInput {
+  input CreateUserInput {
     firstName: String!
     lastName: String!
     email: String!
     role: String!
     password: String!
   }
-input UpdateUserInput {
+  input UpdateUserInput {
     firstName: String
     lastName: String
     email: String
     role: String
     password: String
-}
+  }
 
-type Query {
-  getUsers:[User]
-  getUser(_id: String!): User
-
-}
+  type Query {
+    getUsers: [User]
+    getUser(_id: String!): User
+  }
 
   type Mutation {
     createUser(input: CreateUserInput): User!
-    updatedUser(_id:ID!,input: UpdateUserInput!): User!
-    deletedUser(_id:ID!):User
-}
-`
-
+    updatedUser(_id: ID!, input: UpdateUserInput!): User!
+    deletedUser(_id: ID!): User
+  }
+`;
