af533e022d4e79f6d68e112d655e1cab5db2098e
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-06-20 19:19:51 +0800

    fix: dsf

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_components/modal/AddModal.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_components/modal/AddModal.tsx
@@ -7,10 +7,7 @@ export const AddModal = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button
-          data-testid="addEmployeeBtn"
-          className="bg-[#F7F7F8] text-[#121316] hover:bg-gray-200 duration-600 ease-in-out h-9 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-        >
+        <Button data-testid="addEmployeeBtn" variant={'secondary'} className="bg-[#F7F7F8] text-[#121316] hover:bg-gray-200 duration-600 ease-in-out h-9 px-4 py-2 ">
           <MdOutlineAdd data-testid="add-icon" className="w-5 h-5" />
           Ажилтан нэмэх
         </Button>
@@ -22,11 +19,7 @@ export const AddModal = () => {
         </DialogHeader>
         <Stepper currentStep={0} />
         <DialogFooter>
-          <Button
-            className=" gap-2 bg-[#D6D8DB] ease-in-out hover:bg-gray-200 hover:text-black h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-[#1C20243D]  duration-600"
-            data-testid="SubmitBtn"
-            type="submit"
-          >
+          <Button variant={'secondary'} className=" gap-2 bg-[#D6D8DB] ease-in-out hover:bg-gray-200 hover:text-black h-10 px-4 py-2 " data-testid="SubmitBtn" type="submit">
             Дараах <IoArrowForwardSharp data-testid="arrow-icon" className="w-4 h-4" />
           </Button>
         </DialogFooter>
