371fc74809df249c37fcfd734d79efb3772ee1a4
Haliukaaa <haliuka.aqua@gmail.com>
2024-06-20 14:42:15 +0800

    chore(consolelog): removed console log

--- apps/CMS/cms-dashboard/src/app/articles/add/page.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/add/page.tsx
@@ -4,9 +4,7 @@ import { useState } from "react";
 
 export default async function Index() {
   const [file, setFile] = useState<File | null>(null);
-console.log(file);
 
-  
   return (
     <div>
       <ImageInput setFile={setFile}/>
