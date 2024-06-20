22d06785c5debcf876a20726b5c35215df27f65a
ashrv12 <23LP01611@nest.edu.mn>
2024-06-20 15:48:37 +0800

    fix(fix): fixed hello exports

--- apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/index.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/mutations/index.ts
@@ -1,6 +1,4 @@
-export * from './hello-mutation';
 export * from './user-create-mutation';
 export * from './user-update-mutation';
 export * from './user-delete-mutation';
 export * from './employee-create-mutation';
-
--- apps/HRMS/employee-details-service/src/graphql/resolvers/queries/index.ts
+++ apps/HRMS/employee-details-service/src/graphql/resolvers/queries/index.ts
@@ -1,6 +1,4 @@
-export * from './hello-query';
 export * from './user-all-query';
 export * from './user-query';
 export * from './employee-all-query';
 export * from './employee-query';
-
