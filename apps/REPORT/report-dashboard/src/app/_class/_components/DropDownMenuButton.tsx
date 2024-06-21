8e779ee663b691b2cee91a00a4af872f7c190b27
Дашноров дамдиндорж <23LP9512@G304-CC0229.local>
2024-06-21 18:16:45 +0800

    feat: drop down menu button done

---
+++ apps/REPORT/report-dashboard/src/app/_class/_components/DropDownMenuButton.tsx
@@ -0,0 +1,32 @@
+import DeleteTab from '@/components/svg/DeleteTab';
+import EditTab from '@/components/svg/EditTab';
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { MoreHorizontal } from 'lucide-react';
+
+const DropDownMenuButton = () => {
+  return (
+    <div className="w-[24px] h-[24px] flex items-center justify-center" data-testid="dropdown-menu-button">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" data-testid="more-horizontal-icon" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="gap-[2px]">
+          <DropdownMenuItem className="gap-[8px]">
+            <EditTab />
+            <p className="text-sm text-black font-normal leading-5">Засах</p>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="gap-[8px]">
+            <DeleteTab />
+            <p className="text-sm text-black font-normal leading-5">Устгах</p>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default DropDownMenuButton;
