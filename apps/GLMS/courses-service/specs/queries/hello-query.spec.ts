bea2d8919df28958ec0bf21c33946d8f6957344b
Enkhsaikhan <164152016+enkhsaikhan1@users.noreply.github.com>
2024-06-21 17:03:55 +0800

    fix(delete example code): delete example code

--- apps/GLMS/courses-service/specs/queries/hello-query.spec.ts
+++
@@ -1,7 +0,0 @@
-import { helloQueryFromCoursesService } from './../../src/graphql/resolvers/queries/hello-query';
-
-describe('Hello Query', () => {
-  it('Should call hello query', () => {
-    expect(helloQueryFromCoursesService()).toBeDefined();
-  });
-});
--- apps/GLMS/courses-service/src/graphql/resolvers/queries/course-queries.ts
+++
@@ -1,9 +0,0 @@
-import { CoursesModel } from '@/models/courses';
-
-const courseQuery = {
-  getCourses: async () => {
-    return await CoursesModel.find({});
-  },
-};
-
-module.exports = courseQuery;
