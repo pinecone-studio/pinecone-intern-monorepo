18c2882d361a40b05e879fc928ddee85f5562aa3
Амгаланбаатар Галхүү <23LP5157@G304-CC0235.local>
2024-06-21 19:04:24 +0800

    feat(report-dashboard): fix lint

--- apps/REPORT/report-dashboard/cypress/e2e/app/student/studentsTable.cy.tsx
+++ apps/REPORT/report-dashboard/cypress/e2e/app/student/studentsTable.cy.tsx
@@ -8,15 +8,9 @@ const studentsData = [
     email: 'Bataa@gmail.com',
     phoneNumber: '89898989',
     active: true,
-    profileImgUrl: 'https://images.unsplash.com/photo-1500320464660-ee0ef7d0ae6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-  },
-  {
-    firstName: 'Bataa',
-    studentCode: '23lp5157@',
-    email: 'Bataa@gmail.com',
-    phoneNumber: '89898989',
-    active: true,
-    profileImgUrl: 'https://images.unsplash.com/photo-1500320464660-ee0ef7d0ae6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    /* eslint-disable no-secrets/no-secrets */
+
+    profileImgUrl: 'https://images.unsplash.com/photo-1718762538704-9698cd001106?q=80&w=4160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
 
