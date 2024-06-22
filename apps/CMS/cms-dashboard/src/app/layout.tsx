fb415d0a013e803d5d4a4ce5a8a480b2b2fb4506
Huslen1027 <hvslenhvslen566@gmail.com>
2024-06-22 12:23:42 +0800

    feat: sign-in page remove layout header

--- apps/CMS/cms-dashboard/src/app/layout.tsx
+++ apps/CMS/cms-dashboard/src/app/layout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { FederationProvider } from '../common';
 import './global.css';
 import { ProjectHeader } from '@/components/ProjectHeader';
+import { usePathname } from 'next/navigation';
 
 export const metadata = {
   title: 'Welcome to CMS-dashboard',
@@ -9,12 +12,15 @@ export const metadata = {
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const isSignInPage = pathname === '/sign-in';
+
   return (
     <html lang="en">
       <body className="relative">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <FederationProvider>
-            <ProjectHeader />
+            {!isSignInPage && <ProjectHeader />}
             {children}
           </FederationProvider>
         </ThemeProvider>
