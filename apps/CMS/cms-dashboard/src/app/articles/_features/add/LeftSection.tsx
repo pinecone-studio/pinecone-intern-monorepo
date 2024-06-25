0d01d5b1c730c4dbb23ee4835e840e870269b1f1
khaliun bayndalai <23LP7845@G401-CC0212.local>
2024-06-25 21:14:16 +0800

    feat(text-editor): adds a rich text editor

--- apps/CMS/cms-dashboard/src/app/articles/_features/add/LeftSection.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_features/add/LeftSection.tsx
@@ -1,11 +1,25 @@
-import { InputField } from '../../_components/add/InputField';
+import { InputField } from '../_components';
+import dynamic from 'next/dynamic';
+import { useField, useFormikContext } from 'formik';
+import 'react-quill/dist/quill.snow.css';
 
-export const LeftSection = () => {
+export const TextField = () => {
+  const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+  const { setFieldValue } = useFormikContext();
+  const [field, meta] = useField('body');
+
   return (
     <div className="w-4/5 h-[93.5vh] flex justify-center items-center m-auto">
       <div className="w-4/5">
         <InputField type="text" data-cy="title-input" label={'Гарчиг өгөх'} name={'title'} placeholder={'Энд гарчгаа бичнэ үү...'} addClass="mb-12" />
-        <InputField type="text" data-cy="body-input" label={'Нийтлэлээ бичих'} name={'body'} placeholder={'Бичиж эхлэх...'} as={'textarea'} addClass="resize-none" rows={5} />
+        <label className="font-semibold text-lg">Нийтлэлээ бичих</label>
+        <ReactQuill
+          value={field.value}
+          onChange={(value) => setFieldValue('body', value)}
+          placeholder="Бичиж эхлэх..."
+          className={`form-control bg-white ${meta.touched && meta.error ? 'border-red-500' : ''}`}
+        />
+        {meta.touched && meta.error ? <div className="text-red-500 text-sm">{meta.error}</div> : null}
       </div>
     </div>
   );
