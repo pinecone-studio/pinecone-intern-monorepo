b213d14b95eca1065badeb6a4b5b8b00991b993f
batuka228 <batukagshhh@gmail.com>
2024-06-25 13:03:44 +0800

    fix: test

--- apps/HRMS/hrms-dashboard/cypress/e2e/employee-details/employee-detail/index.cy.ts
+++ apps/HRMS/hrms-dashboard/cypress/e2e/employee-details/employee-detail/index.cy.ts
@@ -5,9 +5,9 @@ describe('employee-detail', () => {
     cy.get('[data-cy="dashboardSidebar').children().should('have.length', 4);
     cy.get('[data-cy="headerIcon"]').should('exist').should('be.visible');
     cy.get('[data-cy="headerProfile"]').should('exist').should('be.visible');
-    cy.get('[data-cy="Home-Page"]').should('exist').should('be visible');
-    cy.get('[data-cy="employeeDetailHeader"]').should('exist').should('be visible');
-    cy.get('[data-cy="Employee-Detail-children"]').should('exist').should('be visible');
+    cy.get('[data-cy="Home-Page"]').should('exist').should('be.visible');
+    cy.get('[data-cy="employeeDetailHeader"]').should('exist').should('be.visible');
+    cy.get('[data-cy="Employee-Detail-children"]').should('exist').should('be.visible');
   });
   it('2.Sidebar menu clickable and redirect to employee details page', () => {
     cy.get('[data-cy="sidebarItem"]').eq(1).should('exist').should('be.visible');
