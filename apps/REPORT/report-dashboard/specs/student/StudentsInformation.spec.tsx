46bf32d1d7dda89360a0f8a420f40cf56f68b4d6
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-06-25 16:55:39 +0800

    fix(fix pls): fix pls

--- apps/REPORT/report-dashboard/specs/student/StudentsInformation.spec.tsx
+++ apps/REPORT/report-dashboard/specs/student/StudentsInformation.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { StudentsInformation } from '../../src/app/student/_features/studentsTable/StudentsInformation';
 
-jest.mock('@/generated');
+jest.mock('../../src/generated');
 
 describe('StudentsInformation component', () => {
   it('renders loading state', async () => {
