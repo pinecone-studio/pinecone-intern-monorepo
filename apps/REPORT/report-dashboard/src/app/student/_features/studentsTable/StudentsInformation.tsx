f9d90a469931b56f4b8cb878ddc723063c4815d2
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-06-26 15:59:08 +0800

    feat(report-dashboard): student add hiij baigaa modal

--- apps/REPORT/report-dashboard/src/app/student/_features/studentsTable/StudentsInformation.tsx
+++ apps/REPORT/report-dashboard/src/app/student/_features/studentsTable/StudentsInformation.tsx
@@ -6,12 +6,12 @@ import StudentsTable from './StudentsTable';
 export const StudentsInformation = () => {
   const { data, loading, error } = useGetStudentByClassIdQuery({
     variables: {
-      classId: '7000',
+      classId: '12345',
     },
   });
   const studentsData = data?.getStudentsByClassId;
   return (
-    <div className="mx-auto container">
+    <div>
       <div>{loading ? <div data-testid="Loading">Loading...</div> : error ? <div data-testid="Error">Error</div> : <StudentsTable studentsData={studentsData as Student[]} />}</div>
     </div>
   );
--- apps/REPORT/report-dashboard/src/app/student/page.tsx
+++ apps/REPORT/report-dashboard/src/app/student/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { StudentAddModal } from './_features/studentsTable/StudentAddModal';
 import { StudentsInformation } from './_features/studentsTable/StudentsInformation';
 
 const StudentPage = () => {
   return (
     <div className="mx-auto container">
+      <StudentAddModal />
       <StudentsInformation />
     </div>
   );
