0eb62a1b8a388ec1d95cec210ba01461d6591dd0
NamelessXd <164151593+GantogtokhC@users.noreply.github.com>
2024-06-26 21:37:12 +0800

    Update page.tsx

--- apps/GLMS/glms-dashboard/src/app/archiv/page.tsx
+++ apps/GLMS/glms-dashboard/src/app/archiv/page.tsx
@@ -4,10 +4,7 @@ import { CardWithForm } from "./_components";
 export default async function Index() {
   return (
     <div className="w-10/12 mx-auto ">
-      <CardWithForm content="Сайтын загвар угсрах үндсэн суурь хэлнүүд бөгөөд бүрэн static болон"
-  id="1"
-  thumbnail="js.png"
-  title="Javascript"/>
+      <CardWithForm />
     </div>
   );
 }
