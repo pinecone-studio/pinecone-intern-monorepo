f03713b2e407f2c89942d6e6df18bf4e957abaca
dashaa1204 <dashaa1204@gmail.com>
2024-06-26 22:10:35 +0800

    fix: next config fix

--- apps/REPORT/report-dashboard/next.config.js
+++ apps/REPORT/report-dashboard/next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     svgr: false,
   },
   env: {
-    FEDERATION_ENDPOINT: process.env.FEDERATION_ENDPOINT || '',
+    ENDPOINT: process.env.ENDPOINT || '',
     ACCESS_KEY_ID: process.env.ACCESS_KEY_ID || '',
     SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || '',
     PUB_URL: process.env.PUB_URL || '',
