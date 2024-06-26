6943d061b2b78b6ce42b62a1a896010fdedcd2a4
Haliukaaa <haliuka.aqua@gmail.com>
2024-06-27 01:09:15 +0800

    style(text-editor): improved the style

--- apps/CMS/cms-dashboard/src/app/articles/_components/add/InputField.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_components/add/InputField.tsx
@@ -16,7 +16,7 @@ export const InputField = ({ ...props }): JSX.Element => {
         as={props.as}
         rows={props.rows}
         key={props.key}
-        className={`input mt-4 rounded-lg py-[18px] px-6 focus:outline-none transition-colors duration ease-in-out focus:border-blue-500 focus:bg-white focus:placeholder-transparent w-full h-full ${props.addClass}`}
+        className={`input rounded-lg py-[18px] px-6 focus:outline-none transition-colors duration ease-in-out focus:border-blue-500 focus:bg-white focus:placeholder-transparent w-full h-full ${props.addClass}`}
       />
       {meta.touched && meta.error && <div className="text-xs text-red-600">{meta.error}</div>}
     </>
--- apps/CMS/cms-dashboard/src/app/articles/_features/add/LeftSection.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/_features/add/LeftSection.tsx
@@ -1,25 +1,27 @@
 import { InputField } from '../_components';
-import dynamic from 'next/dynamic';
+import ReactQuill from 'react-quill';
 import { useField, useFormikContext } from 'formik';
 import 'react-quill/dist/quill.snow.css';
 
 export const TextField = () => {
-  const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
   const { setFieldValue } = useFormikContext();
   const [field, meta] = useField('body');
+
 
   return (
     <div className="w-4/5 h-[93.5vh] flex justify-center items-center m-auto">
       <div className="w-4/5">
-        <InputField type="text" data-cy="title-input" label={'Гарчиг өгөх'} name={'title'} placeholder={'Энд гарчгаа бичнэ үү...'} addClass="mb-12" />
+        <InputField type="text" data-cy="title-input" label={'Гарчиг өгөх'} name={'title'} placeholder={'Энд гарчгаа бичнэ үү...'} />
+        <div className='mt-12'>
         <label className="font-semibold text-lg">Нийтлэлээ бичих</label>
         <ReactQuill
-          value={field.value}
-          onChange={(value) => setFieldValue('body', value)}
-          placeholder="Бичиж эхлэх..."
-          className={`form-control bg-white ${meta.touched && meta.error ? 'border-red-500' : ''}`}
-        />
+        value={field.value || ''}
+        onChange={(value) => setFieldValue('body', value)}
+        placeholder="Бичиж эхлэх..."
+        className={`form-control ql-toolbar ql-editor ql-container rounded-lg py-[18px] border-none px-6 mt-4 text-base bg-white ${meta.touched && meta.error ? 'border-red-500' : ''}`}
+      />
         {meta.touched && meta.error ? <div className="text-red-500 text-sm">{meta.error}</div> : null}
+        </div>
       </div>
     </div>
   );
