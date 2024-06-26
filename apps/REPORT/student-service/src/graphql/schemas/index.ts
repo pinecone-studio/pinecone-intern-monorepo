9a1377cbcd4b1b6f53910fb519342de0c980b1c2
dashaa1204 <dashaa1204@gmail.com>
2024-06-27 00:54:35 +0800

    fix: hello schema deleted

--- apps/REPORT/student-service/src/graphql/schemas/index.ts
+++ apps/REPORT/student-service/src/graphql/schemas/index.ts
@@ -1,7 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
-import { helloStudentSchema } from './hello.schema';
 import { studentsSchema } from './student.schema';
 import { classSchema } from './class.schema';
 import { userTypeDefs } from './user.schema';
 
-export const typeDefs = mergeTypeDefs([helloStudentSchema, studentsSchema, classSchema,userTypeDefs]);
+export const typeDefs = mergeTypeDefs([studentsSchema, classSchema, userTypeDefs]);
