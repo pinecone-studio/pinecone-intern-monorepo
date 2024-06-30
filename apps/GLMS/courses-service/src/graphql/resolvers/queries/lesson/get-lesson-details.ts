f2dfdcf90f05adfc6446620566d234b34ef11fe8
Enkhsaikhan <164152016+enkhsaikhan1@users.noreply.github.com>
2024-06-30 21:59:08 +0800

    fix(backend): update getLessonDetails query

--- apps/GLMS/courses-service/src/graphql/resolvers/queries/lesson/get-lesson-details.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/queries/lesson/get-lesson-details.ts
@@ -1,10 +1,13 @@
 import { QueryResolvers } from '@/graphql/generated';
-import { LessonsModel } from '@/models/lessons';
+import { LessonsModel } from '@/models/lessons.model';
 import { GraphQLError } from 'graphql';
 
-export const getLessonDetails: QueryResolvers['getLessonDetails'] = async (_, { id }: { id: string }) => {
+export const getLessonDetails: QueryResolvers['getLessonDetails'] = async (_: unknown, { _id }: { _id: string }) => {
   try {
-    const lessonDetails = await LessonsModel.findById(id);
+    const lessonDetails = await LessonsModel.findById(_id).populate({
+      path: 'courseId',
+      model: 'GLMS-Courses',
+    });
 
     if (!lessonDetails) {
       throw new GraphQLError('Lesson not found');
