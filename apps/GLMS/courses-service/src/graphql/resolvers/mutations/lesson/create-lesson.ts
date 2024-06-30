5dddad6da9a11be69f0e17e96cb40e98c3b46cd0
Enkhsaikhan <164152016+enkhsaikhan1@users.noreply.github.com>
2024-06-30 22:01:08 +0800

    fix(backend): update createLesson mutation

--- apps/GLMS/courses-service/src/graphql/resolvers/mutations/lesson/create-lesson.ts
+++ apps/GLMS/courses-service/src/graphql/resolvers/mutations/lesson/create-lesson.ts
@@ -1,13 +1,19 @@
 import { CreateLessonInput } from '@/graphql/generated';
-import { LessonsModel } from '@/models/lessons';
+import { LessonsModel } from '@/models/lessons.model';
 import { GraphQLError } from 'graphql';
 
 export const createLesson = async (_: unknown, { createInput }: { createInput: CreateLessonInput }) => {
   try {
     const newLesson = await LessonsModel.create({ ...createInput });
-    return newLesson;
+
+    const populatedLesson = await LessonsModel.findById(newLesson._id).populate({
+      path: 'courseId',
+      model: 'GLMS-Courses',
+    });
+
+    return populatedLesson;
   } catch (error) {
     const message = (error as Error).message;
-    throw new GraphQLError(`Failed to create lesson ${message}`);
+    throw new GraphQLError(`Failed to create lesson: ${message}`);
   }
 };
