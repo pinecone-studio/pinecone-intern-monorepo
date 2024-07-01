676aabe3c4de82d7e7a2db96dc8d6ead9a1d17ee
dashaa1204 <dashaa1204@gmail.com>
2024-07-01 15:13:24 +0800

    revert: revert hello schema

--- apps/REPORT/student-service/src/graphql/schemas/index.ts
+++ apps/REPORT/student-service/src/graphql/schemas/index.ts
@@ -1,6 +1,7 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
+import { helloStudentSchema } from './hello.schema';
 import { studentsSchema } from './student.schema';
 import { classSchema } from './class.schema';
 import { userTypeDefs } from './user.schema';
 
-export const typeDefs = mergeTypeDefs([studentsSchema, classSchema, userTypeDefs]);
+export const typeDefs = mergeTypeDefs([studentsSchema, classSchema, userTypeDefs, helloStudentSchema]);
