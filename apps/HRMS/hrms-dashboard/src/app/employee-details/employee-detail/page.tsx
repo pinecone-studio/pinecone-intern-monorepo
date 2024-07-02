c84328043a626ed61aa68ee79f60b7f5aeefcd29
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-07-02 19:00:07 +0800

    fix(hrms): bug fix

--- apps/HRMS/hrms-dashboard/src/app/employee-details/employee-detail/page.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/employee-detail/page.tsx
@@ -1,4 +1,5 @@
-import { EmployeesOtherInformation } from "../_components/employee-other-information/EmployeesOtherInformation";
+import { EmployeesOtherInformation } from '../_components/employee-other-information/EmployeesOtherInformation';
+import { EmployeePersonalInfo } from './_components/EmployeePersonalInfo';
 
 import EmployeeInfoSection from './_components/EmplyeeInfoSection';
 
@@ -7,17 +8,15 @@ const Index = () => {
     <div className="flex justify-center items-center  bg-[#1C202414] w-full h-[100vh] ">
       <div className="flex gap-[26px]">
         <div className="bg-white px-[20px]  rounded-[8px] py-[20px] h-[696px] w-[358px] ">
-          <h1 data-cy="Home-Page">Hello world</h1>
+          <EmployeePersonalInfo />
         </div>
         <div className="flex flex-col gap-[24px]">
-          <div className="px-[24px] py-[24px]  bg-white rounded-[8px] w-[772px] h-[444px] "></div>
-          <div className="px-[24px] py-[24px] rounded-[8px] bg-white w-[772px] h-[228px]">
-            <EmployeesOtherInformation />
-          </div>
           <div className="px-[24px] py-[24px]  bg-white rounded-[8px] w-[772px] h-[444px] ">
             <EmployeeInfoSection />
           </div>
-          <div className="px-[24px] py-[24px] rounded-[8px] bg-white w-[772px] h-[228px]"></div>
+          <div className="px-[24px] py-[24px] rounded-[8px] bg-white w-[772px] h-[228px]">
+            <EmployeesOtherInformation />
+          </div>
         </div>
       </div>
     </div>
