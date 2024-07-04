cc3dc95cb2dc3d4db9f9ede708b9812217cfb625
temuu-ux <151482172+temuu-ux@users.noreply.github.com>
2024-07-04 21:47:04 +0800

    fix: table connect BE (#1667)
    Co-authored-by: Temuulen Batbileg <23LP8360@G304-CC0225.local>

--- apps/HRMS/hrms-dashboard/src/app/employee-details/_features/EmplyeeDetails.tsx
+++ apps/HRMS/hrms-dashboard/src/app/employee-details/_features/EmplyeeDetails.tsx
@@ -1,17 +1,21 @@
+'use client';
+
+import { Employee, useGetEmployeesQuery } from '@/generated';
 import PaginationDemo from '../_components/Pagination';
 import { TableDemo } from '../_components/TableBoard';
 import { AddEmployee } from '../_components/modal/AddEmployee';
 
 export const EmployeeDetails = () => {
+  const { data } = useGetEmployeesQuery();
+  const employees = data?.getEmployees;
+
   return (
     <div className="bg-[#F7F7F8] p-8 h-screen">
       <div className="bg-white  rounded-xl  pb-6 flex flex-col justify-between">
         <AddEmployee />
         <div className="pb-6">
-          {' '}
-          <TableDemo />
+          <TableDemo employees={employees as Employee[]} />
         </div>
-
         <PaginationDemo />
       </div>
     </div>
