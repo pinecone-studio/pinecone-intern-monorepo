471c45979c2a56fe9c853cc2051b0dee019cab1e
Shrv B <shrvb@Shrvs-Laptop.local>
2024-07-04 14:40:30 +0800

    feat(fixed front end dark): fixed front end dark

--- apps/REPORT/report-dashboard/src/app/sign-in/page.tsx
+++ apps/REPORT/report-dashboard/src/app/sign-in/page.tsx
@@ -4,11 +4,11 @@ import SignInModal from './_features/SignInModal';
 const SignIn = () => {
   return (
     <div data-testid="sign-in-page" className="w-full h-[100vh] flex">
-      <div className="w-[50%] bg-[#FFF] flex items-center justify-center">
+      <div className="w-[50%] bg-[#FFF] dark:bg-[#121316] flex items-center justify-center">
         <SignInModal />
       </div>
       <div className="w-[50%] flex h-full bg-[#121316] items-center justify-center">
-        <div className="flex">
+        <div className="flex flex-col">
           <Image src="/Academy.svg" alt="Pincone Logo" height={128} width={440} />
         </div>
       </div>
