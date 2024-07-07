1833c5a2d45e1aedac48bbc0f9c80730de80eb70
Uuree <151707633+Uurtsaikh111@users.noreply.github.com>
2024-07-07 22:28:58 +0800

    Connect Hrms login [BE] (#1685)
    * feat: connect Hrms Login [BE]
    
    * feat: little bit change
    
    * feat: connect Hrms Login [BE]

--- apps/HRMS/hrms-dashboard/src/app/layout.tsx
+++ apps/HRMS/hrms-dashboard/src/app/layout.tsx
@@ -2,21 +2,24 @@
 import { Header } from '@/components/ProjectHeader';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { FederationProvider } from '../common';
+import { AuthProvider } from '@/common/providers/AuthProvider';
 import './global.css';
 import { usePathname } from 'next/navigation';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const isSignInPage = pathname === '/sign-in';
+  const isSignInPage = pathname === '/login';
   return (
     <html lang="en">
       <body className="relative">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <FederationProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <FederationProvider>
+          <AuthProvider>
             {!isSignInPage && <Header />}
             {children}
-          </FederationProvider>
-        </ThemeProvider>
+            </AuthProvider>
+        </FederationProvider>
+      </ThemeProvider>
       </body>
     </html>
   );
