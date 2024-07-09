2f1148804cd10004dce42b915ea535aae2be0d9d
Enkhsaikhan <164152016+enkhsaikhan1@users.noreply.github.com>
2024-07-09 16:13:30 +0800

    fix(frontend): update card component

--- apps/GLMS/glms-dashboard/src/components/CourseCard.tsx
+++ apps/GLMS/glms-dashboard/src/components/CourseCard.tsx
@@ -4,14 +4,6 @@ import { Card, CardDescription, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const truncateLines: React.CSSProperties = {
-  display: '-webkit-box',
-  WebkitLineClamp: 2,
-  WebkitBoxOrient: 'vertical',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-};
-
 interface LessonCardProps {
   href: string;
   title: string | undefined;
@@ -22,17 +14,17 @@ interface LessonCardProps {
 export const CourseCard = ({ href, title, content, thumbnail }: LessonCardProps) => {
   return (
     <Link href={href}>
-      <Card data-testid="test-card" className="cursor-pointer w-[310px] h-[240px] flex flex-col justify-between rounded-[12px] bg-white shadow-none border-[#0000001A]">
-        {thumbnail ? <Image src={thumbnail} alt="Course Thumbnail" width={500} height={300} className="rounded-t-[11px] object-cover h-[116px]" quality={100} /> : <></>}
-        <div className="flex flex-col px-4 mt-1 pb-3">
+      <Card data-testid="test-card" className="flex flex-col justify-between border-[#0000001A] hover:border-gray-300 bg-white hover:bg-gray-50 rounded-[12px] w-[310px] cursor-pointer">
+        {thumbnail ? <Image src={thumbnail} alt="Course Thumbnail" width={500} height={300} className="rounded-t-[11px] h-[120px] object-cover" quality={100} /> : <></>}
+        <div className="flex flex-col gap-2 p-2">
           <CardTitle data-testid="lessonTile" className="font-semibold text-base">
             {title}
           </CardTitle>
-          <CardDescription style={truncateLines} data-testid="lessonDesc" className="mt-1 text-sm leading-5 font-inter text-[#3F4145] tracking-[-0.3px] font-normal">
+          <CardDescription data-testid="lessonDesc" className="line-clamp-3 text-[#3F4145]">
             {content}
           </CardDescription>
-          <Badge variant="secondary" data-testid="lessonCount" className="bg-[#C1E6CF] text-[#0A4E22] h-[24px] w-[81px] flex justify-center text-center font-normal text-base mt-[8px] ">
-            Lessons
+          <Badge variant="outline" data-testid="lessonCount" className="w-fit text-sm">
+           Lessons
           </Badge>
         </div>
       </Card>
