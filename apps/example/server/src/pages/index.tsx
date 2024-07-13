4280a0e3d3b7e2db2756abc6a613c757e3d629f8
Shagai <73501582+shagai-nest@users.noreply.github.com>
2024-07-13 16:12:18 +0800

    feat(nx-cloud): setup nx cloud workspace
    This commit set up Nx Cloud for your Nx workspace enabling distributed caching
    and GitHub integration for fast CI and improved Developer Experience.
    
    You can access your Nx Cloud workspace by going to
    https://cloud.nx.app/orgs/66876b0ffbfcd0e4a4eff558/workspaces/669236d4de48502a1d6e4b88
    
    **Note:** This commit attempts to maintain formatting of the nx.json, however you may need to correct formatting by running an nx format command and committing the changes.

--- apps/example/server/src/pages/index.tsx
+++ apps/example/server/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Challenges Service </div>
+      <div>Welcome to Challenges Service</div>
     </>
   );
 };
