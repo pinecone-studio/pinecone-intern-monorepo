125f591a84116cff8149d6a98c9c39b7de8c803a
Zaya <zaya@Zayas-MacBook-Air.local>
2024-07-15 19:18:03 +0800

    fix(cms): homePage cypress test

--- apps/CMS/cms-dashboard/cypress/e2e/dashboard/index.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Dashboard Page', () => {
-  beforeEach(() => {
-    cy.visit('/dashboard');
-  });
-
-  it('Should render the TableContent component', () => {
-    cy.get('[data-cy="contentList"]').should('exist');
-  });
-});
--- apps/CMS/cms-dashboard/cypress/e2e/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/index.cy.ts
@@ -3,13 +3,7 @@ describe('Home Page', () => {
     cy.visit('/');
   });
 
-  it('should display the correct heading', () => {
-    cy.contains('h1', 'hello from Home dashboard').should('exist');
+  it('Should render the TableContent component', () => {
+    cy.get('[data-cy="contentList"]').should('exist');
   });
-
-  // it('should display the environment variable', () => {
-  //   // You need to set the environment variable when running Cypress tests
-  //   const env = Cypress.env('ENVIRONMENT') || 'development';
-  //   cy.contains('h1', `This is the environment ${env}`).should('exist');
-  // });
 });
