5141aaf9cd2188fb21accd05daa406a64b993bfc
Zaya <zaya@Zayas-MacBook-Air.local>
2024-07-16 09:45:49 +0800

    fix(cms): remove comment code

--- apps/CMS/cms-dashboard/src/app/page.tsx
+++ apps/CMS/cms-dashboard/src/app/page.tsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div className="bg-[#F7F7F8] h-[93.5vh]">
       <TableContent></TableContent>
-      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
     </div>
   );
 };
