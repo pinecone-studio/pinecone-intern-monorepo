ce4ae5efa98d67e0628575574be05838a0bafe00
Zaya <zaya@Zayas-MacBook-Air.local>
2024-07-16 09:27:59 +0800

    style(cms): remove commented code

--- apps/CMS/cms-dashboard/src/app/page.tsx
+++ apps/CMS/cms-dashboard/src/app/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="bg-[#F7F7F8] h-[93.5vh]">
       <TableContent></TableContent>
-      {/* <h1>This is the environment {process.env.ENVIRONMENT}</h1> */}
+      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
     </div>
   );
 };
