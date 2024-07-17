4dee6f4fb85c5957440e90b233044612f965038b
Huslen1027 <hvslenhvslen566@gmail.com>
2024-07-17 12:17:39 +0800

    fix: bug

--- apps/GLMS/glms-dashboard/cypress/e2e/loginRole/index.cy.ts
+++ apps/GLMS/glms-dashboard/cypress/e2e/loginRole/index.cy.ts
@@ -8,4 +8,12 @@ describe('LoginRole Component', () => {
 
     cy.contains('.header', 'Admin role uptade').should('exist');
   });
+
+  it('checks the layout of the RoleTableFeature', () => {
+    cy.visit('/login-role');
+
+    cy.get('.header').should('have.class', 'w-[1154px] bg-[white] rounded-xl h-[72px] flex items-center justify-start p-[20px]');
+
+    cy.get('.flex.flex-col.w-full.h-full.container.mx-auto.items-center.py-8.gap-7').find('.w-[1154px].h-[216px].bg-white.rounded-xl.px-[20px].py-[20px]').should('exist');
+  });
 });
