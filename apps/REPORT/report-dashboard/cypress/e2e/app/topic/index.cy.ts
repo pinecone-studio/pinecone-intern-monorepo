aa33a330f10e8bbdd847300fb8749363dd2a45fa
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-07-18 17:16:41 +0800

    feat(fixed everything): fixed to earlier commit

--- apps/REPORT/report-dashboard/cypress/e2e/app/topic/index.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('home page', () => {
-  beforeEach(() => cy.visit('/topic'));
-
-  it('Should display welcome message', () => {
-    cy.get('h1').contains('hello from REPORT dashboard');
-  });
-});
