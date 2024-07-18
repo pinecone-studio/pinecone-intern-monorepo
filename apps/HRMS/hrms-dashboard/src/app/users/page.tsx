556b0deb3d76f465893be5621d88928d6c4c0538
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-07-18 11:18:13 +0800

    feat: [FE] hrms-role-updete

---
+++ apps/HRMS/hrms-dashboard/src/app/users/page.tsx
@@ -0,0 +1,10 @@
+import RoleTableFeature from "./_feature/UsersTableFeature";
+const Users = () => {
+    return (
+      <div data-testid="users-page" className="w-full ">
+        <RoleTableFeature/>
+      </div>
+    );
+  };
+  
+  export default Users;
