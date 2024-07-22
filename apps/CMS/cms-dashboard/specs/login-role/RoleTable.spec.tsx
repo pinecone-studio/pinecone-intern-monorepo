97792ed8ced642e6317f6ab7c12165b11e966296
Huslen Batzorig <23LP2437@G304-CC0216.local>
2024-07-22 13:33:19 +0800

    fix: bug

--- apps/CMS/cms-dashboard/specs/login-role/RoleTable.spec.tsx
+++ apps/CMS/cms-dashboard/specs/login-role/RoleTable.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import RoleTable from '../../src/app/login-role/components/RoleTable';
+import RoleTable from '@/app/login-role/components/RoleTable';
 
 describe('RoleTable Component', () => {
   test('renders table headers correctly', () => {
