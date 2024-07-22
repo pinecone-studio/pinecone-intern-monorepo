df086e5ba39d10f7cbd47261a8bf581724fdb19c
Zaya <zaya@Zayas-MacBook-Air.local>
2024-07-23 00:52:28 +0800

    feat(cms): footerButtons

--- apps/CMS/cms-dashboard/src/app/page.tsx
+++ apps/CMS/cms-dashboard/src/app/page.tsx
@@ -1,11 +1,28 @@
 'use client';
-import { TableContent } from './dashboard/_components';
+import { TableContent } from './dashboard/_components/TableContent';
+import { FooterButtons } from './dashboard/_features';
 
 const Home = () => {
+  // const [userRole, setUserRole] = useState<string>('user');
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem('token');
+  //   if (token) {
+  //     const decodedToken = jwt.decode(token) as JwtPayloadType;
+  //     setUserRole(decodedToken?.role ?? 'user');
+  //   }
+  // }, []);
+
+  // style={{ display: userRole === 'user' ? 'none' : 'flex' }}
+
   return (
-    <div className="bg-[#F7F7F8] h-[93.5vh]">
-      <TableContent></TableContent>
+    <div>
+      <TableContent />
+      <div className="flex justify-center py-6 sticky bottom-0">
+        <FooterButtons />
+      </div>
     </div>
   );
 };
+
 export default Home;
