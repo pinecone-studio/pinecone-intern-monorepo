4cfab7c396202a3259f0937b362a0a44002f287d
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-07-22 12:33:11 +0800

    style: add modal

---
+++ apps/HRMS/hrms-dashboard/specs/users/RoleModal.spec.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import RoleModal from '../../src/app/users/components/RoleModal';
+
+describe('RoleModal Component', () => {
+  test('renders the RoleModal component', () => {
+    render(<RoleModal />);
+    const editButton = screen.getByRole('button');
+    expect(editButton).toBeInTheDocument();
+  });
+
+  test('opens the dialog on trigger button click', () => {
+    render(<RoleModal />);
+    const editButton = screen.getByRole('button');
+    fireEvent.click(editButton);
+
+    const dialogTitle = screen.getByText('Edit roles');
+    expect(dialogTitle).toBeInTheDocument();
+
+    const dialogDescription = screen.getByText('ID:901923101');
+    expect(dialogDescription).toBeInTheDocument();
+  });
+
+  test('contains role selection and save button', () => {
+    render(<RoleModal />);
+    const editButton = screen.getByRole('button');
+    fireEvent.click(editButton);
+
+    const selectTrigger = screen.getByText('Select roles');
+    expect(selectTrigger).toBeInTheDocument();
+
+    const saveButton = screen.getByText('Save changes');
+    expect(saveButton).toBeInTheDocument();
+  });
+
+  test('selects a role from the dropdown', async () => {
+    render(<RoleModal />);
+    const editButton = screen.getByRole('button');
+    fireEvent.click(editButton);
+
+    const selectTrigger = screen.getByText('Select roles');
+    fireEvent.mouseDown(selectTrigger);
+
+    await waitFor(() => {
+      expect(screen.getByText('Admin')).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('Admin'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Admin')).toBeInTheDocument();
+    });
+  });
+});
--- apps/HRMS/hrms-dashboard/src/app/users/components/RoleTable.tsx
+++ apps/HRMS/hrms-dashboard/src/app/users/components/RoleTable.tsx
@@ -2,9 +2,37 @@
 import React from 'react';
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { HrmsUser } from '@/generated';
+import { useDeletedHrmsUserMutation } from '@/generated';
+import RoleModal from './RoleModal';
+import DeleteIcon from '@/assets/icons/DeleteIcon';
+import { Button } from '@/components/ui/button';
+import { ApolloError } from '@apollo/client';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
 
 const RoleTable = ({usersData}:{usersData: HrmsUser[]}) => {
+  const  [deletedId] = useDeletedHrmsUserMutation();
+  
+  const router = useRouter();
+  const handleDelete = async (_id: string ) => {
+    try {
+      const { data: deletedData } = await deletedId({
+        variables: {
+          _id,
+        },
+      });
+      router.push('/');
+    } catch (error) {
+        if (error instanceof ApolloError) {
+          toast.error('Sign in error has occurred', {
+            position: 'top-center',
+            autoClose: 3000,
+            hideProgressBar: true,
+          });
+        }
+      }
+    };
 return (
     <Table data-cy="tableBody"  className="px-6 overflow-hidden">
       <TableCaption></TableCaption>
@@ -22,6 +50,12 @@ return (
           <TableHead data-cy="tableHead-4" className="w-[170px] rounded-tr-xl text-black">
             Email
           </TableHead>
+          <TableHead data-cy="tableHead-4" className="w-[5px] rounded-tr-xl text-black">
+          
+          </TableHead>
+          <TableHead data-cy="tableHead-4" className="w-[5px] rounded-tr-xl text-black">
+         
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -31,6 +65,15 @@ return (
       <TableCell >{item?._id}</TableCell>
       <TableCell >{item?.role}</TableCell>
       <TableCell >{item?.email}</TableCell>
+      <TableCell>
+            <RoleModal item={item}/>
+      </TableCell>
+      <TableCell>
+            <Button aria-label="delete"  onClick={() => handleDelete(item?._id as string)} 
+              className="border-none" variant={'outline'}>
+              <DeleteIcon />
+            </Button>
+      </TableCell>
     </TableRow>
 ))}
       </TableBody>
