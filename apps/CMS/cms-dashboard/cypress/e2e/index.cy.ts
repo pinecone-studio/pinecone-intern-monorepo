7fabee29b1d2610572ec0e137dc6e46b8c10f748
Zaya <zaya@Zayas-MacBook-Air.local>
2024-07-23 10:37:43 +0800

    test(cms): page test edit

--- apps/CMS/cms-dashboard/cypress/e2e/index.cy.ts
+++ apps/CMS/cms-dashboard/cypress/e2e/index.cy.ts
@@ -2,7 +2,12 @@ describe('Home Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   it('Should render the TableContent component', () => {
     cy.get('[data-cy="contentList"]').should('exist');
+  });
+
+  it('Should show FooterButtons for admin role', () => {
+    cy.get('[data-cy="admin-navigate-links-feature-cy-id"]').should('be.visible');
   });
 });
--- apps/CMS/cms-dashboard/src/app/page.tsx
+++ apps/CMS/cms-dashboard/src/app/page.tsx
@@ -3,18 +3,6 @@ import { TableContent } from './dashboard/_components/TableContent';
 import { FooterButtons } from './dashboard/_features';
 
 const Home = () => {
-  // const [userRole, setUserRole] = useState<string>('user');
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('token');
-  //   if (token) {
-  //     const decodedToken = jwt.decode(token) as JwtPayloadType;
-  //     setUserRole(decodedToken?.role ?? 'user');
-  //   }
-  // }, []);
-
-  // style={{ display: userRole === 'user' ? 'none' : 'flex' }}
-
   return (
     <div>
       <TableContent />
@@ -26,3 +14,4 @@ const Home = () => {
 };
 
 export default Home;
+
