e384973602265f291b0ae35508d05fd38430a7ea
Haliukaaa <haliuka.aqua@gmail.com>
2024-07-29 10:27:20 +0800

    feat(mainpage): added banner and article card to the main page

--- apps/CMS/cms-dashboard/src/app/articles/page.tsx
+++ apps/CMS/cms-dashboard/src/app/articles/page.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import { FooterButtons } from "../dashboard/_features";
+import Banner from '../articles/_components/Banner';
+import { useGetNewestArticleQuery, useGetCategoriesQuery } from '../../generated';
+import GroupArticles from '../articles/_features/GroupArticle';
+import { FooterButtons } from '../dashboard/_features';
 
-export default async function ArticlesPage() {
+const Home = () => {
+  const { data, loading } = useGetNewestArticleQuery();
+  const { data: categories, loading: categoriesLoading } = useGetCategoriesQuery();
+
   return (
     <div>
-      <h1 data-cy="Article-Page">hello from ArticlesPage</h1>
-      <h1>This is the environment {process.env.ENVIRONMENT}</h1>
-      <div className="flex justify-center m-auto">
-        <FooterButtons />
+      <div className="flex flex-col w-full gap-12 bg-[#F7F7F8] items-center pb-6" suppressHydrationWarning={true}>
+          <Banner
+            articlesTitle={data?.getNewestArticle.title}
+            cover={data?.getNewestArticle.coverPhoto}
+            date={data?.getNewestArticle.publishedAt}
+            categories={data?.getNewestArticle.category.name}
+            id={data?.getNewestArticle.id}
+          />
+        {categoriesLoading ? null : (
+          <div className="flex flex-col md:px-24 sm:px-[48px] gap-12 xl:w-[65%] lg:w-[70%] md:w-[85%] sm:w-[100vw] pb-28">
+            {categories?.getCategories.map((item) => (
+              <GroupArticles key={item.id} title={item.name} categoryId={item.id} />
+            ))}
+          </div>
+        )}
+        <div className="flex justify-center m-auto">
+          <FooterButtons />
+        </div>
       </div>
     </div>
   );
-}
+};
+export default Home;
