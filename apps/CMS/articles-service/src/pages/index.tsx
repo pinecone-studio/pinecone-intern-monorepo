fa60c37740e83f923575ca3ea6157615d1e1370f
Maral Jargalsaikhan <maralisgoood@Marals-MacBook-Pro.local>
2024-10-02 12:04:46 +0800

    testing action

--- apps/CMS/articles-service/src/pages/index.tsx
+++ apps/CMS/articles-service/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Articles Service hello</div>
+      <div>Welcome to Articles Service</div>
     </>
   );
 };
