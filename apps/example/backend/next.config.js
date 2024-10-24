2c62d3d6558769411d185327aaca231ee350bdf4
Sumiyabazar <86002540+sumiyab@users.noreply.github.com>
2024-10-24 20:18:49 +0800

    feat: add deployment on example-backend (#1849)

--- apps/example/backend/next.config.js
+++ apps/example/backend/next.config.js
@@ -8,6 +8,19 @@ const { composePlugins, withNx } = require('@nx/next');
  **/
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
   env: {
     MONGO_URI: process.env.MONGO_URI,
   },
