1c9ffbe62016438fc4d6b4a26631dbf9791ebe6a
Ugtakh <ugtakhbayars@gmail.com>
2024-10-29 19:56:17 +0800

    feat(example-frontend): change about page context

--- apps/example/frontend/src/app/about/page.tsx
+++ apps/example/frontend/src/app/about/page.tsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
       <h1 className="text-4xl font-bold text-center">Бидний тухай</h1>
       <div className="flex flex-col items-center w-1/4 m-auto mt-60">
         <h1 className="p-4 text-5xl font-extrabold">LEAP Хөтөлбөр</h1>
-        <p className="text-xl text-center">Та LEAP хөтөлбөрийн хүрээнд хоёр мэргэжлээс сонгон суралцах боломжтой.</p>
+        <p className="text-xl text-center">Та LEAP хөтөлбөрийн хүрээнд хоёр мэргэжлээс сонгон суралцах боломжтой. </p>
       </div>
     </div>
   );
