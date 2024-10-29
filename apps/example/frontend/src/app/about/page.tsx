9e5d4f5c7c0fb3d0e61d017a9ba859f3b2932b68
Ugtakh <ugtakhbayars@gmail.com>
2024-10-29 19:21:19 +0800

    feat(example-frontend): add about page

---
+++ apps/example/frontend/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="p-4 m-auto">
+      <h1 className="text-4xl font-bold text-center">Бидний тухай</h1>
+      <div className="flex flex-col items-center w-1/4 m-auto mt-60">
+        <h1 className="p-4 text-5xl font-extrabold">LEAP Хөтөлбөр</h1>
+        <p className="text-xl text-center">Та LEAP хөтөлбөрийн хүрээнд хоёр мэргэжлээс сонгон суралцах боломжтой.</p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
