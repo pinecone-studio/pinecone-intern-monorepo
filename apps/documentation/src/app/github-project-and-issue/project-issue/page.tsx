8ccf8196f3ae83981840d1ac39857e81231629bd
Sumiyabazar <86002540+sumiyab@users.noreply.github.com>
2024-10-30 17:14:06 +0800

    feat: add videos on documentation (#1868)

--- apps/documentation/src/app/github-project-and-issue/project-issue/page.tsx
+++ apps/documentation/src/app/github-project-and-issue/project-issue/page.tsx
@@ -8,18 +8,19 @@ const Page = () => {
   return (
     <Stack>
       <Typography variant="h4" fontWeight="bold">
-        Create Pull Request and Add Label
+        Project Issue
       </Typography>
+
       <Stack my={2}>
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/3uhHFhr_7DE?si=3YCBvjankM77ihbE"
+          src="https://www.youtube.com/embed/nnpNw0OBjZ0?si=9qD-1UDK-drNk_E7"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
-        />
+        ></iframe>
       </Stack>
 
       <NextPageButton label="Pull Request Merge" path="/pull-request/pull-request-merge" />
