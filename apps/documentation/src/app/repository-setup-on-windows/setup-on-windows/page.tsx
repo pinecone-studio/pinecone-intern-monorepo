44d7f2fcbee50c7fb88d34204818ef52d02d8f22
Sumiyabazar <sumiyaclub@gmail.com>
2024-11-04 19:17:37 +0800

    feat: add setup guide

--- apps/documentation/src/app/repository-setup-on-windows/setup-on-windows/page.tsx
+++ apps/documentation/src/app/repository-setup-on-windows/setup-on-windows/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { Stack, Typography } from '@mui/material';
-import Link from 'next/link';
+import Image from 'next/image';
 
 const Page = () => {
   return (
@@ -13,11 +13,53 @@ const Page = () => {
 
       <Stack my={2}>
         <Typography variant="h6" fontWeight="bold">
-          <Link href={' https://github.com/Vets-Who-Code/windows-dev-guide'} target="_blank">
-            https://github.com/Vets-Who-Code/windows-dev-guide
-          </Link>
+          Step-1: Windows-ийн start aa нээгээд turn Windows features on or off гэж хайгаад цонхоо нээнэ. 
         </Typography>
+        <Stack my={2}>
+        <Image src="/images/step-1.png" width={900} height={400} alt='step-1'/>
+        </Stack>
       </Stack>
+
+      <Stack my={2}>
+        <Typography variant="h6" fontWeight="bold">
+          Step-2:  Тухайн жагсаалтан дотроос Windows Subsystems for Linux ийг сонгоно.
+        </Typography>
+        <Stack my={2}>
+        <Image src="/images/step-2.png" width={900} height={400} alt='step-1'/>
+        </Stack>
+      </Stack>
+
+      <Stack my={2}>
+        <Typography variant="h6" fontWeight="bold">
+          Step-3: Command prompt оо run as administartor - оор нээнэ.
+        </Typography>
+        <Stack my={2}>
+        <Image src="/images/step-3.png" width={900} height={400} alt='step-1'/>
+        </Stack>
+      </Stack>
+
+      <Stack my={2}>
+        <Typography variant="h6" fontWeight="bold">
+          Step-4: wsl --install command -ийг бичин ажиллууллна.
+          
+        </Typography>
+        <Typography>Энэ нь windows дээр linux subsystems ээр суулгаж байгаа юм.</Typography>
+        <Stack my={2}>
+        <Image src="/images/step-5.png" width={900} height={400} alt='step-1'/>
+        </Stack>
+      </Stack>
+
+      <Stack my={2}>
+        <Typography variant="h6" fontWeight="bold">
+          Step-5: wsl --install command -ийг бичин ажиллууллна.
+          
+        </Typography>
+        <Typography>Энэ нь windows дээр linux subsystems ээр суулгаж байгаа юм.</Typography>
+        <Stack my={2}>
+        <Image src="/images/step-6.png" width={900} height={400} alt='step-1'/>
+        </Stack>
+      </Stack>
+
     </Stack>
   );
 };
