e5cc0ad0936e1bd1b2cd0fd2fa2454820036b7f2
Ugtakh <ugtakhbayars@gmail.com>
2024-11-07 17:52:55 +0800

    feat(server): change context in home page

--- apps/example/server/src/pages/index.tsx
+++ apps/example/server/src/pages/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div>Welcome to Challenges Service </div>
+      <div>
+        <h1 style={{ textAlign: 'center' }}>Welcome to Challenges Service</h1>
+      </div>
     </>
   );
 };
