41c39a09efef9a5c7b4d1c301a355a18f13a69de
sumiyab <sumiyabazaramarjargal@gmail.com>
2024-11-08 12:45:47 +0800

    feat: add video

--- apps/documentation/src/app/github-repository-setup/add-secrets/page.tsx
+++ apps/documentation/src/app/github-repository-setup/add-secrets/page.tsx
@@ -7,24 +7,13 @@ const Page = () => {
   return (
     <Stack>
       <Typography variant="h4" fontWeight="bold">
-        Yarn Commit - Guide of git commit
+        Add Secrets
       </Typography>
       <Stack my={2}>
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/WdUjKuLh5zw?si=Vy9BFLKH7TIWr4mD"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      </Stack>
-      <Stack my={2}>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/FL5Dm5WIDcM?si=LqFfMz7kaVcVKrHK"
+          src="https://www.youtube.com/embed/akVmZaaqyT4?si=GhJ9jp3iz5YZ07Hv"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
