d42e94b0f73d3f7ee26bd083977d3472350148c5
LPD243578 LPD243578 <LPD243578@admins-iMac.local>
2024-11-11 16:49:15 +0800

    feat(instagram-frontend): project-init

---
+++ apps/L1AB/Instagram/instagram-frontend/codegen.ts
@@ -0,0 +1,27 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: process.env.BACKEND_URI ?? 'http://localhost:4200/api/graphql',
+  documents: ['apps/L1AB/Instagram/instagram-frontend/src/**/*.graphql'],
+  generates: {
+    'apps/L1AB/Instagram/instagram-frontend/src/generated/index.ts': {
+      config: {
+        reactApolloVersion: 3,
+        withHOC: true,
+        withHooks: true,
+      },
+      plugins: [
+        {
+          add: {
+            content: '// @ts-nocheck',
+          },
+        },
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+    },
+  },
+};
+export default config;
