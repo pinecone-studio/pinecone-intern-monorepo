8cfd1ac557f03048ce3099003b049aba96c1747b
24LP8045 24LP8045 <24LP8045@pinecones-iMac.local>
2024-11-12 13:45:40 +0800

    feat(concert-ticket-booking): add-get-venues,add-create-venue,tests

--- apps/L1AB/concert-ticket-booking/backend/src/schemas/index.ts
+++ apps/L1AB/concert-ticket-booking/backend/src/schemas/index.ts
@@ -1,8 +1,8 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
-import { typeDefs as UserTypeDefs } from './common.schema';
-import { typeDefs as BookingTypeDefs } from './common.schema';
-import { typeDefs as EventTypeDefs } from './common.schema';
-import { typeDefs as VenueTypeDefs } from './common.schema';
+import { typeDefs as UserTypeDefs } from './user.schema';
+import { typeDefs as BookingTypeDefs } from './booking.schema';
+import { typeDefs as EventTypeDefs } from './event.schema';
+import { typeDefs as VenueTypeDefs } from './venue.schema';
 
 export const typeDefs = mergeTypeDefs([CommonTypeDefs, UserTypeDefs, BookingTypeDefs, EventTypeDefs, VenueTypeDefs]);
