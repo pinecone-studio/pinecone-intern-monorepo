75d50f29f7788173b423a8a982ebe27cb249753a
LPC241896 LPC241896 <LPC241896@admins-iMac.local>
2024-11-11 23:19:08 -0800

    feat: dutuu

--- apps/L1AB/hotel-booking/backend/src/schemas/hotel-ameneties.schema.ts
+++ apps/L1AB/hotel-booking/backend/src/schemas/hotel-ameneties.schema.ts
@@ -1,6 +1,8 @@
 import gql from 'graphql-tag';
 
 export const typeDefs = gql`
+  scalar Date
+
   type Query {
     getAllHotelsAmenities: [HotelAmenities!]!
     getHotelAmenityById(id: ID!): HotelAmenities!
@@ -8,25 +10,11 @@ export const typeDefs = gql`
 
   type HotelAmenities {
     _id: ID!
-    hotel_id: ID!
-    Amenities: [ID!]!
     createdAt: Date!
     updatedAt: Date!
   }
 
-  input CreateHotelAmenitiesInput {
-    hotel_id: ID!
-    amenities: [ID!]!
-  }
-
-  input UpdateHotelAmenitiesInput {
-    hotel_id: ID!
-    amenities: [ID!]!
-  }
-
   type Mutation {
-    createHotelAmenities(input: CreateHotelAmenitiesInput!): HotelAmenities!
-    updateHotelAmenities(id: ID!, input: UpdateHotelAmenitiesInput!): HotelAmenities!
     deleteHotelAmenities(id: ID!): Response!
   }
 `;
