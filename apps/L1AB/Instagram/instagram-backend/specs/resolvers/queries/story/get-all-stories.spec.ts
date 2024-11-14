4cdc1a270a0486bd214fbc092f51b5d71364c854
LPC243012 LPC243012 <LPC243012@admins-iMac.local>
2024-11-14 02:41:15 -0800

    feat: uu

--- apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/story/get-all-stories.spec.ts
+++ apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/story/get-all-stories.spec.ts
@@ -1,16 +1,22 @@
-import { getAllStories } from '../../../../src/resolvers/queries/story/get-all-stories';
+import { getAllStories } from '../../../../src/resolvers/queries';
 import { GraphQLResolveInfo } from 'graphql';
 
 jest.mock('../../../../src/models', () => ({
   storyModel: {
-    find: jest.fn().mockResolvedValueOnce(['test']),
+    find: jest.fn().mockReturnValue({
+      populate: jest.fn().mockResolvedValue([
+        {
+          toObject: () => ['a'],
+        },
+      ]),
+    }),
   },
 }));
 
-describe('get all stories', () => {
+describe('getAllStories resolver', () => {
   it('should get all stories', async () => {
     const res = await getAllStories!({}, {}, {}, {} as GraphQLResolveInfo);
 
-    expect(res).toEqual(['test']);
+    expect(res).toEqual(['a']);
   });
 });
