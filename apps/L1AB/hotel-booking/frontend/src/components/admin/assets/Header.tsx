149be075528faa728319ac0c814f9322ae19c781
LPC241896 LPC241896 <LPC241896@admins-iMac.local>
2024-11-13 21:26:28 -0800

    feat: header component and  container

--- apps/L1AB/hotel-booking/frontend/src/components/admin/assets/Header.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/admin/assets/Header.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import { usePathname } from 'next/navigation';
+import { FiColumns } from 'react-icons/fi';
 
 export const Header = () => {
-    return (
-        <div className='min-h-16 border-y'>Header</div>
-    )
-}
+  const pathname = usePathname();
+  const paths = ['Hotels', 'Hotel Details', 'Room Details', 'Guest Info'];
+  const pathnames = (pathname ?? '').split('/').filter((path) => path);
+  return (
+    <div data-testid="header" className="min-h-16 border-y flex items-center">
+      <div className="mr-4 ml-[22px]" data-testid="header-icon">
+        <FiColumns className='w-4 h-4' />
+      </div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          {pathnames.map((_, index) => {
+            const href = '/' + pathnames.slice(0, index + 1).join('/');
+            return (
+              <React.Fragment key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href={href}>{paths[index]}</BreadcrumbLink>
+                </BreadcrumbItem>
+                {index < pathnames.length - 1 && <BreadcrumbSeparator />}
+              </React.Fragment>
+            );
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
