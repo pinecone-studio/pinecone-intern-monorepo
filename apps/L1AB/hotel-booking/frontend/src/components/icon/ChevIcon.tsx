354db33104d0e876e1d1313eea066b2ad9623e4d
24LP4208 24LP4208 <24LP4208@pinecones-iMac.local>
2024-11-15 10:15:59 +0800

    fix(fix): fix

--- apps/L1AB/hotel-booking/frontend/src/components/icon/ChevIcon.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/icon/ChevIcon.tsx
@@ -1,7 +1,7 @@
 export const ChevIcon = () => {
   return (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4.6665 10.0013L7.99984 13.3346L11.3332 10.0013M4.6665 6.0013L7.99984 2.66797L11.3332 6.0013" stroke="#334155" strokeLinecap="round" stroke-linejoin="round" />
+      <path d="M4.6665 10.0013L7.99984 13.3346L11.3332 10.0013M4.6665 6.0013L7.99984 2.66797L11.3332 6.0013" stroke="#334155" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 };
