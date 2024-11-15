6842fe5f531cf45fde08e96ef3309419fef3ada5
24LP4153 24LP4153 <24LP4153@pinecones-iMac.local>
2024-11-15 09:35:49 +0800

    fix(instagram instagram-frontend): svg fixed

---
+++ apps/L1AB/Instagram/instagram-frontend/specs/componenet/InstagramLogo.spec.tsx
@@ -0,0 +1,9 @@
+import InstagramLogo from "@/components/assets/icons/InstagramLogo"
+import { render } from "@testing-library/react"
+
+
+describe("InstagramLogo" , () => {
+    it("should render components" , () => {
+        render(<InstagramLogo/>)
+    })
+})
