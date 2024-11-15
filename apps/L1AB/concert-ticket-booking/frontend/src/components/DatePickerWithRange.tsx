e050bb64f93d75dc59c7970d553a5c15f1416cf4
24LP5175 24LP5175 <24LP5175@pinecones-iMac.local>
2024-11-15 14:55:10 +0800

    fix(concert-ticket-booking-frontend): fix lint error

--- apps/L1AB/concert-ticket-booking/frontend/src/components/DatePickerWithRange.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/DatePickerWithRange.tsx
@@ -1,36 +1,25 @@
 'use client';
 
 import * as React from 'react';
-import { addDays, format } from 'date-fns';
+import { addDays } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
 import { DateRange } from 'react-day-picker';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { cn } from '../../../../../../libs/shadcn/src/lib/utils';
 
-export function DatePickerWithRange({ className }: React.HTMLAttributes<HTMLDivElement>) {
+export const DatePickerWithRange = () => {
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: new Date(2024, 10, 20),
     to: addDays(new Date(2024, 11, 20), 20),
   });
 
   return (
-    <div className={cn('grid gap-2', className)}>
+    <div className="grid gap-2">
       <Popover>
         <PopoverTrigger asChild>
-          <Button id="date" variant={'outline'} className={cn('w-[300px] justify-start text-left font-normal')}>
+          <Button id="date" variant={'outline'} className="w-[300px] justify-start text-left font-normal">
             <CalendarIcon />
-            {/* {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, 'LLL dd, y')} - {format(date.to, 'LLL dd, y')}
-                </>
-              ) : (
-                format(date.from, 'LLL dd, y')
-              )
-            ) : (
-            )} */}
             <span>Өдөр сонгох</span>
           </Button>
         </PopoverTrigger>
@@ -40,4 +29,4 @@ export function DatePickerWithRange({ className }: React.HTMLAttributes<HTMLDivE
       </Popover>
     </div>
   );
-}
+};
