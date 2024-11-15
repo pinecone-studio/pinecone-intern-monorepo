3807043cf844be3d09c7fa503a8480b736bd4dcd
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-11-14 22:31:46 -0800

    fix(hotel-booking frontend): hotelDetails fixed

--- apps/L1AB/hotel-booking/frontend/src/components/admin/HotelDetails.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/admin/HotelDetails.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { LocationDialog } from './dialogs';
+import { DetailsCard, DetailsContainer, DetailsLeft, DetailsRight } from './assets';
 import { AmenityDialog } from './dialogs/AmenityDialog';
-
+import { LocationDialog } from './dialogs';
 export const HotelDetails = () => {
-  return (
-    <div>
-      <div>HotelDetails</div>
-      <div>
-        <AmenityDialog />
-      </div>
-      <div>
-        <LocationDialog />
-      </div>
-    </div>
-  );
-};
+    return (
+      <DetailsContainer name={'Flower Hotel Ulaanbaatar'}>
+        <DetailsLeft>
+          <DetailsCard>
+            <div>Upcoming Bookings</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Room Types</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>General Info</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Amenities</div>
+            <AmenityDialog />
+          </DetailsCard>
+          <DetailsCard>
+            <div>About this property</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Policies</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Policies</div>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Frequently asked questions</div>
+          </DetailsCard>
+        </DetailsLeft>
+        <DetailsRight>
+          <DetailsCard>
+            <div>Location</div>
+            <LocationDialog/>
+          </DetailsCard>
+          <DetailsCard>
+            <div>Images</div>
+          </DetailsCard>
+        </DetailsRight>
+      </DetailsContainer>
+      
+    );
+  };
