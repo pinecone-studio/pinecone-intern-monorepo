3fecd9fa1b37a3b7bd7a5360618b3482d825eb53
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-11-15 04:49:19 -0800

    feat(hotel booking frontend): generalInfoDialog nes zasah & Dialog folder ruu zuuv

--- apps/L1AB/hotel-booking/frontend/specs/components/admin/assets/GeneralInfoDialog.spec.tsx
+++ apps/L1AB/hotel-booking/frontend/specs/components/admin/assets/GeneralInfoDialog.spec.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { GeneralInfoDialog } from '../../../../src/components/admin/dialogs/GeneralInfoDialog';
 
-describe('Admin GeneralInfoDialog', () => {
+describe('Admin General Info Dialog', () => {
   it('should render the GeneralInfoDialog', () => {
     render(<GeneralInfoDialog />);
   });
