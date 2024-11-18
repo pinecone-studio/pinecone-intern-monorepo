cdabcaccd047fd3ec3de98cd7df3537d7e0fcbae
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-11-18 04:19:14 -0800

    feat(hotel-booking frontend main): main UserHeader & sidebar

--- apps/L1AB/hotel-booking/frontend/specs/components/main/UserHeader.spec.tsx
+++ apps/L1AB/hotel-booking/frontend/specs/components/main/UserHeader.spec.tsx
@@ -3,8 +3,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 
 describe('Main User Header', () => {
-    it('Should render the main user header', () => {
-        render(<UserHeader/>);
-    });
+  it('Should render the main user header', () => {
+    render(<UserHeader />);
+  });
 });
-
