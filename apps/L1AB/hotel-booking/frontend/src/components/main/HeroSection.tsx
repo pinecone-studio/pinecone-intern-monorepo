5201487d3b64ff13632c4cf5feccb629da0c2d9a
24LP6028 24LP6028 <24LP6028@admins-iMac.local>
2024-11-18 03:05:30 -0800

    feat(hotel-booking frontend): herosection

--- apps/L1AB/hotel-booking/frontend/src/components/main/HeroSection.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/main/HeroSection.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import { Container } from './assets';
 
 export const HeroSection = () => {
-  return <Container backgroundColor="bg-white">HeroSection</Container>;
+  return <div className='w-full bg-[#013b94]'>
+  <Container backgroundColor="bg-backblue">
+    <div className='flex flex-col justify-normal items-center p-10 bg-[#013b94] gap-2'>
+      <h1 className='text-4xl font-bold text-white'>Find the Best Hotel for Your Stay</h1>
+      <p className='text-base text-white font-normal text-center'>Book from a wide selection of hotels for your next trip.</p>
+    </div>
+  </Container>;
+  </div>
 };
