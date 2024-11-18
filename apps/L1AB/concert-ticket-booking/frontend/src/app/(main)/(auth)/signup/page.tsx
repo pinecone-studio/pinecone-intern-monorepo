f3d3c1bd49fbf34f312921ac0c68947a5d9cc728
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-11-18 10:44:36 +0800

    fix(concert-ticket-booking frontend): fixed frontend

---
+++ apps/L1AB/concert-ticket-booking/frontend/src/app/(main)/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@/components';
+
+const Page = () => {
+  return (
+    <Container>
+      <div className="text-white">Register please </div>;
+    </Container>
+  );
+};
+
+export default Page;
