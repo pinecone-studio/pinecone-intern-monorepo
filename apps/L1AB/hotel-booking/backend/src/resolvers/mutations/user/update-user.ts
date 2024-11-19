e95cc0c63e9ee1646c3c95038f211639c5187a75
7sogoo <gtsog0915l@gmail.com>
2024-11-19 14:30:58 +0800

    fix(hotel-booking-backend): 'fixed name'

--- apps/L1AB/hotel-booking/backend/src/resolvers/mutations/user/update-user.ts
+++ apps/L1AB/hotel-booking/backend/src/resolvers/mutations/user/update-user.ts
@@ -3,8 +3,8 @@ import { userModel } from '../../../models';
 
 export const updateUser: MutationResolvers['updateUser'] = async (_: unknown, { input }) => {
   try {
-    const updateduser = await userModel.findByIdAndUpdate(input._id, input, { new: true });
-    return updateduser;
+    const updatedUser = await userModel.findByIdAndUpdate(input._id, input, { new: true });
+    return updatedUser;
   } catch (error) {
     throw new Error('Failed to update user');
   }
