64b091b6a85b291c35a53daf5ff8fe78d08412ac
24LP4208 24LP4208 <24LP4208@pinecones-iMac.local>
2024-11-19 13:52:13 +0800

    feat: header and sidebar update footer

--- apps/L1AB/hotel-booking/frontend/specs/components/main/FooterComponent.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { FooterComponent } from '@/components/main/FooterComponent';
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-
-describe("Footer component", () => {
-    it("should render the footer component", () => {
-        render(<FooterComponent />)
-    })
-})
