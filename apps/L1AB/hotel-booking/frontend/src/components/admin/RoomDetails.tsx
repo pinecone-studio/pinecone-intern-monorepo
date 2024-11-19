1413fce50e148856cd5f971a5d79122898898975
24LP4208 24LP4208 <24LP4208@pinecones-iMac.local>
2024-11-19 18:28:37 +0800

    feat: images carousel

--- apps/L1AB/hotel-booking/frontend/src/components/admin/RoomDetails.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/admin/RoomDetails.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import { DetailsCard, DetailsContainer, DetailsLeft, DetailsRight } from './assets';
+import { DetailsCard, DetailsContainer, DetailsLeft, DetailsRight, ImagesCarousel } from './assets';
 import { RoomServicesDialog } from './dialogs/RoomServicesDialog';
 import { GeneralInfoDialog } from './dialogs';
+import { Button } from '@/components/ui/button';
 
 export const RoomDetails = () => {
   return (
     <DetailsContainer name={'Flower Hotel Ulaanbaatar'}>
       <DetailsLeft>
         <DetailsCard>
-          <div>General Info</div>
-          <GeneralInfoDialog/>
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">General Info</h1>
+            <GeneralInfoDialog />
+          </div>
+          <div className="w-full border-t my-6"></div>
+          <div>one</div>
         </DetailsCard>
         <DetailsCard>
-          <div>Upcoming Bookings</div>
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Upcoming Bookings</h1>
+          </div>
+          <div className="mt-4">two</div>
         </DetailsCard>
         <DetailsCard>
-          <div>Room Services</div>
-          <RoomServicesDialog />
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Room Services</h1>
+            <RoomServicesDialog />
+          </div>
+          <div className="w-full border-t my-6"></div>
+          <div>three</div>
         </DetailsCard>
       </DetailsLeft>
       <DetailsRight>
         <DetailsCard>
-          <div>Images</div>
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Images</h1>
+            <Button className="text-blue-600 bg-white hover:bg-white">Edit</Button>
+          </div>
+          <ImagesCarousel />
         </DetailsCard>
       </DetailsRight>
     </DetailsContainer>
