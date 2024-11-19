4e017f05b1843f833dc318efb753399c3377b9bd
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-11-19 14:16:18 +0800

    fix(concert-ticket-booking frontend): test fixed

--- apps/L1AB/concert-ticket-booking/frontend/specs/components/adminspecs/AdminDashboard.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { AdminDash } from '@/components';
-import { render } from '@testing-library/react';
-
-describe('AdminDash', () => {
-  it('should render successfully', () => {
-    render(<AdminDash />);
-  });
-});
