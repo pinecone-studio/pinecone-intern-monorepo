66af1de4228cd479e8ee671b6ce8f56255a17fd3
LPC243012 LPC243012 <LPC243012@admins-iMac.local>
2024-11-18 23:20:18 -0800

    feat(instagram-backend): story-fix

--- apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/story/get-all-stories.spec.ts
+++ apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/story/get-all-stories.spec.ts
@@ -1,12 +1,14 @@
 import { getAllStories } from '../../../../src/resolvers/queries';
 import { GraphQLResolveInfo } from 'graphql';
 
+const mock = { userId: '1' };
+
 jest.mock('../../../../src/models', () => ({
   storyModel: {
     find: jest.fn().mockReturnValue({
       populate: jest.fn().mockResolvedValue([
         {
-          toObject: () => ['a'],
+          toObject: () => mock,
         },
       ]),
     }),
@@ -17,6 +19,6 @@ describe('getAllStories resolver', () => {
   it('should get all stories', async () => {
     const res = await getAllStories!({}, {}, {}, {} as GraphQLResolveInfo);
 
-    expect(res).toEqual(['a']);
+    expect(res).toEqual([{ userId: '1' }]);
   });
 });
