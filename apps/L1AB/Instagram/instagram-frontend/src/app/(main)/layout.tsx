7b4ee10bab41158a57a55cd7d5b5eba8b0d6bc6a
ksubi666 <Ksubi666@gmail.com>
2024-11-21 04:23:15 +0800

    feat: [FE] profile page followers remove UI

--- apps/L1AB/Instagram/instagram-frontend/src/app/(main)/layout.tsx
+++ apps/L1AB/Instagram/instagram-frontend/src/app/(main)/layout.tsx
@@ -1,30 +1,49 @@
 'use client';
-import { PropsWithChildren, Suspense } from 'react';
+import { createContext, PropsWithChildren, Suspense } from 'react';
 import { ApolloWrapper } from '@/components/providers';
 import { LeftSideBar } from '@/components/LeftSideBar';
 import RightSideBar from '@/components/RightSideBar';
 import { SuggestCard } from '@/components/SuggestCard';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useGetUserByIdQuery } from '@/generated';
+
+export const userContext = createContext<object | null>(null);
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const userId = localStorage.getItem('userId');
+  const userToken = localStorage.getItem('userToken');
+
+  if (!userToken) {
+    router.push('/login');
+  }
+  const { data } = useGetUserByIdQuery({
+    variables: { id: userId || '' },
+  });
+
+  const user = data?.getUserById;
+
   return (
-    <div className="flex min-w-full">
-      <LeftSideBar />
-      <div className="flex gap-[72px] mx-auto">
-        <Suspense>
-          <ApolloWrapper>
-            <div>{children}</div>
-          </ApolloWrapper>
-        </Suspense>
-        {pathname == '/home' ? (
-          <div className="flex flex-col py-10 gap-y-4">
-            <RightSideBar />
-            <SuggestCard />
-          </div>
-        ) : null}
+    <userContext.Provider value={{ user }}>
+      <div className="flex min-w-full">
+        <LeftSideBar />
+        <div className="flex gap-[72px] mx-auto">
+          <Suspense>
+            <ApolloWrapper>
+              <div>{children}</div>
+            </ApolloWrapper>
+          </Suspense>
+          {pathname == '/home' ? (
+            <div className="flex flex-col py-10 gap-y-4">
+              <RightSideBar user={user} />
+              <SuggestCard />
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
+    </userContext.Provider>
   );
 };
 
