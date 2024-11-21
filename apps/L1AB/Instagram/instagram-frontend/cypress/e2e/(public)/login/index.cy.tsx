df0699dc60c1f17e98bb41d1a30d9a17b0955ef8
ksubi666 <Ksubi666@gmail.com>
2024-11-22 03:18:11 +0800

    fix: [FE] cypress refactor

--- apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(public)/login/index.cy.tsx
+++ apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(public)/login/index.cy.tsx
@@ -1,26 +1,111 @@
 describe('Login Page', () => {
-    beforeEach(() => {
-      cy.visit('/login');
+  beforeEach(() => {
+    cy.visit('/login');
+  });
+
+  it('should load the login page', () => {
+    cy.get('[data-cy="Login-Page"]').should('exist');
+    cy.get('[data-cy="Login-usernameOrEmail-Input"]').should('be.visible');
+    cy.get('[data-cy="Login-Password-Input"]').should('be.visible');
+    cy.get('[data-cy="Login-Submit-Button"]').should('be.visible');
+    cy.get('[data-cy="forgot-password-button"]').should('exist');
+    cy.get('[data-cy="sign-up-button"]').should('exist');
+  });
+
+  it('should navigate to forgot password page', () => {
+    cy.get('[data-cy="forgot-password-button"]').click();
+    cy.url().should('include', '/forgotpassword');
+  });
+
+  it('should navigate to sign-up page', () => {
+    cy.get('[data-cy="sign-up-button"]').click();
+    cy.url().should('include', '/signup');
+  });
+  it('login hiiged password taarad shiljih', () => {
+    cy.intercept('POST', 'https://instagram-backend-testing.vercel.app/api/graphql', {
+      statusCode: 200,
+      body: {
+        data: {
+          login: {
+            token: 'mocked-token',
+            user: {
+              _id: 'mocked-user-id',
+            },
+          },
+        },
+      },
+    }).as('loginRequest');
+    cy.get('[data-cy="Login-usernameOrEmail-Input"]').type('bat');
+    cy.get('[data-cy="Login-Password-Input"]').type('password');
+    cy.get('[data-cy="Login-Submit-Button"]').click();
+    cy.request({
+      method: 'POST',
+      url: 'https://instagram-backend-testing.vercel.app/api/graphql',
+      body: {
+        query: `
+      mutation Login($username: String!, $email: String!, $password: String!) {
+        login(username: $username, email: $email, password: $password) {
+          token
+          user {
+            _id
+          }
+        }
+      }
+    `,
+        variables: {
+          username: 'mock-user',
+          email: 'mock-user',
+          password: 'password',
+        },
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      if (response.body?.data) {
+        window.localStorage.setItem('userToken', response.body.data.login.token);
+        window.localStorage.setItem('userId', response.body.data.login.user._id);
+      }
     });
-  
-    it('should load the login page', () => {
-      cy.get('[data-cy="Login-Page"]').should('exist');
-      cy.get('[data-cy="Login-usernameOrEmail-Input"]').should('be.visible');
-      cy.get('[data-cy="Login-Password-Input"]').should('be.visible');
-      cy.get('[data-cy="Login-Submit-Button"]').should('be.visible');
-      cy.get('[data-cy="forgot-password-button"]').should('exist');
-      cy.get('[data-cy="sign-up-button"]').should('exist');
-    });
-  
-  
-    it('should navigate to forgot password page', () => {
-      cy.get('[data-cy="forgot-password-button"]').click();
-      cy.url().should('include', '/forgotpassword');
-    });
-  
-    it('should navigate to sign-up page', () => {
-      cy.get('[data-cy="sign-up-button"]').click();
-      cy.url().should('include', '/signup');
+    cy.wait('@loginRequest');
+
+    cy.url().should('include', '/home');
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('userToken')).to.eq('mocked-token');
+      expect(win.localStorage.getItem('userId')).to.eq('mocked-user-id');
     });
   });
-  
+  it('login hiiged buruu shiljihgui', () => {
+    cy.intercept('POST', 'https://instagram-backend-testing.vercel.app/api/graphql', {
+      statusCode: 400,
+      body: {
+        data: {},
+      },
+    }).as('loginRequest');
+    cy.get('[data-cy="Login-usernameOrEmail-Input"]').type('bat');
+    cy.get('[data-cy="Login-Password-Input"]').type('password');
+    cy.get('[data-cy="Login-Submit-Button"]').click();
+    cy.request({
+      method: 'POST',
+      url: 'https://instagram-backend-testing.vercel.app/api/graphql',
+      body: {
+        query: `
+      mutation Login($username: String!, $email: String!, $password: String!) {
+        login(username: $username, email: $email, password: $password) {
+          token
+          user {
+            _id
+          }
+        }
+      }
+    `,
+        variables: {
+          username: 'mock-user',
+          email: 'mock-user',
+          password: 'password',
+        },
+      },
+      failOnStatusCode: false,
+    });
+    cy.wait('@loginRequest');
+    cy.url().should('include', '/login');
+  });
+});
--- apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(public)/signup/index.cy.tsx
+++ apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(public)/signup/index.cy.tsx
@@ -10,14 +10,14 @@ describe('Signup Page', () => {
   it('2. When user does not enter email or mobile number, it should display error message', () => {
     cy.get('[data-cy=Sign-Up-mobileNumberorEmail-Input]').type('12');
     cy.get('[data-cy=Sign-Up-mobileNumberorEmail-Input-Error-Message]').should('be.visible');
-    cy.get('[data-cy=Sign-Up-mobileNumberorEmail-Input-Error-Message]').should('have.text','Invalid email or mobile number');
+    cy.get('[data-cy=Sign-Up-mobileNumberorEmail-Input-Error-Message]').should('have.text', 'Invalid email or mobile number');
   });
 
   it('3. When user does not enter password, it should display error message', () => {
     cy.get('[data-cy=Sign-Up-Submit-Button]').click();
     cy.get('[data-cy=Sign-Up-password-Input-Error-Message]').should('be.visible');
-    cy.get('[data-cy=Sign-Up-password-Input-Error-Message]').should('have.text','Password must be at least 8 characters');
-  }); 
+    cy.get('[data-cy=Sign-Up-password-Input-Error-Message]').should('have.text', 'Password must be at least 8 characters');
+  });
 
   it('4. When user enters invalid fullname, it should display error message', () => {
     cy.get('[data-cy=Sign-Up-fullname-Input]').type('12');
@@ -28,16 +28,50 @@ describe('Signup Page', () => {
   it('5. When user does not enter username, it should display error message', () => {
     cy.get('[data-cy=Sign-Up-Submit-Button]').click();
     cy.get('[data-cy=Sign-Up-username-Input-Error-Message]').should('be.visible');
-    cy.get('[data-cy=Sign-Up-username-Input-Error-Message]').should('have.text','Username must be at least 3 characters');
+    cy.get('[data-cy=Sign-Up-username-Input-Error-Message]').should('have.text', 'Username must be at least 3 characters');
   });
 
-  it('5. when user enters all values, it should navigate to login page', () => {
-
+  it('5.Signup hiiged amjilttai', () => {
+    cy.intercept('POST', 'https://instagram-backend-testing.vercel.app/api/graphql', {
+      statusCode: 200,
+      body: {
+        data: {
+          username: 'mockedusername',
+          email: 'mocked-email',
+          fullname: 'mocked-fullname',
+        },
+      },
+    }).as('signupRequest');
     cy.get('[data-cy=Sign-Up-mobileNumberorEmail-Input]').type('test@gmail.com');
     cy.get('[data-cy=Sign-Up-Password-Input]').type('12345678');
     cy.get('[data-cy=Sign-Up-fullname-Input]').type('Test');
     cy.get('[data-cy=Sign-Up-username-Input]').type('Test');
     cy.get('[data-cy=Sign-Up-Submit-Button]').click();
-    cy.url().should('include', 'login');
+    cy.request({
+      method: 'POST',
+      url: 'https://instagram-backend-testing.vercel.app/api/graphql',
+      body: {
+        query: `mutation Signup($fullname: String!, $username: String!, $email: String!, $password: String!) {
+        signup(fullname: $fullname, username: $username, email: $email, password: $password) {
+        username
+        email
+        fullname
+  }
+}
+    `,
+        variables: {
+          username: 'batt',
+          email: 'batt',
+          fullname: 'test',
+          password: 'password',
+        },
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      if (response.body?.data) {
+        cy.url().should('include', '/login');
+      }
+    });
+    cy.wait('@signupRequest');
   });
 });
