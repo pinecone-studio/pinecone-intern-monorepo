818f6d49493e1e7ea49831c091b3bc030d22fb82
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-11-21 15:33:01 +0800

    feat: eventDetail added

--- apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/MainNavbar.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/MainNavbar.tsx
@@ -21,28 +21,36 @@ export const MainNavbar = () => {
   ];
 
   return (
-    <div className=" py-6 px-12 flex justify-between border-b border-[#27272A]">
-      <Link href={`/`}>
-        <div className="flex items-center">
-          <GoDotFill className="w-8 h-8 text-[#00B7F4]" />
-          <h1 className="text-white font-semibold text-2xl">TICKET BOOKING</h1>
-        </div>
-      </Link>
-
-      <div className="text-white text-sm font-medium flex gap-4">
-        <Link className=" text-white flex relative items-center text-sm" href={`/events`}>
-          <LuSearch className="w-6 h-6" />
+    <Container>
+      <div className=" py-6 px-12 flex justify-between border-b border-[#27272A]">
+        <Link href={`/`}>
+          <div className="flex items-center">
+            <GoDotFill className="w-8 h-8 text-[#00B7F4]" />
+            <h1 className="text-white font-semibold text-2xl">TICKET BOOKING</h1>
+          </div>
         </Link>
 
         <div className="text-white text-sm font-medium flex gap-4">
-          <Link className=" text-white flex relative items-center text-sm" href={`/search`}>
+          <Link className=" text-white flex relative items-center text-sm" href={`/events`}>
             <LuSearch className="w-6 h-6" />
           </Link>
-          {paths.map((path) => (
-            <Link href={path.path} key={path.name}>
-              <Button className={`border border-[#27272A] py-2 px-10 rounded-md hover:bg-[#00B7F4] hover:text-black  ${pathname === path.path ? 'bg-[#00B7F4]' : ''}`}>{path.name}</Button>
+
+          <div className="text-white text-sm font-medium flex gap-4">
+            <Link className=" text-white flex relative items-center text-sm" href={`/search`}>
+              <LuSearch className="w-6 h-6" />
             </Link>
-          ))}
+
+            <div className="text-white text-sm font-medium flex gap-4">
+              <Link className=" text-white flex relative items-center text-sm" href={`/search`}>
+                <LuSearch className="w-6 h-6" />
+              </Link>
+              {paths.map((path) => (
+                <Link href={path.path} key={path.name}>
+                  <Button className={`border border-[#27272A] py-2 px-10 rounded-md hover:bg-[#00B7F4] hover:text-black  ${pathname === path.path ? 'bg-[#00B7F4]' : ''}`}>{path.name}</Button>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Container>
