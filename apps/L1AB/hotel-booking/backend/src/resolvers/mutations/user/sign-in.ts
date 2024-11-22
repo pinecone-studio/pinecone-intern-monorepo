d0d1f25bc373786091c0cc917b1ee4b13c1c83ce
7sogoo <gtsog0915l@gmail.com>
2024-11-22 15:03:19 +0800

    fix(hotel-booking-backend): 'fixed signin added bcrypt

--- apps/L1AB/hotel-booking/backend/src/resolvers/mutations/user/sign-in.ts
+++ apps/L1AB/hotel-booking/backend/src/resolvers/mutations/user/sign-in.ts
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { userModel } from '../../../models';
 import { MutationResolvers } from '../../../generated';
+import bcrypt from 'bcrypt'
 
 export const signIn: MutationResolvers['signIn'] = async (_, { input }) => {
   const { email, password } = input;
 
+
   const user = await userModel.findOne({
     email,
-    password,
   });
 
-  if (!user) throw new Error('Invalid credentials');
+  if (!user) throw new Error('User not found');
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if(!isMatch) throw new Error('Password or email is incorrect')
 
   const token = jwt.sign(
     {
