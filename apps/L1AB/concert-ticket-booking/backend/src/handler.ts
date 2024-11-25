764c47bf6e8a59d7593b75a635d6d7086e6e5fe3
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-11-25 18:27:30 +0800

    fix(concert-ticket-booking backend): update user added

--- apps/L1AB/concert-ticket-booking/backend/src/handler.ts
+++ apps/L1AB/concert-ticket-booking/backend/src/handler.ts
@@ -5,26 +5,31 @@ import { NextRequest } from 'next/server';
 import { resolvers } from './resolvers';
 import { connectToDb } from './utils/connect-to-db';
 import jwt from 'jsonwebtoken';
+import { User } from './types';
 
 connectToDb();
 
-const server = new ApolloServer({
+const server = new ApolloServer<Context>({
   resolvers,
   typeDefs,
   introspection: true,
 });
 
-export const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+export const handler = startServerAndCreateNextHandler<NextRequest, Context>(server, {
   context: async (req) => {
     try {
       const authorization = req.headers.get('Authorization');
       if (!authorization) return { user: null };
 
       const token = authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as User;
       return { user: decoded };
     } catch (error) {
       return { user: null };
     }
   },
 });
+
+export type Context = {
+  user: User | null;
+};
