85afe82b852c5e629767059d947742849fc870a2
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-11-25 13:41:19 +0800

    feat(concert-ticket-booking frontend): backend removed

--- apps/L1AB/concert-ticket-booking/backend/src/resolvers/queries/user/get-user-by-id.ts
+++ apps/L1AB/concert-ticket-booking/backend/src/resolvers/queries/user/get-user-by-id.ts
@@ -1,9 +1,9 @@
-import { QueryResolvers } from '../../../generated';
-import { userModel } from '../../../models';
+import { QueryResolvers } from "../../../generated";
+import { userModel } from "../../../models";
 
-export const getUserById: QueryResolvers['getUserById'] = async (_, { _id }) => {
-  const userId = await userModel.findById(_id);
+export const getUserById: QueryResolvers['getUserById'] = async (_, {_id}) => {
+    const userId = await userModel.findById(_id);
 
-  if (!userId) throw new Error('userId not found');
-  return userId;
-};
+    if(!userId) throw new Error('userId not found');
+    return userId
+}
