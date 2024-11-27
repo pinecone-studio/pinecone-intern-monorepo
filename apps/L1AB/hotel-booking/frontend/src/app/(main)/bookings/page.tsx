fe39315900ae1eeeb23d20b23dd9231ce83d5d0e
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-11-27 04:28:31 -0800

    feat(hotel booking: checkin dialog): main CheckIn Dialog component

--- apps/L1AB/hotel-booking/frontend/src/app/(main)/bookings/page.tsx
+++ apps/L1AB/hotel-booking/frontend/src/app/(main)/bookings/page.tsx
@@ -1,7 +1,13 @@
 'use client';
+import { CheckInDialog } from '@/components/main/assets';
 
 const BookingPage = () => {
-  return <div>Booking Page</div>;
+  return (
+    <>
+      <div>Booking Page</div>
+      <CheckInDialog />
+    </>
+  );
 };
 
 export default BookingPage;
