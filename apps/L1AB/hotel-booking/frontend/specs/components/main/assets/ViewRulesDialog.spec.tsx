b9bd486cfb02dde8f4e1dcf48c6d4ac1ffcdccb2
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-11-27 02:54:24 -0800

    feat(hotel booking viewrules dialog): main ViewRules Dialog

---
+++ apps/L1AB/hotel-booking/frontend/specs/components/main/assets/ViewRulesDialog.spec.tsx
@@ -0,0 +1,9 @@
+import { ViewRulesDialog } from '@/components/main/assets';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+describe('Main ViewRulesDialog', () => {
+  it('should render the main ViewRules Dialog', () => {
+    render(<ViewRulesDialog />);
+  });
+});
