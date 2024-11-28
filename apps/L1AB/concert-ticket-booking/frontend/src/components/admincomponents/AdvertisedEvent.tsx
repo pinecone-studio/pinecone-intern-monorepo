dd73ab91762aa3ce6f4ac29a3f847057720c74cf
Төр-Эрдэнэ Сурмаажав <24LP1247@pinecones-iMac.local>
2024-11-28 17:46:11 +0800

    feat(ticket-booking): special event added

--- apps/L1AB/concert-ticket-booking/frontend/src/components/admincomponents/AdvertisedEvent.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/admincomponents/AdvertisedEvent.tsx
@@ -4,49 +4,69 @@ import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
 
-import { Button } from '@/components/ui/button';
 import { Star, X } from 'lucide-react';
 import { useState } from 'react';
+import { useUpdateEventStatusMutation } from '@/generated';
 
-export const AdvertisedEvent = () => {
-  const [selectedOption, setSelectedOption] = useState<'yes' | 'no'>('no');
+export const AdvertisedEvent = ({ eventId }: { eventId: string }) => {
+  const [updatestatus] = useUpdateEventStatusMutation();
+  const [selectedOption, setSelectedOption] = useState<'Тийм' | 'Үгүй'>('Үгүй');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleUpdateStatus = async () => {
+    const status = selectedOption === 'Тийм' ? 'Онцлох' : 'Reqular';
+
+    await updatestatus({
+      variables: {
+        input: { _id: eventId, status },
+      },
+    });
+
+    setIsDialogOpen(false);
+  };
 
   return (
     <div data-testid="AdvertisedEvent-Component">
-      <AlertDialog>
+      <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <AlertDialogTrigger asChild>
-          <Button type="button" variant="outline" aria-label="Show star">
-            <Star data-testid="AdvertisedEvent-TriggerButton" className="h-5 w-5" />
-          </Button>
+          <Star data-testid="AdvertisedEvent-TriggerButton" className="h-5 w-5 bg-[#F4F4F5] rounded cursor-pointer p-[2px]" />
         </AlertDialogTrigger>
+
         <AlertDialogContent className="w-[511px] border-none shadow-none" data-testid="AdvertisedEvent-DialogContent">
           <Card className="w-full border-none gap-4" data-testid="AdvertisedEvent-Card">
             <CardHeader className="gap-4" data-testid="AdvertisedEvent-CardHeader">
               <div className="flex justify-between items-center" data-testid="AdvertisedEvent-HeaderActions">
                 <CardTitle data-testid="AdvertisedEvent-Title">Онцлох тоглолт болгох</CardTitle>
                 <AlertDialogCancel asChild>
-                  <button type="button" className="text-gray-500 hover:text-gray-700 focus:outline-none" aria-label="Close" data-testid="AdvertisedEvent-CloseButton">
+                  <button type="button" className="text-gray-500 hover:text-gray-700 focus:outline-none" aria-label="Close" data-testid="exit" onClick={() => setIsDialogOpen(false)}>
                     <X className="h-5 w-5" />
                   </button>
                 </AlertDialogCancel>
               </div>
 
-              <RadioGroup value={selectedOption} onValueChange={(value) => setSelectedOption(value as 'yes' | 'no')} className="flex">
+              <RadioGroup value={selectedOption} onValueChange={(value) => setSelectedOption(value as 'Тийм' | 'Үгүй')} className="flex">
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="yes" id="r1" data-testid="Yes-btn" />
+                  <RadioGroupItem value="Тийм" id="r1" data-testid="Yes-btn" />
                   <Label htmlFor="r1">Тийм</Label>
                 </div>
                 <div className="flex items-center space-x-2" data-testid="AdvertisedEvent-RadioOptionNo">
-                  <RadioGroupItem value="no" id="r2" data-testid="No-btn" />
+                  <RadioGroupItem value="Үгүй" id="r2" data-testid="No-btn" />
                   <Label htmlFor="r2">Үгүй</Label>
                 </div>
               </RadioGroup>
             </CardHeader>
 
             <CardFooter className="flex justify-between" data-testid="AdvertisedEvent-CardFooter">
-              <Button className="w-full" data-testid="AdvertisedEvent-SaveButton">
+              <button
+                className="w-full border rounded bg-black text-white p-2"
+                data-testid="btn"
+                placeholder="btn"
+                onClick={async () => {
+                  await handleUpdateStatus();
+                }}
+              >
                 Хадгалах
-              </Button>
+              </button>
             </CardFooter>
           </Card>
         </AlertDialogContent>
