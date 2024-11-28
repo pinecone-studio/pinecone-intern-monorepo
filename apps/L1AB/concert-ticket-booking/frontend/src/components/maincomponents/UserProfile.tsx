8aca871ddd4a87bc93fa9717234057b65b1eb124
24LU8176 24LU8176 <24LU8176@pinecones-iMac.local>
2024-11-28 14:03:00 +0800

    feat(concert-ticket-booking frontend): updateUserProfileLogic

--- apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/UserProfile.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/UserProfile.tsx
@@ -3,9 +3,29 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useAuth } from '../providers';
+import { useUpdateUserMutation } from '@/generated';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export const UserProfile = () => {
   const { signout } = useAuth();
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [UpdateUser] = useUpdateUserMutation();
+
+  const handleUpdateUser = async () => {
+    await UpdateUser({
+      variables: {
+        input: {
+          phone,
+          email,
+        },
+      },
+    });
+    setPhone('');
+    setEmail('');
+    toast.success('Таны мэдээлэл амжилттай шинэчлэгдсэн');
+  };
   return (
     <div className="w-full h-fit flex flex-col gap-6  " data-testid="userProfile">
       <p className="font-semibold text-2xl text-white">Захиалагчийн мэдээлэл</p>
@@ -24,16 +44,31 @@ export const UserProfile = () => {
                 target.value = target.value.slice(0, 8);
               }
             }}
+            value={phone}
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
           />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="Имэйл хаяг:">Имэйл хаяг:</Label>
-          <Input type="email" placeholder="name@example.com" className="px-3 py-1 border-[#27272A] bg-[#09090B]" data-cy="Profile-Email-Input" />
+          <Input
+            type="email"
+            placeholder="name@example.com"
+            className="px-3 py-1 border-[#27272A] bg-[#09090B]"
+            data-cy="Profile-Email-Input"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
         </div>
         <div className="flex justify-end">
           <button
+            data-testid="UpdateButton"
             className="w-fit font-medium text-sm hover:text-black hover:bg-[#00B7F4] text-white hover:border-none px-4 py-2 flex justify-center rounded-md bg-[#272729]"
             data-cy="Profile-Submit-Button"
+            onClick={handleUpdateUser}
           >
             Хадгалах
           </button>
