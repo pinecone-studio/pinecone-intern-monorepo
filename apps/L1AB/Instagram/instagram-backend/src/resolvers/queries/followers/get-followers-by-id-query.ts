deeed2565bed5b8e6712eb63ce0b5a0c53865aae
ksubi666 <Ksubi666@gmail.com>
2024-12-01 23:40:08 +0800

    fix: refactor followers following query

--- apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/followers/get-followers-by-id-query.ts
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/followers/get-followers-by-id-query.ts
@@ -2,11 +2,11 @@ import { QueryResolvers } from '../../../generated';
 import { followersModel } from '../../../models';
 
 export const getFollowersById: QueryResolvers['getFollowersById'] = async (_, { _id }) => {
-  const followers = await followersModel.find({ followeeId: _id });
+  const followers = await followersModel.find({ followeeId: _id }).populate('followerId');
 
   if (!followers.length) {
     throw new Error('Followers not found');
   }
 
-  return followers;
+  return followers.map((follow) => follow.followerId);
 };
