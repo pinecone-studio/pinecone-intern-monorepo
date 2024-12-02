75d80d76f93d4ef025fad5041437a2ecd97a19ab
24LP8661 24LP8661 <24LP8661@admins-iMac.local>
2024-12-01 22:40:32 -0800

    feat(hotel booking frontend bookingconfirmed): main BookingConfirmed

--- apps/L1AB/hotel-booking/frontend/src/components/main/BookingConfirmedBooking.tsx
+++ apps/L1AB/hotel-booking/frontend/src/components/main/BookingConfirmedBooking.tsx
@@ -2,16 +2,17 @@
 import { Container } from './assets';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { FaClockRotateLeft } from 'react-icons/fa6';
 
 export const BookingConfirmedBooking = () => {
   return (
     <Container backgroundColor="bg-white">
-      <div className="bg-pink-100 w-[960px] flex flex-col p-3">
-        <div className="bg-green-50">
-          Confirmed Booking
+      <div className="w-[960px] flex flex-col p-3">
+        <h3 className="text-2xl font-semibold mb-3">Confirmed Booking</h3>
+        <div className="border rounded-md">
           <div className="flex">
             <div className="flex flex-1 bg-blue-100 w-72 h-56">Image</div>
-            <div className="flex flex-1 flex-col p-3">
+            <div className="flex flex-1 flex-col px-3">
               <div className="flex flex-col mt-3 gap-1 mb-12">
                 <div>
                   <Button asChild className="bg-[#18BA51]">
@@ -20,6 +21,8 @@ export const BookingConfirmedBooking = () => {
                 </div>
                 <h1 className="font-bold"> Flower Hotel Ulaanbaatar</h1>
                 <p className="text-[#71717A] text-sm">Standard Room, City View, 1 Queen Bed</p>
+
+                <ul className="flex gap-4 text-sm list-disc">1 night • 1 adult • 1 room</ul>
               </div>
               <div className="flex  justify-between">
                 <div className="flex flex-col text-[#71717A] text-sm">
@@ -42,8 +45,19 @@ export const BookingConfirmedBooking = () => {
             </div>
           </div>
         </div>
+
+        <div className="mt-8">
+          <h3 className="text-2xl font-semibold mb-3">Previous Booking</h3>
+
+          <div className="flex flex-col justify-center items-center">
+            <div className=" text-[#71717A] text-sm">
+              <FaClockRotateLeft className="w-5 h-5" />
+            </div>
+            <div className="text-sm font-medium text-[#09090B]">No Previous Bookings</div>
+            <div className="text-sm font-normal text-[#71717A]">&quot;Your past stays will appear here once completed.&quot;</div>
+          </div>
+        </div>
       </div>
-      <div className="border bg-yellow-50">Previous Booking</div>
     </Container>
   );
 };
