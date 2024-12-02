750f7fd99e2cb014cabeec666117a6ab580c3772
24LP5175 24LP5175 <24LP5175@pinecones-iMac.local>
2024-12-02 21:05:44 +0800

    fix(concert-ticket-booking-frontend): modify auth providers

--- apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/VerifyOtp.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/VerifyOtp.tsx
@@ -3,11 +3,11 @@ import { Container } from '@/components';
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
 import { MoveLeft, RefreshCcw } from 'lucide-react';
 
-interface OTPInputProps {
+interface VerifyOtpProps {
   footerText: string;
 }
 
-export const OTPInput = ({ footerText }: OTPInputProps) => {
+export const VerifyOtp = ({ footerText }: VerifyOtpProps) => {
   return (
     <Container>
       <div className="text-amber-50 flex items-center justify-center h-[48rem] max-sm:px-3" data-cy="OTPInput-Page">
@@ -16,17 +16,19 @@ export const OTPInput = ({ footerText }: OTPInputProps) => {
             <p className="text-[#A1A1AA] self-stretch text-center text-base leading-7 tracking-wide">{footerText}</p>
           </div>
           <div className="flex flex-col items-center justify-center gap-6 self-stretch w-[327px] max-sm:w-full">
-            <InputOTP maxLength={4} data-testid="OTPInput">
+            <InputOTP maxLength={6} data-testid="OTPInput">
               <InputOTPGroup>
                 <InputOTPSlot data-testid="OTPInput-Slot-0" className="h-14 w-14" index={0} />
                 <InputOTPSlot data-testid="OTPInput-Slot-1" className="h-14 w-14" index={1} />
                 <InputOTPSlot data-testid="OTPInput-Slot-2" className="h-14 w-14" index={2} />
                 <InputOTPSlot data-testid="OTPInput-Slot-3" className="h-14 w-14" index={3} />
+                <InputOTPSlot data-testid="OTPInput-Slot-4" className="h-14 w-14" index={4} />
+                <InputOTPSlot data-testid="OTPInput-Slot-5" className="h-14 w-14" index={5} />
               </InputOTPGroup>
             </InputOTP>
             <div className="flex justify-between w-[211px] max-sm:w-full">
-              <MoveLeft data-testid="MoveLeft" className="h-5 w-5" />
-              <RefreshCcw data-testid="RefreshCcw" className="h-5 w-5" />
+              <MoveLeft data-testid="MoveLeft" className="w-5 h-5" />
+              <RefreshCcw data-testid="RefreshCcw" className="w-5 h-5" />
             </div>
           </div>
         </div>
@@ -35,4 +37,4 @@ export const OTPInput = ({ footerText }: OTPInputProps) => {
   );
 };
 
-export default OTPInput;
+export default VerifyOtp;
