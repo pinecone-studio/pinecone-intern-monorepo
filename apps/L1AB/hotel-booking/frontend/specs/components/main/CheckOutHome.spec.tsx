ed89de874594514af895f524f24382fecf4293dc
Tur-Ovhir <ochirtor@gmail.com>
2024-12-04 13:50:32 +0800

    feat: checkOutHome

--- apps/L1AB/hotel-booking/frontend/specs/components/main/CheckOutHome.spec.tsx
+++ apps/L1AB/hotel-booking/frontend/specs/components/main/CheckOutHome.spec.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 
 describe('Check out home', () => {
-  it('should render the Check Out Home', () => {
+  it('should render The Check Out Home', () => {
     render(<CheckOutHome />);
   });
 });
