e8ca8560d749c14116a30154254afb1433123d39
LPD243578 LPD243578 <LPD243578@admins-iMac.local>
2024-12-05 17:17:13 +0800

    feat(ig-be): savedpost query

--- apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
@@ -2,11 +2,12 @@ import { QueryResolvers } from '../../../generated';
 import { savedModel } from '../../../models';
 
 export const getSavedByPostId: QueryResolvers['getSavedByPostId'] = async (_, { postId }) => {
-  const savedPosts = await savedModel.find({ postId: postId });
+  const savedPosts = await savedModel.findOne({ postId: postId }).populate('postId').populate('userId');
 
   if (!savedPosts) {
     throw new Error('No saved posts found for this user');
   }
+  console.log(savedPosts);
 
   return savedPosts;
 };
