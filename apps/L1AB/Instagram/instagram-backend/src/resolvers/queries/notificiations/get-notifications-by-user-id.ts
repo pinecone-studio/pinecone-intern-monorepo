0eb225a98adb1b7f58e8b69ae49603d9add6e16d
LPD243578 LPD243578 <LPD243578@admins-iMac.local>
2024-12-06 18:40:52 +0800

    feat(ig-[be]): like notifications

---
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/notificiations/get-notifications-by-user-id.ts
@@ -0,0 +1,8 @@
+import { QueryResolvers } from '../../../generated';
+import { notificationsModel } from '../../../models';
+
+export const getNotificationsByUserId: QueryResolvers['getNotificationsByUserId'] = async (_, { userId }) => {
+  const notifications = await notificationsModel.find({ notifiedUserId: userId });
+
+  return notifications;
+};
