299210f2ce2e719ccaddc5708e6ea54c658f6b57
ksubi666 <Ksubi666@gmail.com>
2024-12-08 22:25:51 +0800

    fix: fix

--- apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(main)/home/index.cy.tsx
+++ apps/L1AB/Instagram/instagram-frontend/cypress/e2e/(main)/home/index.cy.tsx
@@ -1,11 +1,1 @@
-describe('Home Page', () => {
-  
-  it(`Should render home page`, () => {
-    cy.visit('/');
-    cy.get('[data-cy="Show-Comments-1"]').click();
 
-    cy.get('[data-cy="Show-Comments-Caption]').should('be.visible')
-
-
-  });
-});
