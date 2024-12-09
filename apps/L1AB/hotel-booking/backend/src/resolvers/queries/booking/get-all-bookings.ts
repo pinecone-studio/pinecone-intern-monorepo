f969feb9109d50487a3bb37e480f54687d70db03
7sogoo <gtsog0915@gmail.com>
2024-12-09 14:46:21 +0800

    fix('hotel-booking-backend'): 'added user to booking'

--- apps/L1AB/hotel-booking/backend/src/resolvers/queries/booking/get-all-bookings.ts
+++ apps/L1AB/hotel-booking/backend/src/resolvers/queries/booking/get-all-bookings.ts
@@ -2,11 +2,14 @@ import { QueryResolvers } from '../../../generated';
 import { bookingModel, bookingPopulatedType } from '../../../models';
 
 export const getAllBookings: QueryResolvers['getAllBookings'] = async () => {
-  const bookings = await bookingModel.find().populate<bookingPopulatedType>({
-    path: 'roomId',
-    populate: {
-      path: 'hotelId'
+  const bookings = await bookingModel.find().populate<bookingPopulatedType>([
+    {
+      path: 'roomId',
+      populate: {
+        path: 'hotelId',
+      },
     },
-  });
+    'userId',
+  ]);
   return bookings.map((booking) => booking.toObject());
 };
