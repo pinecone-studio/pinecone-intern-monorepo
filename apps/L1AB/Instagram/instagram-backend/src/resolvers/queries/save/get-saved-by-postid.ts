b411c449a0ec4de63e0615bfb125a8d98f817aa6
24LP3032 24LP3032 <24LP3032@pinecones-iMac.local>
2024-12-10 17:55:15 +0800

    fix(ig): fix pr

--- apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
@@ -2,6 +2,6 @@ import { QueryResolvers } from '../../../generated';
 import { savedModel } from '../../../models';
 
 export const getSavedByPostId: QueryResolvers['getSavedByPostId'] = async (_, { postId }) => {
-  const savedPosts = await savedModel.findOne({ postId: postId }).populate('postId').populate('userId');
-  return savedPosts || {};
+  const savedPost = await savedModel.findOne({ postId: postId }).populate('postId').populate('userId');
+  return savedPost || {};
 };
