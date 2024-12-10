64fdfd0feae67da9f8e8c7c6454972aa4f00d495
24LP3032 24LP3032 <24LP3032@pinecones-iMac.local>
2024-12-10 16:49:18 +0800

    feat(ig-be): saved mutation zasav

--- apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/save/get-saved-by-postid.ts
@@ -3,11 +3,5 @@ import { savedModel } from '../../../models';
 
 export const getSavedByPostId: QueryResolvers['getSavedByPostId'] = async (_, { postId }) => {
   const savedPosts = await savedModel.findOne({ postId: postId }).populate('postId').populate('userId');
-
-  if (!savedPosts) {
-    throw new Error('No saved posts found for this user');
-  }
-  console.log(savedPosts);
-
-  return savedPosts;
+  return savedPosts || {};
 };
