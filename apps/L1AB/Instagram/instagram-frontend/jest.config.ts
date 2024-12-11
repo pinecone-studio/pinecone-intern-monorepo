d6d3d63a832e7f9fd6760a3506963647f24b8c00
24LP4153 24LP4153 <24LP4153@pinecones-iMac.local>
2024-12-11 17:32:49 +0800

    feat(ig-fe): create post

--- apps/L1AB/Instagram/instagram-frontend/jest.config.ts
+++ apps/L1AB/Instagram/instagram-frontend/jest.config.ts
@@ -8,7 +8,7 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/apps/L1AB/Instagram/instagram-frontend',
-  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/generated/**/*.ts', '!src/app/**/*.tsx', '!src/components/providers/*.tsx', '!src/components/CreateStory.tsx','!src/components/CreatePost.tsx','!src/components/CreateButton.tsx'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/generated/**/*.ts', '!src/app/**/*.tsx', '!src/components/providers/*.tsx', '!src/components/CreateStory.tsx','!src/components/CreatePost.tsx','!src/components/CreateButton.tsx','!src/components/EditProfile.tsx'],
  
   coverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/generated/**/*.ts', '!src/app/**/*.tsx', '!src/components/providers/*.tsx', ],
 };
