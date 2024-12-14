44bb435cfce4fa52382a93f16a92959df851f319
24LP5175 24LP5175 <24LP5175@pinecones-iMac.local>
2024-12-14 20:09:08 +0800

    modify send qr to email template without test

--- apps/L1AB/concert-ticket-booking/backend/src/resolvers/mutations/booking/send-qr-to-email.ts
+++ apps/L1AB/concert-ticket-booking/backend/src/resolvers/mutations/booking/send-qr-to-email.ts
@@ -1,5 +1,5 @@
 import QRCode from 'qrcode';
-import { QRModel, userModel } from '../../../models';
+import { bookingModel, EventModel, QRModel, userModel } from '../../../models';
 import { MutationResolvers } from '../../../generated';
 import { QRGenerator } from '../../../library/nodemailer';
 import { Context } from '../../../handler';
@@ -9,6 +9,20 @@ export const sendQrToEmail: MutationResolvers['sendQrToEmail'] = async (_, __, {
     throw new Error('Unauthorized: No user found in context.');
   }
   const signedUser = await userModel.findById({ _id: user.userId });
+
+  const [bookingDetails] = await Promise.all([bookingModel.findOne({ userId: user.userId }).sort({ createdAt: -1 })]);
+
+  if (!bookingDetails) {
+    throw new Error('No bookings found for the user.');
+  }
+
+  const eventId = bookingDetails.eventId;
+  const [eventDetails] = await Promise.all([EventModel.findOne({ _id: eventId }).sort({ date: -1 })]);
+
+  if (!eventDetails) {
+    throw new Error('No events found for the user.');
+  }
+
   const defaultLink = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
   const generated = await QRCode.toDataURL(defaultLink);
 
@@ -18,7 +32,7 @@ export const sendQrToEmail: MutationResolvers['sendQrToEmail'] = async (_, __, {
     generatedQR: generated,
   });
 
-  await QRGenerator(signedUser.email, generated);
+  await QRGenerator(signedUser.email, generated, bookingDetails, eventDetails);
 
   return {
     success: true,
