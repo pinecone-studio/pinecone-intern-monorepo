3b46ad2547d8306046c254de12435c35e3c57e1e
24LP2427 24LP2427 <24LP2427@pinecones-iMac.local>
2024-12-16 10:04:10 +0800

    feat(concert-ticket-booking frontend): skeleton added

--- apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/Skeletons/BookTicketSkeleton.tsx
+++ apps/L1AB/concert-ticket-booking/frontend/src/components/maincomponents/Skeletons/BookTicketSkeleton.tsx
@@ -2,46 +2,15 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const BookTicketSkeleton = () => {
   return (
-    <div className="flex px-[212px] py-12 flex-col md:flex-row gap-8">
-      <div className="flex-1 grid gap-5">
-        <div className="flex justify-between">
-          <div className="flex items-center gap-6">
-            <div className="flex items-center gap-2">
-              <Skeleton className="text-[#FAFAFA80] w-[20px]  h-[20px] bg-gray-900 " />
-              <Skeleton className="text-[#FAFAFA] opacity-80 font-light bg-gray-900 w-[100px] h-[24px]"></Skeleton>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Skeleton className="text-[#FAFAFA80] w-[20px] bg-gray-900 h-[20px]" />
-              <Skeleton className="text-[#FAFAFA] opacity-80 bg-gray-900 w-[100px] h-[24px]"></Skeleton>
-            </div>
-          </div>
-          <div className="flex gap-2 text-[#A1A1AA] items-center">
-            <Skeleton className="w-[20px] bg-gray-900 h-[20px]" />
-            <Skeleton className=" text-[#FAFAFA] bg-gray-900 text-[16px]  w-[100px] h-[24px] underline underline-offset-2"></Skeleton>
-          </div>
-        </div>
-        <div className="h-fit grid gap-2">
-          <Skeleton className="text-[#FAFAFA] font-extralight w-[100px] h-[24px] bg-gray-900 "></Skeleton>
-          <Skeleton className="font-semibold text-[#FFFFFF] px-3 w-[120px] h-[30px] bg-gray-900"></Skeleton>
-          <Skeleton className="text-[#FAFAFA] font-extralight w-[190px] h-[24px] bg-gray-900"></Skeleton>
-          <Skeleton className="font-semibold text-[#FFFFFF] px-3 w-[120px] h-[30px] bg-gray-900"></Skeleton>
-          <Skeleton className="font-semibold text-[#FFFFFF] px-3 w-[160px] h-[30px] bg-gray-900"></Skeleton>
-        </div>
-        <div className="grid gap-2">
-          <Skeleton className="text-[#FAFAFA] font-light w-[100px] h-[24px] bg-gray-900"></Skeleton>
-          <Skeleton className="h-[560px] w-full relative bg-gray-900"></Skeleton>
-        </div>
-      </div>
-      <div className="w-fit">
-        <div className="rounded-2xl px-6">
-          <div className="h-fit grid gap-2 py-6">
-            <Skeleton className="opacity-50 text-white bg-gray-900"></Skeleton>
-          </div>
-          <div className="grid h-fit gap-2 ">
-            <Skeleton className="w-[345px] h-[300px] bg-gray-900"></Skeleton>
-            <Skeleton className=" text-black w-[345px] h-[40px] bg-gray-900 py-2 px-4 my-6 hover:bg-[#6fcceb]"></Skeleton>
-          </div>
-        </div>
+    <div>
+      <nav className="flex items-center justify-between border-b-[2px] dark:border-[#27272A] border-[#c6c6c6] py-8 px-12 max-sm:px-3 max-sm:justify-evenly  max-md:px-3 max-md:justify-evenly max-lg:px-3 max-lg:justify-evenly max-2xl:justify-between">
+        <Skeleton className="bg-gray-900 dark:bg-[#131313]  h-10 w-10 text-white rounded-lg" data-testid="FaArrowLeftClick"></Skeleton>
+        <Skeleton className="text-2xl bg-gray-900 dark:bg-[#131313]  font-semibold w-[220px] h-[32px] dark:text-white text-black max-sm:text-xl">Тасалбар захиалах</Skeleton>
+        <Skeleton className="bg-transparent"></Skeleton>
+      </nav>
+      <div className="flex  max-md:grid max-sm:grid  max-lg:grid  max-lg:gap-12 items-center justify-center gap-96 py-40 max-sm:py-20 ">
+        <Skeleton className="w-[600px] rounded-full max-sm:w-[330px] max-sm:h-[300px] dark:bg-[#131313] bg-gray-900 h-[560px] max-sm:mx-24" />
+        <Skeleton className="dark:bg-[#131313] w-[390px]  bg-gray-900 h-[480px]  rounded-2xl px-6 max-sm:px-3  max-md:px-3"></Skeleton>
       </div>
     </div>
   );
