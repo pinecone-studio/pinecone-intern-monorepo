0577eb90f8d828ac51efa4e842cf8ddcf1da9f7c
Tuguldurbayr <uugantuguldurbayr@gmail.com>
2024-12-17 20:03:50 +0800

    feat(hotel-booking-frontend): improve the signIn component

--- apps/L1AB/hotel-booking/frontend/specs/components/main/SignInForm.spec.tsx
+++ apps/L1AB/hotel-booking/frontend/specs/components/main/SignInForm.spec.tsx
@@ -1,16 +1,24 @@
 import '@testing-library/jest-dom';
 import { act, fireEvent, render, waitFor } from '@testing-library/react';
 import { SignInForm } from '@/components/main';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 jest.mock('@/components/providers/Auth.Provider', () => ({
   useAuth: jest.fn().mockReturnValue({
     signin: jest.fn().mockRejectedValueOnce({ message: 'Error' }).mockResolvedValueOnce({ message: 'success' }),
+    signInLoading: false,
   }),
 }));
 
 describe('Main Sign In Form', () => {
   it('should render the main sign in form', async () => {
-    const { getByTestId } = render(<SignInForm />);
+    const { getByTestId, getByText } = render(
+      <>
+        <ToastContainer />
+        <SignInForm />
+      </>
+    );
 
     const email = getByTestId('email-input');
     const password = getByTestId('password-input');
@@ -20,15 +28,22 @@ describe('Main Sign In Form', () => {
 
     const continueBtn = getByTestId('continue-btn');
 
+    console.log(continueBtn);
+
     act(() => {
       fireEvent.click(continueBtn);
     });
 
-    await waitFor(() => expect(getByTestId('ErrorSignIn')).toBeDefined());
+    await waitFor(() => expect(getByText('An error occurred. Please try again.')));
   });
 
   it('should render the main sign in form', async () => {
-    const { getByTestId } = render(<SignInForm />);
+    const { getByTestId } = render(
+      <>
+        <ToastContainer />
+        <SignInForm />
+      </>
+    );
 
     const email = getByTestId('email-input');
     const password = getByTestId('password-input');
