ce368c6223cdb34422827a2b88e5720ae80de61e
Uzkhu <uzkhuthef@gmail.com>
2024-12-17 09:39:11 +0800

    feat(tinder): setup

---
+++ apps/L1E/tinder/tinder-fe/src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="absolute bg-transparent">
+      {/* <div>
+        <img className="" src="Headerlogo.png" alt="" />
+      </div>
+      <div></div> */}
+    </div>
+  );
+};
+
+export default Header;
