1aa3252367200d22f88e4fbb28f240a4a0e25d35
24LP9087 24LP9087 <24LP9087@admins-iMac.local>
2024-12-18 19:57:56 +0800

    feat(tinder): upload image comp

--- apps/L1E/tinder/tinder-fe/specs/components/User/Date-of-birth.spec.tsx
+++ apps/L1E/tinder/tinder-fe/specs/components/User/Date-of-birth.spec.tsx
@@ -4,6 +4,6 @@ import { render, screen } from '@testing-library/react';
 describe('Date of birth page component', () => {
   it('should render successfully', () => {
     render(<DateOfBirth />);
-    expect(screen.getByText('Back'));
+    expect(screen.getByText('Next'));
   });
 });
