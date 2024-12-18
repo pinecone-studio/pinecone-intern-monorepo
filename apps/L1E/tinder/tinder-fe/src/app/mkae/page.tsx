2220556a8cf992e28bbfc0777100fef996fd1d11
24LP9087 24LP9087 <24LP9087@admins-iMac.local>
2024-12-18 13:42:36 +0800

    feat(tinder): qweqwe

--- apps/L1E/tinder/tinder-fe/src/app/mkae/page.tsx
+++ apps/L1E/tinder/tinder-fe/src/app/mkae/page.tsx
@@ -1,16 +1,12 @@
 import { DateOfBirth } from '@/components/UserDetails/DateOfBirth';
 import { Done } from '@/components/UserDetails/Done';
-import { ImageUpload } from '@/components/UserDetails/Image-upload';
 import { InterestSelect } from '@/components/UserDetails/InterestSelect';
-import { UserDetails } from '@/components/UserDetails/User-details';
 
 const Page = () => {
   return (
     <>
       <InterestSelect />
       <DateOfBirth />
-      <UserDetails />
-      <ImageUpload />
       <Done />
     </>
   );
