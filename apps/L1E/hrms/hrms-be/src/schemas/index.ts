f69c13336aebc4e00c93438eb4a3e75ba608a277
24LU1697 24LU1697 <24LU1697@administrators-iMac-3.local>
2024-12-20 16:04:31 +0800

    feat(hrms): be-schema

--- apps/L1E/hrms/hrms-be/src/schemas/index.ts
+++ apps/L1E/hrms/hrms-be/src/schemas/index.ts
@@ -1,6 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
-import { typeDefs as EmplooyeeTypeDefs } from './emplooyee.schema';
-import { typeDefs as RequestTypeDefs } from './request.schema';
+import { typeDefs as EmplooyeeTypeDefs } from './employee.schema';
+import { typeDefs as RequestTypeDefs } from './requests.schema';
 
 export const typeDefs = mergeTypeDefs([CommonTypeDefs, EmplooyeeTypeDefs, RequestTypeDefs]);
