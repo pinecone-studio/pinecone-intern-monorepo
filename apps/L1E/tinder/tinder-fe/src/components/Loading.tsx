9aead2907c3638eac3053ffa22fec89d2055ba28
24LP9087 24LP9087 <24LP9087@admins-iMac.local>
2024-12-23 11:50:05 +0800

    feat(tinder): loading screen

--- apps/L1E/tinder/tinder-fe/src/components/Loading.tsx
+++ apps/L1E/tinder/tinder-fe/src/components/Loading.tsx
@@ -4,7 +4,7 @@ import Tinder from './common/Tinder';
 export const Loading = () => {
   return (
     <div className="flex flex-col justify-between items-center">
-      <div className="flex flex-col items-center justify-center min-h-screen gap-4 ">
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6 ">
         <Logo />
         <div className="relative w-[38px] h-[38px]">
           <div className="absolute w-full h-full border-4 border-primary/20 rounded-full" />
