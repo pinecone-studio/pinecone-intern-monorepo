b3195b475986e4df82257a25e75aa4b71e845923
LPD243578 LPD243578 <LPD243578@admins-iMac.local>
2024-12-24 19:14:34 +0800

    feat(ig-[be]): getposts query

--- apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/post/get-posts-by-followers-id.spec.ts
+++ apps/L1AB/Instagram/instagram-backend/specs/resolvers/queries/post/get-posts-by-followers-id.spec.ts
@@ -8,38 +8,95 @@ jest.mock('../../../../src/models', () => ({
     }),
   },
   postsModel: {
-    find: jest.fn().mockReturnValue({
-      populate: jest.fn().mockReturnValue([
-        {
-          _id: '67502569e2f6ddc43a5b1891',
-          userId: {
-            _id: '1',
-            username: 'gerle',
+    find: jest
+      .fn()
+      .mockReturnValueOnce({
+        populate: jest.fn().mockReturnValue([
+          {
+            _id: '67502569e2f6ddc43a5b1891',
+            userId: {
+              _id: '3',
+              username: 'zorg',
+            },
+            images: ['https://picsum.photos/200'],
+            caption: 'ehend',
+            createdAt: '2024-12-24T12:29:25.648Z',
           },
-          images: ['https://picsum.photos/200'],
-          caption: 'ehend',
-          createdAt: '2024-12-04T09:48:25.648Z',
-          updatedAt: '2024-12-04T09:48:25.648Z',
-        },
-        {
-          _id: '673da5414a0c0fa573e0d256',
-          userId: {
-            _id: '2',
-            username: 'zorg',
+          {
+            _id: '673da5414a0c0fa573e0d256',
+            userId: {
+              _id: '3',
+              username: 'zorg',
+            },
+            images: ['https://picsum.photos/900'],
+            caption: 'Hello',
+            createdAt: '2024-12-24T12:11:00Z',
           },
-          images: ['https://picsum.photos/900'],
-          caption: 'Hello',
-          createdAt: '2024-11-20T09:00:49.947Z',
-          updatedAt: '2024-11-20T09:00:49.947Z',
-        },
-      ]),
-    }),
+        ]),
+      })
+      .mockReturnValueOnce({
+        populate: jest.fn().mockReturnValue([
+          {
+            _id: '67502569e2f6ddc43a5b1891',
+            userId: {
+              _id: '1',
+              username: 'gerle',
+            },
+            images: ['https://picsum.photos/200'],
+            caption: 'ehend',
+            createdAt: '2024-12-04T09:48:25.648Z',
+            updatedAt: '2024-12-04T09:48:25.648Z',
+          },
+          {
+            _id: '673da5414a0c0fa573e0d256',
+            userId: {
+              _id: '2',
+              username: 'zorg',
+            },
+            images: ['https://picsum.photos/900'],
+            caption: 'Hello',
+            createdAt: '2024-11-20T09:00:49.947Z',
+            updatedAt: '2024-11-20T09:00:49.947Z',
+          },
+        ]),
+      }),
   },
 }));
 describe('getPostsbyFollowersId', () => {
+  let currentDate: Date;
+
+  beforeAll(() => {
+    currentDate = new Date('2024-12-24T12:30:00Z');
+    jest.spyOn(Date, 'now').mockReturnValueOnce(currentDate.getTime());
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
   it('', async () => {
     const res = await getPostsByFollowersId!({}, { followerId: '3' }, {}, {} as GraphQLResolveInfo);
     expect(res).toEqual([
+      {
+        _id: '673da5414a0c0fa573e0d256',
+        userId: {
+          _id: '3',
+          username: 'zorg',
+        },
+        images: ['https://picsum.photos/900'],
+        caption: 'Hello',
+        createdAt: '2024-12-24T12:11:00Z',
+      },
+      {
+        _id: '67502569e2f6ddc43a5b1891',
+        userId: {
+          _id: '3',
+          username: 'zorg',
+        },
+        images: ['https://picsum.photos/200'],
+        caption: 'ehend',
+        createdAt: '2024-12-24T12:29:25.648Z',
+      },
+
       {
         _id: '67502569e2f6ddc43a5b1891',
         userId: {
--- apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/post/get-posts-by-followers-id.ts
+++ apps/L1AB/Instagram/instagram-backend/src/resolvers/queries/post/get-posts-by-followers-id.ts
@@ -2,6 +2,19 @@ import { QueryResolvers } from '../../../generated';
 import { followersModel, postsModel, PostsPopulatedType } from '../../../models';
 
 export const getPostsByFollowersId: QueryResolvers['getPostsByFollowersId'] = async (_, { followerId }) => {
+  const myPosts = await postsModel.find({ userId: followerId }).populate<PostsPopulatedType>('userId');
+  console.log(myPosts);
+
+  const thirtyMinsAgo = new Date(Date.now() - 30 * 60 * 1000);
+  const myFilteredPosts = myPosts.filter((post) => new Date(post.createdAt) > thirtyMinsAgo);
+
+  const mySortedPost = myFilteredPosts.sort((a, b) => {
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+
+    return dateA.getTime() - dateB.getTime();
+  });
+
   const followees = await followersModel.find({ followerId: followerId }).select('followeeId -_id');
 
   const followeeIds = followees.map((followee) => followee.followeeId);
@@ -14,6 +27,7 @@ export const getPostsByFollowersId: QueryResolvers['getPostsByFollowersId'] = as
 
     return dateB.getTime() - dateA.getTime();
   });
+  const allPosts = [...mySortedPost, ...sortedPosts];
 
-  return sortedPosts;
+  return allPosts;
 };
