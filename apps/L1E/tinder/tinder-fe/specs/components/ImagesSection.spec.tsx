3ca3f1995bdf10edd36fc4ef2fbe3980709d4b73
24LP0612 24LP0612 <24LP0612@pinecones-iMac.local>
2024-12-26 14:35:37 +0800

    feat(tinder): image section UI

--- apps/L1E/tinder/tinder-fe/specs/components/ImagesSection.spec.tsx
+++
@@ -1,9 +0,0 @@
-import ImagesSection from '@/components/profile/ImagesSection';
-import { render, screen } from '@testing-library/react';
-
-describe('ImagesSection', () => {
-  it('should render nothing expect text', () => {
-    render(<ImagesSection />);
-    expect(screen.getByText('Images Section'));
-  });
-});
