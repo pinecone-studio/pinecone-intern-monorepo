72437083b0cc6cc527d37a6486132d95be05c3a2
24LP0612 24LP0612 <24LP0612@pinecones-iMac.local>
2024-12-26 12:13:05 +0800

    feat(tinder): bE profile

--- apps/L1E/tinder/tinder-be/src/resolvers/queries/profileUser/get-user.ts
+++ apps/L1E/tinder/tinder-be/src/resolvers/queries/profileUser/get-user.ts
@@ -1,7 +1,7 @@
 import { QueryResolvers } from '../../../generated';
 import { userModel } from '../../../models/user/user.model';
 
-const getUserById: QueryResolvers['getUserById'] = async (_, { userId }) => {
+export const getUserById: QueryResolvers['getUserById'] = async (_, { userId }) => {
   console.log(userId);
   try {
     const user = await userModel.findById({ _id: userId });
@@ -15,5 +15,3 @@ const getUserById: QueryResolvers['getUserById'] = async (_, { userId }) => {
     throw new Error('Failed to fetch user data. Please try again.');
   }
 };
-
-export default getUserById;
