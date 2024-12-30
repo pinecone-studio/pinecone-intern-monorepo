be70addfc0f1d1403c4833be1ff3a7efb997a816
24LP9087 24LP9087 <24LP9087@admins-iMac.local>
2024-12-30 17:51:37 +0800

    feat(tinder): match users backend

---
+++ apps/L1E/tinder/tinder-be/src/models/user/match.model.ts
@@ -0,0 +1,24 @@
+import { Model, Schema, models, model } from 'mongoose';
+
+export type MatchType = {
+  _id: Schema.Types.ObjectId;
+  userId: string;
+  targetUserId: string;
+  stillmatch: boolean;
+  createdAt: Date;
+};
+
+const MatchSchema = new Schema<MatchType>(
+  {
+    userId: { type: String, required: true, ref: 'Users' },
+    targetUserId: { type: String, required: true, ref: 'Users' },
+    stillmatch: { type: Boolean, required: true },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
+
+MatchSchema.index({ userId: 1, targetUserId: 1 }, { unique: true });
+
+export const matchModel: Model<MatchType> = models['Match'] || model<MatchType>('Match', MatchSchema);
