b9c3c5b84c4475fe4896b881e37b5b37606c3c75
24LP6376 24LP6376 <24LP6376@pinecones-iMac.local>
2025-01-03 20:31:23 +0800

    feat(hrms): fe-requeststory

--- apps/L1E/hrms/hrms-fe/src/components/MyRequest.tsx
+++ apps/L1E/hrms/hrms-fe/src/components/MyRequest.tsx
@@ -1,20 +1,11 @@
 'use client';
-import { Button } from '@/components/ui/button';
 
 import { Leave } from './Leave';
 import { PaidLeave } from './PaidLeave';
 import { RemoteWork } from './RemoteWork';
 import RequestStory from './RequestStory';
-import { DateRangePicker } from './DateRangePicker';
-import { addDays } from 'date-fns';
-import { DateRange } from 'react-day-picker';
-import { useState } from 'react';
 
 export const MyRequest = () => {
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: addDays(new Date(), -3),
-    to: new Date(),
-  });
   return (
     <div className="flex flex-col mt-[200px]">
       <div className="flex flex-row gap-5 items-center justify-center">
@@ -24,10 +15,7 @@ export const MyRequest = () => {
       </div>
       <div className="ml-[780px]">
         <div className="text-xl font-semibold mt-6">Миний явуулсан хүсэлтүүд:</div>
-        <div className="flex flex-row mt-4">
-          <DateRangePicker setDate={setDate} date={date} />
-          <Button className="flex ml-[262px]">+ Чөлөө хүсэх</Button>
-        </div>
+
         <RequestStory />
       </div>
     </div>
--- apps/L1E/hrms/hrms-fe/src/components/RequestStory.tsx
+++ apps/L1E/hrms/hrms-fe/src/components/RequestStory.tsx
@@ -1,25 +1,81 @@
 'use client';
 import { PiTagThin } from 'react-icons/pi';
 import { CiCalendar } from 'react-icons/ci';
+import { Button } from '@/components/ui/button';
+import { useGetRequestsByEmployeeQuery } from '@/generated';
+import { addDays, eachDayOfInterval, format } from 'date-fns';
+import { DateRangePicker } from './DateRangePicker';
+import { DateRange } from 'react-day-picker';
+import { useState } from 'react';
 
 const RequestStory = () => {
+  const statusMapping: Record<string, string> = {
+    FREE: 'Чөлөө',
+    PAID_LEAVE: 'Цалинтай чөлөө',
+    REMOTE: 'Зайнаас ажиллах',
+  };
+  const typeMapping: Record<string, string> = {
+    APPROVED: 'Баталгаажсан',
+    PENDING: 'Хүлээгдэж байна',
+    REJECTED: 'Татгалзсан',
+  };
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: addDays(new Date(), -3),
+    to: new Date(),
+  });
+  const daysArray = eachDayOfInterval({
+    start: date?.from || new Date(),
+    end: date?.to || new Date(),
+  });
+
+  const { data } = useGetRequestsByEmployeeQuery({ variables: { employeeId: '676e6e4007d5ae05a35cda9e' } });
+
   return (
     <div className="flex flex-col gap-[7px]">
-      <div className="flex flex-row gap-2 mt-4">
-        <span className="text-base font-medium">10/15</span>
-        <span className="text-[#09090B] text-base font-normal">Өнөөдөр</span>
+      <div
+        data-testid="btn"
+        onClick={() => {
+          setDate(undefined);
+        }}
+      ></div>
+      <div className="flex flex-row mt-4">
+        <DateRangePicker setDate={setDate} date={date} />
+        <Button className="flex ml-[262px]">+ Чөлөө хүсэх</Button>
       </div>
-      <div className="flex flex-col gap-2 text-xs font-normal w-[684px] h-[96px] p-6 border  rounded-xl ">
-        <div className="flex flex-row gap-2">
-          <PiTagThin className="w-4 h-4 " />
-          <div>Чөлөө (1 хоног)</div>
-          <div className="w-[122px] h-5 text-xs font-medium rounded-full bg-[#F9731633] pt-[2px] pb-[2px] pl-[10px] pr-[10px] ">Хүлээгдэж байна</div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <CiCalendar className="w-4 h-4 " />
-          <div>2024/10/25</div>
-        </div>
-      </div>
+      {daysArray.reverse().map((e, index) => {
+        const matchedRequest = data?.getRequestsByEmployee?.filter((el) => {
+          return e.toString() == el?.selectedDay;
+        });
+        if (matchedRequest) {
+          return (
+            <div key={index}>
+              {matchedRequest.map((el, index) => {
+                return (
+                  <div key={index}>
+                    <div className="flex flex-row gap-2 mt-4">
+                      <span className="text-base font-medium">{el?.selectedDay && format(el?.selectedDay, 'yyyy/MM/dd')}</span>
+                      <span className="text-[#09090B] text-base font-normal"></span>
+                    </div>
+                    <div className="flex flex-col gap-2 text-xs font-normal w-[684px] h-[96px] p-6 border  rounded-xl ">
+                      <div className="flex flex-row gap-2">
+                        <PiTagThin className="w-4 h-4 " />
+                        <div>{el?.requestStatus && statusMapping[el.requestStatus]}</div>
+                        <div className="w-[122px] flex justify-center h-5 text-xs font-medium rounded-full bg-[#F9731633] pt-[2px] pb-[2px] pl-[10px] pr-[10px] ">
+                          {el?.requestType && typeMapping[el.requestType]}
+                        </div>
+                      </div>
+                      <div className="flex flex-row gap-2">
+                        <CiCalendar className="w-4 h-4 " />
+                        <div>{el?.createdAt && format(el?.createdAt, 'yyyy/MM/dd')}</div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
