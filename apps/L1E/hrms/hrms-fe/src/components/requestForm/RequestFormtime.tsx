75abc5f26f6d0cb3158407c481a3857970e3bf97
Tserennorov <tsenoo.saruulod@gmail.com>
2025-01-06 16:00:32 +0800

    feat(hrms): fe-admin

--- apps/L1E/hrms/hrms-fe/src/components/requestForm/RequestFormtime.tsx
+++ apps/L1E/hrms/hrms-fe/src/components/requestForm/RequestFormtime.tsx
@@ -21,8 +21,7 @@ interface RequestcomPaidProps {
 const RequestcomTime1 = ({ leads, employee }: RequestcomPaidProps) => {
   const form = useForm<RequestsInput>({
     resolver: zodResolver(requestSchema),
-    defaultValues: { date: new Date(), startTime: '08:00', endTime: '09:00', leadEmployeeId: '', requestStatus: RequestStatus.Free, reason: '', employeeId: employee._id },
-  });
+    defaultValues: { date: new Date(), startTime: '08:00', endTime: '09:00', leadEmployeeId: '', requestStatus: RequestStatus.Free, reason: '', employeeId: employee._id },});
   const [isOpen, setIsOpen] = useState(false);
   const [createRequest] = useCreateRequestMutation();
   const onSubmit = async (data: RequestsInput) => {
@@ -30,10 +29,7 @@ const RequestcomTime1 = ({ leads, employee }: RequestcomPaidProps) => {
     const newdata: RequestInput = { selectedDay: date.toString().slice(0, 15), startTime, endTime, leadEmployeeId, requestStatus, reason, employeeId };
     await createRequest({ variables: { input: newdata } });
     setIsOpen(true);
-    setTimeout(() => {
-      form.reset();
-      setIsOpen(false);
-    }, 1500);
+    setTimeout(() => {form.reset(); setIsOpen(false); }, 1500);
   };
   return (
     <div className="space-y2 flex ">
