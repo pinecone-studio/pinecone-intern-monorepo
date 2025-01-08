822e0654e1f4e89a8f8de4af649da189f03dbaf4
24LU1697 24LU1697 <24LU1697@pinecones-iMac.local>
2025-01-08 19:10:51 +0800

    feat(hrms): page cypress

--- apps/L1E/hrms/hrms-fe/cypress/e2e/(main)/request-form/index.cy.tsx
+++ apps/L1E/hrms/hrms-fe/cypress/e2e/(main)/request-form/index.cy.tsx
@@ -11,7 +11,7 @@ describe('requestform page', () => {
     cy.get('.right-1>.lucide').click();
     cy.get(':nth-child(3) > :nth-child(7) > .rdp-button_reset').click();
     cy.get('[data-cy=lead-button]').click();
-    cy.get('[data-cy=Option-1]').trigger('keydown', { key: 'Enter' });
+    cy.get('[data-cy=lead-input]').click();
     cy.get('[data-cy=notes-input]').type('cypress-test');
     cy.get('[data-cy=paid-submit-button]').click();
     cy.contains('Амжилттай илгээгдлээ').should('not.be.visible');
