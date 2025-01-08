4a03f349272e7aace4923d45035ca5076daf8e57
Amardalai Batsundui <amardalaibatsundui@Amardalais-MacBook-Air.local>
2025-01-09 00:22:32 +0800

    feat(test): test

--- apps/L1FG/real-state/frontend/codegen.ts
+++ apps/L1FG/real-state/frontend/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.LOCAL_BACKEND_URI ?? process.env.BACKEND_URI,
+  schema: process.env.BACKEND_URI ?? process.env.LOCAL_BACKEND_URI,
   documents: ['apps/L1FG/real-state/frontend/src/**/*.graphql'],
   generates: {
     'apps/L1FG/real-state/frontend/src/generated/index.ts': {
