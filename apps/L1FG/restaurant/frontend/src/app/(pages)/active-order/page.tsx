da1f5858876afed36d45397635bc47383da85a90
Энх-Амар Болдбаатар <LPC240855@Users-iMac.local>
2025-01-08 12:44:13 +0800

    make folder stucture

---
+++ apps/L1FG/restaurant/frontend/src/app/(pages)/active-order/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ActiveOrder = () => {
+  return (
+    <div>ActiveOrder</div>
+  )
+}
+
+export default ActiveOrder
---
+++ apps/L1FG/restaurant/frontend/src/app/(pages)/active-orders-list/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ActiveOrdersList = () => {
+  return (
+    <div>ActiveOrdersList</div>
+  )
+}
+
+export default ActiveOrdersList
