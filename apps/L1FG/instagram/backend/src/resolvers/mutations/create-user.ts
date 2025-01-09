e5b4b5794ec52d8982c375fb5b7e138a3302187c
24LP8078 24LP8078 <24LP8078@pinecones-iMac.local>
2025-01-09 16:12:43 +0800

    feat(async): async

--- apps/L1FG/instagram/backend/src/resolvers/mutations/create-user.ts
+++ apps/L1FG/instagram/backend/src/resolvers/mutations/create-user.ts
@@ -1,11 +1,11 @@
 import { MutationResolvers } from "../../generated";
 import { UserModel } from "../../models/user.model";
 
-export const createUser: MutationResolvers['createUser']=(_, {input}) =>{
+export const createUser: MutationResolvers['createUser']=async(_, {input}) =>{
 const {userName, fullName, bio, profileImage, gender, hasStory, isPrivate, password
  }= input
 
- const user = UserModel.create({
+ const user = await UserModel.create({
     userName, fullName, bio, profileImage, gender, hasStory, isPrivate, password,
  })
 
