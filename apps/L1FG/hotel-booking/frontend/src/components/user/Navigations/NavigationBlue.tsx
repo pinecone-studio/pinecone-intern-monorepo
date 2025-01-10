60cf34fa33cc7309e58f316584050bc5493fa252
gereltbaatar <mglgerelt@gmail.com>
2025-01-10 22:25:11 +0800

    feat: navigation url

--- apps/L1FG/hotel-booking/frontend/src/components/user/Navigations/NavigationBlue.tsx
+++ apps/L1FG/hotel-booking/frontend/src/components/user/Navigations/NavigationBlue.tsx
@@ -4,7 +4,7 @@ export const NavigationBlue = () => {
   return (
     <main className="bg-[#013B94] h-[64px]">
       <div className="container mx-auto h-full flex items-center justify-between">
-        <Image src="./LogoBlue.png" alt="Logo" width={86} height={20} />
+        <Image src="/LogoBlue.png" alt="Logo" width={86} height={20} />
         <div className="flex gap-4">
           <div className="py-2 px-4">
             <p className="font-normal font-Inter text-sm text-[#FAFAFA]">Register</p>
--- apps/L1FG/hotel-booking/frontend/src/components/user/Navigations/NavigationWhite.tsx
+++ apps/L1FG/hotel-booking/frontend/src/components/user/Navigations/NavigationWhite.tsx
@@ -4,7 +4,7 @@ export const NavigationWhite = () => {
   return (
     <main className="bg-[#FFFFFF] h-[64px]">
       <div className="container mx-auto h-full flex items-center justify-between">
-        <Image src="./LogoWhite.png" alt="Logo" width={86} height={20} />
+        <Image src="/LogoWhite.png" alt="Logo" width={86} height={20} />
         <div className="flex gap-4">
           <div className="py-2 px-4">
             <p className="font-normal font-Inter text-sm text-[#09090B]">Register</p>
