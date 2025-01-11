c46301e5056cd790de045eb3802101718eafc565
gereltbaatar <mglgerelt@gmail.com>
2025-01-11 16:24:34 +0800

    feat: test

--- apps/L1FG/hotel-booking/frontend/src/components/user/features/Hero.tsx
+++ apps/L1FG/hotel-booking/frontend/src/components/user/features/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
     <main>
-      <div className="pt-10 pb-14s bg-[#013B94]">
+      <div className="pt-10 pb-14 bg-[#013B94]">
         <div className="container mx-auto">
           <div className="w-full flex justify-center items-center flex-col gap-1">
             <p className="font-Inter text-4xl text-[#FAFAFA] not-italic font-bold tracking-[-0.9px] ">Find the Best Hotel for Your Stay</p>
