dea1e9b77317933942406b815d1376083f2dedb5
24LP4300 24LP4300 <24LP4300@hishigee.local>
2025-01-13 14:58:52 +0800

    feat(post query): query bichsen

---
+++ apps/L1FG/real-state/backend/src/models/post.model.ts
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  PropertyOwnerId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  title: { type: String, required: true },
+  desciption: { type: String, required: true },
+  price: { type: Number, required: true },
+  propertyDetail: { type: mongoose.Schema.Types.ObjectId, required: true },
+  status: { type: String, enum: ['PENDING', 'APPROVED', 'DECLINED'], required: true },
+  updatedAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', postSchema);
---
+++ apps/L1FG/real-state/backend/src/schemas/post.schema.tsx
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+
+export const post = gql`
+
+scolar JSON
+
+  enum Status {
+    PENDING
+    APPROVED
+    DECLINED
+  }
+
+  type Post {
+    propertyOwnerId: ID
+    title: String
+    description: String
+    price: Int
+    propertyDetail: ID
+    status: Status
+    updatedAt: String
+    createdAt: String
+  }
+
+  type Query {
+  getPost (_id: ID) : Post !
+  getPosts [Post] !
+  }
+
+  type Mutations{
+  createdPost(title: String): Post!
+  updatedPost(_id : ID title: String): Post!
+  deletedPost(_id : ID) : Post! 
+  }
+`;
