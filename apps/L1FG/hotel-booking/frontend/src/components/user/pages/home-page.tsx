99330d3580c860a9f5b8a613161bb08ef3986249
Гэрэлтбаатар Мягмар <24LP8388@admins-iMac.local>
2025-01-14 19:19:12 +0800

    fix: home page deer HomePageCard ustagsan

--- apps/L1FG/hotel-booking/frontend/src/components/user/pages/home-page.tsx
+++ apps/L1FG/hotel-booking/frontend/src/components/user/pages/home-page.tsx
@@ -1,13 +1,11 @@
 import { Hero } from '../features/Hero';
 import { NavigationBlue } from '../Navigations';
-import { HomePageCard } from '../ui/cards';
 
 const HomePage = () => {
   return (
     <main>
       <NavigationBlue />
       <Hero />
-      <HomePageCard />
     </main>
   );
 };
