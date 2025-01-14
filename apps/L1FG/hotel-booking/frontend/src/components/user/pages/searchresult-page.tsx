306366848262df5d376ccda530e77563bd5040b5
Jwz-jwz <jnglobal444@gmail.com>
2025-01-14 18:02:41 +0800

    feat: made page

---
+++ apps/L1FG/hotel-booking/frontend/src/components/user/pages/searchresult-page.tsx
@@ -0,0 +1,11 @@
+import { MainResultSearch } from '../search-result/MainSearchResult';
+import HomePage from './home-page';
+
+export const SearchResultPage = () => {
+  return (
+    <div>
+      <HomePage />
+      <MainResultSearch />
+    </div>
+  );
+};
