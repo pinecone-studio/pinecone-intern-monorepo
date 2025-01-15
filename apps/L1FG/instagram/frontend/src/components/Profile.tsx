2ac6aa655d5d0842448c6fa361a8b566d87e3e44
Unuruu Anhbayr <unuruuanhbayr@Unuruus-MacBook-Pro.local>
2025-01-15 17:09:01 +0800

    feat: profile

--- apps/L1FG/instagram/frontend/src/components/Profile.tsx
+++ apps/L1FG/instagram/frontend/src/components/Profile.tsx
@@ -1,36 +1,49 @@
 import { IoSettingsOutline } from 'react-icons/io5';
 import Image from 'next/image';
 import { MenuButtons } from './Home/leftSideBar/MenuButtonsSideBar';
+
 const Profile = () => {
   return (
-    <div className="flex gap-[361px]  ">
-      <MenuButtons />
-      <div className="mt-14 flex gap-[100px]">
-        <div className="w-[150px] h-[150px] rounded-full overflow-hidden relative ">
-          <Image src="/images/profilePic.png" alt="zurag orno" fill className="object-cover" />
-        </div>
-        <div className="flex flex-col gap-4 ">
-          <div className="flex gap-3 itemst-center justify-center">
-            <p className="text-[20px] font-semibold">upvox_</p>
-            <button className="border px-4 py-2 bg-[#F4F4F5]  rounded-md text-sm  font-medium">Edit profile </button>
-            <button className="border px-4 py-2 bg-[#F4F4F5] rounded-md  text-sm  font-medium">Ad tools </button>
-            <div className="flex justify-center items-center">
-              <IoSettingsOutline />
-            </div>
-          </div>
-          <div className="flex gap-[32px]">
-            <div className="flex">
-              <p className="text-base font-semibold">11 </p>
-              <p className="text-base font-normal"> posts</p>
+    <div className="flex w-full">
+      <div className="w-[15%] border border-blue-800">
+        <MenuButtons />
+      </div>
+
+      <div className="w-[80%] flex justify-center py-10">
+        <div className="flex gap-20">
+          <Image src="/images/profilePic.png" alt="zurag" width={150} height={150} className="w-[150px] h-[150px] object-cover rounded-full bg-red-700" />
+
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-3 itemst-center justify-center">
+              <p className="text-[20px] font-semibold">upvox_</p>
+              <button className="border px-4 py-2 bg-[#F4F4F5]  rounded-md text-sm  font-medium">Edit profile </button>
+              <button className="border px-4 py-2 bg-[#F4F4F5] rounded-md  text-sm  font-medium">Ad tools </button>
+              <div className="flex justify-center items-center">
+                <IoSettingsOutline />
+              </div>
             </div>
 
-            <div className="flex">
-              <p className="text-base font-semibold">17 </p>
-              <p className="text-base font-normal"> followers</p>
+            <div className="flex w-full justify-between">
+              <div className="flex">
+                <p className="text-base font-semibold">11 </p>
+                <p className="text-base font-normal"> posts</p>
+              </div>
+
+              <div className="flex">
+                <p className="text-base font-semibold">17 </p>
+                <p className="text-base font-normal"> followers</p>
+              </div>
+              <div className="flex">
+                <p className="text-base font-semibold">14 </p>
+                <p className="text-base font-normal"> following </p>
+              </div>
             </div>
-            <div className="flex">
-              <p className="text-base font-semibold">14 </p>
-              <p className="text-base font-normal"> following </p>
+
+            <div>
+              <p className="text-base font-semibold">Upvox</p>
+              <p className="text-xs font-medium text-[#71717A]">Product/service</p>
+              <p className="text-sm font-medium text-[#09090B]"> Your favorite fun clips in language</p>
+              <p className="text-sm font medium text-[#2563EB]">upvox,.net</p>
             </div>
           </div>
         </div>
@@ -38,4 +51,5 @@ const Profile = () => {
     </div>
   );
 };
+
 export default Profile;
