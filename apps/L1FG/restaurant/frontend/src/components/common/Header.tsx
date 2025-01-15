cdad80b42136b4240417604c2af50dc4f003cb39
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-15 17:20:59 +0800

    feat(frontend): header hiiv

---
+++ apps/L1FG/restaurant/frontend/src/components/common/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AlignJustify, Bell, ShoppingCart } from 'lucide-react';
+const Header = () => {
+  return (
+    <div className="flex justify-between border-b border-[#ECEDF0] px-4 py-3">
+      <img className="h-8" src="/Logo.png" alt="" />
+      <div className="flex gap-3 items-center">
+        <ShoppingCart width={16} />
+        <Bell width={16} />
+        <AlignJustify width={16} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
