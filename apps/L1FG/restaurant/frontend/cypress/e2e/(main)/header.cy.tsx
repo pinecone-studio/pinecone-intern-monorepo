ab8829584e0dd6cc3d0208c0b23840e8d413cb4e
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-15 17:29:47 +0800

    feat(frontend): header test

--- apps/L1FG/restaurant/frontend/cypress/e2e/(main)/header.cy.tsx
+++ apps/L1FG/restaurant/frontend/cypress/e2e/(main)/header.cy.tsx
@@ -1,5 +1,22 @@
-describe('Header', () => {
-  it(`Should render home page`, () => {
+describe('Header Component', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('should render the logo', () => {
+    cy.get('img[alt=""]').should('be.visible');
+    cy.get('img[alt=""]').should('have.attr', 'src', '/Logo.png');
+  });
+
+  it('should render the icons', () => {
+    cy.get('svg[data-icon="shopping-cart"]').should('be.visible');
+
+    cy.get('svg[data-icon="bell"]').should('be.visible');
+
+    cy.get('svg[data-icon="align-justify"]').should('be.visible');
+  });
+
+  it('should have correct layout', () => {
+    cy.get('div.flex.justify-between.border-b').should('exist');
   });
 });
