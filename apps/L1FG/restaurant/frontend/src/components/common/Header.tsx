458699aa2940fd0eb22ef9a2a6bdc5229abf93dc
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-15 17:58:36 +0800

    fix(frontend): eslint fix header

--- apps/L1FG/restaurant/frontend/src/components/common/Header.tsx
+++ apps/L1FG/restaurant/frontend/src/components/common/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AlignJustify, Bell, ShoppingCart } from 'lucide-react';
+import Image from 'next/image';
 const Header = () => {
   return (
     <div className="flex justify-between border-b border-[#ECEDF0] px-4 py-3">
-      <img className="h-8" src="/Logo.png" alt="" />
+      <Image className="h-8" src="/Logo.png" alt="" />
       <div className="flex gap-3 items-center">
         <ShoppingCart width={16} />
         <Bell width={16} />
