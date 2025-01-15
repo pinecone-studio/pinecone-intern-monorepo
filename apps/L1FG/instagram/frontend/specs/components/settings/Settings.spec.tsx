18705f3838e4d4ff4689819212cde8df61b5fc2d
Мөнхсаруул Ганбат <24LP9058@pinecones-iMac.local>
2025-01-15 17:02:58 +0800

    feat: aaa

---
+++ apps/L1FG/instagram/frontend/specs/components/settings/Settings.spec.tsx
@@ -0,0 +1,7 @@
+import Settings from '@/components/settings/Settings';
+import { render } from '@testing-library/react';
+describe('Settings', () => {
+  it('Should render', () => {
+    render(<Settings />);
+  });
+});
