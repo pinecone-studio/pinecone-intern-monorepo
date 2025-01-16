2f51325396c7748538236676cd6ea8293cb0f411
24LP5308 24LP5308 <24LP5308@admins-iMac.local>
2025-01-16 15:01:43 +0800

    fix(frontend): e2e test

--- apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/search/index.cy.tsx
+++ apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/search/index.cy.tsx
@@ -5,4 +5,10 @@ describe('Search Page', () => {
     cy.get('[data-cy="search-input"]').type('search');
     cy.get('[data-cy="search-input"]').should('have.value', 'search');
   });
+
+  it('should show loading when data is being fetched', () => {
+    cy.visit('/search');
+    cy.get('[data-cy="search-page-get-data-loading"]').should('be.visible');
+    cy.get('[data-cy="search-page-get-data-loading"]').should('have.text', 'loading');
+  });
 });
