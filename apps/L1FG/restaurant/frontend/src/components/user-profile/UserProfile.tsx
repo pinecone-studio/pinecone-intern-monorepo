e5ef8d28e63db55b9bc7e5ad8d03f416e6b9e63f
Ганжаргал Гантөмөр <24LP0756@G401-CC0212.local>
2025-01-17 14:51:07 +0800

    feat: order history

--- apps/L1FG/restaurant/frontend/src/components/user-profile/UserProfile.tsx
+++ apps/L1FG/restaurant/frontend/src/components/user-profile/UserProfile.tsx
@@ -5,41 +5,42 @@ import { Shop } from '@/components/svg/Shop';
 import Image from 'next/image';
 import React from 'react';
 
-const UserProfile  = () => {
-    const user = {
-        Image:null,
-        phone: 91919191,
-        gmail:"ganjaa@gmail.com",
-        password: 11111
-    }
-    
+const UserProfile = () => {
+  const user = {
+    Image: null,
+    phone: 91919191,
+    gmail: "ganjaa@gmail.com",
+    password: 11111
+  }
+
   return (
     <div className="max-h-[500px] bg-white text-center flex flex-col items-center p-4">
-  
+
       <header className="w-full flex items-center justify-between mb-6">
-        <div>
+        <div className='relativ'>
           <Image
             src="/Logo.png"
             alt=""
-            className="w-full h-full object-contain"
+            fill
+            className=" object-contain"
           />
         </div>
         <div className="flex items-center space-x-4">
           <button>
-            <span className="material-icons"><Shop/></span>
+            <span className="material-icons"><Shop /></span>
           </button>
           <button>
-            <span className="material-icons"><Notification/></span>
+            <span className="material-icons"><Notification /></span>
           </button>
           <button>
-            <span className="material-icons"><Menu/></span>
+            <span className="material-icons"><Menu /></span>
           </button>
         </div>
       </header>
 
-   
+
       <div className="flex flex-col items-center bg-gray-100 min-h-[679px] rounded-xl shadow-md p-6 w-full max-w-sm">
-     
+
         <div className="relative">
           <div className="w-24 h-24 rounded-full bg-gray-300 flex items-center justify-center">
             <span className="text-4xl text-gray-600 material-icons">{user.Image}</span>
@@ -48,13 +49,13 @@ const UserProfile  = () => {
             className="absolute bottom-0 right-0 bg-white p-1 rounded-full shadow-sm"
             aria-label="Edit avatar"
           >
-            <span className="material-icons text-gray-500"><Edit/></span>
+            <span className="material-icons text-gray-500"><Edit /></span>
           </button>
         </div>
 
 
         <div className="mt-4 w-full flex flex-col gap-5 text-left">
-         
+
           <div className="flex flex-col mb-4">
             <p className="text-gray-700 font-medium">Утас:</p>
             <div className="flex items-center justify-between">
@@ -63,7 +64,7 @@ const UserProfile  = () => {
                 className="ml-2 text-gray-500 hover:text-gray-700"
                 aria-label="Edit phone number"
               >
-                <span className="material-icons"><Edit/></span>
+                <span className="material-icons"><Edit /></span>
               </button>
             </div>
           </div>
@@ -77,7 +78,7 @@ const UserProfile  = () => {
                 className="ml-2 text-gray-500 hover:text-gray-700"
                 aria-label="Edit email"
               >
-                <span className="material-icons"><Edit/></span>
+                <span className="material-icons"><Edit /></span>
               </button>
             </div>
           </div>
@@ -91,7 +92,7 @@ const UserProfile  = () => {
                 className="ml-2 text-gray-500 hover:text-gray-700"
                 aria-label="Edit password"
               >
-                <span className="material-icons"><Edit/></span>
+                <span className="material-icons"><Edit /></span>
               </button>
             </div>
           </div>
