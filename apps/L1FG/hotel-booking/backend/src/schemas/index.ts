461a8d75b91e4dcb5ee305daacfc51e42e4f3309
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-01-17 17:10:54 +0800

    admin backend user register done

--- apps/L1FG/hotel-booking/backend/src/schemas/index.ts
+++ apps/L1FG/hotel-booking/backend/src/schemas/index.ts
@@ -2,5 +2,6 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
 import { typeDefs as HotelTypeDefs } from './hotel.schema';
 import { typeDefs as RoomTypeDefs } from './room.schema';
+import { typeDefs as UserTypeDefs } from './user.schema';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs, HotelTypeDefs, RoomTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, HotelTypeDefs, RoomTypeDefs, UserTypeDefs]);
