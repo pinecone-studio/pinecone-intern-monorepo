3339229fbea1f8160b410fab7931d5aa84207776
14Berhee <berhee14@gmail.com>
2025-01-18 22:32:39 +0800

    feat(owner backend): owneriin update, create mutation , model , resolver, jest mock bichev

--- apps/L1FG/real-state/backend/src/schemas/owner-schema.ts
+++ apps/L1FG/real-state/backend/src/schemas/owner-schema.ts
@@ -1,44 +1,56 @@
 import gql from 'graphql-tag';
 
 export const OwnerTypeDefs = gql`
+  scalar JSON
 
-scalar JSON
+  type Owner {
+    _id: ID!
+    propertyOwnerId: ID!
+    title: String!
+    description: String!
+    price: Int!
+    propertyDetail: ID!
+    status: OwnerStats!
+    updatedAt: String!
+    createdAt: String!
+  }
 
-type Owner {
-  _id: ID!
-  propertyOwnerId: ID!
-  title: String!
-  description: String!
-  price: Int!
-  propertyDetail: ID!
-  status: Status!
-  updatedAt: String!
-  createdAt: String!
-}
+  enum OwnerStats {
+    PENDING
+    APPROVED
+    DECLINED
+  }
 
-enum Status {
-  PENDING
-  APPROVED
-  DECLINED
-}
+  input OwnerInput {
+    propertyOwnerId: ID!
+    title: String!
+    description: String!
+    price: Int!
+    propertyDetail: ID!
+    status: OwnerStats!
+    updatedAt: String!
+    createdAt: String!
+  }
 
-input OwnerInput {
-  propertyOwnerId: ID!
-  title: String!
-  description: String!
-  price: Int!
-  propertyDetail: ID!
-  status: Status!
-}
+  input OwnerUpdateInput {
+    propertyOwnerId: ID
+    title: String
+    description: String
+    price: Int
+    propertyDetail: ID
+    status: OwnerStats
+    updatedAt: String
+    createdAt: String
+  }
 
-type Query {
-  getOwner(_id: ID!): Owner!
-  getOwners: [Owner!]!
-}
+  type Query {
+    getOwner(_id: ID!): Owner!
+    getOwners: [Owner!]!
+  }
 
-type Mutation {
-  createOwner(input: OwnerInput!): Owner!
-  updateOwner(_id: ID!, input: OwnerInput!): Owner!
-  deleteOwner(_id: ID!): Owner!
-}
+  type Mutation {
+    createOwner(input: OwnerInput!): Owner!
+    updateOwner(_id: ID!, input: OwnerUpdateInput!): Owner!
+    deleteOwner(_id: ID!): Owner!
+  }
 `;
