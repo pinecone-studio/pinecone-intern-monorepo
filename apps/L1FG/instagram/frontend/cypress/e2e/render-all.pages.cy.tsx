255f5b9e62d00719851845928d55171eba5c6659
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-01-19 20:57:33 +0800

    fix: render all page e2e fixed

--- apps/L1FG/instagram/frontend/cypress/e2e/render-all.pages.cy.tsx
+++ apps/L1FG/instagram/frontend/cypress/e2e/render-all.pages.cy.tsx
@@ -1,24 +1,10 @@
-describe('Home Page', () => {
-  beforeEach(() => {
-    cy.visit('/sign-up');
-  });
-  it(`Should render signup page`, () => {
-    cy.get('[data-cy=signup-form]').should('be.visible');
-  });
-  it.only('Should successfully signup', () => {
-    cy.get('[data-cy=signup-email-input]').type(`${Date.now()}@gmail.com`);
-    cy.get('[data-cy=signup-password-input]').type('1234');
-    cy.get('[data-cy=signup-fullName-input]').type('ankhaa');
-    cy.get('[data-cy=signup-userName-input]').type('hi_');
-    cy.get('[data-cy=sign-up-button]').click();
-    cy.contains('Хэрэглэгч амжилттай бүртгэгдлээ').should('be.visible');
-  });
-  it('Should throw duplicate email error', () => {
-    cy.get('[data-cy=signup-email-input]').type('cypress@gmail.com');
-    cy.get('[data-cy=signup-password-input]').type('1234');
-    cy.get('[data-cy=signup-fullName-input]').type('hi hi');
-    cy.get('[data-cy=signup-userName-input]').type('hi_');
-    cy.get('[data-cy=sign-up-button]').click();
-    cy.contains('Бүртгэл амжилтгүй').should('be.visible');
+import allPages from '../utils/all-pages.json';
+
+describe('render all pages', () => {
+  it(`Should render all page`, () => {
+    cy.log(JSON.stringify(allPages));
+    allPages.forEach((page) => {
+      cy.visit(page.split('/apps/L1E/hrms/hrms-fe')[1]);
+    });
   });
 });
