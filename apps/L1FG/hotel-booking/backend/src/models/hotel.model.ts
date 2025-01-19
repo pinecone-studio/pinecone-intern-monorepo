c5f2116b08b7e7547e3e2a7cc5dcd40a4b2d720f
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-01-19 15:13:29 +0800

    database models addition done:

--- apps/L1FG/hotel-booking/backend/src/models/hotel.model.ts
+++ apps/L1FG/hotel-booking/backend/src/models/hotel.model.ts
@@ -9,14 +9,46 @@ const HotelSchema = new Schema<HotelType>({
   description: { type: String, required: true },
   images: { type: [String], required: true },
   rooms: { type: [Schema.Types.ObjectId], ref: 'rooms', default: [] },
-  faqs: { type: [String], default: [] },
-  policies: { type: [String], default: [] },
-  about: { type: [String], default: [] },
+  faqs: {
+    type: [
+      {
+        key: { type: String, required: true },
+        value: { type: String, required: true },
+      },
+    ],
+    default: [],
+  },
+  policies: {
+    type: [
+      {
+        key: { type: String, required: true },
+        value: { type: String, required: true },
+      },
+    ],
+    default: [],
+  },
+  about: {
+    type: [
+      {
+        key: { type: String, required: true },
+        value: { type: String, required: true },
+      },
+    ],
+    default: [],
+  },
   location: {
-    type: { type: String, enum: ['Point'], required: true },
-    coordinates: { type: [Number], required: true },
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
   },
   locationName: { type: String },
+  amenities: { type: [String] },
 });
 
 export const HotelModel = models['hotels'] || model<HotelType>('hotels', HotelSchema);
