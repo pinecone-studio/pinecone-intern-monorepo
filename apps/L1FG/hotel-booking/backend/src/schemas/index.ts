0892d20d949a7400f0c2f49b109e6f80de039624
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-01-19 18:12:15 +0800

    adminbackend create booking mutation done

--- apps/L1FG/hotel-booking/backend/src/schemas/index.ts
+++ apps/L1FG/hotel-booking/backend/src/schemas/index.ts
@@ -3,5 +3,6 @@ import { typeDefs as CommonTypeDefs } from './common.schema';
 import { typeDefs as HotelTypeDefs } from './hotel.schema';
 import { typeDefs as RoomTypeDefs } from './room.schema';
 import { typeDefs as UserTypeDefs } from './user.schema';
+import { typeDefs as BookingTypeDefs } from './booking.schema';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs, HotelTypeDefs, RoomTypeDefs, UserTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, HotelTypeDefs, RoomTypeDefs, UserTypeDefs, BookingTypeDefs]);
