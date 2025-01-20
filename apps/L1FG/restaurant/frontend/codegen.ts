6f22f1a13c86433d49c7f73a4b311dbd20072470
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-20 16:49:19 +0800

    fix(frontend): fix chema

--- apps/L1FG/restaurant/frontend/codegen.ts
+++ apps/L1FG/restaurant/frontend/codegen.ts
@@ -2,7 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:4200/api/graphql',
+  // schema: 'http://localhost:4200/api/graphql',
+  schema: process.env.BACKEND_URI ?? process.env.LOCAL_BACKEND_URI,
   documents: ['apps/L1FG/restaurant/frontend/src/**/*.graphql'],
   generates: {
     'apps/L1FG/restaurant/frontend/src/generated/index.ts': {
--- apps/L1FG/restaurant/frontend/specs/components/ordering/order-page.spec.tsx
+++ apps/L1FG/restaurant/frontend/specs/components/ordering/order-page.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-
+import '@testing-library/jest-dom';
 import { useGetCategoriesQuery, useGetFoodsQuery } from '@/generated';
 import OrderPageComponent from '@/components/order/OrderPageComponent';
 
