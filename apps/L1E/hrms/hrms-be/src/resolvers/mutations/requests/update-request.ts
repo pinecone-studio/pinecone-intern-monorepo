2d3c42b1b4f299bf34513c17a74cc373982b3b5a
24LP6376 24LP6376 <24LP6376@pinecones-iMac.local>
2025-01-20 19:14:43 +0800

    feat(hrms): update request mutation

--- apps/L1E/hrms/hrms-be/src/resolvers/mutations/requests/update-request.ts
+++ apps/L1E/hrms/hrms-be/src/resolvers/mutations/requests/update-request.ts
@@ -1,8 +1,49 @@
-import { MutationResolvers, Request } from '../../../generated';
-import { RequestModel } from '../../../models';
+/* eslint-disable complexity */
+import { Request, RequestStatus, RequestType, RequestUpdateInput } from '../../../generated';
+import { EmployeeModel, RequestModel } from '../../../models';
 
-export const updateRequest: MutationResolvers['updateRequest'] = async (_: unknown, { input, id }) => {
+export const updateRequest = async (_: unknown, { input, id }: { input: RequestUpdateInput; id: string }) => {
   const { reasonRefuse, requestType } = input;
+
+  const type = requestType;
+
+  if (type === RequestType.Approved) {
+    const request = await RequestModel.findById(id);
+
+    if (request.requestStatus === RequestStatus.Free) {
+      const employee = await EmployeeModel.findById(request.employeeId);
+
+      const { id, freeLimit } = employee;
+
+      const limit = freeLimit - 1;
+
+      await EmployeeModel.findByIdAndUpdate(id, {
+        freeLimit: limit,
+      });
+    }
+    if (request.requestStatus === RequestStatus.Remote) {
+      const employee = await EmployeeModel.findById(request.employeeId);
+
+      const { id, paidLeaveLimit } = employee;
+
+      const limit = paidLeaveLimit - 1;
+
+      await EmployeeModel.findByIdAndUpdate(id, {
+        paidLeaveLimit: limit,
+      });
+    }
+    if (request.requestStatus === RequestStatus.PaidLeave) {
+      const employee = await EmployeeModel.findById(request.employeeId);
+
+      const { id, remoteLimit } = employee;
+
+      const limit = remoteLimit - 1;
+
+      await EmployeeModel.findByIdAndUpdate(id, {
+        remoteLimit: limit,
+      });
+    }
+  }
 
   const updatedAt = new Date();
 
@@ -15,7 +56,6 @@ export const updateRequest: MutationResolvers['updateRequest'] = async (_: unkno
     },
     { new: true }
   ).populate('employeeId leadEmployeeId');
-  console.log(ResponseRequest);
 
   return ResponseRequest as Request;
 };
