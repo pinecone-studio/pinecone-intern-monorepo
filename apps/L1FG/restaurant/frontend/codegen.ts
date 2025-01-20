1325cb263e6e198790ab4bc738eff5cbc8f0a85a
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-20 13:56:56 +0800

    fix(frontend): changed schema

--- apps/L1FG/restaurant/frontend/codegen.ts
+++ apps/L1FG/restaurant/frontend/codegen.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:4200/api/graphql',
-  // schema: 'https://intern-1fg-restaurant-backend-testing-pinecone-studio.vercel.app/api/graphql',
+  // schema: 'http://localhost:4200/api/graphql',
+  schema: 'https://intern-1fg-restaurant-backend-testing-pinecone-studio.vercel.app/api/graphql',
   documents: ['apps/L1FG/restaurant/frontend/src/**/*.graphql'],
   generates: {
     'apps/L1FG/restaurant/frontend/src/generated/index.ts': {
