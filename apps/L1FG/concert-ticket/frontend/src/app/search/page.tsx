d1e06801e0dd644058d8b277d56d5fa45879c688
24LP5308 24LP5308 <24LP5308@admins-iMac.local>
2025-01-20 17:51:15 +0800

    fix(frontend): main hvleen awla

--- apps/L1FG/concert-ticket/frontend/src/app/search/page.tsx
+++ apps/L1FG/concert-ticket/frontend/src/app/search/page.tsx
@@ -39,7 +39,7 @@ const Page = () => {
     m-auto"
     >
       <SearchConcert data-cy="search-page-search-section" selected={date} onSelect={setDate} onChange={handlechange} />
-      {resultSearch?.length !== 0 ? <Cards cards={resultSearch} data-cy="search-page-cards" /> : <div className="text-white"> concerts not found</div>}
+      {resultSearch?.length !== 0 ? <Cards cards={resultSearch} data-cy="search-page-cards" /> : <div className="text-white"> Concerts not found</div>}
     </div>
   );
 };
