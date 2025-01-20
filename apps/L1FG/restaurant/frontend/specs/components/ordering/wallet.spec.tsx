eb6a685068ad94d6c507a23109a621347aeae32d
Ганжаргал Гантөмөр <24LP0756@G401-CC0212.local>
2025-01-20 14:09:04 +0800

    feat: wallet done

--- apps/L1FG/restaurant/frontend/specs/components/ordering/wallet.spec.tsx
+++ apps/L1FG/restaurant/frontend/specs/components/ordering/wallet.spec.tsx
@@ -1,7 +1,5 @@
 import Wallet from '@/components/order/Wallet';
 import { render } from '@testing-library/react';
-
-
 describe('Wallet', () => {
   it('wallet renders successfully', async () => {
     render(<Wallet/>);
