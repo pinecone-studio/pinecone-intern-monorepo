1b25d5837a12bd9a9088830e420300e37264edd6
gereltbaatar <mglgerelt@gmail.com>
2025-01-21 19:22:29 +0800

    feat: git change 2

--- apps/L1FG/hotel-booking/frontend/specs/components/admin/main/Sidebar.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from '@/features/admin/main/Sidebar';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-describe('Sidebar Component', () => {
-  it('renders Sidebar successfully', () => {
-    render(<Sidebar hotels="5" guests="3" />);
-  });
-});
