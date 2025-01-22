df5ef98d3d0adbe70866c0a5944a7c843de81392
24LP5308 24LP5308 <24LP5308@admins-iMac.local>
2025-01-22 17:41:03 +0800

    fix(frontend): e2e error zsw

--- apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/order/index.cy.tsx
+++ apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/order/index.cy.tsx
@@ -1,6 +1,6 @@
 describe('order page', () => {
   it('should render order page', () => {
-    cy.visit('/orderHistory');
+    cy.visit('/order');
     cy.get('[data-cy="order-page"]').should('be.visible');
   });
 });
