1e656ffb8b0ae568cc0deeef4ce59cd49292aef2
zZzombiee <itgelt.t.gi@gmail.com>
2025-01-21 22:43:11 -0800

    Create and Get user back-end

---
+++ apps/L1FG/restaurant/backend/src/schemas/user.schema.ts
@@ -0,0 +1,45 @@
+import gql from 'graphql-tag';
+
+export const userTypeDefs = gql`
+  scalar JSON
+  scalar Date
+
+  enum Response {
+    Success
+  }
+
+  input RegisterInput {
+    userName: String!
+    email: String!
+    password: String!
+    rePassword: String!
+  }
+
+  type UserType {
+    _id: ID!
+    userName: String!
+    email: String!
+    profileImage: String
+  }
+
+  input UpdateUserNameType {
+    _id: ID!
+    userName: String!
+  }
+
+  type UpdatedUserNameType {
+    _id: ID!
+    userName: String!
+  }
+
+  type Query {
+    sampleQuery: String!
+    getUser(_id: ID!): UserType!
+  }
+
+  type Mutation {
+    sampleMutation: String!
+    createUser(input: RegisterInput!): UserType!
+    updateUser(input: UpdateUserNameType!): UpdatedUserNameType!
+  }
+`;
