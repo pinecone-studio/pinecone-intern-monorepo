8710b5c99fbf5b353b83b68006f5a4a192a83033
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-01-23 23:26:18 +0800

    feat: adf

--- apps/L1FG/instagram/backend/src/models/story-view.model.ts
+++ apps/L1FG/instagram/backend/src/models/story-view.model.ts
@@ -1,9 +1,8 @@
 import mongoose, { model, models, Schema } from 'mongoose';
 
 const storyViewSchema = new Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: 'User' },
-  storyNodeId: { type: mongoose.Types.ObjectId, ref: 'StoryNode' },
-  latestStory: { type: mongoose.Types.ObjectId },
+  ownerId: { type: mongoose.Types.ObjectId, ref: 'User' },
+  viewerId: { type: mongoose.Types.ObjectId, ref: 'User' },
   seen: { type: Date, default: new Date() },
 });
 
