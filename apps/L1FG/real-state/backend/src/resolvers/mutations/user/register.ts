bbc2e54c5f96e32e8d6e8499f1851b5e6ffc1202
Amardalai Batsundui <amardalaibatsundui@Amardalais-MacBook-Air.local>
2025-01-23 11:18:26 +0800

    feat(real estate backend user over): hash token and otp

--- apps/L1FG/real-state/backend/src/resolvers/mutations/user/register.ts
+++ apps/L1FG/real-state/backend/src/resolvers/mutations/user/register.ts
@@ -1,18 +1,27 @@
 import { MutationResolvers } from '../../../generated';
 import { UserModel } from '../../../models';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const register: MutationResolvers['register'] = async (_, { input }) => {
-  const { email } = input;
+  const { email, name, phone, password } = input;
 
   const user = await UserModel.findOne({ email });
 
-  if (user) throw new Error('User already exists');
+  if (user) throw new Error('Имэйл бүртгэлтэй байна !');
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await UserModel.create({
-    ...input,
+    email,
+    name,
+    phone,
+    password: hashedPassword,
   });
 
+  const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET!);
   return {
     user: newUser,
+    token,
   };
 };
