9ec382dcc47fe08e687186006e36d7490c2badf4
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-01-24 08:08:12 +0800

    feat: get story seen

--- apps/L1FG/instagram/backend/src/models/story-view.model.ts
+++ apps/L1FG/instagram/backend/src/models/story-view.model.ts
@@ -3,7 +3,7 @@ import mongoose, { model, models, Schema } from 'mongoose';
 const storyViewSchema = new Schema({
   ownerId: { type: mongoose.Types.ObjectId, ref: 'User' },
   viewerId: { type: mongoose.Types.ObjectId, ref: 'User' },
-  seen: { type: Date, default: new Date() },
+  seen: { type: Date, default: 0 },
 });
 
 export const StoryViewModel = models['StoryView'] || model('StoryView', storyViewSchema);
