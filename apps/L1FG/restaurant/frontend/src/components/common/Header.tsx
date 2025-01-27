30097e43c3b0b49da1c006d526aa33d157ddfc11
Энх-Амар Болдбаатар <LPC240855@admins-iMac.local>
2025-01-27 14:11:13 +0800

    feat(frontend): order dine

--- apps/L1FG/restaurant/frontend/src/components/common/Header.tsx
+++ apps/L1FG/restaurant/frontend/src/components/common/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div className="flex justify-between border-b border-[#ECEDF0] px-4 py-3">
+    <div className="flex justify-between border-b bg-white border-[#ECEDF0] px-4 py-3">
       <Image className="h-8" height={32} width={32} src="/Logo.png" alt="" />
       <div className="flex gap-3 items-center">
         <ShoppingCart data-testid="shopping-cart-icon" width={16} />
