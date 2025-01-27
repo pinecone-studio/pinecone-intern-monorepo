d4664ac01eec4f628080b1a523a48cec167614d1
Amardalai Batsundui <amardalaibatsundui@Amardalais-MacBook-Air.local>
2025-01-27 17:13:43 +0800

    feat(real estate backend property queries): not include populate

---
+++ apps/L1FG/real-state/backend/specs/resolvers/queries/property/get-property-by-id.spec.ts
@@ -0,0 +1,18 @@
+import { getPropertyByID } from 'apps/L1FG/real-state/backend/src/resolvers/queries';
+import { GraphQLResolveInfo } from 'graphql';
+
+jest.mock('../../../../src/models', () => ({
+  PropertyFeatureModel: {
+    findById: jest.fn().mockResolvedValueOnce({ _id: '1' }).mockResolvedValueOnce(null),
+  },
+}));
+
+describe('getByPropertyId', () => {
+  it('should get by id', async () => {
+    const response = await getPropertyByID!({}, { _id: '1' }, { userId: '1' }, {} as GraphQLResolveInfo);
+    expect(response).toEqual({ _id: '1' });
+  });
+  it('should not get by id', async () => {
+    await expect(getPropertyByID!({}, { _id: '1' }, { userId: '1' }, {} as GraphQLResolveInfo)).rejects.toThrow('Property not found');
+  });
+});
