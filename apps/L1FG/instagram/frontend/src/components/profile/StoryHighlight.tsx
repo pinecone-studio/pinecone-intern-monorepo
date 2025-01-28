75a1a23f9ba48228c932466446a48db8d8712532
Unuruu Anhbayr <unuruuanhbayr@Unuruus-MacBook-Pro.local>
2025-01-28 18:20:37 +0800

    feat: profile following followers modal

--- apps/L1FG/instagram/frontend/src/components/profile/StoryHighlight.tsx
+++ apps/L1FG/instagram/frontend/src/components/profile/StoryHighlight.tsx
@@ -1,15 +1,18 @@
 import { Plus } from 'lucide-react';
+import StoryHighlightModal from './StoryHighlightModal';
 
 const StoryHighlight = () => {
   return (
-    <div className="p-8">
-      <div className="flex flex-col gap-4">
-        <p className="rounded-full border h-20 w-20 flex justify-center items-center bg-gray-100 ">
-          <Plus className="h-10 w-10  " color="white " />
-        </p>
-        <p className="ml-7">New</p>
+    <StoryHighlightModal>
+      <div className="p-8">
+        <div className="flex flex-col gap-4">
+          <p className="rounded-full border h-20 w-20 flex justify-center items-center bg-gray-100 ">
+            <Plus className="h-10 w-10  " color="white " />
+          </p>
+          <p className="ml-7">New</p>
+        </div>
       </div>
-    </div>
+    </StoryHighlightModal>
   );
 };
 export default StoryHighlight;
