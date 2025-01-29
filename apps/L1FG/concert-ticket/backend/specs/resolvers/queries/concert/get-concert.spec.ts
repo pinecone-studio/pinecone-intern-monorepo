0a8506b04a76e7e0793c5297371a42a5a54c3144
24LP5308 24LP5308 <24LP5308@admins-iMac.local>
2025-01-29 16:13:43 +0800

    feat(backend): get concertid

---
+++ apps/L1FG/concert-ticket/backend/specs/resolvers/queries/concert/get-concert.spec.ts
@@ -0,0 +1,42 @@
+import { getConcert } from 'apps/L1FG/concert-ticket/backend/src/resolvers/queries';
+import { GraphQLResolveInfo } from 'graphql';
+
+jest.mock('apps/L1FG/concert-ticket/backend/src/models', () => ({
+  ConcertModel: {
+    findById: jest
+      .fn()
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce({
+        _id: '1',
+        concertName: '',
+        concertPlan: '',
+        artistName: [],
+        concertDay: '',
+        concertTime: '',
+        concertPhoto: '',
+        vipTicket: { price: 2, quantity: 2 },
+        regularTicket: { price: 2, quantity: 2 },
+        standingAreaTicket: { price: 2, quantity: 2 },
+      }),
+  },
+}));
+describe('getconcert', () => {
+  it('not found concert', async () => {
+    await expect(getConcert!({}, { _id: 'not found' }, {}, {} as GraphQLResolveInfo)).rejects.toThrow('concert not found');
+  });
+  it('should get concert', async () => {
+    const result = await getConcert!({}, { _id: '1' }, {}, {} as GraphQLResolveInfo);
+    expect(result).toEqual({
+      _id: '1',
+      concertName: '',
+      concertPlan: '',
+      artistName: [],
+      concertDay: '',
+      concertTime: '',
+      concertPhoto: '',
+      vipTicket: { price: 2, quantity: 2 },
+      regularTicket: { price: 2, quantity: 2 },
+      standingAreaTicket: { price: 2, quantity: 2 },
+    });
+  });
+});
