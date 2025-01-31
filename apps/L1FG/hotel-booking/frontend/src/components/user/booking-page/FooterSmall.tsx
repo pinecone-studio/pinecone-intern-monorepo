a18641ee3eb4a3349cbfb72459b675fc764bb8ef
Өлзийбадрах Батгэрэл <24LP6749@pinecones-iMac.local>
2025-01-31 17:40:43 +0800

    feat: added booking page

---
+++ apps/L1FG/hotel-booking/frontend/src/components/user/booking-page/FooterSmall.tsx
@@ -0,0 +1,11 @@
+import { LogoIcon } from '../ui/svg/LogoIcon';
+
+export const FooterSmall = () => {
+  return (
+    <>
+      <footer className="container mx-auto flex items-start w-full py-10 mt-auto">
+        <LogoIcon />
+      </footer>
+    </>
+  );
+};
