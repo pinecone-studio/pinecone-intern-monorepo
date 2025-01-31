35922b5a422cf218ddeaba7ae76f0fcb2b84c32b
24LP5308 24LP5308 <24LP5308@admins-iMac.local>
2025-01-31 21:17:07 +0800

    feat(frontend): concert-detail-component

--- apps/L1FG/concert-ticket/frontend/specs/components/ticketReservation/Ticketreservation.spec.tsx
+++ apps/L1FG/concert-ticket/frontend/specs/components/ticketReservation/Ticketreservation.spec.tsx
@@ -1,9 +1,95 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Ticketreservation from '@/components/ticketReservation/Ticketreservation';
+import { TicketReservation } from '@/components/ticketReservation/TicketReservation';
 
-describe('Ticketreservation', () => {
-  it('render Ticketreservation component', () => {
-    render(<Ticketreservation />);
+describe('TicketReservation Component', () => {
+  const getErrorMessage = () => screen.queryByTestId('standart-count');
+  it('renders ticket information (price and available count)', async () => {
+    render(<TicketReservation vip={20} standart={20} rear={20} />);
+
+    expect(
+      screen.getByText((content) => {
+        return content.startsWith('Энгийн тасалбар') && content.includes('(20)');
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('99000₮')).toBeInTheDocument();
+
+    expect(
+      screen.getByText((content) => {
+        return content.startsWith('Арын тасалбар') && content.includes('(20)');
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('129000₮')).toBeInTheDocument();
+
+    expect(
+      screen.getByText((content) => {
+        return content.startsWith('VIP тасалбар') && content.includes('(20)');
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('159000₮')).toBeInTheDocument();
+  });
+
+  it(' decrements standard ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+    const standartDecrement = getByTestId('standart-decrement');
+    fireEvent.click(standartDecrement);
+  });
+
+  it(' increment standard ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={1} standart={1} rear={1} />);
+    const standartIncrement = getByTestId('standart-increment');
+    fireEvent.click(standartIncrement);
+  });
+
+  it(' decrements vip ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+    const vipDecrement = getByTestId('vip-decrement');
+    fireEvent.click(vipDecrement);
+  });
+
+  it(' increment vip ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+    const vipIncrement = getByTestId('vip-increment');
+    fireEvent.click(vipIncrement);
+  });
+
+  it(' decrements rear ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+    const rearDecrement = getByTestId('rear-decrement');
+    fireEvent.click(rearDecrement);
+  });
+
+  it(' increment raer ticket count', async () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+    const rearIncrement = getByTestId('rear-increment');
+    fireEvent.click(rearIncrement);
+  });
+
+  it('error message appears when standard tickets are sold out', () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+
+    const incrementButton = getByTestId('standart-increment');
+
+    for (let i = 0; i < 20; i++) {
+      fireEvent.click(incrementButton);
+    }
+
+    expect(getErrorMessage()).toBeInTheDocument();
+    expect(getErrorMessage()).toHaveTextContent('20-ш тасалбар захиалах боломжгүй байна.');
+  });
+  it('error message appears when rear tickets are sold out', () => {
+    const { getByTestId } = render(<TicketReservation vip={20} standart={20} rear={20} />);
+
+    const incrementButton = getByTestId('rear-increment');
+
+    for (let i = 0; i < 20; i++) {
+      fireEvent.click(incrementButton);
+    }
+
+    expect(getErrorMessage()).toBeInTheDocument();
+    expect(getErrorMessage()).toHaveTextContent('0-ш тасалбар захиалах боломжгүй байна.');
   });
 });
--- apps/L1FG/concert-ticket/frontend/src/components/ticketReservation/Ticketreservation.tsx
+++ apps/L1FG/concert-ticket/frontend/src/components/ticketReservation/Ticketreservation.tsx
@@ -1,73 +1,98 @@
-const Ticketreservation = () => {
+'use client';
+
+import { useState } from 'react';
+
+import StandartTicket from './StandartTicket';
+import RearTicket from './RearTicket';
+import { TotalPrice } from './TicketPrice';
+import VipTicket from './VipTicket';
+import SelectDemo from './SelectDay';
+
+type dataProps = {
+  vip: number;
+  rear: number;
+  standart: number;
+};
+export const TicketReservation = ({ vip, rear, standart }: dataProps) => {
+  const [standardCount, setStandardCount] = useState(0);
+  const [rearCount, setRearCount] = useState(0);
+  const [vipCount, setVipCount] = useState(0);
+
+  const standardPrice = 99000;
+  const rearPrice = 129000;
+  const vipPrice = 159000;
+
+  const standardRealCount = Math.max(0, standart - standardCount);
+  const rearRealCount = Math.max(0, rear - rearCount);
+  const vipRealCount = Math.max(0, vip - vipCount);
+
+  const total = standardCount * standardPrice + rearCount * rearPrice + vipCount * vipPrice;
+  const standartIncrement = () => {
+    setStandardCount(Math.max(0, standardCount + 1));
+  };
+  const standartDecrement = () => {
+    setStandardCount(Math.max(0, standardCount - 1));
+  };
   return (
-    <div className="w-[385px] mx-auto mt-[100px] h-[548px] bg-[#131313] rounded-xl p-[20px] flex-col items-center">
+    <div className="w-[385px] mx-auto mt-[100px] h-fit bg-[#131313] rounded-xl p-[20px] flex-col items-center">
       <div className="">
-        <p className="text-[#ababab]">Тоглолт үзэх өдрөө сонгоно уу.</p>
-        <img className="my-[15px]" src="/ComboBox.svg" />
+        <p className="text-[#ababab]">Тогло үзэх өдрөө сонгоно уу.</p>
+        <SelectDemo />
       </div>
-      <div className="w-[345px] h-[216px] my-[5px]">
+      <div className="w-[345px] h-[255px] my-[5px]">
         <div className="border-dashed border-b-2 border-[#27272A] "></div>
         <div className="w-[345px] h-[72px] flex items-center justify-between">
           <div className="flex items-center  justify-items-center">
             <div className="w-[10px] h-[10px] bg-[#C772C4] rounded-full mr-[10px]"></div>
             <div>
-              <p className="text-[#C772C4]">Завсарын тасалбар (38)</p>
-              <p className="text-white">99 000₮</p>
+              <p className="text-[#C772C4]" data-testid="standart-ticket">
+                Энгийн тасалбар ({standardRealCount})
+              </p>
+              <p className="text-white" data-testid="standart-ticket-price">
+                {standardPrice}₮
+              </p>
             </div>
           </div>
-          <div className="flex items-center">
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">-</div>
-            <div className="text-white mx-4">0</div>
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">+</div>
-          </div>
+
+          <StandartTicket standardCount={standardCount} Increment={standartIncrement} Decrement={standartDecrement} />
         </div>
+        <p data-testid="standart-count" className={`${standardRealCount <= 0 ? 'block' : 'hidden'} text-red-500 text-[12px]`}>
+          {standardCount}-ш тасалбар захиалах боломжгүй байна.
+        </p>
         <div className="border-dashed border-b-2 border-[#27272A] "></div>
         <div className="w-[345px] h-[72px] flex items-center justify-between">
           <div className="flex items-center  justify-items-center">
             <div className="w-[10px] h-[10px] bg-[#D7D7F8] rounded-full mr-[10px]"></div>
             <div>
-              <p className="text-[#D7D7F8]">Завсарын тасалбар (38)</p>
-              <p className="text-white">99 000₮</p>
+              <p className="text-[#D7D7F8]">Арын тасалбар ({rearRealCount})</p>
+              <p datatest-id="backside-" className="text-white">
+                {rearPrice}₮
+              </p>
             </div>
           </div>
-          <div className="flex items-center">
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">-</div>
-            <div className="text-white mx-4">0</div>
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">+</div>
-          </div>
+
+          <RearTicket rearCount={rearCount} Increment={() => setRearCount(Math.max(0, rearCount + 1))} Decrement={() => setRearCount(Math.max(0, rearCount - 1))} />
         </div>
+        <p data-testid="rear-count" className={`${rearRealCount <= 0 ? 'block' : 'hidden'} text-red-500 text-[12px]`}>
+          {rearCount}-ш тасалбар захиалах боломжгүй байна.
+        </p>
         <div className="border-dashed border-b-2 border-[#27272A] "></div>
         <div className="w-[345px] h-[72px] flex items-center justify-between">
           <div className="flex items-center  justify-items-center">
             <div className="w-[10px] h-[10px] bg-[#4651C9] rounded-full mr-[10px]"></div>
             <div>
-              <p className="text-[#4651C9]">Завсарын тасалбар (38)</p>
-              <p className="text-white">99 000₮</p>
+              <p className="text-[#4651C9]">VIP тасалбар ({vipRealCount})</p>
+              <p className="text-white">{vipPrice}₮</p>
             </div>
           </div>
-          <div className="flex items-center">
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">-</div>
-            <div className="text-white mx-4">0</div>
-            <div className="text-white w-[40px] h-[40px] rounded-sm bg-[#1F1F1F] flex justify-center items-center cursor-pointer">+</div>
-          </div>
+
+          <VipTicket vipCount={vipCount} Increment={() => setVipCount(Math.max(0, vipCount + 1))} Decrement={() => setVipCount(Math.max(0, vipCount - 1))} />
         </div>
+        {/* <p data-testid="vip-count" className={`${vipRealCount <= 0 ? 'block' : 'hidden'} text-red-500 text-[12px]`}>
+          {vipCount}-ш тасалбар захиалах боломжгүй байна.
+        </p> */}
       </div>
-      <div className="flex-col justify-around w-[345px] h-[116px] mx-auto my-[20px]">
-        <div className="flex items-center justify-between w-[345px] h-[20px] mb-[20px]">
-          <p className="text-[#A1A1AA] text-[14px]">Энгийн тасалбар x 3</p>
-          <p className="text-[#A1A1AA] text-[14px]">267 000₮</p>
-        </div>
-        <div className="flex items-center justify-between w-[345px] h-[20px] mb-[20px]">
-          <p className="text-[#A1A1AA] text-[14px]">VIP тасалбар x 2</p>
-          <p className="text-[#A1A1AA] text-[14px]">267 000₮</p>
-        </div>
-        <div className="flex items-center justify-between w-[345px] h-[20px] mb-[10px]">
-          <p className="text-[#A1A1AA] text-[14px]">Нийт төлөх дүн:</p>
-          <p className="text-white text-[18px] font-semibold">267 000₮</p>
-        </div>
-      </div>
-      <button className="w-[297px] h-[36px] mx-auto rounded-lg bg-[#00B7F4] hover:bg-[#7d4dbb] flex justify-center items-center">Тасалбар авах</button>
+      <TotalPrice standardCount={standardCount} standardPrice={standardPrice} rearCount={rearCount} rearPrice={rearPrice} vipCount={vipCount} vipPrice={vipPrice} total={total} />
     </div>
   );
 };
-export default Ticketreservation;
