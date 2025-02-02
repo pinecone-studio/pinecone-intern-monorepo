909436c3c33e2ed092ec502de8928e8b3b21f582
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-02-02 20:09:40 +0800

    star rating search query error fix

--- apps/L1FG/hotel-booking/backend/specs/resolvers/queries/hotel/get-hotels-by-star-rating.spec.ts
+++ apps/L1FG/hotel-booking/backend/specs/resolvers/queries/hotel/get-hotels-by-star-rating.spec.ts
@@ -1,5 +1,6 @@
 import { GraphQLResolveInfo } from 'graphql';
 import { getHotelsByStarRating } from 'apps/L1FG/hotel-booking/backend/src/resolvers/queries';
+import { HotelModel } from '../../../../src/models';
 
 jest.mock('../../../../src/models', () => ({
   HotelModel: {
@@ -13,8 +14,8 @@ describe('Get hotels star rating ihees baga ruu', () => {
   });
 
   it('Should get hotels, work fine', async () => {
-    (require('../../../../src/models').HotelModel.find as jest.Mock).mockReturnValue({
-      sort: jest.fn().mockResolvedValueOnce([{ id: '1' }]),
+    (HotelModel.find as jest.Mock).mockReturnValue({
+      sort: jest.fn().mockResolvedValue([{ id: '1' }]),
     });
 
     const mock = [{ id: '1' }];
@@ -23,8 +24,8 @@ describe('Get hotels star rating ihees baga ruu', () => {
   });
 
   it('Unknown error garah', async () => {
-    (require('../../../../src/models').HotelModel.find as jest.Mock).mockReturnValue({
-      sort: jest.fn().mockResolvedValueOnce(null),
+    (HotelModel.find as jest.Mock).mockReturnValue({
+      sort: jest.fn().mockResolvedValue(null),
     });
 
     const response = await getHotelsByStarRating!({}, {}, {}, {} as GraphQLResolveInfo);
