fbbaa295b94d71ae54ed178b72bd007a57eeec3d
24LP8078 24LP8078 <24LP8078@pinecones-iMac.local>
2025-02-04 16:17:57 +0800

    feat(insta): li

--- apps/L1FG/instagram/backend/specs/resolvers/mutations/post/delete.post.like.spec.ts
+++ apps/L1FG/instagram/backend/specs/resolvers/mutations/post/delete.post.like.spec.ts
@@ -1,9 +1,17 @@
 import { PostLikeModal } from 'apps/L1FG/instagram/backend/src/models';
+import { deletePostLike } from 'apps/L1FG/instagram/backend/src/resolvers/mutations';
+import { GraphQLResolveInfo } from 'graphql';
 
 jest.mock('apps/L1FG/instagram/backend/src/models');
 
 describe('delete post like', () => {
-  it('model find and delete', () => {
-    (PostLikeModal.findByIdAndDelete as jest.Mock).mockResolvedValue(null);
+  it('model find and delete', async () => {
+    (PostLikeModal.findByIdAndDelete as jest.Mock).mockResolvedValue(undefined);
+
+    if (!deletePostLike) return;
+
+    const result = await deletePostLike({}, { postId: '1' }, { userId: '2' }, {} as GraphQLResolveInfo);
+
+    expect(result).toEqual(undefined);
   });
 });
--- apps/L1FG/instagram/backend/src/resolvers/queries/post/get-has-like.ts
+++ apps/L1FG/instagram/backend/src/resolvers/queries/post/get-has-like.ts
@@ -2,9 +2,7 @@ import { QueryResolvers } from '../../../generated';
 import { PostLikeModal } from '../../../models';
 
 export const getLikedPost: QueryResolvers['getLikedPost'] = async (_, __, { userId }) => {
-  const liked = await PostLikeModal.findOne({ userId: userId });
+  const liked = await PostLikeModal.findOne({ userId });
 
-  if (!liked) return { hasLiked: false };
-
-  return liked;
+  return liked ? liked : { hasLiked: false };
 };
