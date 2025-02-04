4eeb5b519fc11b54130c04a3306833456bcc17df
Сэнгэдорж Өлзий-Баяр <24LP7143@admins-iMac.local>
2025-02-04 13:55:23 +0800

    feat: done

--- apps/L1FG/concert-ticket/frontend/src/components/providers/AuthProvider.tsx
+++ apps/L1FG/concert-ticket/frontend/src/components/providers/AuthProvider.tsx
@@ -1,74 +1,74 @@
 'use client';
 import { useSignInMutation, useSignUpMutation } from '@/generated';
 import { useRouter } from 'next/navigation';
-import { useContext } from 'react';
-import { createContext } from 'react';
-import { toast } from 'react-toastify';
+import { useState, useContext, createContext, useEffect } from 'react';
+import { useAlert } from './AlertProvider';
 
-type SigninParams = {
-  email: string;
-  password: string;
-};
-
+type SigninParams = { email: string; password: string };
+type User = { email: string };
 type AuthContextType = {
+  user: User | null;
   signin: (_params: SigninParams) => Promise<void>;
   signup: (_params: SigninParams) => Promise<void>;
+  logout: () => void;
 };
 
 const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const { showAlert } = useAlert();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   const [SignUpMutation] = useSignUpMutation({
     onCompleted: () => {
-      toast.success('Бүртгэл амжилттай үүслээ');
-      router.push('/login');
+      showAlert('success', 'Бүртгэл амжилттай үүслээ');
+      router.push('/signin');
     },
     onError: (error) => {
-      toast.error(error.message);
+      showAlert('error', error.message);
     },
   });
 
   const signup = async ({ email, password }: SigninParams) => {
-    await SignUpMutation({
-      variables: {
-        input: {
-          email,
-          password,
-        },
-      },
-    });
+    await SignUpMutation({ variables: { input: { email, password } } });
   };
+
   const [SignInMutation] = useSignInMutation({
     onCompleted: (data) => {
-      const token = data.signIn.token;
-      localStorage.setItem('token', token);
+      localStorage.setItem('token', data.signIn.token);
       localStorage.setItem('user', JSON.stringify(data.signIn.user));
-      toast.success('Амжилттай нэвтэрлээ', { autoClose: 2000 });
+      setUser(data.signIn.user);
+      showAlert('success', 'Амжилттай нэвтэрлээ');
       router.push('/');
     },
-
     onError: (error) => {
-      toast.error(`${error.message}`);
+      showAlert('error', error.message);
     },
   });
 
   const signin = async ({ email, password }: SigninParams) => {
-    await SignInMutation({
-      variables: {
-        input: {
-          email,
-          password,
-        },
-      },
-    });
+    await SignInMutation({ variables: { input: { email, password } } });
   };
 
-  return <AuthContext.Provider value={{ signin, signup }}>{children}</AuthContext.Provider>;
+  const logout = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    setUser(null);
+    showAlert('warning', 'Та гарлаа');
+    router.push('/signin');
+  };
+
+  return <AuthContext.Provider value={{ user, signin, signup, logout }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  return context;
+  return useContext(AuthContext);
 };
