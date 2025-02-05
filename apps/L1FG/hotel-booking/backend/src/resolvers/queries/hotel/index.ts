5fa9078a5a5d5e1c6d2fcdc4bcbb3c049b7e57c1
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-02-06 00:35:15 +0800

    date and traveller search query done

--- apps/L1FG/hotel-booking/backend/src/resolvers/queries/hotel/index.ts
+++ apps/L1FG/hotel-booking/backend/src/resolvers/queries/hotel/index.ts
@@ -3,3 +3,4 @@ export * from './get-hotel-by-id';
 export * from './get-hotels-by-name';
 export * from './get-hotels-by-price';
 export * from './get-hotels-by-star-rating';
+export * from './get-hotels-by-date-traveller';
