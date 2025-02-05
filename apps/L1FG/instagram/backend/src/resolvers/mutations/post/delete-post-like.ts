add29db25182b2c7f4957f6dbd7f69d527f2cb37
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-05 19:35:28 +0800

    feat: qwertyuiopadfghjkl;zxcvbnm,./

--- apps/L1FG/instagram/backend/src/resolvers/mutations/post/delete-post-like.ts
+++ apps/L1FG/instagram/backend/src/resolvers/mutations/post/delete-post-like.ts
@@ -3,6 +3,5 @@ import { PostLikeModal } from '../../../models';
 
 export const deletePostLike: MutationResolvers['deletePostLike'] = async (_, { postId }, { userId }) => {
   const likedPost = await PostLikeModal.findOneAndDelete({ postId }, { userId });
-
   return likedPost;
 };
--- apps/L1FG/instagram/backend/src/resolvers/queries/post/get-post-like.ts
+++
@@ -1,6 +0,0 @@
-// import { QueryResolvers } from '../../../generated';
-// import { PostLikeModal, UserModel } from '../../../models';
-
-// const getPostLike: QueryResolvers['getPostLike'] = async (__, __, { userId }) => {
-//   const postLike = PostLikeModal.find();
-// };
