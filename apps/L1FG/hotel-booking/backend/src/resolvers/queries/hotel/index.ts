934c378b49758c53e09a74063e3230a95192e9e2
Гэрэлтбаатар Мягмар <24LP8388@admins-iMac.local>
2025-02-06 18:50:35 +0800

    feat: getByDate, getByPrice hotel  hoy qeurie negtgesen neg querie bichsen

--- apps/L1FG/hotel-booking/backend/src/resolvers/queries/hotel/index.ts
+++ apps/L1FG/hotel-booking/backend/src/resolvers/queries/hotel/index.ts
@@ -4,3 +4,4 @@ export * from './get-hotels-by-name';
 export * from './get-hotels-by-price';
 export * from './get-hotels-by-star-rating';
 export * from './get-hotels-by-date-traveller';
+export * from './get-all-querie';
