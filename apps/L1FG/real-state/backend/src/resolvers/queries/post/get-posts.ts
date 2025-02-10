e3a72a5dcc4494244a0157e8f8d49d678feddba1
otgonnaran bayar <otgonnaran@gmail.com>
2025-02-10 11:18:18 +0800

    cover uncovered lines

--- apps/L1FG/real-state/backend/src/resolvers/queries/post/get-posts.ts
+++ apps/L1FG/real-state/backend/src/resolvers/queries/post/get-posts.ts
@@ -1,6 +1,5 @@
 import { Post } from '../../../models/post-model';
-
-import { QueryResolvers, Post as PostType } from '../../../generated';
+import { Post as PostType, QueryResolvers } from '../../../generated';
 
 export const getPosts: QueryResolvers['getPosts'] = async (_, input) => {
   const posts = await Post.find<PostType>(input).populate('propertyOwnerId');
