eb7ab913371e29248b68ee93429539192d54765d
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-10 17:13:23 +0800

    feat: asdfhkl;

---
+++ apps/L1FG/instagram/backend/src/resolvers/queries/user/get-following-suggestion.ts
@@ -0,0 +1,16 @@
+import { QueryResolvers } from '../../../generated';
+import { FollowerModel, UserModel } from '../../../models';
+
+export const getFollowingSuggestion: QueryResolvers['getFollowingSuggestion'] = async (_, __, { userId }) => {
+  if (!userId) {
+    throw new Error('Та нэвтэрнэ үү!');
+  }
+  console.log('follower model find', FollowerModel.find());
+  const following = await FollowerModel.find({ followerId: userId }).select('targetID');
+  const followingIds = following.map((entry) => entry._id.toString());
+  const suggestedUsers = await UserModel.find({
+    _id: { $nin: [...followingIds, userId] },
+  }).limit(6);
+
+  return suggestedUsers;
+};
