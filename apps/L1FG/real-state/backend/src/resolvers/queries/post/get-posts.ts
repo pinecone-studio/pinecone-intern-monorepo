a3a5be5c933e80e48753e21bdddc3fe89b9028d0
otgonnaran bayar <otgonnaran@gmail.com>
2025-02-10 09:58:47 +0800

    wq

--- apps/L1FG/real-state/backend/src/resolvers/queries/post/get-posts.ts
+++ apps/L1FG/real-state/backend/src/resolvers/queries/post/get-posts.ts
@@ -1,5 +1,6 @@
 import { Post } from '../../../models/post-model';
-import { Post as PostType, QueryResolvers } from '../../../generated';
+
+import { QueryResolvers, Post as PostType } from '../../../generated';
 
 export const getPosts: QueryResolvers['getPosts'] = async (_, input) => {
   const posts = await Post.find<PostType>(input).populate('propertyOwnerId');
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
