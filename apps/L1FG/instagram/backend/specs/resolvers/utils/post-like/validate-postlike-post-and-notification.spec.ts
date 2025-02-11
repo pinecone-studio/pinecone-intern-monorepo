a2cf996c19cf3f56a31ac7ff4787e511dfde0360
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-11 23:21:06 +0800

    fix: asdf

--- apps/L1FG/instagram/backend/specs/resolvers/utils/post-like/validate-postlike-post-and-notification.spec.ts
+++ apps/L1FG/instagram/backend/specs/resolvers/utils/post-like/validate-postlike-post-and-notification.spec.ts
@@ -1,8 +1,20 @@
-import { NotificationModel, PostModel } from 'apps/L1FG/instagram/backend/src/models';
+import { NotificationModel, PostLikeModal, PostModel } from 'apps/L1FG/instagram/backend/src/models';
 import { validatePostlikePostAndNotification } from 'apps/L1FG/instagram/backend/src/resolvers/mutations/post/delete-post-like-utils/validate-postlike-post-and-notification';
 jest.mock('apps/L1FG/instagram/backend/src/models');
 describe('Validate postlike ,post and notification', () => {
+  it('Should throw зүрх дараагүй байна', async () => {
+    const mockPostlikeFindById = jest.fn().mockResolvedValueOnce(null);
+    (PostLikeModal.findById as jest.Mock) = mockPostlikeFindById;
+    const input = {
+      postLikeid: '1',
+      postId: '4',
+      notificationId: '5',
+    };
+    await expect(validatePostlikePostAndNotification({ input: input })).rejects.toThrow('зүрх дараагүй байна');
+  });
   it('Should throw пост олдсонгүй', async () => {
+    const mockPostlikeFindById = jest.fn().mockResolvedValueOnce({});
+    (PostLikeModal.findById as jest.Mock) = mockPostlikeFindById;
     const mockFindById = jest.fn().mockResolvedValueOnce(null);
     (PostModel.findById as jest.Mock) = mockFindById;
     const input = {
@@ -13,6 +25,8 @@ describe('Validate postlike ,post and notification', () => {
     await expect(validatePostlikePostAndNotification({ input: input })).rejects.toThrow('пост олдсонгүй');
   });
   it('Should throw мэдээлэл олдсонгүй', async () => {
+    const mockPostlikeFindById = jest.fn().mockResolvedValueOnce({});
+    (PostLikeModal.findById as jest.Mock) = mockPostlikeFindById;
     const mockFindById = jest.fn().mockResolvedValueOnce({
       _id: '12',
       caption: 'hi',
