ddb5e93872837c52eab044a15eafc75e79f2195b
Unuruu Anhbayr <unuruuanhbayr@Unuruus-MacBook-Pro.local>
2025-02-11 19:04:44 +0800

    feat: comment components fix frontend

--- apps/L1FG/instagram/frontend/src/components/profile/comment/LikeStyle.tsx
+++ apps/L1FG/instagram/frontend/src/components/profile/comment/LikeStyle.tsx
@@ -6,6 +6,6 @@ type Props = {
   liked: boolean;
 };
 
-export const PostLike = ({ handleClickLike, liked }: Props) => {
+export const LikeStyle = ({ handleClickLike, liked }: Props) => {
   return <HeartIcon onClick={handleClickLike} data-testid="like-icon" className={`${liked && 'fill-red-500 text-red-500 border-none cursor-pointer'} cursor-pointer fill-none`} />;
 };
