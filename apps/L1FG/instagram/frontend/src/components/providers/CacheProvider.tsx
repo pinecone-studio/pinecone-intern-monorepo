492988834bf651d2d74634a918f2dfc77855b3c1
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-11 08:32:35 +0800

    feat: asdf

--- apps/L1FG/instagram/frontend/src/components/providers/CacheProvider.tsx
+++ apps/L1FG/instagram/frontend/src/components/providers/CacheProvider.tsx
@@ -2,18 +2,26 @@
 
 import { gql, useApolloClient } from '@apollo/client';
 import { createContext, PropsWithChildren, useContext } from 'react';
+import { useAuth } from './AuthProvider';
 type cacheLikePostParams = {
   postId: string;
   likeCount: number;
   hasLiked: boolean;
 };
+type cacheFollowParams = {
+  targetId: string;
+  followerCount: number;
+};
 type CacheContextType = {
   cacheLikePost: (_params: cacheLikePostParams) => void;
   cacheUnlikePost: (_params: cacheLikePostParams) => void;
+  cacheFollow: (_params: cacheFollowParams) => void;
+  cacheUnfollow: (_params: cacheFollowParams) => void;
 };
 const CachContext = createContext<CacheContextType>({} as CacheContextType);
 export const CacheProvider = ({ children }: PropsWithChildren) => {
   const client = useApolloClient();
+  const { user } = useAuth();
   const cacheLikePost = ({ postId, likeCount, hasLiked }: cacheLikePostParams) => {
     client.writeFragment({
       id: `UserPostType:${postId}`,
@@ -44,7 +52,68 @@ export const CacheProvider = ({ children }: PropsWithChildren) => {
       },
     });
   };
-  return <CachContext.Provider value={{ cacheLikePost, cacheUnlikePost }}>{children}</CachContext.Provider>;
+
+  const cacheFollow = ({ targetId, followerCount }: cacheFollowParams) => {
+    client.writeFragment({
+      id: `UserTogetherUserType:${targetId}`,
+      fragment: gql`
+        fragment test on UserTogetherUserType {
+          followerCount
+          friendshipStatus {
+            following
+          }
+        }
+      `,
+      data: {
+        followerCount: followerCount,
+        friendshipStatus: {
+          following: true,
+        },
+      },
+    });
+    client.writeFragment({
+      id: `UserWithoutPassword:${user?._id}`,
+      fragment: gql`
+        fragment test on UserPostType {
+          followingCount
+        }
+      `,
+      data: {
+        followingCount: (user?.followingCount as number) + 1,
+      },
+    });
+  };
+  const cacheUnfollow = ({ targetId, followerCount }: cacheFollowParams) => {
+    client.writeFragment({
+      id: `UserTogetherUserType:${targetId}`,
+      fragment: gql`
+        fragment test on UserTogetherUserTypeB {
+          followerCount
+          friendshipStatus {
+            following
+          }
+        }
+      `,
+      data: {
+        followerCount: followerCount,
+        friendshipStatus: {
+          following: false,
+        },
+      },
+    });
+    client.writeFragment({
+      id: `UserWithoutPassword:${user?._id}`,
+      fragment: gql`
+        fragment test on UserPostType {
+          followingCount
+        }
+      `,
+      data: {
+        followingCount: (user?.followingCount as number) - 1,
+      },
+    });
+  };
+  return <CachContext.Provider value={{ cacheLikePost, cacheUnlikePost, cacheFollow, cacheUnfollow }}>{children}</CachContext.Provider>;
 };
 export const useCache = () => {
   const context = useContext(CachContext);
--- apps/L1FG/instagram/frontend/src/features/home-post/Follow.tsx
+++ apps/L1FG/instagram/frontend/src/features/home-post/Follow.tsx
@@ -1,31 +1,14 @@
-import Loading from '@/components/Loading';
-import { GetProfilePreviewQuery, useCreateFollowerMutation } from '@/generated';
-import { ApolloQueryResult } from '@apollo/client';
-import { useState } from 'react';
-
-export const Follow = ({ targetId, profilePrevieRefetch }: { targetId: string; profilePrevieRefetch: () => Promise<ApolloQueryResult<GetProfilePreviewQuery>> }) => {
-  const [follow] = useCreateFollowerMutation();
-  const [loading, setLoading] = useState(false);
-  const handleClick = async () => {
-    setLoading(true);
-    await follow({
-      variables: {
-        input: {
-          targetId: targetId,
-        },
-      },
-    });
-    await profilePrevieRefetch();
-    setLoading(false);
-  };
+export const Follow = ({ handleClickLike }: { handleClickLike: () => Promise<void> }) => {
   return (
     <button
       className="w-full h-[30px] bg-[#2563EB] text-white rounded-[6px]
             flex justify-center items-center mt-2"
       data-testid="friendship-status-follow"
-      onClick={handleClick}
+      onClick={async () => {
+        await handleClickLike();
+      }}
     >
-      {loading ? <Loading /> : 'Follow'}
+      Follow
     </button>
   );
 };
