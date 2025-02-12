61db2b547d99ddf74c0dd54f4f940a044a347a57
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-12 10:45:51 +0800

    feat: hi calm

--- apps/L1FG/instagram/backend/src/resolvers/queries/post/get-posts.ts
+++ apps/L1FG/instagram/backend/src/resolvers/queries/post/get-posts.ts
@@ -1,8 +1,44 @@
+import mongoose from 'mongoose';
 import { QueryResolvers } from '../../../generated';
 import { PostModel } from '../../../models';
+import { catchError } from '../../../utils/catch-error';
+import { authenticate } from '../../../utils/authenticate';
 
-export const getPosts: QueryResolvers['getPosts'] = async (_, { searchingUserId }, { userId }) => {
-  if (!userId) throw new Error('Unauthorized');
-  const posts = await PostModel.find({ userId: searchingUserId });
-  return posts;
+export const getPosts: QueryResolvers['getPosts'] = async (_, { input }, { userId }) => {
+  authenticate(userId);
+  try {
+    let hasNextPage = false;
+    const { searchingUserId, after, first } = input;
+    const filter: {
+      [key: string]: unknown;
+    } = {
+      userId: searchingUserId,
+    };
+    if (after) {
+      const decodedCursor = Buffer.from(after, 'base64').toString('ascii');
+      filter._id = { $lt: new mongoose.Types.ObjectId(decodedCursor) };
+    }
+    const posts = await PostModel.find(filter)
+      .sort({ _id: -1 })
+      .limit(first + 1);
+    if (posts.length > first) {
+      hasNextPage = true;
+      posts.pop();
+    }
+    const edges = posts.map((post) => ({
+      cursor: Buffer.from(post._id as string).toString('base64'),
+      node: post,
+    }));
+    const returnData = {
+      edges: edges,
+      pageInfo: {
+        startCursor: edges[0].cursor,
+        endCursor: edges[edges.length - 1].cursor,
+        hasNextPage,
+      },
+    };
+    return returnData;
+  } catch (error) {
+    throw catchError(error);
+  }
 };
