41a6eeb826c55d5ed518399df41e7c2fcf530cfd
Unuruu Anhbayr <unuruuanhbayr@Unuruus-MacBook-Pro.local>
2025-02-14 16:13:56 +0800

    feat: asdf

--- apps/L1FG/instagram/backend/src/schemas/followers.schema.ts
+++ apps/L1FG/instagram/backend/src/schemas/followers.schema.ts
@@ -7,20 +7,20 @@ export const FollowerTypeDefs = gql`
     targetId: ID!
   }
   type FriendshipStatusType {
-    followedBy: Boolean
-    following: Boolean
-    incomingRequest: Boolean
-    outgoingRequest: Boolean
+    followedBy: Boolean!
+    following: Boolean!
+    incomingRequest: Boolean!
+    outgoingRequest: Boolean!
   }
   type FollowerUserType {
     _id: String!
     userName: String!
     fullName: String!
     bio: String!
-    profileImage: String
+    profileImage: String!
     hasStory: Boolean
-    gender: Gender
-    isPrivate: Boolean
+    gender: Gender!
+    isPrivate: Boolean!
     email: String!
     friendshipStatus: FriendshipStatusType!
   }
@@ -29,20 +29,20 @@ export const FollowerTypeDefs = gql`
     userName: String!
     fullName: String!
     bio: String!
-    profileImage: String
+    profileImage: String!
     hasStory: Boolean
     gender: Gender
-    isPrivate: Boolean
+    isPrivate: Boolean!
     email: String!
     friendshipStatus: FriendshipStatusType
   }
   type FollowerType {
-    followerId: FollowerUserType
+    followerId: UserTogetherUserType
     targetId: ID
   }
   type FollowingType {
     followerId: ID
-    targetId: FollowingUserType
+    targetId: UserTogetherUserType
   }
   input FollowInput {
     targetId: ID!
