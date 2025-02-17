c4ff65a3a4348ec587fe8fb518a25c4bfeb362b6
Munkhzaya <muzaankhbayr@gmail.com>
2025-02-17 21:18:50 +0800

    feat(frontend): change form

--- apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/_features/userProfile/TicketSubscriber.cy.tsx
+++ apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/_features/userProfile/TicketSubscriber.cy.tsx
@@ -1,10 +1,6 @@
-// eslint-disable-next-line no-secrets/no-secrets
 describe('ticket subscriber', () => {
-  const mockValue = {
-    phoneNumber: '98778877',
-    email: 'test@gmail.com',
-  };
   it('should ticketsub successfull', () => {
+    // eslint-disable-next-line no-secrets/no-secrets
     cy.visit(`/ticketReservation/6787a7e26ba06ccedf494956`);
     cy.get('[data-cy="standart-ticket"]').click();
     cy.get('[data-cy="ticket-buy"]').click();
@@ -14,21 +10,4 @@ describe('ticket subscriber', () => {
     cy.get('[data-cy="reservation-email-input"]').type('test@gmail.com');
     cy.get('[data-cy="reservation-button"]').click();
   });
-  // it('email and phoneNumber value', () => {
-  //   cy.visit(`/ticketReservation/6787a7e26ba06ccedf494956`);
-  //   cy.get('[data-cy="standart-ticket"]').click();
-  //   cy.get('[data-cy="ticket-buy"]').click();
-  //   cy.get('[data-cy="reservation-phone-input"]').should('be.visible');
-  //   cy.get('[data-cy="reservation-phone-input"]').type('98989898');
-  //   cy.get('[data-cy="reservation-email-input"]').should('be.visible');
-
-  //   cy.get('[data-cy="reservation-email-input"]').type('test@gmail.com');
-  //   cy.get('[data-cy="reservation-button"]').click();
-  //   // cy.get('[data-cy="payment-qpay"]').click();
-  //   // cy.get('[data-cy="payment-handle-back"]').click();
-  //   cy.get('[data-cy="reservation-phone-input"]').should('be.visible');
-  //   cy.get('[data-cy="reservation-email-input"]').should('be.visible');
-  //   cy.get('[data-cy="reservation-phone-input"]').and('have.attr', 'placeholder', mockValue.phoneNumber);
-  //   cy.get('[data-cy="reservation-email-input"]').and('have.attr', 'placeholder', mockValue.email);
-  // });
 });
