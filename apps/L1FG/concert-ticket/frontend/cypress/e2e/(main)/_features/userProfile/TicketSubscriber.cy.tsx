8822fdb7133d048eb3f879f2249c08f009aff961
Munkhzaya <muzaankhbayr@gmail.com>
2025-02-17 21:30:43 +0800

    feat(fe): chnge form

--- apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/_features/userProfile/TicketSubscriber.cy.tsx
+++ apps/L1FG/concert-ticket/frontend/cypress/e2e/(main)/_features/userProfile/TicketSubscriber.cy.tsx
@@ -8,6 +8,7 @@ describe('ticket subscriber', () => {
     cy.get('[data-cy="reservation-phone-input"]').type('98989898');
     cy.get('[data-cy="reservation-email-input"]').should('be.visible');
     cy.get('[data-cy="reservation-email-input"]').type('test@gmail.com');
+    cy.get('[data-cy="reservation-button"]').should('not.be.disabled');
     cy.get('[data-cy="reservation-button"]').click();
   });
 });
