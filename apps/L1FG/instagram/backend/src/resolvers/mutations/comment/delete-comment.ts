63e4eca3c7d5084d2a6ef379ca4e5c2b72b94f05
Unuruu Anhbayr <unuruuanhbayr@Unuruus-MacBook-Pro.local>
2025-02-18 21:47:48 +0800

    feat: comment-delete backend

---
+++ apps/L1FG/instagram/backend/src/resolvers/mutations/comment/delete-comment.ts
@@ -0,0 +1,25 @@
+import { MutationResolvers } from '../../../generated';
+import { PostModel } from '../../../models';
+import { CommentModel } from '../../../models/comment.model';
+import { authenticate } from '../../../utils/authenticate';
+import { catchError } from '../../../utils/catch-error';
+import { NotFoundError } from '../../../utils/error';
+
+export const deleteComment: MutationResolvers['deleteComment'] = async (_, { commentId }, { userId }) => {
+  authenticate(userId);
+
+  try {
+    const deleteComment = await CommentModel.findOneAndDelete({
+      _id: commentId,
+    });
+
+    if (!deleteComment) {
+      throw new NotFoundError('Unfollow failed: No such follow relationship found.');
+    }
+
+    await PostModel.findByIdAndUpdate(userId, { $inc: { commentCount: -1 } });
+    return deleteComment;
+  } catch (error) {
+    throw catchError(error);
+  }
+};
