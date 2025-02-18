e7f279136f717d1078ce98207550562a548971bd
24LP9088 24LP9088 <24LP9088@administrators-iMac.local>
2025-02-18 12:01:00 +0800

    add toast

--- apps/L1FG/restaurant/frontend/specs/components/About.spec.tsx
+++ apps/L1FG/restaurant/frontend/specs/components/About.spec.tsx
@@ -1,10 +1,8 @@
+import About from '@/components/About';
+import { render } from '@testing-library/react';
 
-
-import About from "@/components/About"
-import { render } from "@testing-library/react"
-
-describe('About',() =>{
-    it('about ',async () =>{
-        render(<About/>)
-    })
-})
+describe('About', () => {
+  it('about ', async () => {
+    render(<About />);
+  });
+});
