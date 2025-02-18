63f0a9e5ba85347551a1db517c32cf8972aa02b6
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-18 14:30:26 +0800

    feat: story first step
    '

--- apps/L1FG/instagram/frontend/src/components/home-post/Avatar.tsx
+++ apps/L1FG/instagram/frontend/src/components/home-post/Avatar.tsx
@@ -1,14 +1,15 @@
-import { PostsEdge } from '@/generated';
 import Image from 'next/image';
 import { imageUrlOptimizer } from '../utils/image-url-optimizer';
 
-export const Avatar = ({ post }: { post: PostsEdge }) => {
-  const hasStoryToSee = post?.node?.user?.latestStoryTimestamp > post?.node?.user?.seenStoryTime;
+export const Avatar = ({ hasStoryToSee, image }: { hasStoryToSee: boolean; image: string }) => {
   return (
-    <div className={`rounded-full mb-3 w-fit  p-[3px] mt-2 ${hasStoryToSee ? 'bg-[linear-gradient(to_top_right,#f9ce34_10%,#ee2a7b_60%)]' : ''}`} data-testid="image hover:cursor-pointer">
+    <div
+      className={`rounded-full mb-3 w-fit  p-[3px] mt-2 image hover:cursor-pointer ${hasStoryToSee ? 'bg-[linear-gradient(to_top_right,#f9ce34_10%,#ee2a7b_60%)]' : ''}`}
+      data-testid="avatar-inside-avatar-link"
+    >
       <div className="rounded-full bg-white w-14 h-14 flex items-center justify-center">
         <div className="w-14 h-14 rounded-full overflow-hidden relative border">
-          <Image src={imageUrlOptimizer(post?.node?.user?.profileImage)} alt="zurag orno" fill className="object-cover" />
+          <Image src={imageUrlOptimizer(image)} alt="zurag orno" fill className="object-cover" />
         </div>
       </div>
     </div>
--- apps/L1FG/instagram/frontend/src/features/story/MainPageStory.tsx
+++ apps/L1FG/instagram/frontend/src/features/story/MainPageStory.tsx
@@ -1,9 +1,11 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { StoryUser, mockStoryData } from './mock-story-data';
+import { useGetPreviewAllStoriesQuery } from '@/generated';
+import { imageUrlOptimizer } from '@/components/utils/image-url-optimizer';
 
 const MainPageStory = () => {
-
+  const { data } = useGetPreviewAllStoriesQuery();
   const formatUsername = (username: string) => {
     if (username.length >= 10) {
       return `${username.slice(0, 9)}...`;
@@ -13,18 +15,18 @@ const MainPageStory = () => {
 
   return (
     <div className="flex gap-4 w-[630px] ">
-      {mockStoryData.map((user: StoryUser) => (
-        <Link href="/story" key={user.id}>
+      {data?.getPreviewAllStories.storyTray.map((node) => (
+        <Link href={`/stories/${node.user.userName}/${node.latestStoryId}`} key={node._id}>
           <div className="flex flex-col gap-2 mt-[25px] w-fit">
             <div className="rounded-full w-fit bg-[linear-gradient(to_top_right,#f9ce34_10%,#ee2a7b_60%)] p-[2px]">
               <div className="rounded-full bg-white w-[60px] h-[60px] flex items-center justify-center">
                 <div className="w-14 h-14 rounded-full overflow-hidden relative">
-                  <Image src={user.profileImage} alt={`${user.username}'s profile`} fill className="object-cover" />
+                  <Image src={imageUrlOptimizer(node.user.profileImage)} alt={`${node.user.userName}'s profile`} fill className="object-cover" />
                 </div>
               </div>
             </div>
-            <span className="text-xs font-normal text-[#262626] text-center" title={user.username}>
-              {formatUsername(user.username)}
+            <span className="text-xs font-normal text-[#262626] text-center" title={node.user.userName}>
+              {formatUsername(node.user.userName)}
             </span>
           </div>
         </Link>
