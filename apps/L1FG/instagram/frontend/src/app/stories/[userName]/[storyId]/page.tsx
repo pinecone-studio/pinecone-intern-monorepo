710d628bfc69625a9c05c0638dc2b4b44113779e
Tuguldur Namjildorj <tuguldurnamjildorj@Tuguldurs-MacBook-Pro.local>
2025-02-21 08:14:04 +0800

    feat: story backend not connected

--- apps/L1FG/instagram/frontend/src/app/stories/[userName]/[storyId]/page.tsx
+++ apps/L1FG/instagram/frontend/src/app/stories/[userName]/[storyId]/page.tsx
@@ -1,13 +1,11 @@
 import { OnePersonStory } from '@/features/story/OnePersonStory';
+import PeopleStory from '@/features/story/PeopleStory';
 import { StoryContainer } from '@/features/story/StoryContainer';
 import React from 'react';
 
-const Page = async ({ params }: { params: Promise<{ userName: string; storyId: string }> }) => {
+const Page = async ({ params, searchParams }: { params: Promise<{ userName: string; storyId: string }>; searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) => {
   const { userName } = await params;
-  return (
-    <StoryContainer>
-      <OnePersonStory userName={userName} />
-    </StoryContainer>
-  );
+  const { lot } = await searchParams;
+  return <StoryContainer>{!lot ? <OnePersonStory userName={userName} /> : <PeopleStory />}</StoryContainer>;
 };
 export default Page;
