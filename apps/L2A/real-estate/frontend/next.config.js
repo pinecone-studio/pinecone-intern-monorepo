0f7df7312a9520653c5e7681d8f0c62df85879a2
glpzghoo <my.as.glpzghoo@gmail.com>
2025-04-15 12:10:52 +0800

    fix

--- apps/L2A/real-estate/frontend/next.config.js
+++ apps/L2A/real-estate/frontend/next.config.js
@@ -8,15 +8,17 @@ const { composePlugins, withNx } = require('@nx/next');
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: '*',
+      },
+    ],
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+const plugins = [withNx];
 
 module.exports = composePlugins(...plugins)(nextConfig);
