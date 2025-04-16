1ccb4455885e16fcc8b76c9f0d88663586f2c53c
24LP3431 24LP3431 <24LP3431@administrators-iMac.local>
2025-04-16 18:56:45 +0800

    started the project

---
+++ apps/L2A/real-estate/real-estate-frontend/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'real-estate-frontend',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'], babelrc: false }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/apps/L2A/real-estate/real-estate-frontend',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/generated/**/*.ts', '!src/app/**/*.tsx', '!src/components/providers/*.tsx'],
+};
---
+++ apps/L2A/real-estate/real-estate-frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const Page = () => {
+  return <div>Home Pagsssse</div>;
+};
+
+export default Page;
