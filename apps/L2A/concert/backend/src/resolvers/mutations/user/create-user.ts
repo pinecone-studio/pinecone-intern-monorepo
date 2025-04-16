19d07db86ccf70e7ce92f8a2212097c3f552c86a
glpzghoo <my.as.glpzghoo@gmail.com>
2025-04-17 01:13:10 +0800

    frontend

--- apps/L2A/concert/backend/src/resolvers/mutations/user/create-user.ts
+++ apps/L2A/concert/backend/src/resolvers/mutations/user/create-user.ts
@@ -1,28 +1,22 @@
 import { GraphQLError } from 'graphql';
 import { MutationResolvers, User } from '../../../generated';
 import { UserModel } from '../../../models';
-import { catchError } from '../../../utils/catchError';
+import { catchError } from '../../../utils/catch-error';
 import bcrypt from 'bcrypt';
+import { checkUserExist } from '../../../utils/check-user-exist';
 export const createUser: MutationResolvers['createUser'] = async (_, { email, password, phone, username }) => {
-  console.log('aaa');
   if (!email || !password || !phone || !username) {
     throw new GraphQLError('Мэдээлэл дутуу байна!');
   }
   try {
-    const userExist = await UserModel.findOne<User>({ $or: [{ email }, { username }, { phone }] });
-    if (userExist) {
-      throw new GraphQLError('Хэрэглэгч бүртгэлтэй байна!');
-    }
+    await checkUserExist({ email, username, phone });
     const encryptedPass = await bcrypt.hash(password, 15);
     const newUser = await UserModel.create({ email, password: encryptedPass, phone, username });
-    console.log('user', newUser);
-
     if (!newUser) {
       throw new GraphQLError('Бүртгүүлж чадсангүй!');
     }
     return newUser;
   } catch (err) {
-    console.log(err);
     throw catchError(err);
   }
 };
--- apps/L2A/concert/backend/src/utils/connect-to-db.ts
+++ apps/L2A/concert/backend/src/utils/connect-to-db.ts
@@ -1,5 +1,6 @@
 import { connect } from 'mongoose';
 
 export const connectToDb = async () => {
-  await connect(process.env.MONGO_URI!);
+  if (!process.env.MONGO_URI) throw new Error('NO ENV');
+  await connect(process.env.MONGO_URI);
 };
