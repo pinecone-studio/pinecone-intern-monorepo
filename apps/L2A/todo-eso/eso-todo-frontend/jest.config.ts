eb8088ec64106ca06ee136a5e7df3ba26ab852b5
24LP1557 24LP1557 <24LP1557@administrators-iMac.local>
2025-04-17 12:27:04 +0800

    feat: first commit to todo branch

---
+++ apps/L2A/todo-eso/eso-todo-frontend/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'eso-todo-frontend',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/apps/L2A/todo-eso/eso-todo-frontend',
+  collectCoverageFrom: ['src/resolvers/**/*.ts', '!src/**/*.schema.ts', '!src/utils/**', '!src/**/index.ts', '!src/handler.ts', '!src/schemas/**'],
+};
