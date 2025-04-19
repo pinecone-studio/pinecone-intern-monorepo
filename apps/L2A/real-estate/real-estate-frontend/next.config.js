cab892267a368f773812709e6a2c28060613f0ab
Duu-Avia <duuavia01@gmail.com>
2025-04-20 03:34:25 +0800

    feat(real-estate): real-estate

--- apps/L2A/real-estate/real-estate-frontend/next.config.js
+++ apps/L2A/real-estate/real-estate-frontend/next.config.js
@@ -8,19 +8,17 @@ const { composePlugins, withNx } = require('@nx/next');
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  env: {
-    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
-    MONGO_URI: process.env.MONGO_URI,
+  images: {
+    remotePatterns: [
+      {
+        hostname: '*',
+      },
+    ],
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+const plugins = [withNx];
 
 module.exports = composePlugins(...plugins)(nextConfig);
