82f314c3ae7fda341c6fd84ecb4a674742ce223c
24LP8321 24LP8321 <24LP8321@G302-CC0120.local>
2025-04-19 19:24:09 +0800

    feat(jsut padding): padding changed for button

--- apps/L2A/leave-request/frontend/src/app/admin/_components/button.tsx
+++ apps/L2A/leave-request/frontend/src/app/admin/_components/button.tsx
@@ -7,7 +7,7 @@ type RegisterNewEmployeeButtonProps = {
 export const RegisterNewEmployeeButton: React.FC<RegisterNewEmployeeButtonProps> = ({ onClick }) => {
   return (
     <button
-      className="bg-black text-white py-2 px-4 rounded-lg"
+      className="bg-black text-white py-3 px-4 rounded-lg"
       onClick={onClick}
     >
       + Шинэ ажилтан бүртгэх
