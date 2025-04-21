c1919d21d5527a0f73256aca4c770e1c9acdd7bf
glpzghoo <my.as.glpzghoo@gmail.com>
2025-04-21 18:43:46 +0800

    try catch

--- apps/L2A/concert/backend/src/utils/hash-password.ts
+++ apps/L2A/concert/backend/src/utils/hash-password.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 export const hashPassword = async (password: string): Promise<string> => {
   if (password.length < 8) {
-    throw new Error('Бүртгүүлж чадсангүй!');
+    throw new Error('Нууц үг богинохон байна!');
   }
   const encryptedPass = await bcrypt.hash(password, 15);
   return encryptedPass;
