9174b2ee3c50d0a44193fd2bb4f273d243b70bc1
glpzghoo <my.as.glpzghoo@gmail.com>
2025-04-21 17:11:50 +0800

    testing

--- apps/L2A/concert/frontend/src/app/__test__/Footer.Test.tsx
+++
@@ -1,8 +0,0 @@
-// import Footer from '../_components/Footer';
-// import { render, screen } from '@testing-library/react';
-// describe('should render footer', () => {
-//   it('footer', () => {
-//     render(<Footer />);
-//     expect(screen.getByText('© 2024 Booking Mongolia. All Rights Reserved.')).toBeInTheDocument();
-//   });
-// });
