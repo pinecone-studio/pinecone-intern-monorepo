38c3dc09684be538a726c88468265e3d264f3e7f
24LP8321 24LP8321 <24LP8321@administrators-iMac.local>
2025-04-21 15:04:17 +0800

    feat(button changed): padding fincreased

--- apps/L2A/leave-request/frontend/src/app/admin/_components/button.tsx
+++ apps/L2A/leave-request/frontend/src/app/admin/_components/button.tsx
@@ -7,7 +7,7 @@ type RegisterNewEmployeeButtonProps = {
 export const RegisterNewEmployeeButton: React.FC<RegisterNewEmployeeButtonProps> = ({ onClick }) => {
   return (
     <button
-      className="bg-black text-white py-3 px-4 rounded-lg"
+      className="bg-black text-white py-4 px-4 rounded-lg"
       onClick={onClick}
     >
       + Шинэ ажилтан бүртгэх
