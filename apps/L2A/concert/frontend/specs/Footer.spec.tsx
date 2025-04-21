30f02b2b29729da1b6ca498df77164b40ab2a78f
glpzghoo <my.as.glpzghoo@gmail.com>
2025-04-21 17:15:39 +0800

    fix

--- apps/L2A/concert/frontend/specs/Footer.spec.tsx
+++ apps/L2A/concert/frontend/specs/Footer.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import Footer from '../src/app/_components/Footer';
+
 describe('Footer', () => {
   it('renders footer', () => {
     render(<Footer />);
--- apps/L2A/concert/frontend/specs/Header.spec.tsx
+++ apps/L2A/concert/frontend/specs/Header.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import Header from '../src/app/_components/Header';
+
 describe('Header', () => {
   it('renders header', () => {
     render(<Header />);
