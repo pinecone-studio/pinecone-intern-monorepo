eb079d4540948979997d0ba9b8d010bb402b65f2
24LP8321 24LP8321 <24LP8321@administrators-iMac.local>
2025-04-22 19:12:41 +0800

    feat: added login dialog and its validation

--- apps/L2A/leave-request/frontend/src/app/login/_features/Dialog.tsx
+++ apps/L2A/leave-request/frontend/src/app/login/_features/Dialog.tsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import Image from 'next/image';
 import { EmailInput } from "../_components/Input";
 import { LoginButton } from "../_components/Buton";
 
@@ -8,7 +9,6 @@ const schema = z.object({
   email: z.string().email(),
 });
 type FormData = z.infer<typeof schema>;
-
 
 export const Dialog = () => {
   const {
@@ -19,7 +19,9 @@ export const Dialog = () => {
     resolver: zodResolver(schema),
     mode: 'onChange',
   });
+
   const onSubmit = (data: FormData) => {
+    console.log("Submitted:", data);
   };
 
   return (
@@ -29,7 +31,13 @@ export const Dialog = () => {
         className="rounded-lg p-6 w-[16%] h-[30%] bg-white flex flex-col items-center"
       >
         <p className="text-xl mt-2 font-semibold">Нэвтрэх</p>
-        <img src="./images/PineconeStudio.png" className="w-[28%] mt-8" />
+        <Image
+          src="/images/PineconeStudio.png"
+          alt="Pinecone Studio Logo"
+          width={100}
+          height={100}
+          className="w-[28%] mt-8"
+        />
         <EmailInput register={register} hasError={!!errors.email} />
         <LoginButton disabled={!isValid} />
       </form>
