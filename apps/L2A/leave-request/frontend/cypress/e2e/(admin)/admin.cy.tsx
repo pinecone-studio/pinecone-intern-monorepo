dad28ec00623948f2af9d641d8fca3c07aab2bf5
mac <mac@macs-MacBook-Air.local>
2025-04-25 04:14:07 +0800

    feat: fix

--- apps/L2A/leave-request/frontend/cypress/e2e/(admin)/admin.cy.tsx
+++ apps/L2A/leave-request/frontend/cypress/e2e/(admin)/admin.cy.tsx
@@ -1,16 +1,12 @@
 describe('Admin page tests', () => {
-    beforeEach(() => {
-      cy.visit('/admin');
-    });
-  
-    it('should open and close the modal when the register button and close button are clicked', () => {
-        cy.get('[data-cy="open-register-modal"]').click();
-      
-        cy.get('[data-cy="register-employee-modal"]').should('be.visible');
-    
-        cy.get('[data-cy="close-register-modal"]').click();
-      
-        cy.get('[data-cy="register-employee-modal"]').should('not.exist');
-      });
+  beforeEach(() => {
+    cy.visit('/admin');
   });
-  
+
+  it('should open and close the modal when the register button and close button are clicked', () => {
+    cy.get('[data-cy="open-register-modal"]').click();
+    cy.get('[data-cy="register-employee-modal"]').should('be.visible')
+    cy.get('[data-cy="close-register-modal"]').click();
+    cy.get('[data-cy="register-employee-modal"]').should('not.exist');
+  });
+});
