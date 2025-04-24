dbbe5e6cfbb21ce78df59a43e8ff2e490aa9a2e6
erhme <admin@erhmes-MacBook-Air.local>
2025-04-24 15:37:21 +0800

    feat(admin.cy.tsx): tested

--- apps/L2A/leave-request/frontend/src/app/admin/_components/AdminHeader.tsx
+++ apps/L2A/leave-request/frontend/src/app/admin/_components/AdminHeader.tsx
@@ -1,20 +1,29 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { Moon, Sun } from 'lucide-react';
 
 export const AdminHeader = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   return (
-    <div>
-      <div className="mt-5 ml-10 mr-10 flex items-center justify-between">
-        <img src="Pinecone studio.png" />
-        <div className="flex items-center gap-4">
-          <button className="rounded-full border p-2">
-            <Sun />
-          </button>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
+    <div className="mt-5 ml-10 mr-10 flex items-center justify-between">
+      <img src="PineconeStudio.png" className="bg-white" />
+      <div className="flex items-center gap-4">
+        <button className="rounded-full border p-2" onClick={() => setIsDarkMode(!isDarkMode)}>
+          {isDarkMode ? <Moon /> : <Sun />}
+        </button>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </div>
     </div>
   );
