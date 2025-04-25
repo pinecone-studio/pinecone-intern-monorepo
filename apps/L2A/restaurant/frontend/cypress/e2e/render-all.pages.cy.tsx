156e1175f43c6f7e93bc5535003d74038d6bf188
24LP3357 24LP3357 <24LP3357@administrators-iMac.local>
2025-04-25 12:40:16 +0800

    fix

--- apps/L2A/restaurant/frontend/cypress/e2e/render-all.pages.cy.tsx
+++ apps/L2A/restaurant/frontend/cypress/e2e/render-all.pages.cy.tsx
@@ -1,12 +1,10 @@
-describe('Login Page', () => {
-  beforeEach(() => {
-    cy.visit('/login');
-  });
-  it('should render the Logo', () => {
-    cy.get('[data-testid="Logo-image"]').should('be.visible');
-    cy.get('[data-cy="login-btn"]').should('be.visible').and('contain.text', 'Нэвтрэх');
-  });
-  it('should open the sign-in modal when login button is clicked', () => {
-    cy.get('[data-cy="login-btn"]').click();
+import allPages from '../utils/all-pages.json';
+
+describe('render all pages', () => {
+  it(`Should render all page`, () => {
+    cy.log(JSON.stringify(allPages));
+    allPages.forEach((page) => {
+      cy.visit(page);
+    });
   });
 });
