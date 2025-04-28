e03799fd5433dd37f1a3ae0ed57f398f7c403c93
24LP8639 24LP8639 <24LP8639@Admins-iMac-2.local>
2025-04-28 14:46:49 +0800

    feat: header

--- apps/L2A/leave-request/frontend/src/app/page.tsx
+++ apps/L2A/leave-request/frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import UserHeader from "./user/_components/UserHeader";
+
 const Page = () => {
   return (
     <div>
+      <UserHeader/>
       <p>Home Page</p>
     </div>
   );
