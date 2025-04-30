ac09ba8f04b6c9199e9ce272fb16006d72b208a7
24LP3083 24LP3083 <24LP3083@administrators-iMac.local>
2025-04-30 13:40:26 +0800

    feat(real-estate-backend): add otp

--- apps/L2A/real-estate/real-estate-backend/src/resolvers/mutations/index.ts
+++ apps/L2A/real-estate/real-estate-backend/src/resolvers/mutations/index.ts
@@ -1,3 +1,5 @@
 export * from './sample-mutation';
-export {loginUser} from "./user/login-user"
-export {createUser} from "./user/create-user"
+export { loginUser } from './user/login-user';
+export { createUser } from './user/create-user';
+export * from './otp/request-otp';
+export * from './otp/verify-otp';
