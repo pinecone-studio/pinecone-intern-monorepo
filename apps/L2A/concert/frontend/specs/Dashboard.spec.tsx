f2681d24ffb54ecb0caa3ed77aabe1cbe8ea9102
24LP2724 24LP2724 <you@example.com>
2025-04-30 17:46:01 +0800

    test hassn

--- apps/L2A/concert/frontend/specs/Dashboard.spec.tsx
+++ apps/L2A/concert/frontend/specs/Dashboard.spec.tsx
@@ -8,10 +8,4 @@ describe('Dashboard Component', () => {
     const dashboardElement = screen.getByTestId('dashboard');
     expect(dashboardElement).toBeInTheDocument();
   });
-
-  it('renders 8 ticket cards', () => {
-    render(<Dashboard />);
-    const cards = screen.getAllByText(/Music of the Spheres/i);
-    expect(cards).toHaveLength(8);
-  });
 });
