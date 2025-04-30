712d49af8d11908e47bee2969ce3e60d5b207182
“dashka-bat” <dushkab02@gmail.com>
2025-04-30 18:46:28 +0800

    fix test

--- apps/L2A/restaurant/frontend/specs/userHeader.spec.tsx
+++ apps/L2A/restaurant/frontend/specs/userHeader.spec.tsx
@@ -7,19 +7,8 @@ describe('Header Component', () => {
   it('renders the Logo component', () => {
     render(<Header />);
     screen.getByTestId('Logo-image');
-  });
-
-  it('renders the shopping cart icon', () => {
-    render(<Header />);
     screen.getByTestId('shopping-cart-icon');
-  });
-
-  it('renders the notification icon', () => {
-    render(<Header />);
     screen.getByTestId('notification-icon');
-  });
-
-  it('renders the HomeSheet component', () => {
     render(<Header />);
     render(<HomeSheet />);
   });
