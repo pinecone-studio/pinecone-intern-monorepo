3db30bcf6e4908131f1891e25d669873311bad3a
“dashka-bat” <dushkab02@gmail.com>
2025-04-30 12:37:33 +0800

    create homepage and fix some errors

--- apps/L2A/restaurant/frontend/src/app/page.tsx
+++ apps/L2A/restaurant/frontend/src/app/page.tsx
@@ -2,13 +2,15 @@
 import Link from 'next/link';
 import HomeBody from './_features/HomeBody';
 import { Button } from '@/components/ui/button';
+import HomeMain from './_features/HomeMain';
 
 const Page = () => {
   return (
-    <div className='flex justify-between'>
+    <div className="flex justify-between">
       <HomeBody />
-      <Link href={"/login"}>
-      <Button>sign in</Button>
+      <HomeMain />
+      <Link href={'/login'}>
+        <Button>sign in</Button>
       </Link>
     </div>
   );
