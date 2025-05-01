2d83513b60fa2355d18e08ead4ddc74d9f9afdef
Solongooyaa <u.solongo2018@gmail.com>
2025-05-01 18:30:29 +0800

    price

---
+++ apps/L2A/real-estate/real-estate-frontend/src/app/create-post/_components/CreatePostPrice.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const CreatePostPrice = () => {
+  const [price, setPrice] = useState('');
+
+  return (
+    <div>
+      <label htmlFor="post-price" className="block text-sm text-[#09090B] pb-2">
+        Үнэ
+      </label>
+      <input
+        id="post-price"
+        name="price"
+        type="number"
+        value={price}
+        data-testid="price"
+        onChange={(e) => setPrice(e.target.value)}
+        className="w-full block px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-orange-500"
+      />
+    </div>
+  );
+};
