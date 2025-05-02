fe5a50f1a32456dff521c6bd92b192aa46bb389c
dashka <dushkab02@gmail.com>
2025-05-02 11:24:27 +0800

    create userheader and fix errors (#4710)
    * create userheader and fix errors
    
    * fix test
    
    ---------
    
    Co-authored-by: “dashka-bat” <“dushkab02@gmail.com”>

--- apps/L2A/restaurant/frontend/src/app/page.tsx
+++ apps/L2A/restaurant/frontend/src/app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 import Link from 'next/link';
-import HomeBody from './_features/HomeBody';
 import { Button } from '@/components/ui/button';
 import HomeMain from './_features/HomeMain';
 
 const Page = () => {
   return (
-    <div className="flex justify-between">
-      <HomeBody />
-      <HomeMain />
-      <Link href={'/login'}>
-        <Button>sign in</Button>
-      </Link>
+    <div>
+      <div className="flex justify-between">
+        <HomeMain />
+        <Link href={'/login'}>
+          <Button>sign in</Button>
+        </Link>
+      </div>
     </div>
   );
 };
