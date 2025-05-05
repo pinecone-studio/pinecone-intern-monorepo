b6a710e34f170e7c8bba29895746b333bf80141f
Jargaltogtokh <jagaa155@gmail.com>
2025-05-05 13:02:45 +0800

    feat: add-category, update-category, delete-category (#4706)
    * feat: add-category, update-category, delete-category
    
    * feat: add-category, update-category, delete-category
    
    * feat(add-category, update-category, delete-category and validation has been fixed): backend
    
    ---------
    
    Co-authored-by: Jargaltogtokh <jagaa@Jargaltogtokhs-MacBook-Pro.local>
    Co-authored-by: Jargaltogtokh <jagaa@192.168.1.10>

--- apps/L2A/restaurant/backend/src/resolvers/mutations/delete-product.ts
+++ apps/L2A/restaurant/backend/src/resolvers/mutations/delete-product.ts
@@ -1,13 +1,14 @@
-import { productModel } from '../../models/product.model';  
-import { ProductType } from '../../models/product.model'; 
+import { productModel } from '../../models/product.model';
+import { ProductType } from '../../models/product.model';
 
 export const deleteProduct = async (_id: string): Promise<ProductType | null> => {
-  try {const product = await productModel.findByIdAndDelete(_id);
-  if (!product) {
-    throw new Error('Product not found');
-  }
-  return product;
-} catch (err) {
+  try {
+    const product = await productModel.findByIdAndDelete(_id);
+    if (!product) {
+      throw new Error('Product not found');
+    }
+    return product;
+  } catch (err) {
     throw new Error(`Error deleting product: ${err}`);
-}
+  }
 };
