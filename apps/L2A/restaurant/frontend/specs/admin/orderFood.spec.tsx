d71378715a9fcc39262792fa7c290748e5be7f5b
Enkhsolongo10 <rrainbow1018@gmail.com>
2025-05-05 12:49:37 +0800

    .

--- apps/L2A/restaurant/frontend/specs/admin/orderFood.spec.tsx
+++ apps/L2A/restaurant/frontend/specs/admin/orderFood.spec.tsx
@@ -5,9 +5,7 @@ import orderData from "@/app/admin/_components/orders.json";
 describe("OrderFood Component", () => {
   it("renders food items from JSON correctly", () => {
     const firstOrderItems = orderData.orders[0].items;
-  
     render(<OrderFood orders={firstOrderItems} />);
-  
     firstOrderItems.forEach((item) => {
       expect(screen.getByText(item.name));
       expect(screen.getByText(`${item.price}₮`));
