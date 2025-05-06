03946b0808cd3db720ef511d663631554fcfbb3c
Duk3 <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-06 13:14:50 +0800

    docs: created folders for our project

---
+++ apps/L2B/hotel-booking/hotel-server/codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'apps/L2B/hotel-booking/hotel-server/src/schemas',
+  generates: {
+    'apps/L2B/hotel-booking/hotel-server/src/generated/index.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        contextType: '../types#Context',
+        makeResolverTypeCallable: true,
+        maybeValue: 'T',
+      },
+    },
+  },
+};
+
+export default config;
