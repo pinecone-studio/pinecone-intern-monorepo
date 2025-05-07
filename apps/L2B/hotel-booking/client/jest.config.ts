45a191ed9965aa76cc4c6eabc2dd55b85420a662
Duk3 <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-07 14:29:42 +0800

    feat(setup): created our forntend folders for our project

---
+++ apps/L2B/hotel-booking/client/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'Hotel-Booking-FE',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'], babelrc: false }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/apps/L2B/hotel-booking/client',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/generated/**/*.ts', '!src/app/**/*.tsx', '!src/components/providers/*.tsx'],
+};
