ab67ed5e8e36c16fdc745ed2264aa84b3649cbc5
sanchir sanchir <sanchirsanchir@sanchirs-MacBook-Pro.local>
2025-05-07 15:42:19 +0800

    recover password

--- apps/L2A/concert/backend/src/utils/check-password.ts
+++ apps/L2A/concert/backend/src/utils/check-password.ts
@@ -1,8 +1,9 @@
 import bcrypt from 'bcrypt';
 
-export const checkPassword = async (password: string, userPassword: string) => {
-  const isPasswordValid = await bcrypt.compare(password, userPassword);
-  if (!isPasswordValid) {
+export const checkPassword = async (inputPassword: string, hash: string) => {
+  const match = await bcrypt.compare(inputPassword, hash);
+  if (!match) {
     throw new Error('Нууц үг буруу байна!');
   }
+  return true;
 };
