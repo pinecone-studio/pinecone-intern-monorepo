48faa0857a337f8f50b51ac49c3c106b40dbbd12
Duu-Avia <duuavia01@gmail.com>
2025-05-08 03:49:51 +0800

    fix(real-estate-frontend): e2e fixed

--- apps/L2A/real-estate/real-estate-frontend/cypress/e2e/(public)/signup.cy.tsx
+++ apps/L2A/real-estate/real-estate-frontend/cypress/e2e/(public)/signup.cy.tsx
@@ -1,6 +1,6 @@
 describe('SignUp Page Flow with Mocked OTP', () => {
   it('should complete signup using mocked OTP', () => {
-    const randomEmail = `_user${Date.now()}@example.com`;
+    const generatedMail = `_user${Date.now()}@example.com`;
     cy.intercept('POST', '/api/graphql', (req) => {
       const body = req.body;
       if (body.query.includes('verifyOTP')) {
@@ -27,15 +27,11 @@ describe('SignUp Page Flow with Mocked OTP', () => {
         return;
       }
     }).as('graphql');
-
     cy.visit('/signup');
-
-    cy.get('[data-cy=email-input]').type(randomEmail);
+    cy.get('[data-cy=email-input]').type(generatedMail);
     cy.get('form').submit();
-
     cy.get('[data-cy=otp-input]').type('123456'); 
     cy.wait('@graphql'); 
-
     cy.get('[data-cy=password-input]').type('mockpassword123');
     cy.get('[data-cy=confirm-password-input]').type('mockpassword123');
     cy.get('form').submit();
