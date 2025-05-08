7e6d4a36d919398f1274554ddac5cb42b0b59c9a
Khosbayar707 <torutora0415@gmail.com>
2025-05-08 17:02:49 +0800

    get concert query update

--- apps/L2A/concert/backend/src/resolvers/queries/get-concert.ts
+++ apps/L2A/concert/backend/src/resolvers/queries/get-concert.ts
@@ -1,12 +1,17 @@
 import { QueryResolvers } from '../../generated';
 import { concertModel } from '../../models/concert.model';
 import { catchError } from '../../utils/catch-error';
+import { normalizeConcert } from '../../utils/get-concert/normalize-concert';
 
 export const concert: QueryResolvers['concert'] = async (_, { concertId }) => {
   try {
-    const concert = await concertModel.findById(concertId).populate('venue').exec();
-    return concert;
+    const concert = await concertModel.findById(concertId).populate('venue').populate('seatData').exec();
+
+    if (!concert) {
+      throw new Error('Concert not found');
+    }
+    return normalizeConcert(concert);
   } catch (error) {
-    catchError(error);
+    throw catchError(error);
   }
 };
