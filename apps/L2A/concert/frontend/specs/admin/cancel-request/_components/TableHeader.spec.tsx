8671bb04059e3b480221214b8050803cdd247d44
glpzghoo <my.as.glpzghoo@gmail.com>
2025-05-08 22:02:03 +0800

    update

--- apps/L2A/concert/frontend/specs/admin/cancel-request/_components/TableHeader.spec.tsx
+++ apps/L2A/concert/frontend/specs/admin/cancel-request/_components/TableHeader.spec.tsx
@@ -2,8 +2,8 @@ import TableHeader from '@/app/admin/cancel-request/_components/TableHeader';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-describe('render add ticket dialog component', () => {
-  it('should render add ticket dialog component', async () => {
+describe('render table', () => {
+  it('should render table render', async () => {
     render(<TableHeader />);
 
     expect(screen.getByText('Дансны мэдээлэл')).toBeInTheDocument();
