e585612bf9c11d7c15b6a2d98ba7810818a7bc0d
Enkhsolongo10 <rrainbow1018@gmail.com>
2025-05-09 09:56:47 +0800

    fixed

--- apps/L2A/restaurant/frontend/src/app/admin/orders/page.tsx
+++ apps/L2A/restaurant/frontend/src/app/admin/orders/page.tsx
@@ -2,10 +2,12 @@
 
 import OrderMainCard from "@/app/admin/_features/OrderMainCard";
 import orderData from "@/app/admin/_components/orders.json";
+import OrderHeader from "../_components/OrderHeader";
 
 const Orders = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-5 gap-5">
+      <OrderHeader />
       {orderData.orders.map((ordersData) => (
         <OrderMainCard key={ordersData.orderNumber} order={ordersData} />
       ))}
