fcaf58440afc3e060cbfbe58523fa580584d11cc
glpzghoo <my.as.glpzghoo@gmail.com>
2025-05-09 10:07:29 +0800

    frontend landing page

--- apps/L2A/concert/frontend/src/app/page.tsx
+++ apps/L2A/concert/frontend/src/app/page.tsx
@@ -18,15 +18,15 @@ const Page = () => {
         {data?.concerts?.slice(0, 8).map((concert: any) => (
           <div key={concert.id} className="bg-[#141414] w-[425px] h-[360px] rounded-lg overflow-hidden">
             <div className="h-48 bg-gray-700">
-              <Image src={concert.Url || `/placeholder.webp`} alt={concert.title} className="w-full h-full object-cover" width={425} height={370} />
+              <Image src={`https://picsum.photos/1280/720`} alt={concert.title} className="w-full h-full object-cover" width={425} height={370} />
             </div>
             <div className="p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">{concert.title}</h3>
-              <p className="text-sm text-gray-400 mb-2">{concert.artistName}</p>
-              <p className="font-semibold">{concert.primaryPrice}</p>
+              <h3 className="font-semibold">{concert?.title}</h3>
+              <p className="text-sm text-gray-400 mb-2">{concert?.artistName}</p>
+              <p className="font-semibold">Price: {concert?.primaryPrice}</p>
               <div className="flex justify-between text-xs text-gray-500 mt-1">
-                <span>📅 {concert.musicStart}</span>
-                <span>📍 {concert.venue?.name}</span>
+                <span>📅 {concert?.musicStart}</span>
+                <span>📍 {concert?.venue?.name}</span>
               </div>
             </div>
           </div>
