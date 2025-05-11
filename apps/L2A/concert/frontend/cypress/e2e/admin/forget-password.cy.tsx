675fe08af16a49391ebca1924aa6d5e2116225df
glpzghoo <my.as.glpzghoo@gmail.com>
2025-05-11 19:39:39 +0800

    forget-password cypress test

--- apps/L2A/concert/frontend/cypress/e2e/admin/forget-password.cy.tsx
+++ apps/L2A/concert/frontend/cypress/e2e/admin/forget-password.cy.tsx
@@ -1,6 +1,6 @@
 describe('Forget Password Page', () => {
   it('should click on onSubmit', () => {
-    cy.visit('/auth/forget-password');
+    cy.visit('/admin/forget-password');
     cy.get('[data-testid="forget-password-email-input"]').type('glpzghoo@gmail.com');
     cy.get('button[type="submit"]').click();
   });
