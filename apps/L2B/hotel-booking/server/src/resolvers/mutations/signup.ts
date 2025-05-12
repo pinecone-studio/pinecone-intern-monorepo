6e64172163bfb00c0f12f8c3db46c9e223c9210d
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-05-12 09:48:42 +0800

    feat(server): fixed eslint

--- apps/L2B/hotel-booking/server/src/resolvers/mutations/signup.ts
+++ apps/L2B/hotel-booking/server/src/resolvers/mutations/signup.ts
@@ -3,16 +3,12 @@ import { userModel } from '../../models';
 import bcrypt from 'bcrypt';
 
 export const addUser: MutationResolvers['addUser'] = async (_, { email, password }) => {
-  try {
-    const userExist = await userModel.findOne({ email });
-    if (userExist) {
-      throw new Error('User already exists.');
-    }
-
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    const user = await userModel.create({ email, password: encryptedPassword });
-    return user;
-  } catch (err) {
-    throw err;
+  const userExist = await userModel.findOne({ email });
+  if (userExist) {
+    throw new Error('User already exists.');
   }
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const user = await userModel.create({ email, password: encryptedPassword });
+  return user;
 };
