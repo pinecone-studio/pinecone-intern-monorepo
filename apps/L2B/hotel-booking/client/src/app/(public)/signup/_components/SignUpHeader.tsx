023f38132b370be17d4cff48c0e66e10ca9211d5
Duk3 <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-12 17:52:19 +0800

    fix: added test to component

--- apps/L2B/hotel-booking/client/src/app/(public)/signup/_components/SignUpHeader.tsx
+++ apps/L2B/hotel-booking/client/src/app/(public)/signup/_components/SignUpHeader.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const SignUpHeader = ({ h3, p }: Props) => {
   return (
-    <div className="flex flex-col items-center font-medium">
+    <div data-testid="SignUpHeader" className="flex flex-col items-center font-medium">
       <div className="flex items-center gap-2 mb-[24px]">
         <div className="w-5 h-5 rounded-full bg-[#2563eb]"></div>
         <h2 className="text-[#09090b] text-[20px]">Pedia</h2>
