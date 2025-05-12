16bea79e420a88136843c92520e119200ba18750
Temm Kss <temmkss@Temms-MacBook-Air.local>
2025-05-13 01:55:55 +0800

    feat: card edited

--- apps/L2B/hotel-booking/client/src/app/_components/Card/Page.tsx
+++ apps/L2B/hotel-booking/client/src/app/_components/Card/Page.tsx
@@ -6,14 +6,14 @@ const Card =()=>
     <Image width={380} height={216} className="bg-gray-200" src={""} alt="" />
     <div className="p-4">
         <h1 className="font-bold text-base">Hotel Name</h1>
-        <Star fill="orange"/>
-        <div className="flex text-sm font-normal gap-3 mt-2 flex-col ">
-            <p className="flex gap-2"><Wifi size={20} /> Free wifi</p>
-            <p className="flex gap-2"><Flower size={20}/>Spa access</p>
-            <p className="flex gap-2"><ParkingCircle size={20}/>Free self parking</p>
+        <Star fill="orange" color="orange" size={16}/>
+        <div className="flex font-[intern] text-sm font-normal gap-3 mt-2 flex-col ">
+            <p className="flex gap-2"><Wifi size={20} width={18} /> Free wifi</p>
+            <p className="flex gap-2"><Flower size={20} width={18}/>Spa access</p>
+            <p className="flex gap-2"><ParkingCircle size={20} width={18}/>Free self parking</p>
         </div>
         <div className="flex py-4 gap-2">
-            <p className="bg-[#18BA51] rounded-full w-[39px] h-[20px] text-center ">8.6</p>
+            <p className="bg-[#18BA51] rounded-full w-[39px] h-[20px] font-md text-sm flex items-center justify-center text-white ">8.6</p>
             <p className="text-sm font-medium">Excellent</p>
         </div>
     </div>
