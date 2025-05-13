dc86492812b928360377055bb2759275a08cf9b0
gandak <gandak0930@gmail.com>
2025-05-13 22:12:26 +0800

    fix(component): fixed bug
    fixed bug

--- apps/L2B/tinder/tinder-frontend/specs/sign-up/ThirdStep.spec.tsx
+++ apps/L2B/tinder/tinder-frontend/specs/sign-up/ThirdStep.spec.tsx
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ThirdStep from '@/app/auth/sign-up/_components/ThirdStep';
 
