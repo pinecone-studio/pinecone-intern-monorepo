163d742a2035dc84353ea16e645120b95c91725c
24LP8321 24LP8321 <24LP8321@administrators-iMac.local>
2025-05-13 16:34:51 +0800

    feat: added get all table query

--- apps/L2A/restaurant/backend/src/resolvers/queries/get-all-table.ts
+++ apps/L2A/restaurant/backend/src/resolvers/queries/get-all-table.ts
@@ -3,13 +3,12 @@ import { tableModel } from '../../models/table.model';
 export const getAllTables = async () => {
   try {
     const tables = await tableModel.find();
-    if (!tables) {
-      throw new Error(' Tables not found');
-    }
-    return tables
-  } 
-    catch (error) {
-    throw new Error(`Error fetching table: ${(error as Error).message}`);
-  };
-  }
 
+    if (!tables) {
+      throw new Error('Tables not found');
+    }
+    return tables;
+  } catch (error) {
+    throw new Error(`Error fetching table: ${(error as Error).message}`);
+  }
+};
