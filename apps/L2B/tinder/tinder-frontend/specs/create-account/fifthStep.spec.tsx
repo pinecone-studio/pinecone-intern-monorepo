116cf521fa692c5c2e729caddcdec62fb7443ca8
sukherdenei <sukherdene.ba@gmail.com>
2025-05-13 11:28:25 +0800

    feat: fifth-step&jest

--- apps/L2B/tinder/tinder-frontend/specs/create-account/fifthStep.spec.tsx
+++ apps/L2B/tinder/tinder-frontend/specs/create-account/fifthStep.spec.tsx
@@ -1,11 +1,29 @@
-import FifthStep from '@/app/auth/create-account/_components/FifthStep';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import Fifth from '@/app/auth/create-account/_components/FifthStep';
 
-describe('fifth step components', () => {
-  it('renders the text', () => {
-    render(<FifthStep />);
-    const text = screen.getByText('5step');
-    expect(text).toBeInTheDocument();
+describe('Fifth component', () => {
+  it('renders the icon', () => {
+    render(<Fifth />);
+    const icon = screen.getByTestId('lucide-icon');
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('renders the heading text', () => {
+    render(<Fifth />);
+    const heading = screen.getByText("You`'`re all set!");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders the description paragraph', () => {
+    render(<Fifth />);
+    const paragraph = screen.getByText(/Your account is all set/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it('renders the button with correct text', () => {
+    render(<Fifth />);
+    const button = screen.getByRole('button', { name: /Start Swipping!/i });
+    expect(button).toBeInTheDocument();
   });
 });
