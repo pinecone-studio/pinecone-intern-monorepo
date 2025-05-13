9b47041fea9d074967316b6acb77aef705e37acc
24LP3531 24LP3531 <24LP3531@administrators-iMac.local>
2025-05-13 10:58:20 +0800

    feat(real-estate-backend): update post by id

--- apps/L2A/real-estate/real-estate-backend/src/resolvers/mutations/index.ts
+++ apps/L2A/real-estate/real-estate-backend/src/resolvers/mutations/index.ts
@@ -4,6 +4,8 @@ export { createUser } from './user/create-user';
 export {completeSignup} from "./user/complete-user"
 export * from './otp/request-otp';
 export * from './otp/verify-otp';
-export * from './post/create-post';
+export * from './post/create-post'
+
+export * from './post/update-post-by-id';
 
 
