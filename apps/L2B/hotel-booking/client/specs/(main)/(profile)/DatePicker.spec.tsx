722e938ccf12d72b7d3a095e1f548b5bad483259
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-05-15 12:43:28 +0800

    feat(profile): personal information

---
+++ apps/L2B/hotel-booking/client/specs/(main)/(profile)/DatePicker.spec.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react';
+import { useForm, Controller } from 'react-hook-form';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { DatePicker } from '@/app/(main)/(profiles)/profile/_components/DatePicker';
+
+type FormValues = {
+  birth: Date | null;
+};
+
+const Wrapper = () => {
+  const { control } = useForm<FormValues>({
+    defaultValues: {
+      birth: null,
+    },
+  });
+
+  return <Controller control={control} name="birth" render={({ field }) => <DatePicker field={field} />} />;
+};
+
+describe('DatePicker', () => {
+  it('renders without crashing', () => {
+    render(<Wrapper />);
+    expect(screen.getByText('Date of Birth')).toBeInTheDocument();
+    expect(screen.getByText('Pick a date')).toBeInTheDocument();
+    expect(screen.getByTestId('Personal-Birth-Drop-Down')).toBeInTheDocument();
+  });
+
+  it('opens calendar popover when clicked', async () => {
+    render(<Wrapper />);
+    const button = screen.getByTestId('Personal-Birth-Drop-Down');
+    await userEvent.click(button);
+    expect(await screen.findByRole('dialog')).toBeInTheDocument(); // Popover open
+  });
+
+  it('shows age helper text when date is selected', () => {
+    const birthday = new Date(2000, 0, 1); // Jan 1, 2000
+
+    const CustomWrapper = () => {
+      const { control } = useForm<FormValues>({
+        defaultValues: {
+          birth: birthday,
+        },
+      });
+
+      return <Controller control={control} name="birth" render={({ field }) => <DatePicker field={field} />} />;
+    };
+
+    render(<CustomWrapper />);
+
+    // Энэ хэсэг таны `getHelperText()`-ийн буцаадаг тексттэй таарах ёстой.
+    expect(screen.getByText(/years old/i)).toBeInTheDocument();
+  });
+});
--- apps/L2B/hotel-booking/client/src/app/(main)/(profiles)/_components/MiddleHeader.tsx
+++ apps/L2B/hotel-booking/client/src/app/(main)/(profiles)/_components/MiddleHeader.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const MiddleHeader = ({ h3, p }: Props) => {
   return (
-    <div className="w-[672px] border-b-[1px] pb-6 mb-6">
+    <div data-testid="middle" className="w-[672px] border-b-[1px] pb-6 mb-6">
       <h3 className="text-[18px] font-[500 leading-[28px]">{h3}</h3>
       <p className="text-[14px] font-[300] text-[#71717a] leading-[20px]">{p}</p>
     </div>
