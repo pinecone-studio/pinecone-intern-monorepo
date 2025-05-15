c0fbe11ec7c4d3fd4619a3f4c0bf66260b9ac093
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-05-15 16:03:29 +0800

    docs: back-end signin and test done

--- apps/L2B/tinder/tinder-backend/src/schemas/mutation.schema.ts
+++ apps/L2B/tinder/tinder-backend/src/schemas/mutation.schema.ts
@@ -2,12 +2,28 @@ import gql from 'graphql-tag';
 
 export const mutationTypeDefs = gql`
   scalar JSON
-
   scalar Date
+
+  type User {
+    _id: ID!
+    email: String!
+    password: String!
+    isVerified: Boolean
+    verficationCode: String!
+    createdAt: String
+    updatedAt: String
+  }
+
+  type SignInResponse {
+    message: String!
+    token: String!
+    user: User!
+  }
 
   type Mutation {
     sendOTP(email: String!): User!
     isVerified(email: String!, otp: String!): String!
     addUser(email: String!, password: String!): User!
+    signIn(email: String!, password: String!): SignInResponse!
   }
 `;
