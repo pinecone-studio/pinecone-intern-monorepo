5ae76a317d50f3fd025477a9b90531318c5a9fc3
Ochir00 <mongolbucker@gmail.com>
2025-05-15 17:08:47 +0800

    feat: cypress create account

--- apps/L2B/tinder/tinder-frontend/src/app/auth/create-account/_components/StepFourthCart.tsx
+++ apps/L2B/tinder/tinder-frontend/src/app/auth/create-account/_components/StepFourthCart.tsx
@@ -6,7 +6,7 @@ export const StepFourthCart = ({ index, selectedImages, removeImage }: { index: 
     <div className="relative w-[197px] h-[296px] overflow-hidden rounded-md bg-gray-100">
       <Image src={selectedImages[index] || '/placeholder.svg'} alt={`Uploaded image ${index + 1}`} fill className="object-cover" />
       <button onClick={() => removeImage(index)} className="absolute w-8 h-8 right-1 top-1 rounded-[10px] bg-white p-1 shadow-md">
-        <X className="h-4 w-4 text-gray-600 m-auto" />
+        <X data-cydata-cy='deleteImage' className="h-4 w-4 text-gray-600 m-auto" />
       </button>
     </div>
   );
