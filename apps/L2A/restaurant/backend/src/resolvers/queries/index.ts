38fd4ae2f02278f5ab062caf457b42ff9f371a02
azaaa-9 <az95342030@gmail.com>
2025-05-15 19:07:22 +0800

    category-query

--- apps/L2A/restaurant/backend/src/resolvers/queries/index.ts
+++ apps/L2A/restaurant/backend/src/resolvers/queries/index.ts
@@ -3,3 +3,5 @@ export * from './get-table-by-id'
 export * from './get-all-table';
 export * from './get-orders';
 export * from './get-order-by-id';
+export * from './get-categories';
+export * from './get-category-by-id';
