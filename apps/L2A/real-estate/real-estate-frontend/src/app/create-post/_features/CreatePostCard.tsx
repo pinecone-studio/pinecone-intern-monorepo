fa511affaaca2ae3d60e590f570b878b719174a3
Solongooyaa <u.solongo2018@gmail.com>
2025-05-16 14:27:36 +0800

    last

--- apps/L2A/real-estate/real-estate-frontend/src/app/create-post/_features/CreatePostCard.tsx
+++ apps/L2A/real-estate/real-estate-frontend/src/app/create-post/_features/CreatePostCard.tsx
@@ -8,8 +8,6 @@ import { CreatePostRestroom } from '../_components/CreatePostRestroom';
 import { CreatePostRoom } from '../_components/CreatePostRoom';
 import { CreatePostParking } from '../_components/CreatePostParking';
 import { CreatePostText } from '../_components/CreatePostText';
-import * as Yup from 'yup';
-import { useFormik, FormikTouched, FormikErrors } from 'formik';
 import { CreatePostLocation } from '../_components/CreatePostLocation';
 import { CreatePostDistrict } from '../_components/CreatePostDistrict';
 import { CreatePostSection } from '../_components/CreatePostSection';
@@ -23,75 +21,16 @@ import { CreatePostApartFloor } from '../_components/CreatePostApartFloor';
 import { CreatePostGround } from '../_components/CreatePostGround';
 import { CreatePostBalcony } from '../_components/CreatePostBalcony';
 import { CreatePostImages } from '../_components/CreatePostImages';
+import { useCreatePostFormik } from '../formUtils';
+import { FormikErrors, FormikTouched } from 'formik';
 
-const validationSchema = Yup.object({
-  type: Yup.string().required('Төрлөө сонгоно уу!'),
-  name: Yup.string().required('Нэр заавал оруулна уу!'),
-  price: Yup.number()
-    .transform((_value, originalValue) => Number(originalValue))
-    .required('Үнэ заавал оруулна уу!')
-    .min(0, 'Үнэ 0-ээс их байх ёстой!'),
-  field: Yup.number()
-    .transform((_value, originalValue) => Number(originalValue))
-    .required('Талбайн утгыг заавал оруулна уу!')
-    .min(10, 'Талбайн утга 2-оос дээш оронтой байх ёстой!'),
-  room: Yup.number()
-    .transform((_value, originalValue) => Number(originalValue))
-    .required('Өрөөний тоог заавал оруулна уу!')
-    .min(1, 'Өрөөний тоо 1-ээс их байх ёстой!'),
-  restroom: Yup.number()
-    .transform((_value, originalValue) => Number(originalValue))
-    .required('Ариун цэврийн өрөөний тоог заавал оруулна уу!')
-    .min(1, 'Ариун цэврийн өрөөний тоо 1-ээс их байх ёстой!'),
-  parking: Yup.string().required('Зогсоол сонгоно уу!'),
-  text: Yup.string().required('Дэлгэрэнгүй тайлбар оруулна уу!'),
-  images: Yup.string().required('Зураг заавал оруулна уу!'),
-  district: Yup.string().required('Дүүрэг заавал оруулна уу!'),
-  section: Yup.string().required('Хороо заавал оруулна уу!'),
-  year: Yup.number().required('Ашиглалтанд орсон он заавал оруулна уу!'),
-  windows: Yup.number().required('Цонхны тоо заавал оруулна уу!').min(1, 'Цонхны тоо 1-ээс их байх ёстой!'),
-  window: Yup.string().required('Цонхны загварыг заавал оруулна уу!'),
-  door: Yup.string().required('Хаалганы загварыг заавал оруулна уу!'),
-  floor: Yup.number().required('Давхар заавал оруулна уу!').min(1, 'Давхар 1-ээс их байх ёстой!'),
-  aptfloor: Yup.number().required('Давхрын тоог заавал оруулна уу!').min(1, 'Давхрын тоо 1-ээс их байх ёстой!'),
-  ground: Yup.string().required('Шалны загвар заавал оруулна уу!'),
-  balcony: Yup.string().required('Тагтны тоо заавал оруулна уу!'),
-});
-
-const getFieldError = (touched: FormikTouched<any>, errors: FormikErrors<any>, field: string): string | undefined => {
-  const error = errors[field];
-  return touched[field] && typeof error === 'string' ? error : undefined;
-};
 
 export const CreatePostCard = () => {
-  const formik = useFormik({
-    initialValues: {
-      type: '',
-      name: '',
-      price: '',
-      field: '',
-      room: '',
-      restroom: '',
-      parking: '',
-      text: '',
-      images: [],
-      location: '',
-      district: '',
-      section: '',
-      year: '',
-      windows: '',
-      window: '',
-      door: '',
-      floor: '',
-      aptfloor: '',
-      ground: '',
-      balcony: '',
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      console.log('Form data', values);
-    },
-  });
+  const formik = useCreatePostFormik();
+  const getFieldError = (touched: FormikTouched<any>, errors: FormikErrors<any>, field: string): string | undefined => {
+    const error = errors[field];
+    return touched[field] && typeof error === 'string' ? error : undefined;
+  };
 
   return (
     <form onSubmit={formik.handleSubmit} className="space-y-2 gap-2">
@@ -105,13 +44,7 @@ export const CreatePostCard = () => {
               <CreatePostPrice name="price" value={formik.values.price} onChange={formik.handleChange} onBlur={formik.handleBlur} error={getFieldError(formik.touched, formik.errors, 'price')} />
               <CreatePostField name="field" value={formik.values.field} onChange={formik.handleChange} onBlur={formik.handleBlur} error={getFieldError(formik.touched, formik.errors, 'field')} />
               <CreatePostRoom name="room" value={formik.values.room} onChange={formik.handleChange} onBlur={formik.handleBlur} error={getFieldError(formik.touched, formik.errors, 'room')} />
-              <CreatePostRestroom
-                name="restroom"
-                value={formik.values.restroom}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={getFieldError(formik.touched, formik.errors, 'restroom')}
-              />
+              <CreatePostRestroom name="restroom" value={formik.values.restroom} onChange={formik.handleChange} onBlur={formik.handleBlur}error={getFieldError(formik.touched, formik.errors, 'restroom')}/>
               <CreatePostParking
                 name="parking"
                 value={formik.values.parking}
