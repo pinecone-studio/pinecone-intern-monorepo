65035531646b21658438c3921b9e20263e4983c9
“dashka-bat” <dushkab02@gmail.com>
2025-05-20 16:29:02 +0800

    fix

--- apps/L2A/restaurant/frontend/cypress/e2e/HomePage/Profile.cy.tsx
+++ apps/L2A/restaurant/frontend/cypress/e2e/HomePage/Profile.cy.tsx
@@ -3,9 +3,11 @@ describe('Enrollment Page', () => {
     cy.clerkLogin('/profile', 'test+clerk_test@gmail.com');
   });
   it('should display the user section heading', () => {
-    cy.get('[data-cyid="Хэрэглэгчийн хэсэг"]').should('be.visible');
-  });
-  it('should render the user profile component', () => {
-    cy.get('[data-cyid="user-profile"]').should('be.visible');
+    cy.visit('/profile');
+    cy.get('[data-cyid="Хэрэглэгчийн хэсэг"]').should('exist').and('have.text', 'Хэрэглэгчийн хэсэг').and('have.class', 'text-center').and('have.class', 'font-bold');
+
+    it('1. Should render the page successfully")', () => {
+      cy.get('[data-cy="user-profile"]').should('be.visible');
+    });
   });
 });
