e2f51e512192765de13c1925dc927a14a5e8e47c
SAPAR SMAGUL <saparsmagul@SAPARs-MacBook-Pro.local>
2025-05-20 14:21:20 +0800

    feat(hotel query & mutation): hotel query and mutation

--- apps/L2B/hotel-booking/server/src/resolvers/queries/index.ts
+++ apps/L2B/hotel-booking/server/src/resolvers/queries/index.ts
@@ -1,3 +1,5 @@
 export * from './sample-query';
 export * from './get-user';
 export * from './get-users';
+export * from './get-hotels';
+export * from './get-hotel';
--- apps/L2B/hotel-booking/server/src/schemas/index.ts
+++ apps/L2B/hotel-booking/server/src/schemas/index.ts
@@ -2,5 +2,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
 import { UserTypeDefs } from './user.schema';
 import { authTypeDefs } from './auth.schema';
+import { HotelTypeDefs } from './hotel.schema';
+import { RoomTypeDefs } from './room.shema';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs, UserTypeDefs, authTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, UserTypeDefs, authTypeDefs, HotelTypeDefs, RoomTypeDefs]);
