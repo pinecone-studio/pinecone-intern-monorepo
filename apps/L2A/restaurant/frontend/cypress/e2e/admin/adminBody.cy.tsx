1c9e648e346bec4e9a14e28e4da8b57eed1ed81e
“dashka-bat” <dushkab02@gmail.com>
2025-05-20 17:17:54 +0800

    don123

--- apps/L2A/restaurant/frontend/cypress/e2e/admin/adminBody.cy.tsx
+++ apps/L2A/restaurant/frontend/cypress/e2e/admin/adminBody.cy.tsx
@@ -12,8 +12,6 @@ describe('Admin Body Tests', () => {
     cy.get('[data-testid="nav-item-Table"]').should('be.visible');
   });
   it('should go to correct pages when clicking menu items', () => {
-    cy.get('[data-testid="nav-item-Orders"]').click();
-    cy.url().should('include', '/admin/orders');
     cy.get('[data-testid="nav-item-Menu"]').click();
     cy.url().should('include', '/admin/menu');
     cy.get('[data-testid="nav-item-Food"]').click();
