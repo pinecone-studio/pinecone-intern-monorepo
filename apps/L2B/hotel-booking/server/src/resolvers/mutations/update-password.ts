e32f3497316849c52f9c5cd7dc1e36974f946469
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-05-20 17:36:03 +0800

    feat(profile): fixed

---
+++ apps/L2B/hotel-booking/server/src/resolvers/mutations/update-password.ts
@@ -0,0 +1,13 @@
+import { MutationResolvers } from '../../generated';
+import { userModel } from '../../models';
+import { hashPassword } from '../../utils/auth';
+
+export const UpdatePassword: MutationResolvers['updatePassword'] = async (_, { _id, password }) => {
+  const hashedPassword = await hashPassword(password);
+
+  const updatedUser = await userModel.findByIdAndUpdate(_id, { $set: { password: hashedPassword } }, { new: true });
+
+  if (!updatedUser) throw new Error('User not found');
+
+  return updatedUser;
+};
