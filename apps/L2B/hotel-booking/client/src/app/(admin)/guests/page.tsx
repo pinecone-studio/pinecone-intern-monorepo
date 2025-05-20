456ea47e3ee411c3af684e1dbb012afc0a99ba17
24LP4202 24LP4202 <24LP4202@administrators-iMac.local>
2025-05-20 12:45:43 +0800

    dssd

--- apps/L2B/hotel-booking/client/src/app/(admin)/guests/page.tsx
+++ apps/L2B/hotel-booking/client/src/app/(admin)/guests/page.tsx
@@ -1,7 +1,13 @@
+import { PanelLeft } from 'lucide-react';
+
 const Guests = () => {
   return (
-    <div data-cy="Guests-Page" className="ml-5 ">
-      Guests Page
+    <div>
+      <div data-cy="Guests-Page" className="w-screen h-[64px] bg-green-50 flex gap-20  ">
+        <PanelLeft />
+        <div className="border-2  h-[20px]"></div>
+        <h1>Hotels</h1>
+      </div>
     </div>
   );
 };
--- apps/L2B/hotel-booking/client/src/app/page.tsx
+++ apps/L2B/hotel-booking/client/src/app/page.tsx
@@ -1,9 +1,18 @@
 'use client';
 
-import Card from "./_components/Card/Page";
+import Card from './_components/Card/Page';
+import { Footer } from './_components/Footer';
+import Header from './_components/Header';
 
 const Page = () => {
-  return <div><Card/></div>;
+  return (
+    <div>
+      {' '}
+      <Header />
+      <Card />
+      <Footer />
+    </div>
+  );
 };
 
 export default Page;
