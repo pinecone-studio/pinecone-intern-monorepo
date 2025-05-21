141a1d8b3285c28f512aee203876450b796fcf72
“dashka-bat” <dushkab02@gmail.com>
2025-05-21 12:23:18 +0800

    ingej neg ym duusla

--- apps/L2A/restaurant/frontend/cypress/e2e/login/Login.cy.tsx
+++ apps/L2A/restaurant/frontend/cypress/e2e/login/Login.cy.tsx
@@ -1,19 +1,8 @@
 describe('Login Page', () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.clerkLogin('/login', 'test+clerk_test@gmail.com');
   });
-
-  it('should display the logo', () => {
-    cy.get('[data-testid="Logo-image"]').should('be.visible');
-  });
-
-  it('should open the Clerk sign-in modal when login button is clicked', () => {
-    cy.get('[data-cy="login-btn"]').click();
-  });
-
-  it('should redirect to home if already signed in', () => {
-    cy.setCookie('__session', 'mocked-session-token');
+  it('should display the login button', () => {
     cy.visit('/');
-    cy.url().should('eq', `${Cypress.config().baseUrl}/`);
   });
 });
