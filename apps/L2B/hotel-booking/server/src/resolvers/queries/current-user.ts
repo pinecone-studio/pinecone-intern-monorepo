56b6da42a7700bee976a5a6a308bb5e75d1cacb4
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-05-21 12:01:48 +0800

    feat(user): getCurrentUser

---
+++ apps/L2B/hotel-booking/server/src/resolvers/queries/current-user.ts
@@ -0,0 +1,9 @@
+import { QueryResolvers } from '../../generated';
+import jwt from 'jsonwebtoken';
+import { userModel } from '../../models';
+
+export const getCurrentUser: QueryResolvers['getCurrentUser'] = async (_, { JWT }) => {
+  const verify = jwt.verify(JWT, process.env.JWT_SECRET as string) as { id: string; email: string };
+  const user = await userModel.findOne({ email: verify.email });
+  return user;
+};
