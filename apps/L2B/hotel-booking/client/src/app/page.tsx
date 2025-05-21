ef4348dd56a9b8d83ebce70f255d9393da9c3adc
DZ-lys <idoY0UTUBEbtw@gmail.com>
2025-05-21 09:47:40 +0800

    feat(home page): styled Home page

--- apps/L2B/hotel-booking/client/src/app/page.tsx
+++ apps/L2B/hotel-booking/client/src/app/page.tsx
@@ -1,6 +1,20 @@
+'use client';
+import Card from './_components/Card/Page';
+import { Footer } from './_components/Footer';
+import Header from './_components/Header';
+import { HomeHeadline } from './_components/HomeHeadline';
 
 const Page = () => {
-  return <div></div>;
-
+  return (
+    <div>
+      <Header bg={'blue'} />
+      <main>
+        <HomeHeadline />
+        <Card />
+      </main>
+      <Footer />
+    </div>
+  );
 };
+
 export default Page;
