1b99dba4044d06877e3bf650077c88ce8765ba3a
“dashka-bat” <dushkab02@gmail.com>
2025-05-21 12:58:56 +0800

    fixed

--- apps/L2A/restaurant/frontend/src/app/_features/HomeOrder.tsx
+++ apps/L2A/restaurant/frontend/src/app/_features/HomeOrder.tsx
@@ -1,11 +1,6 @@
 'use client';
 import CartItem from '@/app/_components/CartItems';
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import { X } from 'lucide-react';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -29,30 +24,25 @@ const HomeOrder = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <div className="w-full p-4 shadow-lg flex flex-col items-center">
-        <div className="text-2xl font-bold mb-4 text-[#441500] flex justify-center">
-          Таны захиалга
-        </div>
+        <div className="text-2xl font-bold mb-4 text-[#441500] flex justify-center">Таны захиалга</div>
 
         <CartItem item={product} data-testid="cart-item" />
 
         <div className="flex w-full justify-center bg-black items-center mt-12">
           <AlertDialog>
             <AlertDialogTrigger asChild>
-              <button
-                className="text-[#FAFAFA] bg-[#441500] w-full h-[36px] rounded-md font-medium text-[14px]"
-                data-testid="order-button"
-              >
+              <button className="text-[#FAFAFA] bg-[#441500] w-full h-[36px] rounded-md font-medium text-[14px]" data-testid="order-button">
                 Захиалах
               </button>
             </AlertDialogTrigger>
 
-            <AlertDialogContent className="rounded-lg">
+            <AlertDialogContent className="rounded-lg w-[350px] h-[140px]">
               <AlertDialogCancel className="absolute top-2 right-4" data-testid="close-dialog">
-                <X  />
+                <X />
               </AlertDialogCancel>
 
               <div className="flex justify-between items-center mb-6">
-                <h2 className="text-[20px] font-md">Зааланд суух эсэх</h2>
+                <h2 className="text-[20px] ml-[30px] font-md">Зааланд суух эсэх</h2>
               </div>
 
               <div className="flex gap-8 justify-center">
@@ -61,7 +51,7 @@ const HomeOrder = () => {
                     type="radio"
                     name="dineOption"
                     value="dinein"
-                    onChange={() => handleRouteChange('dinein')} 
+                    onChange={() => handleRouteChange('dinein')}
                     checked={option === 'dinein'}
                     data-testid="radio-dinein"
                     className="w-[20px] h-[20px]"
