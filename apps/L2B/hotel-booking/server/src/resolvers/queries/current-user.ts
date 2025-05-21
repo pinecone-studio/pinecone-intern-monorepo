29ddd14df736b8e8aad110f139c221386a8f5411
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-05-21 12:07:27 +0800

    feat(user): added error message

--- apps/L2B/hotel-booking/server/src/resolvers/queries/current-user.ts
+++ apps/L2B/hotel-booking/server/src/resolvers/queries/current-user.ts
@@ -3,7 +3,11 @@ import jwt from 'jsonwebtoken';
 import { userModel } from '../../models';
 
 export const getCurrentUser: QueryResolvers['getCurrentUser'] = async (_, { JWT }) => {
-  const verify = jwt.verify(JWT, process.env.JWT_SECRET as string) as { id: string; email: string };
-  const user = await userModel.findOne({ email: verify.email });
-  return user;
+  try {
+    const verify = jwt.verify(JWT, process.env.JWT_SECRET as string) as { id: string; email: string };
+    const user = await userModel.findOne({ email: verify.email });
+    return user;
+  } catch {
+    throw new Error('invalid token');
+  }
 };
