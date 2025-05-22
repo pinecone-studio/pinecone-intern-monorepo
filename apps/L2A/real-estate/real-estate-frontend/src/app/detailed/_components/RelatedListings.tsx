b178e12935a87443cddfb9f0fd2c81b9f815240c
Duu-Avia <duuavia01@gmail.com>
2025-05-22 23:18:31 +0800

    feat(real-estate): linked on related post

--- apps/L2A/real-estate/real-estate-frontend/src/app/detailed/_components/RelatedListings.tsx
+++ apps/L2A/real-estate/real-estate-frontend/src/app/detailed/_components/RelatedListings.tsx
@@ -1,11 +1,13 @@
 import ListingCard from '@/app/home/_components/ListingCard';
 import { GetPostsQuery } from '@/generated';
+import Link from 'next/link';
 
 const RelatedListings = ({ posts }: { posts?: GetPostsQuery['getPosts'] }) => (
   <div className="mt-12">
     <h2 className="text-2xl font-bold mb-4">Төстэй зарууд</h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" data-testid="listing-Grid" data-cy="listing-Grid">
       {posts?.map((item) => (
+        <Link key={`related-${item?._id}`} href={`/detailed/${item?._id}`}>
         <ListingCard
           key={`detailed-${item._id}`}
           image={item?.images?.[0] || '/placeholder.png'}
@@ -16,7 +18,7 @@ const RelatedListings = ({ posts }: { posts?: GetPostsQuery['getPosts'] }) => (
           size={item?.size}
           city={item?.location?.city}
           district={item?.location?.district}
-        />
+        /></Link>
       ))}
     </div>
   </div>
