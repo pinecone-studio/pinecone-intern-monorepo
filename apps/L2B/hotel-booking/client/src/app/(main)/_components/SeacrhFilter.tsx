0d89db2324d7ec5a1bba99303370657623615ab7
Temm Kss <temmkss@Temms-MacBook-Air.local>
2025-05-22 18:26:41 +0800

    feat: search filter

---
+++ apps/L2B/hotel-booking/client/src/app/(main)/_components/SeacrhFilter.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Button } from '@/components/ui/button';
+import CalendarDate from './CalendarDate';
+import GuestOptions from '../_features/Guest';
+import { DateRange } from 'react-day-picker';
+
+const SearchFilter = () => {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2025, 9, 19),
+    to: new Date(2025, 9, 20),
+  });
+
+  const handleDateChange = (newDate: DateRange | undefined) => {
+    setDate(newDate);
+  };
+  return (
+    <div className="max-w-[1120px] flex justify-between h-[94px] rounded-2xl w-full border-orange-300 border-2 m-auto mt-40 ">
+      <div className="max-w-[500px] h-[62px] w-full p-3 flex flex-col gap-2">
+        <div className="text-sm  font-medium ">Dates</div>
+        <CalendarDate date={date} handleDateChange={handleDateChange} />
+      </div>
+      <div className="max-w-[500px] h-[62px] w-full flex flex-col gap-2 p-3">
+        <h2 className="font-medium text-sm ">Guest</h2>
+        <GuestOptions />
+      </div>
+      <Button className="max-w-[95px] w-full h-[40px] bg-blue-600 m-auto mt-10">Search</Button>
+    </div>
+  );
+};
+
+export default SearchFilter;
