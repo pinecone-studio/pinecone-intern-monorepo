06c618bfcba4464038d918f27dac580d9f73b5f2
macbook pro <macbookpro@Gegeea.local>
2025-05-27 16:47:51 +0800

    search

--- apps/L2A/concert/backend/src/resolvers/mutations/search-events.ts
+++ apps/L2A/concert/backend/src/resolvers/mutations/search-events.ts
@@ -1,15 +1,14 @@
-import { MutationResolvers } from '../../generated';
 import { concertModel } from '../../models';
 import { catchError } from '../../utils/catch-error';
 import escapeRegex from '../../utils/escape-regex';
 
-export const searchEvents: MutationResolvers['searchEvents'] = async (_, { name }) => {
+export const searchEvents = async (_: any, { name }: { name: string }) => {
   try {
     if (!name) {
       throw new Error('Name parameter is required for searching events.');
     }
     const safeRegex = new RegExp(escapeRegex(name), 'i');
-    const concerts = await concertModel.find({ title: safeRegex });
+    const concerts = await concertModel.find({ title: safeRegex }).populate('venue').populate('seatData');
     return concerts;
   } catch (err) {
     return catchError(err);
