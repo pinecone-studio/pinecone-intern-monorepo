0e219a73082aa033b43c4d4044c7aac1df549cfe
zaya z <zayaz@zayas-MacBook-Pro.local>
2025-05-28 14:34:25 +0800

    feat: array orj irdeg bolgoson update image

--- apps/L2B/tinder/tinder-backend/src/resolvers/mutations/profile/update-image.ts
+++ apps/L2B/tinder/tinder-backend/src/resolvers/mutations/profile/update-image.ts
@@ -1,7 +1,11 @@
 import { profileModel } from '../../../models/profile.model';
 
-export const updateProfileImage = async (_: unknown, { userId, images }: { userId: string; images: string }) => {
-  const updatedProfile = await profileModel.findOneAndUpdate({ user: userId }, { $set: { images: [images] } }, { new: true });
+export const updateProfileImage = async (_: unknown, { userId, images }: { userId: string; images: string[] }) => {
+  const updatedProfile = await profileModel.findOneAndUpdate(
+    { user: userId },
+    { $set: { images } }, // directly use the array
+    { new: true }
+  );
 
   if (!updatedProfile) {
     throw new Error('Profile not found. Please create a profile first.');
