195133bad722129fda11a2f1a50d0578f4655413
gandak <gandak0930@gmail.com>
2025-05-29 19:54:30 +0800

    fix(fix): fix
    fixed test for util function

--- apps/L2B/tinder/tinder-frontend/src/app/utils/upload-to-cloudinary.test.ts
+++ apps/L2B/tinder/tinder-frontend/src/app/utils/upload-to-cloudinary.test.ts
@@ -10,7 +10,6 @@ describe('uploadToCloudinary', () => {
     process.env.NEXT_PUBLIC_CLOUDINARY_NAME = 'test_cloud_name';
 
     global.alert = jest.fn();
-
     global.fetch = jest.fn();
   });
 
@@ -37,12 +36,10 @@ describe('uploadToCloudinary', () => {
   });
 
   it('uploads files and returns array of URLs', async () => {
-    (global.fetch as jest.Mock).mockImplementation(() =>
-      Promise.resolve({
-        // eslint-disable-next-line camelcase
-        json: () => Promise.resolve({ secure_url: 'https://cloudinary.com/image1.png' }),
-      })
-    );
+    (global.fetch as jest.Mock).mockResolvedValue({
+      // eslint-disable-next-line camelcase
+      json: () => Promise.resolve({ secure_url: 'https://cloudinary.com/image1.png' }),
+    });
 
     const file = new File(['dummy content'], 'test.png', { type: 'image/png' });
 
@@ -60,9 +57,7 @@ describe('uploadToCloudinary', () => {
           json: () => Promise.resolve({ secure_url: 'https://cloudinary.com/image1.png' }),
         })
       )
-      .mockImplementationOnce(() => {
-        throw new Error('Upload error');
-      });
+      .mockImplementationOnce(() => Promise.reject(new Error('Upload error')));
 
     const file1 = new File(['dummy1'], 'test1.png', { type: 'image/png' });
     const file2 = new File(['dummy2'], 'test2.png', { type: 'image/png' });
@@ -79,5 +74,17 @@ describe('uploadToCloudinary', () => {
     );
 
     consoleErrorSpy.mockRestore();
+  });
+
+  it('returns empty array if upload response has no secure_url', async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+      json: () => Promise.resolve({}),
+    });
+
+    const file = new File(['dummy'], 'file.png', { type: 'image/png' });
+
+    const result = await uploadToCloudinary([file], 'user1');
+
+    expect(result).toEqual([]);
   });
 });
