225ed1f9900d925311307c5ecc2953651d438d19
erhme <admin@erhmes-MacBook-Air.local>
2025-05-30 14:42:05 +0800

    fixed gap

--- apps/L2A/real-estate/real-estate-frontend/src/app/listing/_components/RestRoomsComp.tsx
+++ apps/L2A/real-estate/real-estate-frontend/src/app/listing/_components/RestRoomsComp.tsx
@@ -1,27 +1,27 @@
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { RestRoomsCompProps } from "@/lib/typescripts";
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
+import { RestRoomsCompProps } from '@/lib/typescripts';
 
-const RestRoomsComp = ({restrooms, setTotalRestrooms}:RestRoomsCompProps) =>{
-    const selectedRestrooms = restrooms?.split(',').map(Number) ?? [];
+const RestRoomsComp = ({ restrooms, setTotalRestrooms }: RestRoomsCompProps) => {
+  const selectedRestrooms = restrooms?.split(',').map(Number) ?? [];
 
-    const toggleRestroom = (value: number) => {
-    const updated = selectedRestrooms.includes(value)? selectedRestrooms.filter((v) => v !== value): [...selectedRestrooms, value];
+  const toggleRestroom = (value: number) => {
+    const updated = selectedRestrooms.includes(value) ? selectedRestrooms.filter((v) => v !== value) : [...selectedRestrooms, value];
     setTotalRestrooms(updated.join(','));
   };
-    return (
-        <>
-           <div>
-              <Label className="block mb-2">Ариун цэврийн өрөө</Label>
-              {[1, 2, 3].map((bath) => (
-                <div key={bath} className="flex items-center gap-2" data-cy={`bath-${bath}`}>
-                  <Checkbox id={`bath-${bath}`} checked={selectedRestrooms.includes(bath)} onCheckedChange={() => toggleRestroom(bath)} />
-                  <Label htmlFor={`bath-${bath}`}>{bath} өрөө</Label>
-                </div>
-              ))}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="space-y-2">
+        <Label className="block mb-2">Ариун цэврийн өрөө</Label>
+        {[1, 2, 3].map((bath) => (
+          <div key={bath} className="flex items-center gap-2" data-cy={`bath-${bath}`}>
+            <Checkbox id={`bath-${bath}`} checked={selectedRestrooms.includes(bath)} onCheckedChange={() => toggleRestroom(bath)} />
+            <Label htmlFor={`bath-${bath}`}>{bath} өрөө</Label>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default RestRoomsComp
+export default RestRoomsComp;
