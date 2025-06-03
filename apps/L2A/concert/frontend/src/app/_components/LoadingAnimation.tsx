fa724080a2c3550da60b3ab5bbf503c94e4a323a
macbook pro <macbookpro@Gegeea.local>
2025-06-03 14:32:24 +0800

    loading animation

---
+++ apps/L2A/concert/frontend/src/app/_components/LoadingAnimation.tsx
@@ -0,0 +1,15 @@
+import LinearColor from '../_utils/LoadingMui';
+
+const LoadingAnimation = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-2">
+      <video src="/loading.mp4" autoPlay loop muted className="w-16 h-16 mb-4 rounded-full shadow-lg bg-[#09090B]" />
+      <div>Түр хүлээнэ үү!</div>
+      <div className="w-2/12">
+        <LinearColor />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
