b3f5dc42b7068627ac245fcbd213819e154d026d
gandak <gandak0930@gmail.com>
2025-06-04 22:41:23 +0800

    fix: fix
    fix

--- apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
+++ apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
@@ -1,11 +1,12 @@
-
 describe('Main page', () => {
-  it('renders all main components', () => {
-    cy.visit('/'); 
-cy.get('[data-testid="guest-header"]').should('be.visible');
-cy.get('[data-testid="main"]').should('be.visible');
+  it('renders all main components and triggers Page render logic', () => {
+    cy.visit('/');
+
+    cy.get('[data-testid="guest-header"]').should('be.visible');
+    cy.get('[data-testid="main"]').should('be.visible');
     cy.get('[data-testid="footer"]').should('be.visible');
 
+    cy.contains('Tinder').should('exist'); 
+    cy.contains('Terms of Service').should('exist'); 
   });
 });
-
