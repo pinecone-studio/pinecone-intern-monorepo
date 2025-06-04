622820f9c149251a8bd62990999c408f27b3fc88
gandak <gandak0930@gmail.com>
2025-06-04 21:41:28 +0800

    fix(bug): fixed bug
    fixed bug

--- apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
+++ apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
@@ -1,5 +1,10 @@
-describe('Home Page', () => {
-  it(`Should render home page`, () => {
-    cy.visit('/');
+
+describe('Main page', () => {
+  it('renders all main components', () => {
+    cy.visit('/'); 
+
+    cy.get('header').should('exist'); 
+    cy.get('main').should('exist'); 
+    cy.get('footer').should('exist'); 
   });
 });
