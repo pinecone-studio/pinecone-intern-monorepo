651b99ef1326f9f175d0d0289a2657bdf51d8e65
gandak <gandak0930@gmail.com>
2025-06-04 22:15:03 +0800

    fix(fix): fix
    fix

--- apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
+++ apps/L2B/tinder/tinder-frontend/cypress/e2e/(main)/home-page.cy.tsx
@@ -2,8 +2,9 @@
 describe('Main page', () => {
   it('renders all main components', () => {
     cy.visit('/'); 
-
-    cy.get('[data-testid="container"]').should('be.visible');
+cy.get('[data-testid="guest-header"]').should('be.visible');
+cy.get('[data-testid="main"]').should('be.visible');
+    cy.get('[data-testid="footer"]').should('be.visible');
 
   });
 });
--- apps/L2B/tinder/tinder-frontend/src/app/page.tsx
+++ apps/L2B/tinder/tinder-frontend/src/app/page.tsx
@@ -2,24 +2,24 @@ import Container from './_components/Container';
 import Footer from './_components/Footer';
 import GuestHeader from './_components/GuestHeader';
 
+const backgroundStyle = {
+  backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/homeBackground.png')`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'cover',
+  minHeight: '100vh',
+  color: '#FAFAFA',
+};
+
 const Page = () => {
   return (
-    <div data-testid="container"
-      style={{
-        backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/homeBackground.png')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'cover',
-        minHeight: '100vh',
-        color: '#FAFAFA',
-      }}
-      className="flex  flex-col  items-center gap-6 "
-    >
+    <div style={backgroundStyle} className="flex flex-col items-center gap-6">
       <GuestHeader />
       <Container />
       <Footer />
     </div>
   );
 };
+
 
 export default Page;
