883eeb5c5ba9424bdbb2dc37b71ae2ef8b2341a7
zaya z <zayaz@zayas-MacBook-Pro.local>
2025-06-05 13:57:17 +0800

    feat: done

--- apps/L2B/tinder/tinder-backend/src/resolvers/queries/profile/fetch-profile.ts
+++ apps/L2B/tinder/tinder-backend/src/resolvers/queries/profile/fetch-profile.ts
@@ -3,7 +3,7 @@ import { profileModel } from '../../../models';
 export const fetchProfile = async (_: unknown, { _id }: { _id: string }) => {
   const profile = await profileModel.findById(_id).populate('user').populate('matched');
   if (!profile) {
-    throw new Error('Profile not found');
+    throw new Error('Profile not found.');
   }
   return profile;
 };
