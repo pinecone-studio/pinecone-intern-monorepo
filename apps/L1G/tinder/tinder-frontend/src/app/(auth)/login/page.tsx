141bc4559829650d5cf88925aa158506febe9624
24LP0441 24LP0441 <24LP0441@G303-CC0131.local>
2025-07-01 16:20:31 +0800

    login test

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/login/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/login/page.tsx
@@ -1,10 +1,11 @@
 import { LoginForm } from '@/components/Login';
 import { MainHeader } from '@/components/MainHeader';
-
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 const SignInPage = () => {
   return (
-    <div className="min-w-screen min-h-screen flex justify-center items-center">
-      <div className="w-fit flex flex-col gap-6">
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-[350px] h-fit flex flex-col gap-6 items-center">
         <MainHeader />
 
         <div className="flex flex-col gap-[4px] py-2 justify-center items-center">
@@ -12,7 +13,21 @@ const SignInPage = () => {
           <p className="inter text-[14px] font-[400]">Enter your email below to sign in</p>
         </div>
 
-        <LoginForm />
+        <div className="w-full h-fit flex flex-col gap-4">
+          <LoginForm />
+
+          <div className="w-full flex justify-between items-center gap-[10px] py-4">
+            <Separator className="w-[156px]" />
+            <p className="inter font-[400] text-[12px] text-[#71717A]">OR</p>
+            <Separator className="w-[156px]" />
+          </div>
+
+          <Button className="rounded-full border-[1px] border-[#E4E4E7] bg-white text-[#18181B] inter text-[14px] font-[500] shadow-sm">Create an account</Button>
+        </div>
+
+        <p className="text-center inter text-wrap w-[249px] text-[14px] text-[#71717A] font-[400]">
+          By clicking continue, you agree to our <span className="underline">Terms of Service</span> and <span className="underline">Privacy Policy</span>.
+        </p>
       </div>
     </div>
   );
