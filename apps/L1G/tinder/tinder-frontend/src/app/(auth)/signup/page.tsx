f37ddaa81f2e89e2c70284121c05f01dbe9d2f37
24LP6774 24LP6774 <24LP6774@G305-CC0243.local>
2025-07-01 13:27:04 +0800

    TinderLogo component

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,5 +1,13 @@
+import TinderLogo from '@/components/TinderLogo';
+
 const Signup = () => {
-  return <div>Sign up page ju</div>;
+  return (
+    <div>
+      Sign up page ju
+      <TinderLogo />
+      <TinderLogo size={48} />
+    </div>
+  );
 };
 
 export default Signup;
