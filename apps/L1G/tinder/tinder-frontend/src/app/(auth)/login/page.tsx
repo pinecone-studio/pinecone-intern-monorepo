5daf91ccc726fe84eb0bf8761d71a4198a434fc1
chimegerdene <chimgeebatuh@gmail.com>
2025-07-01 13:37:42 +0800

    login header and form

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/login/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/login/page.tsx
@@ -1,5 +1,21 @@
-const LoginPage = () => {
-  return <div>Login page</div>;
+import { LoginForm } from '@/components/Login';
+import { MainHeader } from '@/components/MainHeader';
+
+const SignInPage = () => {
+  return (
+    <div className="min-w-screen min-h-screen flex justify-center items-center">
+      <div className="w-fit flex flex-col gap-6">
+        <MainHeader />
+
+        <div className="flex flex-col gap-[4px] py-2 justify-center items-center">
+          <p className="inter text-[24px] font-[600]">Sign in</p>
+          <p className="inter text-[14px] font-[400]">Enter your email below to sign in</p>
+        </div>
+
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
-export default LoginPage;
+export default SignInPage;
