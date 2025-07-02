4e69c7410b9aa5a65d2dff3dd9c484895e37bfd1
zayabdmr <zayabudmir@gmail.com>
2025-07-02 15:26:49 +0800

    feat(tinder): tur

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,13 +1,11 @@
-import { CreateAccount2 } from '@/components/CreateAccount2';
 import TinderLogo from '@/components/TinderLogo';
 
 const Signup = () => {
   return (
     <div>
-      {/* Sign up page ju
+      Sign up page ju
       <TinderLogo />
-      <TinderLogo size={40} /> */}
-      <CreateAccount2 />
+      <TinderLogo size={40} />
     </div>
   );
 };
