6de08e2cd985b28d8e074df44a12a396cd4608b0
24LP0441 24LP0441 <24LP0441@Tuvshins-iMac.local>
2025-07-02 16:17:34 +0800

    bug fix

--- apps/L1G/tinder/tinder-frontend/specs/LoginForm.spec.tsx
+++ apps/L1G/tinder/tinder-frontend/specs/LoginForm.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { LoginForm } from '@/components/Login';
 
 describe('Login form ', () => {
@@ -8,10 +8,8 @@ describe('Login form ', () => {
     const password = screen.getByPlaceholderText('Please enter your password') as HTMLInputElement;
     const submitBtn = screen.getByRole('button', { name: /Continue/i });
 
-    act(() => {
-      fireEvent.change(email, { target: { value: 'test@example.com' } });
-      fireEvent.change(password, { target: { value: 'password123' } });
-    });
+    fireEvent.change(email, { target: { value: 'test@example.com' } });
+    fireEvent.change(password, { target: { value: 'password123' } });
 
     expect(email.value).toBe('test@example.com');
     expect(password.value).toBe('password123');
