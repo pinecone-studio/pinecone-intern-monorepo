ca312524edc2be1c8304da975e76e3b611cc6cd2
24LP1634 24LP1634 <24LP1634@G305-CC0240.local>
2025-07-02 12:30:21 +0800

    codegen complete

---
+++ apps/L1G/tinder/tinder-frontend/specs/TinderLogo.spec.tsx
@@ -0,0 +1,13 @@
+import TinderLogo from '@/components/TinderLogo';
+import { render, } from '@testing-library/react';
+
+
+describe('TinderLogo', () => {
+    it('renders without crashing', () => {
+        render(<TinderLogo />);
+
+
+
+    });
+
+});
