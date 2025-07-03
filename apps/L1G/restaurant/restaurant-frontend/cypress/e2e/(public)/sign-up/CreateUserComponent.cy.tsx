2f3b92e389b2d1d5667933085d245abdca79b697
24LP7422 24LP7422 <24LP7422@administors-iMac.local>
2025-07-03 18:05:31 +0800

    feat(create user component): register new user
    register new user and navigate to sign-in

---
+++ apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(public)/sign-up/CreateUserComponent.cy.tsx
@@ -0,0 +1,53 @@
+
+
+
+describe('CreateUser Page', () => {
+  it('1. should render CreateUser page', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=create-user]').should('be.visible');
+  });
+  it('2. an error message should be displayed when the username is too short', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-username]').type('1');
+    cy.get('[data-cy=createUser-submit-btn]').click();
+    cy.get('[data-cy=createUser-username-error]').should('be.visible');
+    cy.get('[data-cy=createUser-username-error]').should('have.text', 'Username must be at least 2 characters.');
+  });
+  it('3. an error message should be displayed when the user did not enter an email', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-submit-btn]').click();
+    cy.get('[data-cy=createUser-email-error]').should('be.visible');
+    cy.get('[data-cy=createUser-email-error]').should('have.text', 'Please enter a valid email');
+
+  });
+  it('4. an error message should be displayed when the user did not enter password', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-password]').type('12');
+    cy.get('[data-cy=createUser-submit-btn]').click();
+    cy.get('[data-cy=createUser-password-error]').should('be.visible');
+    cy.get('[data-cy=createUser-password-error]').should('have.text', 'Please enter at least 8 letters');
+  });
+  it('5. an error message should be displayed when the user did not enter confrim password', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-confirmPassword]').type('12');
+    cy.get('[data-cy=createUser-submit-btn]').click();
+    cy.get('[data-cy=createUser-confirmPassword-error]').should('be.visible');
+    cy.get('[data-cy=createUser-confirmPassword-error]').should('have.text', 'Please enter at least 8 letters');
+  });
+  it('6. when user enters all values, it should navigate to login page', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-username]').type('User1');
+    cy.get('[data-cy=createUser-email]').type('user1@gmail.com');
+    cy.get('[data-cy=createUser-password]').type('user1@123');
+    cy.get('[data-cy=createUser-confirmPassword]').type('user1@123');
+    cy.get('[data-cy=createUser-submit-btn]').click();
+    cy.url().should('include', 'sign-in');
+  });
+  it('7. when user click the login button, it should navigate to login page', () => {
+    cy.visit('/sign-up');
+    cy.get('[data-cy=createUser-login-btn]').click();
+    cy.url().should('include', 'sign-in');
+  });
+});
+
+
