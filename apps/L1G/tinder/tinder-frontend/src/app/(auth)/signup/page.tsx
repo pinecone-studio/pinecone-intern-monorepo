fcc73670c81e5f3b1aa08a774311e320c794e8a0
zayabdmr <zayabudmir@gmail.com>
2025-07-03 10:34:43 +0800

    k

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,13 +1,11 @@
-import { GenderSelect } from '@/components/GenderSelect';
 import TinderLogo from '@/components/TinderLogo';
 
 const Signup = () => {
   return (
     <div>
-      Sign up page ju
+      Sign up page juu
       <TinderLogo />
       <TinderLogo size={40} />
-      <GenderSelect />
     </div>
   );
 };
