93f09035a2353360b8ebddfdec3e861bdf6b4e5f
24LP6774 24LP6774 <24LP6774@administrators-iMac.local>
2025-07-03 16:40:03 +0800

    page 9 FE done

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,11 +1,10 @@
 import TinderLogo from '@/components/TinderLogo';
+import YourDetailsPage from '@/components/YourDetailsPage';
 
 const Signup = () => {
   return (
     <div>
-      Sign up page ju
-      <TinderLogo />
-      <TinderLogo size={40} />
+      <YourDetailsPage />
     </div>
   );
 };
