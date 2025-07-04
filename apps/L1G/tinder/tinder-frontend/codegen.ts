f90ad4fe33434d4cbf32a762e9d200157ae6543a
zayabdmr <zayabudmir@gmail.com>
2025-07-04 10:28:35 +0800

    hi

--- apps/L1G/tinder/tinder-frontend/codegen.ts
+++ apps/L1G/tinder/tinder-frontend/codegen.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import type { CodegenConfig } from '@graphql-codegen/cli';
+import { existsSync } from 'fs';
+
+const schemaPath = existsSync('apps/L1G/tinder/tinder-frontend/schema.json') ? 'apps/L1G/tinder/tinder-frontend/schema.json' : process.env.BACKEND_URI;
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.BACKEND_URI,
+  schema: schemaPath,
   documents: ['apps/L1G/tinder/tinder-frontend/src/**/*.{graphql,ts,tsx}'],
   generates: {
     'apps/L1G/tinder/tinder-frontend/src/generated/index.ts': {
@@ -25,4 +28,5 @@ const config: CodegenConfig = {
     },
   },
 };
+
 export default config;
