a73edbf5ce95c44abe0388f50f41711733d4c8c7
bayarjavkhlan0603 <bayarjavkhlan8005@gmail.com>
2025-07-04 11:03:15 +0800

    q

---
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,11 @@
+import Forgetpassword from '@/components/ForgetPassword';
+
+const ForgetPasswordPage = () => {
+  return (
+    <div>
+      <Forgetpassword />
+    </div>
+  );
+};
+
+export default ForgetPasswordPage;
