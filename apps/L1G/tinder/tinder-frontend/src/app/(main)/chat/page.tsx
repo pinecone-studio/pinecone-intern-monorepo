afefd06e92fd120091ddbd5fe443dc321263d0c0
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-08-05 16:13:36 +0800

    bolgochii

--- apps/L1G/tinder/tinder-frontend/src/app/(main)/chat/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(main)/chat/page.tsx
@@ -1,15 +1,4 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-
 const Chat = () => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
   return <div>chat page</div>;
 };
 
