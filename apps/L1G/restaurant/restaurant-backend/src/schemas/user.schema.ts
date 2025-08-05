9800b8acfa7894f99e3d41fc923c27f876362d2f
mnkhbldd <mnkhbldd@gmail.com>
2025-08-05 19:27:38 +0800

    fix(restaurant-backend): fixed reset password response name

--- apps/L1G/restaurant/restaurant-backend/src/schemas/user.schema.ts
+++ apps/L1G/restaurant/restaurant-backend/src/schemas/user.schema.ts
@@ -27,9 +27,24 @@ export const userTypeDefs = gql`
     createUser(input: CreateUserInput!): User!
     updateUser(userId: ID!, input: UpdateUserInput!): User!
     deleteUser(userId: ID!): User!
-    sendResetCode(input: sendResetCodeInput!): Boolean
-    verifyResetCode(input: verifyResetCodeInput!): Boolean
-    resetPassword(input: resetPasswordInput!): Boolean
+    sendResetCode(input: sendResetCodeInput!): sendResetCodeResponse!
+    verifyResetCode(input: verifyResetCodeInput!): verifyResetCodeResponse!
+    resetPassword(input: resetPasswordInput!): resetPasswordResponse!
+  }
+
+  type sendResetCodeResponse {
+    input: Boolean
+    output: Boolean
+  }
+
+  type verifyResetCodeResponse {
+    input: Boolean
+    output: Boolean
+  }
+
+  type resetPasswordResponse {
+    input: Boolean
+    output: Boolean
   }
 
   input sendResetCodeInput {
