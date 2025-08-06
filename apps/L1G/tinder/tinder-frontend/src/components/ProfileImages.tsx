7565dc2a1473729533ba3f58f0036d7e78f228f4
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-06 17:20:48 +0800

    bug

--- apps/L1G/tinder/tinder-frontend/src/components/ProfileImages.tsx
+++ apps/L1G/tinder/tinder-frontend/src/components/ProfileImages.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { Button } from '@/components/ui/button';
 import { X, Plus, Loader2 } from 'lucide-react';
 import Image from 'next/image';
