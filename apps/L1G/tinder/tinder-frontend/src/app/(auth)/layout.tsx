4d9a39b6d83dd6c214ad2eea55bf03ca2ed05181
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-06 13:27:39 +0800

    auth layout wrapped in authprovider and apollowrapper

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/layout.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/layout.tsx
@@ -1,14 +1,21 @@
+'use client';
 import { AuthFooter } from '@/components/AuthFooter';
+import { ApolloWrapper } from '@/components/providers/ApolloWrapper';
+import { AuthProvider } from '@/components/providers/AuthProvider';
 import { PropsWithChildren } from 'react';
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
-      <body>
-        {children}
-        <AuthFooter />
-      </body>
-    </html>
+    <ApolloWrapper>
+      <AuthProvider>
+        <div className="flex flex-col min-h-screen">
+          <div className="flex-1">
+            {children}
+            <AuthFooter />
+          </div>
+        </div>
+      </AuthProvider>
+    </ApolloWrapper>
   );
 };
 
