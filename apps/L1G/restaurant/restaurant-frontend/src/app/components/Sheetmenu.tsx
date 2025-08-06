5ad1170e9b9d39a47bd0976e9369e6a12dd069ad
24LP4296 24LP4296 <24LP4296@administrators-iMac.local>
2025-08-06 17:54:01 +0800

    feat: navbar on layout

---
+++ apps/L1G/restaurant/restaurant-frontend/src/app/components/Sheetmenu.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { Menu } from 'lucide-react';
+
+import { Info } from 'lucide-react';
+
+import { User } from 'lucide-react';
+import { Wallet } from 'lucide-react';
+import { Home } from 'lucide-react';
+import { List } from 'lucide-react';
+export const SheetMenu = () => {
+  const handleOnClick = () => {
+    window.location.href = '/Aboutus';
+  };
+  const handleHomePage = () => {
+    window.location.href = '/';
+  };
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu size={15} />
+      </SheetTrigger>
+      <SheetContent>
+        <Menu className="absolute top-4" size={15} />
+        <SheetHeader>
+          <SheetTitle className="border-b pt-5"></SheetTitle>
+          <SheetDescription className="flex-col">
+            <div onClick={handleHomePage} className="flex gap-4 pl-3 items-center w-full h-[60px] border-b">
+              <Home size={12} />
+              <p className="font-[30px]">Нүүр хуудас</p>
+            </div>
+            <div className="flex gap-4 pl-3 items-center w-full h-[60px] border-b">
+              <Wallet size={12} />
+              <p className="font-[30px]">Хэтэвч</p>
+            </div>
+            <div className="flex gap-4 pl-3 items-center w-full h-[60px] border-b">
+              <User size={12} />
+              <p className="font-[30px]">Хэрэглэгч</p>
+            </div>
+            <div className="flex gap-4 pl-3 items-center w-full h-[60px] border-b">
+              <List size={12} />
+              <p className="font-[30px]">Захиалгын түүх</p>
+            </div>
+            <div onClick={handleOnClick} className="flex gap-4 pl-3 items-center w-full h-[60px] border-b">
+              <Info size={12} />
+              <p className="font-[30px]">Бидний тухай</p>
+            </div>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
--- apps/L1G/restaurant/restaurant-frontend/src/app/layout.tsx
+++ apps/L1G/restaurant/restaurant-frontend/src/app/layout.tsx
@@ -3,11 +3,13 @@ import './global.css';
 import { CssBaseline } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ApolloWrapper } from '../components/providers/ApolloWrapper';
+import { Navbar } from './components/Navbar';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="mn">
       <body>
+        <Navbar />
         <AppRouterCacheProvider>
           <ApolloWrapper>{children}</ApolloWrapper>
           <CssBaseline />
