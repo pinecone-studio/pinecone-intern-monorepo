42da683059ac751dfbc3dec5dbe588de9ae928bf
mnkhbldd <mnkhbldd@gmail.com>
2025-08-06 20:40:25 +0800

    fix(restaurant-frontend): resets default render all page of cypress test

--- apps/L1G/restaurant/restaurant-frontend/cypress/e2e/render-all-pages.cy.tsx
+++ apps/L1G/restaurant/restaurant-frontend/cypress/e2e/render-all-pages.cy.tsx
@@ -1,10 +1,10 @@
-// import allPages from '../utils/all-pages.json';
+import allPages from '../utils/all-pages.json';
 
-// describe('render all pages', () => {
-//   it(`Should render all page`, () => {
-//     cy.log(JSON.stringify(allPages));
-//     allPages.forEach((page) => {
-//       cy.visit(page);
-//     });
-//   });
-// });
+describe('render all pages', () => {
+  it(`Should render all page`, () => {
+    cy.log(JSON.stringify(allPages));
+    allPages.forEach((page) => {
+      cy.visit(page);
+    });
+  });
+});
