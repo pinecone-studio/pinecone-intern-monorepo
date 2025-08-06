e87ec27de1fc3b3f22ce8e024e84a3bf9a6c2955
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-06 19:12:07 +0800

    fix

--- apps/L1G/tinder/tinder-frontend/cypress/e2e/render-all-pages.cy.tsx
+++ apps/L1G/tinder/tinder-frontend/cypress/e2e/render-all-pages.cy.tsx
@@ -3,8 +3,10 @@ import allPages from '../utils/all-pages.json';
 describe('render all pages', () => {
   it(`Should render all page`, () => {
     cy.log(JSON.stringify(allPages));
-    allPages.forEach((page) => {
-      cy.visit(page);
-    });
+    for (const page of allPages) {
+      it(`Should render ${page}`, () => {
+        cy.visit(page);
+      });
+    }
   });
 });
