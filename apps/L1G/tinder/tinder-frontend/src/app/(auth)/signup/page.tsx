e795528502015d27e7d686903ada6e731afd2312
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-07 13:03:43 +0800

    feat(youareallset.tsx): signup last component

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,11 +1,13 @@
-
 import { TinderLogo } from '@/components/TinderLogo';
+import { YouAreAllSet } from '@/components/YouAreAllSet';
 
 const Signup = () => {
   return (
-    <div>
-      Sign up page ju
-      <TinderLogo />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-[350px] h-fit flex flex-col gap-6 items-center">
+        <TinderLogo />
+        <YouAreAllSet />
+      </div>
     </div>
   );
 };
---
+++ apps/L1G/tinder/tinder-frontend/src/components/YouAreAllSet.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+import { CircleCheck } from 'lucide-react';
+import React from 'react';
+
+export const YouAreAllSet = () => {
+  return (
+    <div className="w-[320px] max-w-[320px] font-sans h-fit flex flex-col justify-center items-center gap-6">
+      <div className="w-full flex flex-col items-center gap-4 py-2">
+        <CircleCheck size={48} color="#18BA51" strokeWidth={1} />
+        <div className="flex flex-col items-center justify-center gap-1">
+          <p className="text-[24px] font-sans font-semibold text-[#09090B] text-center">You're all set!</p>
+          <p className="text-[14px] font-sans font-normal text-[#71717A] text-center">Your account is all set. You're ready to explore and connect!</p>
+        </div>
+      </div>
+
+      <Button className="flex h-[40px] justify-center items-center bg-[#E11D48] font-sans font-[500] text-[14px] py-2 px-4 text-[#FAFAFA] rounded-full">Start Swiping!</Button>
+    </div>
+  );
+};
