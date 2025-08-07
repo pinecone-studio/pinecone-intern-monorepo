05f2706858344f83ac31a55a100510ce3d475b7c
24LP6733 24LP6733 <24LP6733@administrators-iMac-3.local>
2025-08-07 17:46:28 +0800

    feat: homepage

--- apps/L1G/restaurant/restaurant-frontend/specs/components/homepage/HomePageContainer.spec.tsx
+++ apps/L1G/restaurant/restaurant-frontend/specs/components/homepage/HomePageContainer.spec.tsx
@@ -1,7 +1,8 @@
 import HomePageContainer from '@/components/home/HomePageContainer';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+const mockhandleOnClick = jest.fn();
 describe('HomePageContainer', () => {
   it('should render successfully', async () => {
     render(<HomePageContainer />);
--- apps/L1G/restaurant/restaurant-frontend/src/components/home/HomePageContainer.tsx
+++ apps/L1G/restaurant/restaurant-frontend/src/components/home/HomePageContainer.tsx
@@ -1,67 +1,17 @@
 'use client';
 import { Button } from '@/components/ui/button';
-
 import { useState } from 'react';
 import MenuCard from './MenuCard';
-const menuItems = [
-  {
-    id: 1,
-    name: 'Taco',
-    price: '15.6к',
-    image: 'https://img1.wsimg.com/isteam/ip/0d953ba0-4d92-4d01-8e07-da1813873883/Sweetish%20meatballs%20pic.jpg',
-    category: 'main',
-  },
-  {
-    id: 2,
-    name: 'Taco',
-    price: '15.6к',
-    image: 'https://www.tasteofhome.com/wp-content/uploads/2025/01/German-Meatballs_EXPS_TOHD24_4166_SarahTramonte_4.jpg?w=700',
-    category: 'main',
-  },
-  {
-    id: 3,
-    name: 'Кофе, цай',
-    price: '15.6к',
-    image:
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/6/12/3/FNM070116_Penne-with-Vodka-Sauce-and-Mini-Meatballs-recipe_s4x3.jpg.rend.hgtvcom.616.462.85.suffix/1465939620872.webp',
-    category: 'coffee',
-  },
-  {
-    id: 4,
-    name: 'Кофе, цай',
-    price: '15.6к',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3lHi-tUwsg1u2vM222H8Q8g35GY9IM-AAz3KJlnU6l0LuXkIb_jVCZosjKZPlDpkz74M&usqp=CAU',
-    category: 'coffee',
-  },
-  {
-    id: 5,
-    name: 'Салат',
-    price: '15.6к',
-    image:
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/6/12/3/FNM070116_Penne-with-Vodka-Sauce-and-Mini-Meatballs-recipe_s4x3.jpg.rend.hgtvcom.616.462.85.suffix/1465939620872.webp',
-    category: 'main',
-  },
-  {
-    id: 6,
-    name: 'Taco',
-    price: '15.6к',
-    image:
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/6/12/3/FNM070116_Penne-with-Vodka-Sauce-and-Mini-Meatballs-recipe_s4x3.jpg.rend.hgtvcom.616.462.85.suffix/1465939620872.webp',
-    category: 'main',
-  },
-];
+import { useGetCategoriesQuery, useGetFoodsQuery } from '@/generated';
 
-const categories = [
-  { id: 'main', name: 'Үндсэн' },
-  { id: 'coffee', name: 'Кофе, цай' },
-  { id: 'drinks', name: 'Үндаа, ус' },
-  { id: 'desserts', name: 'Амттан' },
-];
 const HomePageContainer = () => {
-  const [activeCategory, setActiveCategory] = useState('main');
-
-  const filteredItems = menuItems.filter((item) => (activeCategory === 'main' ? true : item.category === activeCategory));
-
+  const { data: foodsData, loading: foodsLoading } = useGetFoodsQuery();
+  const { data: categoriesData, loading: categoriesLoading } = useGetCategoriesQuery();
+  console.log(foodsData, 'food');
+  console.log(categoriesData, 'catergories');
+  const [activeCategory, setActiveCategory] = useState('Үндсэн хоол');
+  const filteredItems = foodsData?.getFoods.filter((item) => item?.category.categoryName === activeCategory);
+  console.log(filteredItems, '/////////');
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="bg-white px-4 py-6 text-center border-b">
@@ -69,23 +19,29 @@ const HomePageContainer = () => {
       </div>
       <div className="bg-white px-4 py-4 border-b">
         <div className="flex space-x-6 overflow-x-auto">
-          {categories.map((category) => (
+          {categoriesData?.getCategories.map((category) => (
             <button
-              key={category.id}
-              onClick={() => setActiveCategory(category.id)}
+              data-testid="homepage-container-filter-button"
+              key={category?.categoryId}
+              onClick={() => {
+                if (category?.categoryId) {
+                  setActiveCategory(category.categoryName);
+                  console.log(activeCategory, 'category');
+                }
+              }}
               className={`whitespace-nowrap text-sm font-medium pb-2 border-b-2 transition-colors ${
-                activeCategory === category.id ? 'text-orange-600 border-orange-600' : 'text-gray-500 border-transparent hover:text-gray-700'
+                activeCategory === category?.categoryName ? 'text-orange-600 border-orange-600' : 'text-gray-500 border-transparent hover:text-gray-700'
               }`}
             >
-              {category.name}
+              {category?.categoryName}
             </button>
           ))}
         </div>
       </div>
       <div className="p-4">
         <div className="grid grid-cols-2 gap-4 max-w-2xl mx-auto">
-          {filteredItems.map((value) => (
-            <MenuCard key={value.id} image={value.image} foodName={value.name} price={value.price} id={value.id} category={value.category} />
+          {filteredItems?.map((value) => (
+            <MenuCard key={value?.foodId} image={value.image} foodName={value.foodName} price={value.price} id={value.foodId} category={value.category.categoryId} />
           ))}
         </div>
       </div>
