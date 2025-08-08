4d763105536fc993b4de6a20acb1c514c16b0213
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-08 10:10:38 +0800

    fix(multistepform): complexity zasaj 2 huwaaw
    fix #5731

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/signup/page.tsx
@@ -1,13 +1,7 @@
 'use client';
-import { ConfirmEmail } from '@/components/ConfirmEmail';
-import { CreateAccount } from '@/components/CreateAcc';
-import { CreatePassword } from '@/components/CreatePassword';
-import { GenderSelect } from '@/components/GenderSelect';
-import HowOldAreYou from '@/components/HowOldAreYou';
 import { MainHeader } from '@/components/MainHeader';
-import { ProfileImages } from '@/components/ProfileImages';
-import { YouAreAllSet } from '@/components/YouAreAllSet';
-import YourDetailsPage from '@/components/YourDetailsPage';
+import { MultiStepForm1 } from '@/components/MultiStepForm1';
+import { MultiStepForm2 } from '@/components/MultiStepForm2';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -16,34 +10,15 @@ const Signup = () => {
 
   const [step, setStep] = useState<'createAccount' | 'confirmEmail' | 'createPass' | 'genderSelect' | 'ageSelect' | 'details' | 'uploadImages' | 'allSet'>('createAccount');
 
-  const renderStep = () => {
-    switch (step) {
-      case 'createAccount':
-        return <CreateAccount onSuccess={() => setStep('confirmEmail')} />;
-      case 'confirmEmail':
-        return <ConfirmEmail onSuccess={() => setStep('createPass')} />;
-      case 'createPass':
-        return <CreatePassword onSuccess={() => setStep('genderSelect')} />;
-      case 'genderSelect':
-        return <GenderSelect onSuccess={() => setStep('ageSelect')} />;
-      case 'ageSelect':
-        return <HowOldAreYou onSuccess={() => setStep('details')} onBack={() => setStep('genderSelect')} />;
-      case 'details':
-        return <YourDetailsPage onSuccess={() => setStep('uploadImages')} onBack={() => setStep('ageSelect')} />;
-      case 'uploadImages':
-        return <ProfileImages onSuccess={() => setStep('allSet')} />;
-      case 'allSet':
-        return <YouAreAllSet onSuccess={() => router.push('/home')} />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <div className="w-screen h-screen flex justify-center items-center relative">
-      <div className="w-[640px] h-fit flex flex-col gap-6 items-center justify-start top-[200px] fixed">
+      <div className="w-[640px] h-fit flex flex-col gap-6 items-center justify-start top-[150px] fixed">
         <MainHeader />
-        <div className="w-fit max-w-[640px] h-fit flex flex-col gap-4">{renderStep()}</div>
+
+        <div className="w-fit max-w-[640px] h-fit flex flex-col gap-4">
+          <MultiStepForm1 step={step} setStep={setStep} />
+          <MultiStepForm2 step={step} setStep={setStep} router={router} />
+        </div>
       </div>
     </div>
   );
