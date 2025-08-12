bf3a69657634d082c8d9b7267233b4de558b6574
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-12 14:51:45 +0800

    refactor(myprofile): my profile page components

--- apps/L1G/tinder/tinder-frontend/src/components/MyProfileForm.tsx
+++ apps/L1G/tinder/tinder-frontend/src/components/MyProfileForm.tsx
@@ -1,31 +1,21 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import z from 'zod';
-import { format } from 'date-fns';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { CalendarIcon } from 'lucide-react';
-import { Calendar } from '@/components/ui/calendar';
 import { MultiSelect } from './MultiSelect';
-
-const formSchema = z.object({
-  name: z.string().min(2).max(50),
-  email: z.string().email({ message: 'Please enter a valid email' }),
-  birthDate: z.date({ message: 'A date of birth is required.' }),
-  genderPrefence: z.string().min(1),
-  bio: z.string().max(200, { message: 'Bio must be less than 200 characters' }),
-  interests: z.array(z.string()).optional(),
-  profession: z.string().max(100, { message: 'Profession must be less than 100 characters' }),
-  school: z.string().max(100, { message: 'School must be less than 100 characters' }),
-});
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { profileFormSchema } from './schema/ProfileFormSchema';
+import { NameEmailFields } from './NameEmailFields';
+import { BirthDateField } from './BirthDateField';
+import { Separator } from '@/components/ui/separator';
 
 export const MyProfileForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof profileFormSchema>>({
+    resolver: zodResolver(profileFormSchema),
     defaultValues: {
       name: '',
       email: '',
@@ -38,20 +28,54 @@ export const MyProfileForm = () => {
     },
   });
 
-  function onSubmit(_values: z.infer<typeof formSchema>) {
+  function onSubmit(_values: z.infer<typeof profileFormSchema>) {
     console.log('working');
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2">
-        <div className="flex flex-col gap-6">
-          <div className="flex gap-6">
+    <>
+      <div className="flex flex-col gap-[1px] justify-start items-start ">
+        <p className="text-[18px] font-sans font-[500] text-[#09090B]">Personal Information</p>
+        <p className="text-[14px] font-sans font-[400] text-[#71717A]">This is how others will see you on the site.</p>
+      </div>
+
+      <div className="py-6">
+        <Separator />
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2">
+          <div className="flex flex-col gap-6">
+            <NameEmailFields control={form.control} />
+            <BirthDateField control={form.control} />
+
             <FormField
               control={form.control}
-              name="name"
+              name="genderPrefence"
               render={({ field }) => (
-                <FormItem className="w-1/2">
-                  <FormLabel className="flex">Name</FormLabel>
+                <FormItem>
+                  <FormLabel className="flex">Gender Preference</FormLabel>
+                  <FormControl>
+                    <Select {...field} onValueChange={field.onChange} defaultValue={field.value}>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="Men">Men</SelectItem>
+                        <SelectItem value="Women">Women</SelectItem>
+                        <SelectItem value="Both">Both</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="bio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="flex">Bio</FormLabel>
                   <FormControl>
                     <Input className="rounded-md" {...field} />
                   </FormControl>
@@ -61,10 +85,54 @@ export const MyProfileForm = () => {
             />
             <FormField
               control={form.control}
-              name="email"
+              name="interests"
               render={({ field }) => (
-                <FormItem className="w-1/2">
-                  <FormLabel className="flex justify-between">Email </FormLabel>
+                <FormItem>
+                  <FormLabel className="flex">Interests</FormLabel>
+                  <FormControl>
+                    <MultiSelect
+                      options={[
+                        { value: 'music', label: 'Music' },
+                        { value: 'sports', label: 'Sports' },
+                        { value: 'reading', label: 'Reading' },
+                        { value: 'coding', label: 'Coding' },
+                        { value: 'travel', label: 'Travel' },
+                        { value: 'gaming', label: 'Gaming' },
+                        { value: 'cooking', label: 'Cooking' },
+                        { value: 'art', label: 'Art' },
+                        { value: 'photography', label: 'Photography' },
+                        { value: 'fitness', label: 'Fitness' },
+                      ]}
+                      value={field.value}
+                      maxCount={10}
+                    />
+                  </FormControl>
+                  <FormDescription>You can select up to a maximum of 10 interests.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="profession"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="flex">Profession</FormLabel>
+                  <FormControl>
+                    <Input className="rounded-md" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="school"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="flex">School/Work</FormLabel>
                   <FormControl>
                     <Input className="rounded-md" {...field} />
                   </FormControl>
@@ -73,130 +141,11 @@ export const MyProfileForm = () => {
               )}
             />
           </div>
-
-          <FormField
-            control={form.control}
-            name="birthDate"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Date of birth</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl className="w-[280px]">
-                      <Button data-testid="date-picker-button" variant="outline" className={`border w-full pl-3 text-left font-normal ${!field.value ? 'text-muted-foreground' : ''}`}>
-                        {field.value ? format(field.value, 'PPP') : <span>{field.value}</span>}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date > new Date() || date < new Date('1900-01-01')} captionLayout="dropdown" />
-                  </PopoverContent>
-                </Popover>
-                <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="genderPrefence"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="flex">Gender Preference</FormLabel>
-                <FormControl>
-                  <Select>
-                    <SelectTrigger className="w-[180px]">
-                      <SelectValue placeholder="Theme" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="light">Light</SelectItem>
-                      <SelectItem value="dark">Dark</SelectItem>
-                      <SelectItem value="system">System</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="bio"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="flex">Bio</FormLabel>
-                <FormControl>
-                  <Input className="rounded-md" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="interests"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="flex">Interests</FormLabel>
-                <FormControl>
-                  <MultiSelect
-                    options={[
-                      { value: 'music', label: 'Music' },
-                      { value: 'sports', label: 'Sports' },
-                      { value: 'reading', label: 'Reading' },
-                      { value: 'coding', label: 'Coding' },
-                      { value: 'travel', label: 'Travel' },
-                      { value: 'gaming', label: 'Gaming' },
-                      { value: 'cooking', label: 'Cooking' },
-                      { value: 'art', label: 'Art' },
-                      { value: 'photography', label: 'Photography' },
-                      { value: 'fitness', label: 'Fitness' },
-                    ]}
-                    value={field.value}
-                    maxCount={10}
-                  />
-                </FormControl>
-                <FormDescription>You can select up to a maximum of 10 interests.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="profession"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="flex">Profession</FormLabel>
-                <FormControl>
-                  <Input className="rounded-md" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="school"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="flex">School/Work</FormLabel>
-                <FormControl>
-                  <Input className="rounded-md" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button type="submit" className="rounded-md w-fit py-2 px-4 bg-[#E11D48E5] bg-opacity-90 font-sans hover:bg-[#E11D48E5] hover:bg-opacity-100">
-          Update Profile
-        </Button>
-      </form>
-    </Form>
+          <Button type="submit" className="rounded-md w-fit py-2 px-4 bg-[#E11D48E5] bg-opacity-90 font-sans hover:bg-[#E11D48E5] hover:bg-opacity-100">
+            Update Profile
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 };
