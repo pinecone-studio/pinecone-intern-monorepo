4a952e2daa50a5e9ce168df11fe8a2ac961534ff
Javhaa14 <javhaa1410@gmail.com>
2025-08-14 12:16:50 +0800

    fix(restaurant-backend): updateTable mutation
    updateTable mutation

--- apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/table/update-table.ts
+++ apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/table/update-table.ts
@@ -3,6 +3,8 @@ import { TableModel } from 'src/models/table.model';
 import QRCode from 'qrcode';
 
 export const updateTable: MutationResolvers['updateTable'] = async (_, { tableId, input: { tableName } }) => {
+  const existingTable = await TableModel.findOne({ tableName });
+  if (existingTable) throw new Error('table already exists');
   const generatedQr = await QRCode.toDataURL(`http://localhost:4201/sign-in?table=${tableName}`);
   const updatedTable = await TableModel.findByIdAndUpdate(tableId, { $set: { tableName, tableQr: generatedQr } }, { new: true, runValidators: true });
   if (!updatedTable) {
