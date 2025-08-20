7761230fc3d069ec6204dc77d8394afe36d1b62c
Narangerel0121 <nrngrl0121@gmail.com>
2025-08-20 13:47:42 +0800

    fix: try to branch to branch merge

--- apps/L1E/instagram/instagram-frontend/src/components/SearchResult.tsx
+++ apps/L1E/instagram/instagram-frontend/src/components/SearchResult.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useGetOthersProfileLazyQuery } from "@/generated";
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const SearchResults = () => {
@@ -15,31 +16,47 @@ const SearchResults = () => {
 
 
   return (
-    <div className="flex flex-col justify-center items-center border border-gray-200 w-fit mx-auto p-4">
-      <input
-        type="text"
-        value={searchName}
-        onChange={(e) => setSearchName(e.target.value)}
-        placeholder="Search username..."
-        className="border p-2 rounded mb-4"
-      />
+    <div className="flex flex-col gap-5 w-[410px] px-4 mx-auto">
+      <h3 className="items-start font-semibold text-2xl">Search</h3>
+      <div>
+        <div className="flex items-center gap-2 p-3 bg-white text-[#71717A]">
+          <Search strokeWidth={1} size={16} />
+          <input
+            type="text"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+            placeholder="Search..."
+            className="rounded p-2 w-full outline-none focus:border-none placeholder:text-[#71717A] placeholder:text-sm"
+          />
+        </div>
+        <hr className="py-4"></hr>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between items-center">
+            <h1 className="font-bold text-base">Recent</h1>
+            <h6 className="font-medium text-sm text-[#2563EB]">Clear all</h6>
+          </div>
+          {loading && <p>Loading...</p>}
+          {error && <p className="text-red-500">Error: {error.message}</p>}
 
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">Error: {error.message}</p>}
-
-      {!loading && !error && data?.getProfiles?.length ? (
-        <ul>
-          {data.getProfiles.map((user) => (
-           <a href={`/${user.userName}`}>
-            <div key={user.userName}>
-              <p>{user.userName}</p>
+          {!loading && !error && data?.getProfiles?.length ? (
+            <div>
+              {data.getProfiles.map((user) => (
+                <a href={`/${user.userName}`}>
+                  <div key={user.userName} className="flex gap-2 items-center">
+                    <img src={`/${user.profileImage}`} className="w-[44px] h-[44px] rounded-full"></img>
+                   <div>
+                     <p className="font-medium text-sm">{user.userName}</p>
+                     <p className="font-medium text-xs">{user.bio}</p>
+                   </div>
+                  </div>
+                </a>
+              ))}
             </div>
-           </a>
-          ))}
-        </ul>
-      ) : (
-        !loading && !error && <p>No users found.</p>
-      )}
+          ) : (
+            !loading && !error && searchName && <p>No users found.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- apps/L1E/instagram/instagram-frontend/src/components/Sidebar.tsx
+++ apps/L1E/instagram/instagram-frontend/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from 'next/link';
-import { Home,Search,  AlignJustify, Heart, PlusSquare ,ImagePlus ,BookOpenCheck} from 'lucide-react';
+import { Home, Search, AlignJustify, Heart, PlusSquare, ImagePlus, BookOpenCheck, Instagram } from 'lucide-react';
 import { usePost } from "./context/PostContext"
 import {
   DropdownMenu,
@@ -17,75 +17,144 @@ import {
 } from "@/components/ui/avatar"
 
 import Image from 'next/image';
+import { useState } from 'react';
+import SearchResults from './SearchResult';
 
 export const Sidebar: React.FC = () => {
 
-  const { handlePostClick , postStep} = usePost();
+  const { handlePostClick, postStep } = usePost();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const handleSearchClick = () => {
+    setIsSearchOpen(!isSearchOpen);
+  }
   console.log('PostStep in Sidebar:', postStep);
-  console.log(handlePostClick , "clickinggg")
+  console.log(handlePostClick, "clickinggg")
   return (
-    <div className="fixed left-0 top-0 h-screen border-r p-4 bg-red-50">
-      <div className="inline-flex flex-col h-[1000px] bg-slate-200   p-[36px_32px_16px_16px] justify-between items-start flex-shrink-0">
-      
-        <div className='flex flex-col space-y-6 gap-[48px] items-start self-stretch'>       
-            <Image src={'/logo.svg'} alt="Logo" width={103} height={29}/>
+    <div className="fixed left-0 top-0 h-screen border-r bg-white">
+      {!isSearchOpen &&
+        <div className="inline-flex flex-col h-[1000px] bg-slate-200  p-[36px_32px_16px_16px] justify-between items-start flex-shrink-0">
 
-        <nav className="flex flex-col items-start bg-green-200 gap-2 self-stretch">
-         <Link href="/" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
-            <Home className="h-[24px] w-[24px]" />
-            <span className='text-[16px] font-normal'>Home</span>
-          </Link>
-          
-          <Link href="/explore" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
-            <Search className="h-[24px] w-[24px]" />
-            <span className='text-[16px] font-normal'>Explore</span>
-          </Link>
-          
-          
-          <Link href="/notifications" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
-            <Heart className="h-[24px] w-[24px]" />
-            <span className='text-[16px] font-normal'>Notifications</span>
-          </Link>
+          <div className='flex flex-col space-y-6 gap-[48px] items-start self-stretch pt-[16px]'>
+            <Image src={'/logo.svg'} alt="Logo" width={103} height={29} />
 
-<DropdownMenu >
-  <DropdownMenuTrigger asChild className=""> 
-    <Link 
-      href="#" 
-      className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors"  
-    >
-      <PlusSquare className="h-[24px] w-[24px]" />
-      <span data-cy="create" className='text-[16px] font-normal'>Create</span>
-    </Link>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent className='w-[154px] h-[72px] rounded-md flex flex-col mt-2 items-start justify-start mr-4'>
-    <DropdownMenuItem  data-cy="create-post"  onClick={(e) =>{ e.preventDefault(); handlePostClick();}} className='flex pt-[6px] pr-[8px] justify-between items-center self-stretch'>
-      Post <ImagePlus className='w-[16px] h-[16px]'/>
-    </DropdownMenuItem>
-    <DropdownMenuItem className='flex p-[6px_8px] justify-between items-center self-stretch'>
-      Story <BookOpenCheck className='w-[16px] h-[16px]'/>
-    </DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
+            <nav className="flex flex-col items-start bg-green-200 gap-2 self-stretch">
+              <Link href="/" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
+                <Home className="h-[24px] w-[24px]" />
+                <span className='text-[16px] font-normal'>Home</span>
+              </Link>
 
-          <Link href="/profile" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
-            <Avatar className='w-[24px] h-[24px]'>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" width={24} height={24} />
-               {/* <AvatarImage src={user?.avatar || '/default-avatar.png'} alt={user?.username || 'User'} width={24} height={24} /> */}
-               {/* <AvatarFallback>{user?.username?.charAt(0).toUpperCase() || 'U'}</AvatarFallback> */}
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-            <span className='text-[16px] font-normal'>Profile</span>
-          </Link>
-        </nav>
+              <Link href="/" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors" onClick={handleSearchClick}>
+                <Search className="h-[24px] w-[24px]" />
+                <span className='text-[16px] font-normal'>Search</span>
+              </Link>
+
+              <Link href="/notifications" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
+                <Heart className="h-[24px] w-[24px]" />
+                <span className='text-[16px] font-normal'>Notifications</span>
+              </Link>
+
+              <DropdownMenu >
+                <DropdownMenuTrigger asChild className="">
+                  <Link
+                    href="#"
+                    className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors"
+                  >
+                    <PlusSquare className="h-[24px] w-[24px]" />
+                    <span data-cy="create" className='text-[16px] font-normal'>Create</span>
+                  </Link>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className='w-[154px] h-[72px] rounded-md flex flex-col mt-2 items-start justify-start mr-4'>
+                  <DropdownMenuItem data-cy="create-post" onClick={(e) => { e.preventDefault(); handlePostClick(); }} className='flex pt-[6px] pr-[8px] justify-between items-center self-stretch'>
+                    Post <ImagePlus className='w-[16px] h-[16px]' />
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className='flex p-[6px_8px] justify-between items-center self-stretch'>
+                    Story <BookOpenCheck className='w-[16px] h-[16px]' />
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <Link href="/profile" className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors">
+                <Avatar className='w-[24px] h-[24px]'>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" width={24} height={24} />
+                  {/* <AvatarImage src={user?.avatar || '/default-avatar.png'} alt={user?.username || 'User'} width={24} height={24} /> */}
+                  {/* <AvatarFallback>{user?.username?.charAt(0).toUpperCase() || 'U'}</AvatarFallback> */}
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <span className='text-[16px] font-normal'>Profile</span>
+              </Link>
+            </nav>
+          </div>
+          <div>
+            <Link href="/more" className="flex items-center self-stretch p-[12px_120.984px_12px_12px]  hover:text-primary gap-4 bg-yellow-200">
+              <AlignJustify className="h-[24px] w-[24px]" />
+              <span className='text-[16px] font-normal'>More</span>
+            </Link>
+          </div>
+        </div>}
+
+      {isSearchOpen &&
+        <div className='flex flex-row'>
+          <div className="inline-flex flex-col h-[1130px] p-4 justify-between items-center flex-shrink-0">
+
+            <div className='flex flex-col justify-between items-center space-y-6 gap-[48px] self-stretch  pt-[36px]'>
+              <div className='w-[29px] h-[29px]'><Instagram /></div>
+
+              <nav className="flex flex-col items-start gap-2 self-stretch">
+                <Link href="/" className="flex items-center w-full p-3 hover:text-primary rounded-lg hover:bg-gray-100 transition-colors">
+                  <Home className="h-[24px] w-[24px]" />
+                </Link>
+
+                <Link href="/" className="flex items-center w-full p-3 hover:text-primary rounded-lg hover:bg-gray-100 transition-colors" onClick={handleSearchClick}>
+                  <Search className="h-[24px] w-[24px]" />
+                </Link>
+
+                <Link href="/notifications" className="flex items-center w-full p-3 hover:text-primary rounded-lg hover:bg-gray-100 transition-colors">
+                  <Heart className="h-[24px] w-[24px]" />
+                </Link>
+
+                <DropdownMenu >
+                  <DropdownMenuTrigger asChild className="">
+                    <Link
+                      href="#"
+                      className="flex items-center w-full p-3 hover:text-primary gap-4 rounded-lg hover:bg-gray-100 transition-colors"
+                    >
+                      <PlusSquare className="h-[24px] w-[24px]" />
+                    </Link>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className='w-[154px] h-[72px] rounded-md flex flex-col mt-2 items-start justify-start mr-4'>
+                    <DropdownMenuItem data-cy="create-post" onClick={(e) => { e.preventDefault(); handlePostClick(); }} className='flex pt-[6px] pr-[8px] justify-between items-center self-stretch'>
+                      Post <ImagePlus className='w-[16px] h-[16px]' />
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className='flex p-[6px_8px] justify-between items-center self-stretch'>
+                      Story <BookOpenCheck className='w-[16px] h-[16px]' />
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                <Link href="/profile" className="flex items-center w-full p-3 hover:text-primary rounded-lg hover:bg-gray-100 transition-colors">
+                  <Avatar className='w-[24px] h-[24px]'>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" width={24} height={24} />
+                    {/* <AvatarImage src={user?.avatar || '/default-avatar.png'} alt={user?.username || 'User'} width={24} height={24} /> */}
+                    {/* <AvatarFallback>{user?.username?.charAt(0).toUpperCase() || 'U'}</AvatarFallback> */}
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </Link>
+              </nav>
+            </div>
+
+            <div className="flex items-center justify-center self-stretch hover:text-primary">
+              <Link href="/more">
+                <AlignJustify className="h-[24px] w-[24px]" />
+              </Link>
+            </div>
+          </div>
+          {/* <hr className='w-full rotate-90'/> */}
+          <div className='w-px h-screen bg-[#E4E4E7]'></div>
+          <div className=' pt-[36px]'>
+            <SearchResults />
+          </div>
         </div>
-
-        <div>
-          <Link href="/more" className="flex items-center self-stretch p-[12px_120.984px_12px_12px]  hover:text-primary gap-4 bg-yellow-200">
-            <AlignJustify className="h-[24px] w-[24px]" />
-            <span className='text-[16px] font-normal'>More</span>
-          </Link>
-        </div>
-      </div>
+      }
     </div>
   );
 }
