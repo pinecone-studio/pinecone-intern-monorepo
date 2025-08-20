198c31d78586741ced3c89840d1f53f4d1b7bbe1
magnai_r <r.magnaibayar@gmail.com>
2025-08-20 11:27:02 +0800

    perf(restaurant-backend): added hashed password to update-user

--- apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/user/update-user.ts
+++ apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/user/update-user.ts
@@ -1,8 +1,10 @@
 import { MutationResolvers } from 'src/generated';
 import { UserModel } from 'src/models/user.model';
+import bcrypt from 'bcryptjs';
 
 export const updateUser: MutationResolvers['updateUser'] = async (_, { userId, input: { phoneNumber, email, password, profile, username } }) => {
-  const updatedUser = await UserModel.findByIdAndUpdate(userId, { $set: { phoneNumber, email, password, profile, username } }, { new: true, runValidators: true });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const updatedUser = await UserModel.findByIdAndUpdate(userId, { $set: { phoneNumber, email, password: hashedPassword, profile, username } }, { new: true, runValidators: true });
 
   if (!updatedUser) {
     throw new Error(`User with ID ${userId} not found`);
