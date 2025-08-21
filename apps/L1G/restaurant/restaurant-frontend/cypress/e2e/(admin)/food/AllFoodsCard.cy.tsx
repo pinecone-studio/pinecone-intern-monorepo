71032d7a8b1bc003061e63f931d3b6ca58a2ff9d
24LP7422 24LP7422 <24LP7422@admins-iMac-2.local>
2025-08-21 14:01:00 +0800

    feat(restaurant-frontend admin-food): get all foods data, update food

--- apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/AllFoodsCard.cy.tsx
+++ apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/AllFoodsCard.cy.tsx
@@ -4,10 +4,21 @@ describe('AllFoodsCard', () => {
     cy.get('[data-cy=allfoods]').should('be.visible');
   });
   it('should delete food and show toast success', () => {
+    cy.intercept('POST', '**/graphql', (req) => {
+      if (req.body.operationName === 'DeleteFood') {
+        req.reply({
+          statusCode: 200,
+          body: {
+            data: { deleteFood: { foodId: '1' } },
+          },
+        });
+      }
+    }).as('deleteFoodSuccess');
     cy.visit('/food');
-    cy.get('[data-cy=delete-dialog-trigger-0]').click().wait(1000);
-    cy.get('[data-cy=delete-dialog-delete-button]').click().wait(1000);
-    cy.get('body').should('contain.text', 'Хоол амжилттай устгагдлаа!').should('be.visible');
+    cy.get('[data-cy=delete-dialog-trigger-0]').click();
+    cy.get('[data-cy=delete-dialog-delete-button]').click();
+    cy.wait('@deleteFoodSuccess');
+    cy.contains('Хоол амжилттай устгагдлаа!').should('be.visible');
   });
   it('should show toast error when delete food dails', () => {
     cy.intercept('POST', '**/graphql', (req) => {
--- apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/FoodUpdateDialog.cy.tsx
+++ apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/FoodUpdateDialog.cy.tsx
@@ -4,12 +4,23 @@ describe('FoodUpdateDialog', () => {
     cy.get('[data-cy=allfoods]').should('be.visible');
   });
   it('should update food and show toast success', () => {
-    cy.visit('/food').wait(3000);
+    cy.intercept('POST', '**/graphql', (req) => {
+      if (req.body.operationName === 'UpdateFood') {
+        req.reply({
+          statusCode: 200,
+          body: {
+            data: { updateFood: { foodId: '1' } },
+          },
+        });
+      }
+    }).as('updateFoodSuccess');
+    cy.visit('/food');
     cy.get('[data-cy=allfoods-food-0]').find('[data-cy=food-update-dialog-open]').click();
     cy.get('[data-cy=food-update-foodName-input]').clear();
     cy.get('[data-cy=food-update-foodName-input]').type('Tested');
     cy.get('[data-cy=food-update-submit-button]').click();
-    cy.get('body').should('contain.text', 'Хоол амжилттай шинэчлэгдлээ!').should('be.visible');
+    cy.wait('@updateFoodSuccess');
+    cy.contains('Хоол амжилттай шинэчлэгдлээ!').should('be.visible');
   });
   it('should show toast error when update food fails', () => {
     cy.intercept('POST', '**/graphql', (req) => {
