ef429cb11a41dbf1eda13e43be07c3171c80d971
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-08-21 16:39:41 +0800

    fix: like,dislike resolver fix

--- apps/L1G/tinder/tinder-frontend/src/app/(main)/home/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(main)/home/page.tsx
@@ -10,6 +10,7 @@ const HomePage = () => {
   const [like] = useLikeUserMutation();
   const [dislike] = useDislikeMutation();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [matchedProfile, setMatchedProfile] = useState<UserProfile | null>(null);
 
   const currentUserId = '689453c9fb642555405b451c';
   console.log(data, 'data');
