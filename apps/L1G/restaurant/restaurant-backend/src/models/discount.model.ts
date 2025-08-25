2409965700b07f74a6ccd06e2fc48e280728442b
24LP7422 24LP7422 <24LP7422@admins-iMac-2.local>
2025-08-25 14:06:04 +0800

    feat(restaurant-backend-discount): create, update, delete discount

---
+++ apps/L1G/restaurant/restaurant-backend/src/models/discount.model.ts
@@ -0,0 +1,37 @@
+import { model, models, Schema, Types } from 'mongoose';
+
+export type DiscountType = {
+  _id: Types.ObjectId;
+  discountName: string;
+  discountRate: number;
+  startDate: Date;
+  endDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const DiscountSchema = new Schema<DiscountType>(
+  {
+    discountName: {
+      type: String,
+      required: true,
+    },
+    discountRate: {
+      type: Number,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const DiscountModel = models.Discount || model<DiscountType>('discount', DiscountSchema);
--- apps/L1G/restaurant/restaurant-backend/src/schemas/index.ts
+++ apps/L1G/restaurant/restaurant-backend/src/schemas/index.ts
@@ -4,5 +4,6 @@ import { authTypeDefs } from './auth.schema';
 import { categoryTypeDefs } from './category.schema';
 import { tableTypeDefs } from './table.schema';
 import { foodTypeDefs } from './food.schema';
+import { discountTypeDefs } from './discount.schema';
 
-export const typeDefs = mergeTypeDefs([userTypeDefs, authTypeDefs, tableTypeDefs, categoryTypeDefs, foodTypeDefs]);
+export const typeDefs = mergeTypeDefs([userTypeDefs, authTypeDefs, tableTypeDefs, categoryTypeDefs, foodTypeDefs, discountTypeDefs]);
