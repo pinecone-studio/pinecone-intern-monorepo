1c0996cc79cfd52d91434dc7d37ef8d3fd37d924
Javhaa14 <javhaa1410@gmail.com>
2025-08-25 14:57:52 +0800

    test

---
+++ apps/L1G/restaurant/restaurant-backend/src/models/order.model.ts
@@ -0,0 +1,68 @@
+import { model, models, Schema, Types } from 'mongoose';
+
+export type FoodOrderItem = {
+  food: Types.ObjectId;
+  quantity: Number;
+};
+
+export type FoodOrder = {
+  _id: Types.ObjectId;
+  status: 'READY' | 'PREPARING' | 'DONE' | 'PENDING';
+  totalPrice: Number;
+  orderNumber: Number;
+  user: Types.ObjectId;
+  table: Types.ObjectId;
+  foodOrder: FoodOrderItem[];
+};
+
+const FoodOrderItemSchema = new Schema<FoodOrderItem>(
+  {
+    food: {
+      type: Schema.Types.ObjectId,
+      ref: 'Food',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+  },
+  { _id: false }
+);
+
+const FoodOrderSchema = new Schema<FoodOrder>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    table: {
+      type: Schema.Types.ObjectId,
+      ref: 'Table',
+    },
+    foodOrder: {
+      type: [FoodOrderItemSchema],
+    },
+    orderNumber: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: ['READY', 'PREPARING', 'DONE', 'PENDING'],
+      default: 'PENDING',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const FoodOrderModel = models.FoodOrder || model<FoodOrder>('FoodOrder', FoodOrderSchema);
