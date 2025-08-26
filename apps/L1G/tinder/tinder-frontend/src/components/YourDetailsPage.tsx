3ebb6c4cd0483e6970d4ab5af7f3558a003c243b
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-26 11:25:44 +0800

    your details comp

--- apps/L1G/tinder/tinder-frontend/src/components/YourDetailsPage.tsx
+++ apps/L1G/tinder/tinder-frontend/src/components/YourDetailsPage.tsx
@@ -1,19 +1,21 @@
-import ProfileForm from './FormFIeld';
+import { UserData } from '@/app/(auth)/signup/page';
 import TitleContainer from './TitleContainer';
+import { ProfileForm } from './FormField';
 
 type YourDetailsPageProps = {
   onSuccess: () => void;
   onBack: () => void;
+  userData: UserData;
+  updateUserData: (_: Partial<UserData>) => void;
 };
 
-const YourDetailsPage = ({ onSuccess, onBack }: YourDetailsPageProps) => {
+export const YourDetailsPage = ({ onSuccess, onBack, userData, updateUserData }: YourDetailsPageProps) => {
   return (
-    <div className="w-[400px] flex flex-col items-center justify-center bg-background px-4 overflow-scroll " data-testid="details-page-container">
+    <div className="w-[400px] flex flex-col items-center justify-center bg-background px-4 overflow-scroll" data-testid="details-page-container">
       <div className="w-full max-w-[400px] flex flex-col items-center gap-6">
         <TitleContainer boldTitle="Your Details" greyText="Please provide the following information to help us get to know you better." />
-        <ProfileForm onSuccess={onSuccess} onBack={onBack} />
+        <ProfileForm onSuccess={onSuccess} onBack={onBack} updateUserData={updateUserData} userData={userData} data-testid="profile-form" />
       </div>
     </div>
   );
 };
-export default YourDetailsPage;
