1b02f29764817fdbfa9119bd29a314b0603da322
24LP7422 24LP7422 <24LP7422@admins-iMac-2.local>
2025-08-26 17:24:21 +0800

    fix(restaurant-backend food): change food mongoose model

--- apps/L1G/restaurant/restaurant-backend/specs/resolvers/mutation/food/create-food.spec.ts
+++ apps/L1G/restaurant/restaurant-backend/specs/resolvers/mutation/food/create-food.spec.ts
@@ -5,32 +5,30 @@ import { createFood } from 'src/resolvers/mutations';
 jest.mock('src/models/food.model', () => ({
   FoodModel: {
     create: jest.fn().mockReturnValue({
-      populate: jest.fn(() => ({
-        populate: jest.fn().mockResolvedValue({
-          _id: '2',
-          foodName: 'Test',
-          price: '20',
-          image: 'image.jpg',
-          status: 'Идэвхитэй',
-          category: {
-            _id: '1',
-            categoryName: 'Test1',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-          discount: {
-            _id: '1',
-            discountName: 'Test1',
-            discountRate: 0.15,
-            startDate: Date.now(),
-            endDate: Date.now(),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
+      populate: jest.fn().mockResolvedValue({
+        _id: '2',
+        foodName: 'Test',
+        price: '20',
+        image: 'image.jpg',
+        status: 'Идэвхитэй',
+        category: {
+          _id: '1',
+          categoryName: 'Test1',
           createdAt: new Date(),
           updatedAt: new Date(),
-        }),
-      })),
+        },
+        discount: {
+          _id: '1',
+          discountName: 'Test1',
+          discountRate: 0.15,
+          startDate: Date.now(),
+          endDate: Date.now(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
     }),
   },
 }));
@@ -51,9 +49,7 @@ describe('createFood', () => {
 
   it('should handle database errors', async () => {
     (FoodModel.create as jest.Mock).mockReturnValue({
-      populate: jest.fn(() => ({
-        populate: jest.fn().mockRejectedValue(new Error('Failed to create food')),
-      })),
+      populate: jest.fn().mockRejectedValue(new Error('Failed to create food')),
     });
     await expect(createFood?.({}, { input: { foodName: 'Test', price: '20', image: 'image.jpg', status: 'Идэвхитэй', categoryId: '1' } }, {}, {} as GraphQLResolveInfo)).rejects.toThrow(
       'Failed to create food'
--- apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/food/create-food.ts
+++ apps/L1G/restaurant/restaurant-backend/src/resolvers/mutations/food/create-food.ts
@@ -5,7 +5,7 @@ import { mapFood } from 'src/utils/types/food-type';
 export const createFood: MutationResolvers['createFood'] = async (_, { input: { foodName, price, image, status, categoryId } }) => {
   try {
     const newFood = await FoodModel.create({ foodName, price, image, status, category: categoryId });
-    const populatedFood = (await newFood.populate('category').populate('discount')) as FoodPopulatedType;
+    const populatedFood = (await newFood.populate('category')) as FoodPopulatedType;
     return mapFood(populatedFood);
   } catch (error) {
     throw new Error(`Failed to create food`);
