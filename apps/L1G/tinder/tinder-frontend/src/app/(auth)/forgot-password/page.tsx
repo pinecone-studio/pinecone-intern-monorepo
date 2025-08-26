a9acb5b9b19f636e4ba5e5b7160e5cf34d47bb7f
24LP0441 24LP0441 <24LP0441@administrators-iMac.local>
2025-08-26 14:01:08 +0800

    fix: build fix

--- apps/L1G/tinder/tinder-frontend/src/app/(auth)/forgot-password/page.tsx
+++ apps/L1G/tinder/tinder-frontend/src/app/(auth)/forgot-password/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+import { useState } from 'react';
 import { ForgetPassword } from '@/components/ForgetPassword';
 import { MainHeader } from '@/components/MainHeader';
+import { UserData } from '../signup/page';
 
 const ForgotPasswordPage = () => {
+  const [userData, setUserData] = useState<UserData>({});
+
+  const updateUserData = (newData: Partial<UserData>) => {
+    setUserData((prev) => ({ ...prev, ...newData }));
+    console.log(userData, 'newdata');
+  };
+
   return (
     <div className="w-screen h-screen flex justify-center items-start relative ">
       <div className="w-[350px] h-fit flex flex-col gap-6 items-center relative top-[200px]">
         <MainHeader />
-
-        <div className="w-[350px]  ">
-          <ForgetPassword />
+        <div className="w-[350px]">
+          <ForgetPassword updateUserData={updateUserData} />
         </div>
       </div>
     </div>
