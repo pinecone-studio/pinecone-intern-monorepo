0497fb7fb59cd7f0610704b07842c132efe8895f
24LP6733 24LP6733 <24LP6733@administrators-iMac-3.local>
2025-08-27 17:49:24 +0800

    gg

--- apps/L1G/restaurant/restaurant-frontend/src/components/home/HomePageContainer.tsx
+++ apps/L1G/restaurant/restaurant-frontend/src/components/home/HomePageContainer.tsx
@@ -6,6 +6,7 @@ import MenuCard from './MenuCard';
 import { useGetCategoriesQuery, useGetFoodsQuery } from '@/generated';
 import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
 import OrderList from './OrderList';
+import OrderType from './OrderType';
 
 export type AddPayload = {
   id: string;
@@ -51,6 +52,7 @@ const HomePageContainer = () => {
   const { data: categoriesData } = useGetCategoriesQuery();
   const [activeCategory, setActiveCategory] = useState('Үндсэн хоол');
   const [cart, setCart] = useState<CartItem[]>([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   const filteredItems = (foodsData?.getFoods ?? []).filter((item): item is NonNullable<typeof item> => item?.category?.categoryName === activeCategory);
 
@@ -132,7 +134,16 @@ const HomePageContainer = () => {
               ))
             )}
             <DrawerFooter>
-              <Button className="w-full bg-amber-800 hover:bg-amber-900 text-white py-4 text-lg font-medium rounded-lg">Захиалах</Button>
+              <Button
+                onClick={() => {
+                  setIsClicked(true);
+                  console.log(isClicked, 'clicked');
+                }}
+                className="w-full bg-amber-800 hover:bg-amber-900 text-white py-4 text-lg font-medium rounded-lg"
+              >
+                Захиалах
+              </Button>
+              <OrderType isClicked={isClicked} />
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
