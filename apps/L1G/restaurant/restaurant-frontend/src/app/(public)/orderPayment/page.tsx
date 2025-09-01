1fe6ab28f111c83ca49d036a886941f92ccea01d
24LP6733 24LP6733 <24LP6733@administrators-iMac-3.local>
2025-09-01 19:40:27 +0800

    jj

---
+++ apps/L1G/restaurant/restaurant-frontend/src/app/(public)/orderPayment/page.tsx
@@ -0,0 +1,13 @@
+import Payment from '@/components/payment/Payment';
+import PaymentSelection from '@/components/payment/PaymentSelection';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="max-w-sm mx-auto bg-white min-h-screen">
+      <PaymentSelection />
+    </div>
+  );
+};
+
+export default page;
