00776ab949c945ac4e14fc723306dc410a8648da
24LP7422 24LP7422 <24LP7422@admins-iMac-2.local>
2025-09-01 11:26:05 +0800

    fix(restaurant-frontend admin-food): fix admin-food comp, fix create discount test

--- apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/FoodCreateDialog.cy.tsx
+++ apps/L1G/restaurant/restaurant-frontend/cypress/e2e/(admin)/food/FoodCreateDialog.cy.tsx
@@ -1,20 +1,17 @@
 describe('FoodCreateDialog', () => {
   it('should display an error message if food values are not provided', () => {
     cy.visit('/food');
-    cy.get('[data-cy=create-food-dialog-open]').wait(2000).click();
+    cy.get('[data-cy=create-food-dialog-open]').click();
     cy.get('[data-cy=create-food-submit-button]').click();
     cy.get('[data-cy=food-foodName-error-message]').should('have.text', 'Хоолны нэр оруулна уу!');
     cy.get('[data-cy=create-food-image-error-message]').should('have.text', 'Зураг оруулна уу!');
-    cy.get('[data-cy=create-food-category-select-error-message]').should('have.text', 'Категори сонгоно уу!');
     cy.get('[data-cy=food-price-error-message]').should('have.text', 'Үнэ оруулна уу!');
   });
   it('should create new food and show toast success', () => {
     cy.visit('/food');
-    cy.get('[data-cy=create-food-dialog-open]').wait(2000).click();
+    cy.get('[data-cy=create-food-dialog-open]').click();
     cy.get('[data-cy=create-food-foodName-input]').type('Test');
     cy.get('[data-cy=create-food-image-input]').selectFile('cypress/fixtures/foodimage.png');
-    cy.get('[data-cy=create-food-category-select]').click();
-    cy.get('[data-cy=create-food-category-option-Амттан]').click();
     cy.get('[data-cy=create-food-price-input]').type('20000');
     cy.get('[data-cy=create-food-submit-button]').click();
     cy.contains('Хоол амжилттай үүслээ!').should('be.visible');
@@ -32,13 +29,11 @@ describe('FoodCreateDialog', () => {
       }
     }).as('createFoodError');
     cy.visit('/food');
-    cy.get('[data-cy=create-food-dialog-open]').wait(2000).click();
+    cy.get('[data-cy=create-food-dialog-open]').click();
     cy.get('[data-cy=create-food-foodName-input]').type('Test2');
     cy.get('[data-cy=create-food-image-input]').selectFile('cypress/fixtures/foodimage.png');
-    cy.get('[data-cy=create-food-category-select]').click();
-    cy.get('[data-cy=create-food-category-option-Амттан]').click();
     cy.get('[data-cy=create-food-price-input]').type('15000');
-    cy.get('[data-cy=create-food-submit-button]').click().wait(3000);
+    cy.get('[data-cy=create-food-submit-button]').click().wait(1000);
     cy.wait('@createFoodError');
     cy.get('body').should('contain.text', 'Хоол үүсгэхэд алдаа гарлаа!').should('be.visible');
   });
