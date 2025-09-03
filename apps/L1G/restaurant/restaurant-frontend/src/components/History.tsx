8ed3407501fd3ab2639e034045e314e3be005d50
24LP4296 24LP4296 <24LP4296@administrators-iMac.local>
2025-09-03 13:13:27 +0800

    changed date notif

--- apps/L1G/restaurant/restaurant-frontend/src/components/History.tsx
+++ apps/L1G/restaurant/restaurant-frontend/src/components/History.tsx
@@ -14,14 +14,14 @@ const OrdersHistory = () => {
       <Navbar />
       <div className="text-center">
         <p className="font-medium text-[#441500] pt-5 text-[20px]">Захиалгын түүх</p>
-        <div className="flex justify-center pt-4">
-          <div className="w-[345px] bg-muted flex justify-start rounded-md p-4">
+        <div className="flex justify-center  pt-4">
+          <div className="w-[345px] flex flex-col gap-4  justify-start rounded-md p-4">
             {data?.getFoodOrdersByUser?.length === 0 ? (
               <p>Захиалгаа олдсонгүй</p>
             ) : (
               data?.getFoodOrdersByUser?.map((order) => (
-                <div key={order?.orderId} className="w-full">
-                  <div className="flex gap-">
+                <div key={order?.orderId} className="w-full border-[1px] rounded-md bg-muted p-4 ">
+                  <div className="flex ">
                     <div className="flex items-center gap-4 ">
                       <p className="text-[#441500] font-bold text-[20px]">#{order?.orderNumber}</p>
                       <div className="w-[120px] h-[20px] border-[1px] bg-white rounded-md">
@@ -31,7 +31,14 @@ const OrdersHistory = () => {
                   </div>
                   <div className="flex justify-between w-full">
                     <p className="text-[10px] flex items-end">
-                      {new Date().getFullYear()}/{new Date().getMonth() + 1}/{new Date().getDate()}
+                      {new Intl.DateTimeFormat('en-GB', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      }).format(order?.createdAt as any)}
                     </p>
 
                     <p>{order?.totalPrice}₮</p>
