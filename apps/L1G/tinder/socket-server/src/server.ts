c418580b3e50e4c45fc02085b6989428ad5b2157
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-09-09 13:24:59 +0800

    fix: url add

--- apps/L1G/tinder/socket-server/src/server.ts
+++ apps/L1G/tinder/socket-server/src/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:4201', 'http://localhost:3000'],
+    origin: ['https://tinder-frontend-prod-iota.vercel.app', 'http://localhost:4201', 'http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
